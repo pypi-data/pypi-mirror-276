047404abfe4a34da764b58a8f60d2a87b4b28f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:22 +0000

    Add package fake-bpy-module/20240529/fake_bpy_module-20240529-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20240529-py3-none-any.whl/bpy/ops/palette/__init__.pyi
@@ -0,0 +1,120 @@
+import typing
+import collections.abc
+import bpy.types
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+def color_add(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+):
+    """Add new color to active palette
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def color_delete(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+):
+    """Remove active color from palette
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def color_move(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+    *,
+    type: str | None = "UP",
+):
+    """Move the active Color up/down in the list
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    :param type: Type
+    :type type: str | None
+    """
+
+    ...
+
+def extract_from_image(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+    *,
+    threshold: typing.Any | None = 1,
+):
+    """Extract all colors used in Image and create a Palette
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    :param threshold: Threshold
+    :type threshold: typing.Any | None
+    """
+
+    ...
+
+def join(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+    *,
+    palette: str | typing.Any = "",
+):
+    """Join Palette Swatches
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    :param palette: Palette, Name of the Palette
+    :type palette: str | typing.Any
+    """
+
+    ...
+
+def new(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+):
+    """Add new palette
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def sort(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+    *,
+    type: str | None = "HSV",
+):
+    """Sort Palette Colors
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    :param type: Type
+    :type type: str | None
+    """
+
+    ...
---
+++ packages/fake-bpy-module/fake_bpy_module-20240529-py3-none-any.whl/bpy/ops/world/__init__.pyi
@@ -0,0 +1,34 @@
+import typing
+import collections.abc
+import bpy.types
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+def convert_volume_to_mesh(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+):
+    """Convert the volume of a world to a mesh. The world's volume used to be rendered by EEVEE Legacy. Conversion is needed for it to render properly
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def new(
+    override_context: dict[str, typing.Any] | bpy.types.Context | None = None,
+    execution_context: str | int | None = None,
+    undo: bool | None = None,
+):
+    """Create a new world Data-Block
+
+    :type override_context: dict[str, typing.Any] | bpy.types.Context | None
+    :type execution_context: str | int | None
+    :type undo: bool | None
+    """
+
+    ...
