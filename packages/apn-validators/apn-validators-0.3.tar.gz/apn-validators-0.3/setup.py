37dc3d9bc8dcfd4baf4da7316907b3f88eb6b336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 17:03:03 +0000

    Add package apn-validators/0.3/apn-validators-0.3.tar.gz

---
+++ packages/apn-validators/apn-validators-0.3.tar.gz/apn-validators-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="apn-validators",
+    version="0.3",
+    packages=find_packages(),
+    install_requires=[
+        # dependencies we need to install
+    ],
+)
