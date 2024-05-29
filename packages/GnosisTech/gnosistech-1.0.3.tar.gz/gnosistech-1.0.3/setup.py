6502124cfc14f70917b19fc36a702f9b6e55f44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:13 +0000

    Add package GnosisTech/1.0.3/gnosistech-1.0.3.tar.gz

---
+++ packages/GnosisTech/gnosistech-1.0.3.tar.gz/gnosistech-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as req_file:
+        return req_file.readlines()
+    
+setup(
+    install_requires=read_requirements(),
+    name='GnosisTech',
+    version='1.0.3',
+    author='Phung Ngoc An',
+    author_email='phungankh2@gmail.com',
+    description='PyQuantTrader là một thư viện Python hỗ trợ những vấn đề về quant trading.',
+    packages=find_packages(),
+    package_data= {'pyquanttrade': ['_backtest_source/*.js']},       
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    python_requires='>=3.6',
+)
