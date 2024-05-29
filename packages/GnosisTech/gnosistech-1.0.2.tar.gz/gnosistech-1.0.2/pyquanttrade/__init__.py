ecb5ce505e5f1da40ce7a12842aaff612fd5d4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:12 +0000

    Add package GnosisTech/1.0.2/gnosistech-1.0.2.tar.gz

---
+++ packages/GnosisTech/gnosistech-1.0.2.tar.gz/gnosistech-1.0.2/pyquanttrade/__init__.py
@@ -0,0 +1,20 @@
+from .backtest.event_base import (
+    use_changes
+)
+from .backtest.vectorized import (
+
+    use_position,
+    use_signal_ma,
+    use_trailing,
+)
+from .data.get_data import (
+    get_crypto,
+    get_vnstock,
+)
+from .plotlib.plot import (
+    Multivariate_Density,
+    Isolation_Forest,
+    IQR,
+    MAD,
+    Seasonal_Decomposition,
+)
