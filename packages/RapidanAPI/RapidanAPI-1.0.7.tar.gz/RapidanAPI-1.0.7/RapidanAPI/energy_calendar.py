107b85b9f97717d9bdc9780b3d8afcbaec9afe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:32 +0000

    Add package RapidanAPI/1.0.7/RapidanAPI-1.0.7.tar.gz

---
+++ packages/RapidanAPI/RapidanAPI-1.0.7.tar.gz/RapidanAPI-1.0.7/RapidanAPI/energy_calendar.py
@@ -0,0 +1,12 @@
+import requests
+import pandas as pd
+
+def get_ec_data(api_key):
+    url = f"https://rapidan-api-sabineleffler.replit.app/calendar/{api_key}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return pd.DataFrame(data)
+    else:
+        raise Exception("Failed to fetch data.")
