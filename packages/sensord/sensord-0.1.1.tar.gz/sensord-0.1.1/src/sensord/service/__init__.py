d01d39ab4a61821a84032d7c304836b772dbd3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:02:58 +0000

    Add package sensord/0.1.1/sensord-0.1.1.tar.gz

---
+++ packages/sensord/sensord-0.1.1.tar.gz/sensord-0.1.1/src/sensord/service/__init__.py
@@ -0,0 +1,140 @@
+import logging
+import signal
+
+import rich_click as click
+import tomli
+
+from sensation.common import SensorType
+from sensord.common.socket import SocketBindException
+from sensord.service import api, mqtt, paths, sen0395, log
+from sensord.service.err import UnknownSensorType, MissingConfigurationField, AlreadyRegistered, InvalidConfiguration, \
+    ServiceAlreadyRunning
+from sensord.service.paths import ConfigFileNotFoundError
+
+logger = logging.getLogger(__name__)
+
+@click.command()
+def cli():
+    log.configure(True)
+    run()
+
+def missing_config_field(entity, field, config):
+    logger.warning(f"[invalid_{entity}] reason=[missing_configuration_field] field=[{field}] config=[{config}]")
+
+
+def missing_sensor_config_field(field, config):
+    missing_config_field('sensor', field, config)
+
+
+def missing_mqtt_config_field(field, config):
+    missing_config_field('mqtt_broker', field, config)
+
+
+def run():
+    logger.info('[service_started]')
+    try:
+        api.start()
+    except SocketBindException as e:
+        logger.error(f"[socket_bind_error] reason=[{e}] check=[Is the service already running?]")
+        print(f"You can try removing `/tmp/{paths.API_SOCKET}` if you are absolutely sure the service is not running.")
+        exit(1)
+    except ServiceAlreadyRunning:
+        logger.warning(f"[service_is_already_running] result=[exiting..]")
+        exit(1)
+
+    try:
+        init_mqtt()
+        init_sensors()
+    except KeyboardInterrupt:
+        logger.info('[service_exit] detail=[Initialization stage interrupted by user]')
+        api.stop()
+        unregister_sensors()
+        unregister_mqtt()
+        return
+
+    signal.signal(signal.SIGTERM, shutdown)
+    signal.signal(signal.SIGINT, shutdown)
+
+
+def init_sensors():
+    try:
+        config_file = paths.lookup_sensors_config_file()
+    except ConfigFileNotFoundError as e:
+        logger.warning(f"[no_sensors_config_file] detail=[{e}]")
+        return
+
+    with open(config_file, 'rb') as f:
+        config = tomli.load(f)
+    sensors = config.get('sensor')
+
+    if sensors:
+        register_sensors(sensors)
+    else:
+        logger.warning('[no_sensors_loaded] detail=[No sensors configured in the config file: %s]', config_file)
+
+
+def register_sensors(sensors):
+    for sensor in sensors:
+        if 'type' not in sensor:
+            missing_sensor_config_field('type', sensor)
+            continue
+        if 'name' not in sensor:
+            missing_sensor_config_field('name', sensor)
+            continue
+        try:
+            register_sensor(sensor)
+            logger.info("[sensor_registered] type=[%s] name=[%s]", sensor['type'], sensor['name'])
+        except MissingConfigurationField as e:
+            missing_sensor_config_field(e.field, sensor)
+        except InvalidConfiguration as e:
+            logger.warning(f"[invalid_sensor] reason=[invalid_configuration] reason=[{e}] config=[{sensor}]")
+        except UnknownSensorType as e:
+            logger.warning(f"[invalid_sensor] reason=[unknown_sensor_type] type=[{e.sensor_type}] config=[{sensor}]")
+        except AlreadyRegistered:
+            logger.warning(f"[invalid_sensor] reason=[duplicated_sensor] type=[{sensor['type']}] config=[{sensor}]")
+
+
+def register_sensor(config):
+    if config["type"] == SensorType.SEN0395.value:
+        sen0395.register(**config)
+        return
+
+    raise UnknownSensorType(config["type"])
+
+
+def unregister_sensors():
+    sen0395.unregister_all()
+
+
+def init_mqtt():
+    try:
+        config_file = paths.lookup_mqtt_config_file()
+    except ConfigFileNotFoundError:
+        return
+
+    with open(config_file, 'rb') as f:
+        config = tomli.load(f)
+
+    brokers = config.get('broker')
+    if not brokers:
+        return
+
+    for broker in brokers:
+        try:
+            mqtt.register(**broker)
+        except MissingConfigurationField as e:
+            missing_sensor_config_field(e.field, broker)
+        except AlreadyRegistered:
+            logger.warning(f"[invalid_mqtt_broker] reason=[duplicated_broker] config=[{broker}]")
+
+
+def unregister_mqtt():
+    mqtt.unregister_all()
+
+
+def shutdown(_, __):
+    logger.info("[exit_signal_received]")
+    api.stop()
+    unregister_sensors()
+    unregister_mqtt()
+    logger.info("[service_exited] reason=[signal]")
