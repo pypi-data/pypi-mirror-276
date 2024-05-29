93f54705f725fcec7c9309487797d26aceda1061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 17:03:07 +0000

    Add package PyNIExp/0.28.post3/PyNIExp-0.28.post3-py3-none-any.whl

---
+++ packages/PyNIExp/PyNIExp-0.28.post3-py3-none-any.whl/pyniexp/__init__.py
@@ -0,0 +1,1 @@
+from . import mlplugins, network, scanner, stimulator
---
+++ packages/PyNIExp/PyNIExp-0.28.post3-py3-none-any.whl/pyniexp/scanner/triggers.py
@@ -0,0 +1,34 @@
+import serial
+from ..utils import getLogger, listSerial
+
+logger = getLogger()
+
+class BrainVision:
+    _port = []
+    _isConnected = False
+
+    def isConnected(self):
+        return self._isConnected
+
+    def __init__(self,port='COM6'):
+        if not any([p == port for p in listSerial()]):
+            logger.error('port {} is not available'.format(port))
+        else:
+            self._port = serial.Serial(port)
+            self._port.write([0x00])
+            self._isConnected = True
+    
+    def __del__(self):
+        if self.isConnected: 
+            self.close()
+
+    def close(self):
+        self._port.write([0xFF])
+        self._port.close()
+    
+    def send(self,val):
+        if val > 255:
+            logger.error('trigger value {:d} is larger than 255'.format(val))
+        else:
+            self._port.write([val])
+            self._port.write([0x00])
