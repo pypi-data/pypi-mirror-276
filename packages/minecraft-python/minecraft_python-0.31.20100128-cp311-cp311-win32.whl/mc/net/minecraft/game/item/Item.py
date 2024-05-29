5675aba37deaa9c9d927dd650f1cd9259d31f330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:24 +0000

    Add package minecraft-python/0.31.20100128/minecraft_python-0.31.20100128-cp311-cp311-win32.whl

---
+++ packages/minecraft-python/minecraft_python-0.31.20100128-cp311-cp311-win32.whl/mc/net/minecraft/game/item/Item.py
@@ -0,0 +1,27 @@
+from mc.JavaUtils import Random
+
+class Item:
+    TOTAL_STACK_SIZE = 100
+    _rand = Random()
+
+    def __init__(self, items, itemId):
+        self.items = items
+        self.shiftedIndex = itemId + 256
+        items.itemsList[itemId + 256] = self
+        self._maxStackSize = Item.TOTAL_STACK_SIZE
+        print(f'Setting items[{(itemId + 256)}] to {self}')
+
+    def getIconIndex(self):
+        return self.iconIndex
+
+    def onItemUse(self, stack, world, x, y, z, sideHit):
+        pass
+
+    def getStrVsBlock(self, block):
+        return 1.0
+
+    def onItemRightClick(self, stack, world, player):
+        return False
+
+    def getItemStackLimit(self):
+        return self._maxStackSize
