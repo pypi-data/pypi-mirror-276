6f6b78b2d41bd89a234a40deb153392e7d55f01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:30 +0000

    Add package RapidanAPI/1.0.10/RapidanAPI-1.0.10-py3-none-any.whl

---
+++ packages/RapidanAPI/RapidanAPI-1.0.10-py3-none-any.whl/RapidanAPI/__init__.py
@@ -0,0 +1,2 @@
+from .global_oil_balance import get_gob_data as global_oil_balance
+from .energy_calendar import get_ec_data as energy_calendar
---
+++ packages/RapidanAPI/RapidanAPI-1.0.10-py3-none-any.whl/RapidanAPI/global_oil_balance.py
@@ -0,0 +1,12 @@
+import requests
+import pandas as pd
+
+def get_gob_data(api_key, balance_date, columns):
+    url = f"https://rapidan-api-sabineleffler.replit.app/global_oil_balance/{api_key}/{balance_date}/{columns}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return pd.DataFrame(data)
+    else:
+        raise Exception("Failed to fetch data.")
