9359d24b8dd5fa27c543ed1fec1f3e820f925ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:24 +0000

    Add package CVE-2020-3452/0.0.1/CVE-2020-3452-0.0.1.tar.gz

---
+++ packages/CVE-2020-3452/CVE-2020-3452-0.0.1.tar.gz/CVE-2020-3452-0.0.1/cve20203452/includes/writefile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+ * CVE-2020-3452
+ * CVE-2020-3452 Bug scanner for WebPentesters and Bugbounty Hunters
+ *
+ * @Developed By Cappricio Securities <https://cappriciosec.com>
+ */
+
+
+"""
+
+
+def writedata(output, data):
+    with open(output, 'a') as file:
+        file.write(data)
