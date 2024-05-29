12d3b523b0ad413d9d8a1666f1b39de5ec9b85a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:03:07 +0000

    Add package dtfilterthumbor/1.0.3/dtfilterthumbor-1.0.3-py3-none-any.whl

---
+++ packages/dtfilterthumbor/dtfilterthumbor-1.0.3-py3-none-any.whl/dtfilterthumbor/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+__version__ = '0.0.2'
+
+try:
+    from dtfilterthumbor.dtwatermark import Filter  # NOQA
+    from dtfilterthumbor.watermarkdt2 import Filter
+except ImportError:
+    logging.exception('Could not import thumbor_text_filter. Probably due to setup.py installing it.')
