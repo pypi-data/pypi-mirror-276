7bc52ac1c254e2d8d00d1f3b682f9f2245879957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:04:29 +0000

    Add package metrit/0.0.4/metrit-0.0.4.tar.gz

---
+++ packages/metrit/metrit-0.0.4.tar.gz/metrit-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="metrit",
+    version="0.0.4",
+    author="mcrespoae",
+    author_email="info@mariocrespo.es",
+    packages=["metrit"],
+    description="A dead simple resources monitoring decorator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcrespoae/metrit",
+    install_requires=["psutil==5.9.8", "multiprocess==0.70.16"],
+    setup_requires=["psutil==5.9.8", "multiprocess==0.70.16"],
+    python_requires=">=3.8",
+    keywords=["metrit"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ],
+)
