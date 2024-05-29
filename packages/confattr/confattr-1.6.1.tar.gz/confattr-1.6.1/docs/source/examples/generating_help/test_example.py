2fb6e70691a14b853d76c9f3241c46741f73b8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:23 +0000

    Add package confattr/1.6.1/confattr-1.6.1.tar.gz

---
+++ packages/confattr/confattr-1.6.1.tar.gz/confattr-1.6.1/docs/source/examples/generating_help/test_example.py
@@ -0,0 +1,99 @@
+#!../../../../venv/bin/pytest
+
+import os
+import sys
+
+import pytest
+from utils import run_and_get_stdout
+
+from confattr import ConfigFile
+
+
+def test__output(monkeypatch: pytest.MonkeyPatch) -> None:
+	monkeypatch.setattr(ConfigFile, 'config_directory', None)
+	monkeypatch.setenv('HOME', '/home/username')
+	path = os.path.dirname(__file__)
+	fn_script = os.path.join(path, 'example.py')
+	fn_expected_output = os.path.join(path, 'output.txt')
+	stdout = run_and_get_stdout(fn_script)
+
+	with open(fn_expected_output, 'rt') as f:
+		expected_output = f.read()
+
+	if sys.version_info < (3, 10):
+		expected_output = expected_output.replace('options:', 'optional arguments:')
+
+	assert stdout == expected_output
+
+def test_normal_config(monkeypatch: pytest.MonkeyPatch) -> None:
+	monkeypatch.setattr(ConfigFile, 'config_directory', None)
+	monkeypatch.setenv('HOME', '/home/username')
+	path = os.path.dirname(__file__)
+	fn_script = os.path.join(path, 'example_config.py')
+	fn_expected_output = os.path.join(path, 'expected-config')
+	stdout = run_and_get_stdout(fn_script)
+
+	with open(fn_expected_output, 'rt') as f:
+		expected_output = f.read()
+
+	assert stdout == expected_output
+
+
+def test_raw_help(monkeypatch: pytest.MonkeyPatch) -> None:
+	monkeypatch.setattr(ConfigFile, 'config_directory', None)
+	monkeypatch.setenv('HOME', '/home/username')
+	path = os.path.dirname(__file__)
+	fn_script = os.path.join(path, 'example_raw_help.py')
+	fn_expected_output = os.path.join(path, 'expected-raw-help.txt')
+	stdout = run_and_get_stdout(fn_script)
+
+	with open(fn_expected_output, 'rt') as f:
+		expected_output = f.read()
+
+	if sys.version_info < (3, 10):
+		expected_output = expected_output.replace('options:', 'optional arguments:')
+
+	assert stdout == expected_output
+
+def test_raw_config(monkeypatch: pytest.MonkeyPatch) -> None:
+	monkeypatch.setattr(ConfigFile, 'config_directory', None)
+	monkeypatch.setenv('HOME', '/home/username')
+	path = os.path.dirname(__file__)
+	fn_script = os.path.join(path, 'example_raw_config.py')
+	fn_expected_output = os.path.join(path, 'expected-raw-config')
+	stdout = run_and_get_stdout(fn_script)
+
+	with open(fn_expected_output, 'rt') as f:
+		expected_output = f.read()
+
+	assert stdout == expected_output
+
+
+def test_no_multi_help(monkeypatch: pytest.MonkeyPatch) -> None:
+	monkeypatch.setattr(ConfigFile, 'config_directory', None)
+	monkeypatch.setenv('HOME', '/home/username')
+	path = os.path.dirname(__file__)
+	fn_script = os.path.join(path, 'no_multi_example.py')
+	fn_expected_output = os.path.join(path, 'expected-no-multi-help.txt')
+	stdout = run_and_get_stdout(fn_script)
+
+	with open(fn_expected_output, 'rt') as f:
+		expected_output = f.read()
+
+	if sys.version_info < (3, 10):
+		expected_output = expected_output.replace('options:', 'optional arguments:')
+
+	assert stdout == expected_output
+
+def test_no_multi_config(monkeypatch: pytest.MonkeyPatch) -> None:
+	monkeypatch.setattr(ConfigFile, 'config_directory', None)
+	monkeypatch.setenv('HOME', '/home/username')
+	path = os.path.dirname(__file__)
+	fn_script = os.path.join(path, 'no_multi_example_config.py')
+	fn_expected_output = os.path.join(path, 'expected-no-multi-config')
+	stdout = run_and_get_stdout(fn_script)
+
+	with open(fn_expected_output, 'rt') as f:
+		expected_output = f.read()
+
+	assert stdout == expected_output
---
+++ packages/confattr/confattr-1.6.1.tar.gz/confattr-1.6.1/docs/source/examples/type_regex/example_save.py
@@ -0,0 +1,10 @@
+#!../../../../venv/bin/python3
+__package__ = 'type_regex'
+
+import sys
+
+from .example_usage import App
+a = App()
+if True:
+# ------- start -------
+	a.save()
