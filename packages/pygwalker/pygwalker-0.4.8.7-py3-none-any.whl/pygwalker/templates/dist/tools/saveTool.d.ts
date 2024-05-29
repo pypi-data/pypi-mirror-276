b9caabf27762c31fe427981b53af87494d0fdf0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:03:24 +0000

    Add package pygwalker/0.4.8.7/pygwalker-0.4.8.7-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.4.8.7-py3-none-any.whl/pygwalker/templates/dist/tools/saveTool.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import type { IAppProps } from '../interfaces';
+import type { IGWHandler } from '@kanaries/graphic-walker/interfaces';
+import type { ToolbarButtonItem } from "@kanaries/graphic-walker/components/toolbar/toolbar-button";
+import type { VizSpecStore } from '@kanaries/graphic-walker/store/visualSpecStore';
+export declare function hidePreview(id: string): void;
+export declare function getSaveTool(props: IAppProps, gwRef: React.MutableRefObject<IGWHandler | null>, storeRef: React.MutableRefObject<VizSpecStore | null>, isChanged: boolean, setIsChanged: React.Dispatch<React.SetStateAction<boolean>>): ToolbarButtonItem;
