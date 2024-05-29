2aeb55741e7f5866b81c4aee9da1455471f5dfaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:02:58 +0000

    Add package myfaasmctl/0.0.6/myfaasmctl-0.0.6-py3-none-any.whl

---
+++ packages/myfaasmctl/myfaasmctl-0.0.6-py3-none-any.whl/faasmctl/tasks/stream.py
@@ -0,0 +1,44 @@
+from base64 import b64encode
+from faasmctl.util.invoke import invoke_wasm
+from faasmctl.util.planner import (
+    get_available_hosts,
+    reset_batch_size,
+    scale_function_parallelism,
+)
+from faasmctl.util.results import (
+    get_execution_time_from_message_results,
+    get_return_code_from_message_results,
+)
+from invoke import task
+from sys import exit
+from time import time
+
+
+@task(default=True)
+def scale(
+    ctx,
+    user,
+    function,
+    parallelism,
+    ini_file=None,
+):
+    """
+    Change the parallelism of a function
+    """
+    req_dict = {"user": user, "function": function, "parallelism": parallelism}
+
+    if user is None or function is None or parallelism is None:
+        print("ERROR: user, function and parallelism must be provided")
+        return 1
+    
+    req_dict["user"] = user
+    req_dict["function"] = function
+    req_dict["parallelism"] = int(parallelism)
+    scale_function_parallelism(user, function, parallelism)
+
+@task
+def batch(ctx, batchsize):
+    """
+    Reset the batch size
+    """
+    reset_batch_size(batchsize)
