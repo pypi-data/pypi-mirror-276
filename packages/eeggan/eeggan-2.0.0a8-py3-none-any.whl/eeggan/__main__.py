a46dd1c1c03bcb0e0f6a88c7503dd8e06c71e230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:32 +0000

    Add package eeggan/2.0.0a8/eeggan-2.0.0a8-py3-none-any.whl

---
+++ packages/eeggan/eeggan-2.0.0a8-py3-none-any.whl/eeggan/__main__.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+
+from .autoencoder_training_main import main as autoencoder_training_main
+from .gan_training_main import main as gan_training_main
+from .generate_samples_main import main as generate_samples_main
+from .visualize_main import main as visualize_main
+from .vae_training_main import main as vae_training_main
+
+def main():
+    args = sys.argv
+    command = args[1]
+    args = args[2:]
+    print(args)
+    
+    if command == 'gan_training_main':
+        gan_training_main(args)
+    else:
+        print('Unrecognized command')
+        #parser.print_help()
+        sys.exit(1)
