82bc6ce4ed6c1a238081417dcacd45bceb871276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:04:27 +0000

    Add package greer2018/1.1.0/greer2018-1.1.0-py3-none-any.whl

---
+++ packages/greer2018/greer2018-1.1.0-py3-none-any.whl/greer2018/__init__.py
@@ -0,0 +1,14 @@
+"""
+Data and formalisms from Greer et al. (2018)
+"""
+# {# pkglts, src
+# FYEO
+# #}
+# {# pkglts, version, after src
+from . import version
+
+__version__ = version.__version__
+# #}
+# {# pkglts, glabdata, after version
+from .info import *
+# #}
