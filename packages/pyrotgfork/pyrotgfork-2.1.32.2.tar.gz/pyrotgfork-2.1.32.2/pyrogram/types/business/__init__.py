c689aca78130b115bf5c3262b3676c4f8b352b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:03:08 +0000

    Add package pyrotgfork/2.1.32.2/pyrotgfork-2.1.32.2.tar.gz

---
+++ packages/pyrotgfork/pyrotgfork-2.1.32.2.tar.gz/pyrotgfork-2.1.32.2/pyrogram/types/business/__init__.py
@@ -0,0 +1,35 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from .business_connection import BusinessConnection
+from .business_intro import BusinessIntro
+from .business_location import BusinessLocation
+from .business_opening_hours import BusinessOpeningHours
+from .business_opening_hours_interval import BusinessOpeningHoursInterval
+from .collectible_item_info import CollectibleItemInfo
+from .invoice import Invoice
+
+__all__ = [
+    "BusinessConnection",
+    "BusinessIntro",
+    "BusinessLocation",
+    "BusinessOpeningHours",
+    "BusinessOpeningHoursInterval",
+    "CollectibleItemInfo",
+    "Invoice",
+]
