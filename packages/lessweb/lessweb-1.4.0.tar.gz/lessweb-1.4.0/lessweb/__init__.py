a9b916e9be1e81e745d528e1cb68e0a7d3e55185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:40 +0000

    Add package lessweb/1.4.0/lessweb-1.4.0.tar.gz

---
+++ packages/lessweb/lessweb-1.4.0.tar.gz/lessweb-1.4.0/lessweb/__init__.py
@@ -0,0 +1,3 @@
+from .bridge import (Bridge, autowire, delete_mapping, get_mapping,
+                     patch_mapping, post_mapping, put_mapping, rest_error,
+                     rest_mapping, rest_response, service)
