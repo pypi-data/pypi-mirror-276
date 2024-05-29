e1f9779b2a38bb757680f286f33aecb1ee546b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:03:00 +0000

    Add package recommendation-lib/1.4/recommendation_lib-1.4.tar.gz

---
+++ packages/recommendation-lib/recommendation_lib-1.4.tar.gz/recommendation_lib-1.4/app/general_tools/logger.py
@@ -0,0 +1,27 @@
+import logging
+
+def setup_logger():
+    # Create a logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # Create a file handler
+    file_handler = logging.FileHandler("app.log")
+    file_handler.setLevel(logging.INFO)
+
+    # Create a console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    # Create a formatter and add it to the handlers
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+logger = setup_logger()
