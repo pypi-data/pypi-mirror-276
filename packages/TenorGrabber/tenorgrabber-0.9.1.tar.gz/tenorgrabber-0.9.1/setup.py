dc73bd058a51969beb35d820636645f076c5d1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:03:12 +0000

    Add package TenorGrabber/0.9.1/tenorgrabber-0.9.1.tar.gz

---
+++ packages/TenorGrabber/tenorgrabber-0.9.1.tar.gz/tenorgrabber-0.9.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='TenorGrabber',
+    packages=find_packages(),
+    version='0.9.1',
+    description='Tired of /view links on tenor? This python library allows you to get the direct link of the GIF!',
+    author='user0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
