919736750a6dac2988c9886ff78c95d0d34bde80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:03:45 +0000

    Add package mosqlient/1.1.0/mosqlient-1.1.0-py3-none-any.whl

---
+++ packages/mosqlient/mosqlient-1.1.0-py3-none-any.whl/mosqlient/config.py
@@ -0,0 +1,6 @@
+import typing
+
+APPS = ["registry", "datastore", "vis"]
+APPS_TYPE = typing.Literal["registry", "datastore", "vis"]
+API_DEV_URL = "http://0.0.0.0:8042/api/"
+API_PROD_URL = "https://api.mosqlimate.org/api/"
---
+++ packages/mosqlient/mosqlient-1.1.0-py3-none-any.whl/mosqlient/models/temporal.py
@@ -0,0 +1,8 @@
+from mosqlient.models import ModelBase
+
+
+class TemporalModel(ModelBase): ...
+
+
+if __name__ == "__main__":
+    TemporalModel("x")
