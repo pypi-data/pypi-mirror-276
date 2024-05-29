b923be57d9ddb57c05246b256b012e329b84e8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:03:12 +0000

    Add package logisticbank/0.0.1/logisticbank-0.0.1.tar.gz

---
+++ packages/logisticbank/logisticbank-0.0.1.tar.gz/logisticbank-0.0.1/ logisticbank/__init__.py
@@ -0,0 +1,17 @@
+from .logistic_regression_module import (
+    preprocess_universal_bank_data, 
+    logistic_regression, 
+    nominal_logistic_regression, 
+    ordinal_logistic_regression, 
+    plot_charts
+)
+
+__version__ = '0.0.1'
+
+__all__ = [
+    'preprocess_universal_bank_data', 
+    'logistic_regression', 
+    'nominal_logistic_regression', 
+    'ordinal_logistic_regression', 
+    'plot_charts'
+]
