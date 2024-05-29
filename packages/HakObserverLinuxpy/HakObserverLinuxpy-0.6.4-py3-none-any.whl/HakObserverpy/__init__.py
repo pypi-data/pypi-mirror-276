336c25d7e24d9698c8ec5c890fa5f76860c5839c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:04:31 +0000

    Add package HakObserverLinuxpy/0.6.4/HakObserverLinuxpy-0.6.4-py3-none-any.whl

---
+++ packages/HakObserverLinuxpy/HakObserverLinuxpy-0.6.4-py3-none-any.whl/HakObserverpy/__init__.py
@@ -0,0 +1,9 @@
+# Import key functions and classes
+from HakObserverLinuxpy import HakObserverpy
+from HakObserverLinuxpy import LinInitiateCollecection
+from HakObserverLinuxpy import InitiateCollecection
+# Optional: Define a version number for the package
+__version__ = "0.6.4"
+
+# Optional: Define the package's public interface
+__all__ = ["HakObserverLinuxpy"]
