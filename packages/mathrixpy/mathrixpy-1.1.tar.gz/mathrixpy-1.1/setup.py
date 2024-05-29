c0b48c86fe6316166311fd084522d8031e8b3471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:02:58 +0000

    Add package mathrixpy/1.1/mathrixpy-1.1.tar.gz

---
+++ packages/mathrixpy/mathrixpy-1.1.tar.gz/mathrixpy-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mathrixpy",
+    version="1.1",
+    author="AmJoJADeOrg",
+    license='MIT',
+    author_email="glroberto1810@gmail.com",
+    description="Operaciones con matrices",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ampere-G/mathrixPy",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
