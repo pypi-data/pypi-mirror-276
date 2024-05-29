9f1a642c1472b3ac1f298719d4583024d476593a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:34 +0000

    Add package torchao-nightly/2024.5.29/torchao_nightly-2024.5.29-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torchao-nightly/torchao_nightly-2024.5.29-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torchao/__init__.py
@@ -0,0 +1,23 @@
+import torch
+_IS_FBCODE = (
+    hasattr(torch._utils_internal, "IS_FBSOURCE") and
+    torch._utils_internal.IS_FBSOURCE
+)
+
+if not _IS_FBCODE:
+    from . import _C
+    from . import ops
+
+from torchao.quantization import (
+    apply_weight_only_int8_quant,
+    apply_dynamic_quant,
+    autoquant,
+)
+from . import dtypes
+
+__all__ = [
+    "dtypes",
+    "apply_dynamic_quant",
+    "apply_weight_only_int8_quant",
+    "autoquant",
+]
