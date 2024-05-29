2d23aa60eb724f07c6ee9c9e6ba49a49f9b71e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:03:35 +0000

    Add package prompt-learner/0.2.7/prompt_learner-0.2.7.tar.gz

---
+++ packages/prompt-learner/prompt_learner-0.2.7.tar.gz/prompt_learner-0.2.7/prompt_learner/optimizers/grid_search.py
@@ -0,0 +1,59 @@
+"""Class for grid search optimization."""
+from prompt_learner.optimizers.optimizer import Optimizer
+from prompt_learner.evals.metrics.accuracy import Accuracy
+
+
+class GridSearch(Optimizer):
+    """Grid search optimizer."""
+    def search(self, param_grid: dict):
+        """Search for the best hyperparameters given in the 
+        parameter grid."""
+        best_score = 0
+        best_params = {}
+        all_results = []
+        all_samplers = param_grid.get('sampler',None)
+        all_templates = param_grid.get('template', [self.prompt.template])
+        all_adapters = param_grid.get('adapter',None)
+        if all_samplers is not None:
+            return self.search_samplers(all_samplers, all_templates, all_adapters)
+        for template in all_templates:
+            self.prompt.translate(template)
+            for adapter in all_adapters:
+                acc, _ = Accuracy(self.prompt.template.task).compute(self.prompt, adapter)
+                score = acc
+                curr_params = {'score': score, 
+                               'template': template.class_repr(),
+                               'adapter': repr(adapter)}
+                all_results.append(curr_params)
+                if score > best_score:
+                    best_score = score
+                    best_params = curr_params
+
+        return best_params, all_results
+    
+    def search_samplers(self, all_samplers, all_templates, all_adapters):
+        """Search when samplers are in param grid"""
+        best_score = 0
+        best_params = {}
+        all_results = []
+        for sampler in all_samplers:
+            sampler.task = self.prompt.template.task
+            sampler.select_examples()
+            for template in all_templates:
+                self.prompt.translate(template)
+                for adapter in all_adapters:
+                    acc, _ = Accuracy(sampler.task).compute(self.prompt, adapter)
+                    score = acc
+                    curr_params = {'score': score, 'sampler': repr(sampler),
+                                   'template': template.class_repr(),
+                                   'adapter': repr(adapter)}
+                    all_results.append(curr_params)
+                    if score > best_score:
+                        best_score = score
+                        best_params = curr_params
+
+        return best_params, all_results
+
+    def compute_metrics(self):
+        """Compute metrics for a given prompt and adapter on a task."""
+        pass
