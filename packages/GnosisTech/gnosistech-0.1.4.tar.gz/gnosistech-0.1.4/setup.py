cde45fda121a4a4a49186e15ef2d955c5001da73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:45 +0000

    Add package GnosisTech/0.1.4/gnosistech-0.1.4.tar.gz

---
+++ packages/GnosisTech/gnosistech-0.1.4.tar.gz/gnosistech-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='GnosisTech',
+    version='0.1.4',
+    author='Phung Ngoc An',
+    author_email='phungankh2@gmail.com',
+    description='PyQuantTrader là một thư viện Python hỗ trợ những vấn đề về quant trading.',
+    packages=find_packages(),
+    package_data= {'': ['*.json']},       
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    python_requires='>=3.6',
+)
