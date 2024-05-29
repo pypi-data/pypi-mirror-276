f6b232934424dfb5fd2eb7e5da158ea51d12ec45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 17:03:03 +0000

    Add package qwak-core/0.4.28/qwak_core-0.4.28.tar.gz

---
+++ packages/qwak-core/qwak_core-0.4.28.tar.gz/qwak_core-0.4.28/qwak/exceptions/qwak_external_exception.py
@@ -0,0 +1,8 @@
+class QwakExternalException(Exception):
+    """
+    An external system to Qwak, had an internal error
+    """
+
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
