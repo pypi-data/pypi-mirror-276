d3ea425bc2ef1f52622c4ffd6a38699bc6e7e992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:16 +0000

    Add package tse-motion/0.2.8/tse_motion-0.2.8-py3-none-any.whl

---
+++ packages/tse-motion/tse_motion-0.2.8-py3-none-any.whl/tse_rating/__init__.py
@@ -0,0 +1,19 @@
+# from .artifact_rating import rate
+from monai.networks.nets import DenseNet121
+from pkg_resources import resource_filename
+import torch
+import pdb
+from tse_rating.unet import UNet
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = DenseNet121(spatial_dims=2, in_channels=1, out_channels=5)
+model_path = resource_filename('tse_rating', 'weight.pth')
+model.load_state_dict(torch.load(model_path, map_location=device))
+model = model.to(device)
+model.eval()
+
+unet = UNet(in_channels=1, out_channels=2, init_features=32)
+# unet_path = resource_filename('tse_rating', 'unet-checkpoint.pth')
+unet.load_state_dict(torch.load('/Users/jinghangli/tse-rating/tse_rating/unet-checkpoint.pth', map_location=device))
+unet = unet.to(device)
+unet.eval()
