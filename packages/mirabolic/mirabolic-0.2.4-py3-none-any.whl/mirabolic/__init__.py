4402f10daeeb2ee0a17a1eecd6ca7d61146e6103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:41 +0000

    Add package mirabolic/0.2.4/mirabolic-0.2.4-py3-none-any.whl

---
+++ packages/mirabolic/mirabolic-0.2.4-py3-none-any.whl/mirabolic/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "version"), mode="r") as fp:
+    __version__ = fp.readline().rstrip()
+
+# We import some functions/classes for ease of reference.
+
+# Tool for plotting CDFs with confidence intervals
+from mirabolic.cdf.cdf_tools import cdf_plot
+from mirabolic.rates.rate_tools import rate_comparison
