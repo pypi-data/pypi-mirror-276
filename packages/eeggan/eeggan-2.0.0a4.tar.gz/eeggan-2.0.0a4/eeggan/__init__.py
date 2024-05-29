b4e999ff46b051f4fd23a92ca063fb43f466a19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:53 +0000

    Add package eeggan/2.0.0a4/eeggan-2.0.0a4.tar.gz

---
+++ packages/eeggan/eeggan-2.0.0a4.tar.gz/eeggan-2.0.0a4/eeggan/__init__.py
@@ -0,0 +1,6 @@
+from .autoencoder_training_main import main as autoencoder_training_main
+from .gan_training_main import main as gan_training_main
+from .generate_samples_main import main as generate_samples_main
+from .visualize_main import main as visualize_main
+
+from .vae_training_main import main as vae_training_main
