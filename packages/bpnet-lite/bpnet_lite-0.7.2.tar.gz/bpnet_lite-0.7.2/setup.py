2acbca760bc53fcbe07f1bb9eba8277925b4a6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:24 +0000

    Add package bpnet-lite/0.7.2/bpnet_lite-0.7.2.tar.gz

---
+++ packages/bpnet-lite/bpnet_lite-0.7.2.tar.gz/bpnet_lite-0.7.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='bpnet-lite',
+    version='0.7.2',
+    author='Jacob Schreiber',
+    author_email='jmschreiber91@gmail.com',
+    packages=['bpnetlite'],
+    scripts=['bpnet', 'chrombpnet'],
+    url='https://github.com/jmschrei/bpnet-lite',
+    license='LICENSE.txt',
+    description='bpnet-lite is a minimal implementation of BPNet, a neural network aimed at interpreting regulatory activity of the genome.',
+    install_requires=[
+        "numpy >= 1.14.2",
+        "scipy >= 1.0.0",
+        "pandas >= 1.3.3",
+        "pyBigWig >= 0.3.17",
+        "torch >= 1.9.0",
+        "h5py >= 3.7.0",
+        "pyfaidx >= 0.7.2.1",
+        "tqdm >= 4.64.1",
+        "numba >= 0.55.1",
+        "seaborn >= 0.11.2",
+        "modisco-lite >= 2.0.0",
+        "joblib >= 1.3.2",
+        "tangermeme >= 0.2.0"
+    ],
+)
