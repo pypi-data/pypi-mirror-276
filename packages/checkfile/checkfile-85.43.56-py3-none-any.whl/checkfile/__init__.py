2eaf276aadf5833656795c6411e82d8ad10ac2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:40 +0000

    Add package checkfile/85.43.56/checkfile-85.43.56-py3-none-any.whl

---
+++ packages/checkfile/checkfile-85.43.56-py3-none-any.whl/checkfile/__init__.py
@@ -0,0 +1,26 @@
+import sys
+import time
+def type(txt):
+    for char in txt:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.005)
+
+def check_file(file, file_content_file):
+    with open(file, "r") as file1:
+        read_file = file1.read()
+        act_file = open(file_content_file, "r")
+        read_act_file = act_file.read()
+        if read_file != read_act_file:
+            update_file(file, file_content_file)
+
+def update_file(file, file_content_file):
+    with open(file,"w") as real_file:
+        act_file = open(file_content_file, "r")
+        read_act_file = act_file.read()
+        try:
+            real_file.write(read_act_file)
+            type("Quiting Program Because File(s) Was/Were Edited...")
+            quit()
+        except Exception as e:
+            print(f"Error: {e}")
