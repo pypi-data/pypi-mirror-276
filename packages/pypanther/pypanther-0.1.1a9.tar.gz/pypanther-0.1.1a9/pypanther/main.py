6456d28314da2e5efc94d96e45fe98d2fc2a249d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:03 +0000

    Add package pypanther/0.1.1a9/pypanther-0.1.1a9.tar.gz

---
+++ packages/pypanther/pypanther-0.1.1a9.tar.gz/pypanther-0.1.1a9/pypanther/main.py
@@ -0,0 +1,34 @@
+import argparse
+
+from pypanther.upload import run as upload
+from pypanther.vendor.panther_analysis_tool import util
+from pypanther.vendor.panther_analysis_tool.command import standard_args
+from pypanther.vendor.panther_analysis_tool.config import dynaconf_argparse_merge, setup_dynaconf
+
+
+def run():
+    parser = argparse.ArgumentParser(description="Command line tool for uploading files.")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Upload command
+    upload_parser = subparsers.add_parser("upload", help="Upload a file")
+
+    standard_args.for_public_api(upload_parser, required=False)
+    upload_parser.set_defaults(func=util.func_with_backend(upload))
+    upload_parser.add_argument(
+        "--max-retries",
+        help="Retry to upload on a failure for a maximum number of times",
+        default=10,
+        type=int,
+        required=False,
+    )
+
+    args = parser.parse_args()
+    if args.command is None:
+        parser.print_help()
+        return
+
+    config_file_settings = setup_dynaconf()
+    dynaconf_argparse_merge(vars(args), config_file_settings)
+
+    args.func(args)
