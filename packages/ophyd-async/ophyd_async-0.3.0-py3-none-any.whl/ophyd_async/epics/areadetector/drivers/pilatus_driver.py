17230780d24903f3b8c29f95f148af8be349b23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 17:03:06 +0000

    Add package ophyd-async/0.3.0/ophyd_async-0.3.0-py3-none-any.whl

---
+++ packages/ophyd-async/ophyd_async-0.3.0-py3-none-any.whl/ophyd_async/epics/areadetector/drivers/pilatus_driver.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+from ophyd_async.epics.signal.signal import epics_signal_rw_rbv
+
+from .ad_base import ADBase
+
+
+class PilatusTriggerMode(str, Enum):
+    internal = "Internal"
+    ext_enable = "Ext. Enable"
+    ext_trigger = "Ext. Trigger"
+    mult_trigger = "Mult. Trigger"
+    alignment = "Alignment"
+
+
+class PilatusDriver(ADBase):
+    def __init__(self, prefix: str, name: str = "") -> None:
+        self.trigger_mode = epics_signal_rw_rbv(
+            PilatusTriggerMode, prefix + "TriggerMode"
+        )
+        super().__init__(prefix, name)
