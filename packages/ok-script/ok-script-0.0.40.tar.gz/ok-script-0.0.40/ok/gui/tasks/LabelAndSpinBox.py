634c2b639b6cdbaebcf53d9fb34f893be7c09965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:03:07 +0000

    Add package ok-script/0.0.40/ok-script-0.0.40.tar.gz

---
+++ packages/ok-script/ok-script-0.0.40.tar.gz/ok-script-0.0.40/ok/gui/tasks/LabelAndSpinBox.py
@@ -0,0 +1,22 @@
+from qfluentwidgets import SpinBox
+
+from ok.gui.tasks.ConfigLabelAndWidget import ConfigLabelAndWidget
+
+
+class LabelAndSpinBox(ConfigLabelAndWidget):
+
+    def __init__(self, config, config_desc, key: str):
+        super().__init__(config_desc, key)
+        self.key = key
+        self.config = config
+        self.spin_box = SpinBox()
+        self.spin_box.setFixedWidth(130)
+        self.update_value()
+        self.spin_box.valueChanged.connect(self.value_changed)
+        self.add_widget(self.spin_box)
+
+    def update_value(self):
+        self.spin_box.setValue(self.config.get(self.key))
+
+    def value_changed(self, value):
+        self.config[self.key] = value
