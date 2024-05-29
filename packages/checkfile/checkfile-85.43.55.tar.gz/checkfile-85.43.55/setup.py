c911a7fa110f8ab4a05bc2e50720e75c75305e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:40 +0000

    Add package checkfile/85.43.55/checkfile-85.43.55.tar.gz

---
+++ packages/checkfile/checkfile-85.43.55.tar.gz/checkfile-85.43.55/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='checkfile',
+    version='85.43.55',
+    description='A Python package that stops files from being edited.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='JZ Enterprises',
+    packages=find_packages(),  # Automatically find packages in the current directory
+    python_requires='>=3.6',
+)
