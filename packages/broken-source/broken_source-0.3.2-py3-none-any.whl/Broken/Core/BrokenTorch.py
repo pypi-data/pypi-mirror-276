83a8097ea2455fa9b086f7f9a525ffccda4ef0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:04:11 +0000

    Add package broken-source/0.3.2/broken_source-0.3.2-py3-none-any.whl

---
+++ packages/broken-source/broken_source-0.3.2-py3-none-any.whl/Broken/Core/BrokenTorch.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+import os
+import sys
+from pathlib import Path
+from typing import Optional
+
+from loguru import logger as log
+
+import Broken
+from Broken import BrokenEnum, BrokenPath, BrokenPlatform, shell
+
+
+class TorchFlavor(BrokenEnum):
+    CPU   = "2.2.1+cpu"
+    CUDA  = "2.2.1+cu121"
+    ROCM  = "2.2.1+rocm6.0"
+    MACOS = ""
+
+class BrokenTorch:
+    """
+    The Bane of my Existence and the SSD Killer - Packaging PyTorch
+    """
+
+    flavor_file: str = "PyTorch.txt"
+    """A relative path to a Project's Resources defining the PyTorch Flavor"""
+
+    @staticmethod
+    def manage(resources: Path):
+        if os.environ.get("SKIP_TORCH", "0") == "1":
+            return
+
+        import site
+        current_flavor = None
+
+        # Try getting current installed flavor, if any, without importing torch
+        # Note: Reversed as Windows lists system first, and we might have multiple on Linux
+        for site_packages in map(Path, reversed(site.getsitepackages())):
+            if (torch_version := (site_packages/"torch"/"version.py")).exists():
+                exec(torch_version.read_text(), namespace := {})
+                current_flavor = namespace["__version__"].split("+")[1]
+                break
+
+        # Workaround (#pyapp): Until we can send envs to PyAapp, do this monsterous hack
+        if Broken.PYAPP:
+            version_flavor = os.environ.get("PYAPP_COMMAND_NAME", "")
+            if ("+" not in version_flavor):
+                return None
+
+        # Development mode: No PyTorch was found
+        elif (current_flavor is None) or (os.environ.get("MANAGE_TORCH", "0") == "1"):
+            from rich.prompt import Prompt
+            log.warning("This project requires PyTorch but it is not installed.")
+
+            if BrokenPlatform.OnMacOS:
+                version_flavor = TorchFlavor.MACOS
+            else:
+                version_flavor = Prompt.ask("\n".join((
+                        "\nCheck Hardware/Platform availability at:",
+                        "• (https://pytorch.org/get-started/locally)",
+                        "• (https://brokensrc.dev/get/pytorch)",
+                        "\n:: What PyTorch flavor do you want to use?"
+                    )),
+                    choices=[f"{flavor.name.lower()}" for flavor in TorchFlavor] + ["none"],
+                    default="cpu",
+                )
+        else:
+            return
+
+        # Must be a valid Enum item of TorchFlavor
+        if not (version_flavor := TorchFlavor.get(version_flavor)):
+            raise ValueError(f"Invalid PyTorch Flavor ({version_flavor})")
+
+        version, flavor = version_flavor.value.split("+")
+
+        # If flavors mismatch, install the correct one
+        if (current_flavor != flavor):
+            log.info(f"Installing PyTorch Flavor ({version_flavor}), current is ({current_flavor})")
+            PIP = (sys.executable, "-m", "pip")
+            source_url = f"https://download.pytorch.org/whl/{flavor}"
+            shell(PIP, "uninstall", "torch", "-y")
+            shell(PIP, "install", f"torch=={version}", "--index-url", source_url)
+            shell(PIP, "install", "transformers")
+        else:
+            log.info(f"PyTorch Flavor ({version_flavor}) already installed")
+
+    @staticmethod
+    def season(resources: Path, flavor: Optional[TorchFlavor]) -> Optional[Path]:
+        if not bool(flavor):
+            return None
+        flavor = (f"+{flavor.value}" * bool(flavor.value))
+        file = (resources/BrokenTorch.flavor_file)
+        file.write_text(BrokenTorch.version + flavor)
+        return file
+
+    @staticmethod
+    def unseason(resources: Path) -> None:
+        for file in resources.rglob(BrokenTorch.flavor_file):
+            BrokenPath.remove(file)
---
+++ packages/broken-source/broken_source-0.3.2-py3-none-any.whl/ShaderFlow/Module.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+import itertools
+from abc import abstractmethod
+from typing import Any, Iterable, Self, Type, Union
+
+from attr import Factory, define, field
+from loguru import logger as log
+
+from Broken import BrokenAttrs, BrokenFluentBuilder
+from Broken.Externals.FFmpeg import BrokenFFmpeg
+from ShaderFlow.Message import Message
+from ShaderFlow.Variable import ShaderVariable
+
+
+@define
+class ShaderModule(BrokenFluentBuilder, BrokenAttrs):
+    scene: Any = field(default=None, repr=False)
+    name:  str = "Unknown"
+    uuid:  int = Factory(itertools.count(1).__next__)
+
+    def __post__(self):
+        self.scene = self.scene or self
+        self.scene.modules.append(self)
+        log.info(f"{self.who} Module added to the Scene")
+
+    @property
+    def who(self) -> str:
+        return f"({self.uuid:>2}) {type(self).__name__[:18].ljust(18)} │ ▸"
+
+    def find(self, type: Type[ShaderModule]) -> Iterable[ShaderModule]:
+        for module in self.scene.modules:
+            if isinstance(module, type):
+                yield module
+
+    # # Messaging
+
+    def relay(self, message: Union[Message, Type[Message]]) -> Self:
+        if isinstance(message, type):
+            message = message()
+        for module in self.scene.modules:
+            module.handle(message)
+        return self
+
+    # ------------------------------------------|
+
+    # # Shader definitions
+
+    def __process_include__(self, include: str) -> str:
+        return include.format({f"${k}": v for k, v in vars(self).items()})
+
+    @abstractmethod
+    def includes(self) -> Iterable[str]:
+        yield ""
+
+    @abstractmethod
+    def defines(self) -> Iterable[str]:
+        yield None
+
+    # ------------------------------------------|
+
+    @property
+    @abstractmethod
+    def duration(self) -> float:
+        return 0.0
+
+    @abstractmethod
+    def build(self) -> None:
+        pass
+
+    @abstractmethod
+    def setup(self) -> None:
+        pass
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def handle(self, message: Message) -> None:
+        pass
+
+    @abstractmethod
+    def pipeline(self) -> Iterable[ShaderVariable]:
+        return []
+
+    @abstractmethod
+    def ffmpeg(self, ffmpeg: BrokenFFmpeg) -> None:
+        pass
+
+    # ------------------------------------------|
+
+    # # User interface
+
+    def __shaderflow_ui__(self) -> None:
+        """Basic info of a Module"""
+        # Todo: Make automatic Imgui methods
+
+        # Module - self.__ui__ must be implemented
+        if not getattr(self.__ui__, "__isabstractmethod__", False):
+            self.__ui__()
+
+        # Module - self.ui must be implemented
+        if not getattr(self.ui, "__isabstractmethod__", False):
+            self.ui()
+
+    @abstractmethod
+    def __ui__(self) -> None:
+        """Internal method for self.ui"""
+        pass
+
+    @abstractmethod
+    def ui(self) -> None:
+        """
+        Draw the UI for this module
+        """
+        pass
