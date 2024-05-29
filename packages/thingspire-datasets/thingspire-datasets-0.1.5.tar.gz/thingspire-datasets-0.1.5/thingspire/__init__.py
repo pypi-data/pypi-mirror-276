82ebd4cb098559ad3e08255dcf292d03d680d846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:04:15 +0000

    Add package thingspire-datasets/0.1.5/thingspire-datasets-0.1.5.tar.gz

---
+++ packages/thingspire-datasets/thingspire-datasets-0.1.5.tar.gz/thingspire-datasets-0.1.5/thingspire/__init__.py
@@ -0,0 +1,4 @@
+from .datasets.ts_calendar import get_calendar
+from .datasets.ts_weather import get_weather
+
+__all__ = ['get_calendar', 'get_weather']
---
+++ packages/thingspire-datasets/thingspire-datasets-0.1.5.tar.gz/thingspire-datasets-0.1.5/thingspire/datasets/__init__.py
@@ -0,0 +1,4 @@
+from .ts_calendar import get_calendar
+from .ts_weather import get_weather
+
+__all__ = ['get_calendar', 'get_weather']
