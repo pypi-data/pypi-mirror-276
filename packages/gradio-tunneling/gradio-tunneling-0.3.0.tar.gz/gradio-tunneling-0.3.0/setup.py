117e58b0b69138b1e5605aaa4635cbc87bc976cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:30 +0000

    Add package gradio-tunneling/0.3.0/gradio-tunneling-0.3.0.tar.gz

---
+++ packages/gradio-tunneling/gradio-tunneling-0.3.0.tar.gz/gradio-tunneling-0.3.0/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gradio-tunneling',
+    version='0.3.0',
+    packages=find_packages(),
+    package_data={
+        'gradio_tunneling': ['frpc*'],
+    },
+    install_requires=[
+        # List your dependencies here
+        'requests',
+        'gradio',
+        # 'some-other-package',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gradio-tunneling=gradio_tunneling.main:main',
+        ],
+    },
+    python_requires='>=3.8',
+)
+
