edd894dd5e6269b07817d0cef2eb247e0051ab18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:49 +0000

    Add package sbi4onnx/1.0.7/sbi4onnx-1.0.7.tar.gz

---
+++ packages/sbi4onnx/sbi4onnx-1.0.7.tar.gz/sbi4onnx-1.0.7/README.md
@@ -0,0 +1,168 @@
+# sbi4onnx
+A very simple script that only initializes the batch size of ONNX. **S**imple **B**atchsize **I**nitialization for **ONNX**.
+
+https://github.com/PINTO0309/simple-onnx-processing-tools
+
+[![Downloads](https://static.pepy.tech/personalized-badge/sbi4onnx?period=total&units=none&left_color=grey&right_color=brightgreen&left_text=Downloads)](https://pepy.tech/project/sbi4onnx) ![GitHub](https://img.shields.io/github/license/PINTO0309/sbi4onnx?color=2BAF2B) [![PyPI](https://img.shields.io/pypi/v/sbi4onnx?color=2BAF2B)](https://pypi.org/project/sbi4onnx/) [![CodeQL](https://github.com/PINTO0309/sbi4onnx/workflows/CodeQL/badge.svg)](https://github.com/PINTO0309/sbi4onnx/actions?query=workflow%3ACodeQL)
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/33194443/170157713-78a1b84e-caf6-4abe-92e4-c9ea51bcaacd.png" />
+</p>
+
+# Key concept
+
+- [x] Initializes the ONNX batch size with the specified characters.
+- [x] This tool is not a panacea and may fail to initialize models with very complex structures. For example, there is an ONNX that contains a `Reshape` that involves a batch size, or a `Gemm` that contains a batch output other than 1 in the output result.
+- [x] A `Reshape` in a graph cannot contain more than two undefined shapes, such as `-1` or `N` or `None` or `unk_*`. Therefore, before initializing the batch size with this tool, make sure that the `Reshape` does not already contain one or more `-1` dimensions. If it already contains undefined dimensions, it may be possible to successfully initialize the batch size by pre-writing the undefined dimensions of the relevant `Reshape` to static values using **[sam4onnx](https://github.com/PINTO0309/sam4onnx)**.
+
+## 1. Setup
+### 1-1. HostPC
+```bash
+### option
+$ echo export PATH="~/.local/bin:$PATH" >> ~/.bashrc \
+&& source ~/.bashrc
+
+### run
+$ pip install -U onnx \
+&& python3 -m pip install -U onnx_graphsurgeon --index-url https://pypi.ngc.nvidia.com \
+&& pip install --no-deps -U onnx-simplifier \
+&& pip install -U sbi4onnx
+```
+### 1-2. Docker
+https://github.com/PINTO0309/simple-onnx-processing-tools#docker
+
+## 2. CLI Usage
+```
+$ sbi4onnx -h
+
+usage:
+  sbi4onnx [-h]
+  -if INPUT_ONNX_FILE_PATH
+  -of OUTPUT_ONNX_FILE_PATH
+  -ics INITIALIZATION_CHARACTER_STRING
+  [-dos]
+  [-n]
+
+optional arguments:
+  -h, --help
+      show this help message and exit.
+
+  -if INPUT_ONNX_FILE_PATH, --input_onnx_file_path INPUT_ONNX_FILE_PATH
+      Input onnx file path.
+
+  -of OUTPUT_ONNX_FILE_PATH, --output_onnx_file_path OUTPUT_ONNX_FILE_PATH
+      Output onnx file path.
+
+  -ics INITIALIZATION_CHARACTER_STRING, --initialization_character_string INITIALIZATION_CHARACTER_STRING
+      String to initialize batch size. "-1" or "N" or "xxx", etc...
+      Default: '-1'
+
+  -dos, --disable_onnxsim
+      Suppress the execution of onnxsim on the backend and dare to leave redundant processing.
+
+  -n, --non_verbose
+      Do not show all information logs. Only error logs are displayed.
+```
+
+## 3. In-script Usage
+```python
+>>> from sbi4onnx import initialize
+>>> help(initialize)
+
+Help on function initialize in module sbi4onnx.onnx_batchsize_initialize:
+
+initialize(
+  input_onnx_file_path: Union[str, NoneType] = '',
+  onnx_graph: Union[onnx.onnx_ml_pb2.ModelProto, NoneType] = None,
+  output_onnx_file_path: Union[str, NoneType] = '',
+  initialization_character_string: Union[str, NoneType] = '-1',
+  non_verbose: Union[bool, NoneType] = False,
+  disable_onnxsim: Union[bool, NoneType] = False,
+) -> onnx.onnx_ml_pb2.ModelProto
+
+    Parameters
+    ----------
+    input_onnx_file_path: Optional[str]
+        Input onnx file path.
+        Either input_onnx_file_path or onnx_graph must be specified.
+        Default: ''
+
+    onnx_graph: Optional[onnx.ModelProto]
+        onnx.ModelProto.
+        Either input_onnx_file_path or onnx_graph must be specified.
+        onnx_graph If specified, ignore input_onnx_file_path and process onnx_graph.
+
+    output_onnx_file_path: Optional[str]
+        Output onnx file path. If not specified, no ONNX file is output.
+        Default: ''
+
+    initialization_character_string: Optional[str]
+        String to initialize batch size. "-1" or "N" or "xxx", etc...
+        Default: '-1'
+
+    disable_onnxsim: Optional[bool]
+        Suppress the execution of onnxsim on the backend and dare to leave redundant processing.
+        Default: False
+
+    non_verbose: Optional[bool]
+        Do not show all information logs. Only error logs are displayed.
+        Default: False
+
+    Returns
+    -------
+    changed_graph: onnx.ModelProto
+        Changed onnx ModelProto.
+```
+
+## 4. CLI Execution
+```bash
+$ sbi4onnx \
+--input_onnx_file_path whenet_224x224.onnx \
+--output_onnx_file_path whenet_Nx224x224.onnx \
+--initialization_character_string N
+
+$ sbi4onnx \
+--input_onnx_file_path whenet_224x224.onnx \
+--output_onnx_file_path whenet_Nx224x224.onnx \
+--initialization_character_string -1
+
+$ sbi4onnx \
+--input_onnx_file_path whenet_224x224.onnx \
+--output_onnx_file_path whenet_Nx224x224.onnx \
+--initialization_character_string abcdefg
+```
+
+## 5. In-script Execution
+```python
+from sbi4onnx import initialize
+
+onnx_graph = initialize(
+  input_onnx_file_path="whenet_224x224.onnx",
+  output_onnx_file_path="whenet_Nx224x224.onnx",
+  initialization_character_string="abcdefg",
+)
+
+# or
+
+onnx_graph = initialize(
+  onnx_graph=graph,
+  initialization_character_string="abcdefg",
+)
+```
+
+## 6. Sample
+### Before
+![image](https://user-images.githubusercontent.com/33194443/166225839-3b8d6378-e76f-4139-b5d1-db547ba16d16.png)
+
+### After
+![image](https://user-images.githubusercontent.com/33194443/166225927-cb39ea2f-85f6-4fdd-afbc-78a46a2475a1.png)
+
+## 7. Reference
+1. https://github.com/onnx/onnx/blob/main/docs/Operators.md
+2. https://docs.nvidia.com/deeplearning/tensorrt/onnx-graphsurgeon/docs/index.html
+3. https://github.com/NVIDIA/TensorRT/tree/main/tools/onnx-graphsurgeon
+4. https://github.com/PINTO0309/simple-onnx-processing-tools
+5. https://github.com/PINTO0309/PINTO_model_zoo
+
+## 8. Issues
+https://github.com/PINTO0309/simple-onnx-processing-tools/issues
---
+++ packages/sbi4onnx/sbi4onnx-1.0.7.tar.gz/sbi4onnx-1.0.7/sbi4onnx/onnx_batchsize_initialize.py
@@ -0,0 +1,226 @@
+#! /usr/bin/env python
+
+import sys
+import onnx
+import onnx_graphsurgeon as gs
+from typing import Optional
+import struct
+from argparse import ArgumentParser
+from onnxsim import simplify
+
+class Color:
+    BLACK          = '\033[30m'
+    RED            = '\033[31m'
+    GREEN          = '\033[32m'
+    YELLOW         = '\033[33m'
+    BLUE           = '\033[34m'
+    MAGENTA        = '\033[35m'
+    CYAN           = '\033[36m'
+    WHITE          = '\033[37m'
+    COLOR_DEFAULT  = '\033[39m'
+    BOLD           = '\033[1m'
+    UNDERLINE      = '\033[4m'
+    INVISIBLE      = '\033[08m'
+    REVERCE        = '\033[07m'
+    BG_BLACK       = '\033[40m'
+    BG_RED         = '\033[41m'
+    BG_GREEN       = '\033[42m'
+    BG_YELLOW      = '\033[43m'
+    BG_BLUE        = '\033[44m'
+    BG_MAGENTA     = '\033[45m'
+    BG_CYAN        = '\033[46m'
+    BG_WHITE       = '\033[47m'
+    BG_DEFAULT     = '\033[49m'
+    RESET          = '\033[0m'
+
+
+def initialize(
+    input_onnx_file_path: Optional[str] = '',
+    onnx_graph: Optional[onnx.ModelProto] = None,
+    output_onnx_file_path: Optional[str] = '',
+    initialization_character_string: Optional[str] = '-1',
+    disable_onnxsim: Optional[bool] = False,
+    non_verbose: Optional[bool] = False,
+) -> onnx.ModelProto:
+    """
+    Parameters
+    ----------
+    input_onnx_file_path: Optional[str]
+        Input onnx file path.\n\
+        Either input_onnx_file_path or onnx_graph must be specified.\n\
+        Default: ''
+
+    onnx_graph: Optional[onnx.ModelProto]
+        onnx.ModelProto.\n\
+        Either input_onnx_file_path or onnx_graph must be specified.\n\
+        onnx_graph If specified, ignore input_onnx_file_path and process onnx_graph.
+
+    output_onnx_file_path: Optional[str]
+        Output onnx file path. If not specified, no ONNX file is output.\n\
+        Default: ''
+
+    initialization_character_string: Optional[str]
+        String to initialize batch size. "-1" or "N" or "xxx", etc...\n
+        Default: '-1'
+
+    disable_onnxsim: Optional[bool]
+        Suppress the execution of onnxsim on the backend and dare to leave redundant processing.\n\
+        Default: False
+
+    non_verbose: Optional[bool]
+        Do not show all information logs. Only error logs are displayed.\n\
+        Default: False
+
+    Returns
+    -------
+    changed_graph: onnx.ModelProto
+        Changed onnx ModelProto.
+    """
+
+    # Unspecified check for input_onnx_file_path and onnx_graph
+    if not input_onnx_file_path and not onnx_graph:
+        print(
+            f'{Color.RED}ERROR:{Color.RESET} '+
+            f'One of input_onnx_file_path or onnx_graph must be specified.'
+        )
+        sys.exit(1)
+
+    if not initialization_character_string:
+        print(
+            f'{Color.RED}ERROR:{Color.RESET} '+
+            f'The initialization_character_string cannot be empty.'
+        )
+        sys.exit(1)
+
+    # Loading Graphs
+    # onnx_graph If specified, onnx_graph is processed first
+    if not onnx_graph:
+        onnx_graph = onnx.load(input_onnx_file_path)
+    try:
+        # onnx-simplifier does not support optimization of ONNX files containing custom domains,
+        # so skip simplify if it contains custom domains
+        if not disable_onnxsim:
+            onnx_graph, _ = simplify(onnx_graph)
+    except:
+        pass
+
+    # domain, ir_version
+    domain: str = onnx_graph.domain
+    ir_version: int = onnx_graph.ir_version
+    meta_data = {'domain': domain, 'ir_version': ir_version}
+    metadata_props = None
+    if hasattr(onnx_graph, 'metadata_props'):
+        metadata_props = onnx_graph.metadata_props
+
+    graph = gs.import_onnx(onnx_graph)
+    graph.cleanup().toposort()
+    target_model = gs.export_onnx(graph, do_type_check=False, **meta_data)
+    if metadata_props is not None:
+        target_model.metadata_props.extend(metadata_props)
+    target_graph = target_model.graph
+
+    # Change batch size in input, output and value_info
+    taget_nodes = \
+        list(target_graph.input) + \
+        list(target_graph.value_info) + \
+        list(target_graph.output)
+
+    for tensor in taget_nodes:
+        if len(tensor.type.tensor_type.shape.dim)>0:
+            tensor.type.tensor_type.shape.dim[0].dim_param = initialization_character_string
+
+    # Set dynamic batch size in reshapes (-1)
+    for node in  target_graph.node:
+        if node.op_type != 'Reshape':
+            continue
+        for init in target_graph.initializer:
+            # node.input[1] is expected to be a reshape
+            if init.name != node.input[1]:
+                continue
+            # Shape is stored as a list of ints
+            if len(init.int64_data) > 0:
+                # This overwrites bias nodes' reshape shape but should be fine
+                init.int64_data[0] = -1
+            # Shape is stored as bytes
+            elif len(init.raw_data) > 0:
+                shape = bytearray(init.raw_data)
+                struct.pack_into('q', shape, 0, -1)
+                init.raw_data = bytes(shape)
+
+    # infer_shapes
+    if len(target_graph.value_info) > 0:
+        target_model = onnx.shape_inference.infer_shapes(target_model)
+
+    # Save
+    if output_onnx_file_path:
+        onnx.save(target_model, output_onnx_file_path)
+
+    if not non_verbose:
+        print(f'{Color.GREEN}INFO:{Color.RESET} Finish!')
+
+    # Return
+    return target_model
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-if',
+        '--input_onnx_file_path',
+        type=str,
+        required=True,
+        help='Input onnx file path.'
+    )
+    parser.add_argument(
+        '-of',
+        '--output_onnx_file_path',
+        type=str,
+        required=True,
+        help='Output onnx file path.'
+    )
+    parser.add_argument(
+        '-ics',
+        '--initialization_character_string',
+        type=str,
+        required=True,
+        default='-1',
+        help=\
+            'String to initialize batch size. "-1" or "N" or "xxx", etc... \n'+
+            'Default: \'-1\''
+    )
+    parser.add_argument(
+        '-dos',
+        '--disable_onnxsim',
+        action='store_true',
+        help='Suppress the execution of onnxsim on the backend and dare to leave redundant processing.'
+    )
+    parser.add_argument(
+        '-n',
+        '--non_verbose',
+        action='store_true',
+        help='Do not show all information logs. Only error logs are displayed.'
+    )
+    args = parser.parse_args()
+
+    input_onnx_file_path = args.input_onnx_file_path
+    output_onnx_file_path = args.output_onnx_file_path
+    initialization_character_string = args.initialization_character_string
+    disable_onnxsim = args.disable_onnxsim
+    non_verbose = args.non_verbose
+
+    # Load
+    onnx_graph = onnx.load(input_onnx_file_path)
+
+    # Batchsize change
+    changed_graph = initialize(
+        input_onnx_file_path=None,
+        onnx_graph=onnx_graph,
+        output_onnx_file_path=output_onnx_file_path,
+        initialization_character_string=initialization_character_string,
+        disable_onnxsim=disable_onnxsim,
+        non_verbose=non_verbose,
+    )
+
+
+if __name__ == '__main__':
+    main()
