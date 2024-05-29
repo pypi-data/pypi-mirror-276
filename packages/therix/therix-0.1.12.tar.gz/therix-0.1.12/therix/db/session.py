16a355b386cc34b3e38e2101a364191b990d51cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:19 +0000

    Add package therix/0.1.12/therix-0.1.12.tar.gz

---
+++ packages/therix/therix-0.1.12.tar.gz/therix-0.1.12/therix/db/session.py
@@ -0,0 +1,13 @@
+# db/session.py
+
+from sqlalchemy.orm import sessionmaker
+from therix.db.db_manager import DatabaseManager
+
+# Example PostgreSQL connection string
+# Format: postgresql://user:password@hostname/database_name
+db_manager = DatabaseManager()
+SQLALCHEMY_DATABASE_URL = DatabaseManager._construct_db_url()
+
+engine = DatabaseManager()._engine
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
