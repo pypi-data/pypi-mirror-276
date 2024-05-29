4d34163cdf09acb0268d3bc9d4bc2a50ae36a1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:02:54 +0000

    Add package logistic-regression-module-universal-bank/0.0.4/logistic_regression_module_universal_bank-0.0.4-py3-none-any.whl

---
+++ packages/logistic-regression-module-universal-bank/logistic_regression_module_universal_bank-0.0.4-py3-none-any.whl/ logistic-regression-module-universal-bank/__init__.py
@@ -0,0 +1,3 @@
+from .logistic_regression_module import preprocess_universal_bank_data, logistic_regression, nominal_logistic_regression, ordinal_logistic_regression, plot_charts
+
+__version__ = '0.0.4'
