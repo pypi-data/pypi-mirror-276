9781a2fd33b118ad144b2d5e25e3c4b91ff686e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:13 +0000

    Add package furiosa-native-postprocess/0.10.0/furiosa_native_postprocess-0.10.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/furiosa-native-postprocess/furiosa_native_postprocess-0.10.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/furiosa_native_postprocess/__init__.py
@@ -0,0 +1,5 @@
+from .furiosa_native_postprocess import *
+
+__doc__ = furiosa_native_postprocess.__doc__
+if hasattr(furiosa_native_postprocess, "__all__"):
+    __all__ = furiosa_native_postprocess.__all__
