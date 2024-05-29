a5ec440908dc0b139e64aef687f95426603e25b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 15:03:32 +0000

    Add package py-alpaca-daily-losers/0.3.3/py_alpaca_daily_losers-0.3.3-py3-none-any.whl

---
+++ packages/py-alpaca-daily-losers/py_alpaca_daily_losers-0.3.3-py3-none-any.whl/py_alpaca_daily_losers/src/global_fuctions.py
@@ -0,0 +1,23 @@
+import os
+from .slack import Slack
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+production = os.getenv("PRODUCTION")
+slack_username = os.getenv("SLACK_USERNAME")
+
+
+def send_message(message):
+    """
+    Send a message to Slack
+    :param message: str: message to send
+    """
+    slack = Slack()
+    if production == "False":
+        print(message)
+    else:
+        slack.send_message(
+            channel="#app-development", message=message, username=slack_username
+        )
