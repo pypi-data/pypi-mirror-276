7b056d0f25b01d91fa639e14c0a047dbeb23dc48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 14:03:37 +0000

    Add package xcoll/0.4.0/xcoll-0.4.0.tar.gz

---
+++ packages/xcoll/xcoll-0.4.0.tar.gz/xcoll-0.4.0/xcoll/beam_elements/__init__.py
@@ -0,0 +1,18 @@
+# copyright ############################### #
+# This file is part of the Xcoll Package.   #
+# Copyright (c) CERN, 2024.                 #
+# ######################################### #
+
+from .base import BaseBlock, BaseCollimator, BaseCrystal
+from .absorber import BlackAbsorber, BlackCrystal
+from .everest import EverestBlock, EverestCollimator, EverestCrystal
+
+block_classes = tuple(v for v in globals().values()
+                      if isinstance(v, type) and issubclass(v, BaseBlock) and v != BaseBlock)
+# Includes crystals
+collimator_classes = tuple(v for v in globals().values()
+                           if isinstance(v, type) and (issubclass(v, BaseCollimator) or issubclass(v, BaseCrystal))
+                           and v != BaseCollimator and v != BaseCrystal)
+crystal_classes = tuple(v for v in globals().values()
+                        if isinstance(v, type) and issubclass(v, BaseCrystal) and v != BaseCrystal)
+element_classes = block_classes + collimator_classes
