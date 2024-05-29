913c53614abddb6286b054a25374ef43923ae035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 16:03:29 +0000

    Add package openbot-training-frontend/0.8.0/openbot_training_frontend-0.8.0.tar.gz

---
+++ packages/openbot-training-frontend/openbot_training_frontend-0.8.0.tar.gz/openbot_training_frontend-0.8.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="openbot_training_frontend",
+    version="0.8.0",
+    description="OpenBot model training package",
+    url="https://github.com/3dwesupport/OpenBot",
+    author="Hardik Garg",
+    author_email="hardik.garg@itinker.io",
+    license="MIT",
+    packages=find_packages(include=["openbot_training_frontend", "openbot_training_frontend.*"]),
+    include_package_data=True,
+    zip_safe=False,
+)
