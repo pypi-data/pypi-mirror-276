f25defdf44b073c8ebb2ad928d0acf4667a219de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-29 03:03:36 +0000

    Add package mkdocsi/1.2.1/mkdocsi-1.2.1.tar.gz

---
+++ packages/mkdocsi/mkdocsi-1.2.1.tar.gz/mkdocsi-1.2.1/mkdocsi/Main.py
@@ -0,0 +1,11 @@
+import sys  , os 
+from mkdocsi import generate_site
+
+def main():
+    docs_folder = os.path.join(os.getcwd() , "docs")
+    Template_mkdocs   = None
+    site_name = os.path.basename(os.getcwd())
+    generate_site(docs_folder = docs_folder , Template_mkdocs = Template_mkdocs , site_name = site_name )
+
+if __name__ == "__main__":
+    main()
