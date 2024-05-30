08a68e1eee881cbb35517fcb2a7e00fdc036e28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 15:03:34 +0000

    Add package baostockhelloworld/0.0.4/baostockhelloworld-0.0.4-py3-none-any.whl

---
+++ packages/baostockhelloworld/baostockhelloworld-0.0.4-py3-none-any.whl/baostockhelloworld/__init__.py
@@ -0,0 +1,12 @@
+# 定义包的版本
+#__version__ = '0.1.0'
+
+# 导入包中所有的公共函数和类
+#from .module1 import MyClass, my_function
+
+# 可以直接从包中访问这些对象
+# from your_package import MyClass, my_function
+
+# sayhelloworld
+from baostockhelloworld.util.helloworldutil import (sayhelloworld)
+
