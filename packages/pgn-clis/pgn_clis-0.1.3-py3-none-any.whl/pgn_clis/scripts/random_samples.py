da04329f3fd87d3773ac06d8db6fe39e1df5c441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:49 +0000

    Add package pgn-clis/0.1.3/pgn_clis-0.1.3-py3-none-any.whl

---
+++ packages/pgn-clis/pgn_clis-0.1.3-py3-none-any.whl/pgn_clis/scripts/random_samples.py
@@ -0,0 +1,45 @@
+from argparse import ArgumentParser
+
+def main():
+
+  parser = ArgumentParser()
+  parser.add_argument('-n', '--num-samples', type=int, default=int(1e6))
+  parser.add_argument('-l', '--max-len', type=int, default=150)
+  parser.add_argument('-s', '--seed', type=int, default=0)
+  parser.add_argument('-v', '--verbose', action='store_true')
+  parser.add_argument('-r', '--replace', action='store_true')
+
+  io_group = parser.add_mutually_exclusive_group(required=True)
+  files_spec = io_group.add_argument_group()
+  files_spec.add_argument('-i', '--inputs', type=str)
+  files_spec.add_argument('-u', '--ucis', type=str)
+
+  io_group.add_argument('-o', '--output', type=str)
+
+  args = parser.parse_args()
+
+  import os
+
+  if args.output:
+    os.makedirs(args.output, exist_ok=True)
+    input_path = os.path.join(args.output, 'inputs.txt')
+    uci_path = os.path.join(args.output, 'ucis.txt')
+  else:
+    input_path = args.inputs
+    uci_path = args.ucis
+
+  print(f'Generating samples...')
+  print(f'  inputs: {input_path}')
+  print(f'  ucis: {uci_path}')
+  print()
+
+  import sys
+  from pgn_clis.lib.random_samples import random_samples
+
+  mode = 'w' if args.replace else 'x'
+  with open(input_path, mode) as inputs_f, open(uci_path, mode) as ucis_f:
+    random_samples(
+      inputs_f, ucis_f,
+      num_samples=args.num_samples, max_len=args.max_len,
+      seed=args.seed, logstream=sys.stderr if args.verbose else None
+    )
