0206b1dbac787664c052dd3b8cc4580a9c8134ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:04:08 +0000

    Add package langwatch/0.0.26/langwatch-0.0.26-py3-none-any.whl

---
+++ packages/langwatch/langwatch-0.0.26-py3-none-any.whl/langwatch/__init__.py
@@ -0,0 +1,19 @@
+"""
+LangWatch
+
+This is the top level module for [LangWatch](https://github.com/langwatch/langwatch),
+it holds the api_key and the endpoint the tracing is going to be send to.
+
+For LLM and library-specific tracing functions, check out the other files on this module.
+"""
+
+import os
+
+from langwatch.tracer import span, create_span, capture_rag
+from langwatch.dspy import langwatch_dspy as dspy
+from langwatch.login import login
+
+endpoint = os.environ.get("LANGWATCH_ENDPOINT") or "https://app.langwatch.ai"
+api_key = os.environ.get("LANGWATCH_API_KEY")
+
+__all__ = ("endpoint", "api_key", "span", "create_span", "capture_rag", "dspy", "login")
