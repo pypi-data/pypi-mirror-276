aa686173c9c003eecdfa0ddc6fd2667f2a2170cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:32 +0000

    Add package CTG-Utils/1.2.2/CTG_Utils-1.2.2.tar.gz

---
+++ packages/CTG-Utils/CTG_Utils-1.2.2.tar.gz/CTG_Utils-1.2.2/ctgutils/ctgFuncts/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'F. Bertin'
+__license__ = "MIT"
+__version__ = '1.1.3'
+
+from ctgutils.ctgfuncts.ctg_plot import *
+from ctgutils.ctgfuncts.ctg_effectif import *
+from ctgutils.ctgfuncts.ctg_synthese import *
+from ctgutils.ctgfuncts.ctg_classes import *
+from ctgutils.ctgfuncts.ctg_tools import *
