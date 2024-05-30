f557b11bb28aaec7aa10653b23c81f48c2d4d2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:51 +0000

    Add package b22ao/0.0.26/b22ao-0.0.26.tar.gz

---
+++ packages/b22ao/b22ao-0.0.26.tar.gz/b22ao-0.0.26/b22ao/log.py
@@ -0,0 +1,34 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter('%(message)s'))
+
+logger.addHandler(stream_handler)
+
+def configure(filepath=None, graylog_host=None, graylog_port=None):
+    
+    formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s || %(message)s",
+                datefmt="%d-%m-%Y %I:%M:%S")
+
+    def setup_file_handler(file_path):
+        file_handler = logging.FileHandler(file_path, encoding="utf-8")
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    
+    def setup_graylog_handler(host, port):
+        from graypy import GELFUDPHandler
+        graylog_handler = GELFUDPHandler(host, port)
+        graylog_handler.setFormatter(formatter)
+        logger.addHandler(graylog_handler)
+    
+    if filepath:
+        setup_file_handler(filepath)
+    
+    if graylog_host and graylog_port:
+        setup_graylog_handler(graylog_host, graylog_port)
