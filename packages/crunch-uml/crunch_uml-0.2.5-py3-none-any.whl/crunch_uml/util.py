ec9744565abbbe321621a6c1d06a4ba7e3cbe375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:51 +0000

    Add package crunch-uml/0.2.5/crunch_uml-0.2.5-py3-none-any.whl

---
+++ packages/crunch-uml/crunch_uml-0.2.5-py3-none-any.whl/crunch_uml/util.py
@@ -0,0 +1,22 @@
+import argparse
+import re
+import uuid
+from urllib.parse import urlparse, urlunparse
+
+
+def valid_url(value):
+    parsed = urlparse(value)
+    if not all([parsed.scheme, parsed.netloc]):
+        raise argparse.ArgumentTypeError(f"'{value}' is geen geldige URL.")
+    return urlunparse(value)
+
+
+def remove_substring(s, substring):
+    pattern = re.compile(re.escape(substring), re.IGNORECASE)
+    return pattern.sub('', s).strip()
+
+
+def getEAGuid():
+    str_uuid = uuid.uuid4()
+    str_uuid = str(str_uuid).replace("-", "_")
+    return 'EAID_' + str_uuid
