570a210312f404f2bec9c5046000af0a11515b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:04:26 +0000

    Add package sweego/0.0.1/sweego-0.0.1-py3-none-any.whl

---
+++ packages/sweego/sweego-0.0.1-py3-none-any.whl/sweego/api/__init__.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+
+# import apis into api package
+from sweego.api.auth_api import AuthApi
+from sweego.api.billing_api import BillingApi
+from sweego.api.channels_api import ChannelsApi
+from sweego.api.client_api import ClientApi
+from sweego.api.client_api_key_api import ClientApiKeyApi
+from sweego.api.client_billing_api import ClientBillingApi
+from sweego.api.client_credentials_api import ClientCredentialsApi
+from sweego.api.client_domain_api import ClientDomainApi
+from sweego.api.client_mta_server_api import ClientMTAServerApi
+from sweego.api.client_sftp_server_api import ClientSFTPServerApi
+from sweego.api.client_template_api import ClientTemplateApi
+from sweego.api.client_template_variable_api import ClientTemplateVariableApi
+from sweego.api.client_user_api import ClientUserApi
+from sweego.api.image_hosting_api import ImageHostingApi
+from sweego.api.logs_api import LogsApi
+from sweego.api.send_api import SendApi
+from sweego.api.stats_api import StatsApi
+from sweego.api.webhook_api import WebhookApi
+from sweego.api.default_api import DefaultApi
+
