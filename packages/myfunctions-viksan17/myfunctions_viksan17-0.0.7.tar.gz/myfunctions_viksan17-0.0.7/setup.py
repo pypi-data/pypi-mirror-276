50e1d9361b49e5977b167628a1590b55f80e24da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:29 +0000

    Add package myfunctions-viksan17/0.0.7/myfunctions_viksan17-0.0.7.tar.gz

---
+++ packages/myfunctions-viksan17/myfunctions_viksan17-0.0.7.tar.gz/myfunctions_viksan17-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_viksan17',
+    version='0.0.7',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/viksan17/',
+    license='MIT',
+    author='Victor Sanabria',
+    author_email='vicsanab92@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
