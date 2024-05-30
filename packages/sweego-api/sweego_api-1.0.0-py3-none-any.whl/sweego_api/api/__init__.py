3375c68ade988842ed9e2134124557cab8a501d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:04:01 +0000

    Add package sweego-api/1.0.0/sweego_api-1.0.0-py3-none-any.whl

---
+++ packages/sweego-api/sweego_api-1.0.0-py3-none-any.whl/sweego_api/api/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+
+# import apis into api package
+from sweego_api.api.auth_api import AuthApi
+from sweego_api.api.billing_api import BillingApi
+from sweego_api.api.channels_api import ChannelsApi
+from sweego_api.api.client_api import ClientApi
+from sweego_api.api.client_api_key_api import ClientApiKeyApi
+from sweego_api.api.client_billing_api import ClientBillingApi
+from sweego_api.api.client_credentials_api import ClientCredentialsApi
+from sweego_api.api.client_domain_api import ClientDomainApi
+from sweego_api.api.client_mta_server_api import ClientMTAServerApi
+from sweego_api.api.client_sftp_server_api import ClientSFTPServerApi
+from sweego_api.api.client_template_api import ClientTemplateApi
+from sweego_api.api.client_template_variable_api import ClientTemplateVariableApi
+from sweego_api.api.client_user_api import ClientUserApi
+from sweego_api.api.configuration_api import ConfigurationApi
+from sweego_api.api.image_hosting_api import ImageHostingApi
+from sweego_api.api.logs_api import LogsApi
+from sweego_api.api.mtaip_api import MTAIpApi
+from sweego_api.api.mta_server_api import MTAServerApi
+from sweego_api.api.sftp_server_api import SFTPServerApi
+from sweego_api.api.send_api import SendApi
+from sweego_api.api.stats_api import StatsApi
+from sweego_api.api.webhook_api import WebhookApi
+from sweego_api.api.default_api import DefaultApi
+
