5a8044aef35df3c848fdadaf63662a0ba9e2071f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:07 +0000

    Add package fairbalance/0.1.8/fairbalance-0.1.8-py3-none-any.whl

---
+++ packages/fairbalance/fairbalance-0.1.8-py3-none-any.whl/fairbalance/_tools/__init__.py
@@ -0,0 +1,15 @@
+"""
+The :mod:`fairbalance._tools` module should not be used. it includes different useful classes and functions internal to the process.
+"""
+
+# from ._fairness_analysis import FairnessAnalysis
+from ._mitigator import Mitigator
+from ._processor import _Processor, RandomOverSamplerProcessor, SMOTENCProcessor, RandomUnderSamplerProcessor
+
+__all__ = [
+    # "FairnessAnalysis",
+    "Mitigator",
+    "SMOTENCProcessor",
+    "RandomOverSamplerProcessor",
+    "RandomUnderSamplerProcessor",
+]
