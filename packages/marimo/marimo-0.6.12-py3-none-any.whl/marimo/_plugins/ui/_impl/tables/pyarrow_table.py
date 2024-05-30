6c1e3baab6cf01aa3ecd2230503f780359f489df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:04:00 +0000

    Add package marimo/0.6.12/marimo-0.6.12-py3-none-any.whl

---
+++ packages/marimo/marimo-0.6.12-py3-none-any.whl/marimo/_plugins/ui/_impl/tables/pyarrow_table.py
@@ -0,0 +1,96 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import io
+from typing import Any, Union, cast
+
+from marimo._plugins.ui._impl.tables.table_manager import (
+    FieldType,
+    FieldTypes,
+    TableManager,
+    TableManagerFactory,
+)
+
+
+class PyArrowTableManagerFactory(TableManagerFactory):
+    @staticmethod
+    def package_name() -> str:
+        return "pyarrow"
+
+    @staticmethod
+    def create() -> type[TableManager[Any]]:
+        import pyarrow as pa  # type: ignore
+
+        class PyArrowTableManager(
+            TableManager[Union[pa.Table, pa.RecordBatch]]
+        ):
+            def to_csv(self) -> bytes:
+                import pyarrow.csv as csv  # type: ignore
+
+                buffer = io.BytesIO()
+                csv.write_csv(self.data, buffer)
+                return buffer.getvalue()
+
+            def to_json(self) -> bytes:
+                # Arrow does not have a built-in JSON writer
+                return (
+                    self.data.to_pandas()
+                    .to_json(orient="records")
+                    .encode("utf-8")
+                )
+
+            def select_rows(self, indices: list[int]) -> PyArrowTableManager:
+                if not indices:
+                    return PyArrowTableManager(
+                        pa.Table.from_pylist([], schema=self.data.schema)
+                    )
+                return PyArrowTableManager(self.data.take(indices))
+
+            def get_row_headers(
+                self,
+            ) -> list[tuple[str, list[str | int | float]]]:
+                return []
+
+            @staticmethod
+            def is_type(value: Any) -> bool:
+                import pyarrow as pa  # type: ignore
+
+                return isinstance(value, pa.Table) or isinstance(
+                    value, pa.RecordBatch
+                )
+
+            def get_field_types(self) -> FieldTypes:
+                return {
+                    column: PyArrowTableManager._get_field_type(
+                        cast(Any, self.data)[idx]
+                    )
+                    for idx, column in enumerate(self.data.schema.names)
+                }
+
+            def limit(self, num: int) -> PyArrowTableManager:
+                if num < 0:
+                    raise ValueError("Limit must be a positive integer")
+                return PyArrowTableManager(self.data.take(list(range(num))))
+
+            @staticmethod
+            def _get_field_type(column: pa.Array[Any, Any]) -> FieldType:
+                if isinstance(column, pa.NullArray):
+                    return "unknown"
+                elif pa.types.is_string(column.type):
+                    return "string"
+                elif pa.types.is_boolean(column.type):
+                    return "boolean"
+                elif pa.types.is_integer(column.type):
+                    return "integer"
+                elif pa.types.is_floating(column.type) or pa.types.is_decimal(
+                    column.type
+                ):
+                    return "number"
+                elif pa.types.is_date(column.type) or pa.types.is_timestamp(
+                    column.type
+                ):
+                    return "date"
+                else:
+                    return "unknown"
+
+        return PyArrowTableManager
---
+++ packages/marimo/marimo-0.6.12-py3-none-any.whl/marimo/_plugins/ui/_impl/tables/table_manager.py
@@ -0,0 +1,75 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import abc
+from typing import Any, Dict, Generic, Literal, TypeVar
+
+import marimo._output.data.data as mo_data
+from marimo._plugins.core.web_component import JSONType
+
+T = TypeVar("T")
+
+
+# This is the frontend type for how the frontend should parse the data
+FieldType = Literal[
+    "string", "boolean", "integer", "number", "date", "unknown"
+]
+FieldTypes = Dict[str, FieldType]
+
+
+class TableManager(abc.ABC, Generic[T]):
+    DEFAULT_LIMIT = 10_000
+
+    def __init__(self, data: T) -> None:
+        self.data = data
+
+    def to_data(self) -> JSONType:
+        """
+        The best way to represent the data in a table as JSON.
+
+        By default, this method calls `to_csv` and returns the result as
+        a string.
+        """
+        return mo_data.csv(self.to_csv()).url
+
+    @abc.abstractmethod
+    def to_csv(self) -> bytes:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def to_json(self) -> bytes:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def select_rows(self, indices: list[int]) -> TableManager[T]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_row_headers(self) -> list[tuple[str, list[str | int | float]]]:
+        raise NotImplementedError
+
+    def get_field_types(self) -> FieldTypes:
+        # By default, don't provide any field types
+        # so the frontend can infer them
+        return {}
+
+    @abc.abstractmethod
+    def limit(self, num: int) -> TableManager[T]:
+        raise NotImplementedError
+
+    @staticmethod
+    @abc.abstractmethod
+    def is_type(value: Any) -> bool:
+        raise NotImplementedError
+
+
+class TableManagerFactory(abc.ABC):
+    @staticmethod
+    @abc.abstractmethod
+    def package_name() -> str:
+        raise NotImplementedError
+
+    @staticmethod
+    @abc.abstractmethod
+    def create() -> type[TableManager[Any]]:
+        raise NotImplementedError
