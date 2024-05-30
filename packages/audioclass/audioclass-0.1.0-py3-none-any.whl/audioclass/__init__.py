14d705c5bba7223e9c9acab668387c2ec68c3c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:04:09 +0000

    Add package audioclass/0.1.0/audioclass-0.1.0-py3-none-any.whl

---
+++ packages/audioclass/audioclass-0.1.0-py3-none-any.whl/audioclass/__init__.py
@@ -0,0 +1,24 @@
+from audioclass.batch import (
+    BaseIterator,
+    BatchGenerator,
+    SimpleIterator,
+    process_iterable,
+)
+from audioclass.models import ClipClassificationModel, ModelOutput
+from audioclass.preprocess import (
+    load_clip,
+    load_recording,
+)
+
+__all__ = [
+    "BaseIterator",
+    "BatchGenerator",
+    "ClipClassificationModel",
+    "ClipClassificationModel",
+    "ModelOutput",
+    "ModelOutput",
+    "SimpleIterator",
+    "load_clip",
+    "load_recording",
+    "process_iterable",
+]
