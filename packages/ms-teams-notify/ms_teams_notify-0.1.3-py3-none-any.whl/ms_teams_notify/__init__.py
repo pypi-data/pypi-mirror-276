cacb89bcbdff69369a48c52512802851402974f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:03:51 +0000

    Add package ms-teams-notify/0.1.3/ms_teams_notify-0.1.3-py3-none-any.whl

---
+++ packages/ms-teams-notify/ms_teams_notify-0.1.3-py3-none-any.whl/ms_teams_notify/__init__.py
@@ -0,0 +1,12 @@
+import notify_to_teams
+
+# __copyright__ = 'Copyright (C) 2018 Your Name'
+# __version__ = '1.0.0'
+# __license__ = 'BSD-3-Clause'
+# __author__ = 'Your Name'
+# __author_email__ = 'Your@Email'
+# __url__ = 'http://github.com/account/repository'
+
+__all__ = [
+    notify_to_teams,
+]
