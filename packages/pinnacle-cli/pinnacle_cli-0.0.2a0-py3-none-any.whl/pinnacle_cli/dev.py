370f2827e4bdae50742cde15eaba7f7dcc157346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:04:01 +0000

    Add package pinnacle-cli/0.0.2a0/pinnacle_cli-0.0.2a0-py3-none-any.whl

---
+++ packages/pinnacle-cli/pinnacle_cli-0.0.2a0-py3-none-any.whl/pinnacle_cli/dev.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import importlib
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pinnacle_python.endpoints import endpoints
+from pinnacle_cli.constants import HOST, PORT, DIRECTORY
+
+app = FastAPI()
+
+sys.path.append(os.getcwd())
+
+for filename in os.listdir(DIRECTORY):
+    if filename.endswith(".py"):
+        module_name = os.path.splitext(filename)[0]
+        module_parent = (
+            DIRECTORY[2:] if DIRECTORY.startswith("./") else DIRECTORY
+        ).replace("/", ".")
+        importlib.import_module(f"{module_parent + "" if module_parent else ""}{module_name}")
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+available_routes = []
+
+for endpoint in endpoints:
+    route_name = endpoint.__name__
+    params = endpoint.__annotations__.items()
+
+    @app.api_route(f"/{route_name}", methods=["POST"])
+    async def route_handler(request: Request):
+        params = await request.json()
+
+        params_dict = {}
+
+        for param, _ in params.items():
+            if param != "return":
+                params_dict[param] = params[param]
+
+        return JSONResponse(content={"data": endpoint(**params_dict)})
+
+    available_routes.append((route_name, params))
+
+print("Available routes:")
+for route, params in available_routes:
+    print(f"{HOST}:{PORT}/{route}")
+    for param, param_type in params:
+        if param == "return":
+            print(f"Return Type: {param_type.__name__}")
+        else:
+            print(f"Parameter: {param}, Type: {param_type.__name__}")
