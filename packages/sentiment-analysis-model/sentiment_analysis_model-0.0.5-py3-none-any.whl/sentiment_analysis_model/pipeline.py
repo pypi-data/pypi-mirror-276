6787941a01734a4ac66f274f0470f841632d2183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:03:44 +0000

    Add package sentiment-analysis-model/0.0.5/sentiment_analysis_model-0.0.5-py3-none-any.whl

---
+++ packages/sentiment-analysis-model/sentiment_analysis_model-0.0.5-py3-none-any.whl/sentiment_analysis_model/pipeline.py
@@ -0,0 +1,10 @@
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import svm
+from sentiment_analysis_model.processing.features import preprocess_text
+
+def create_pipeline() -> Pipeline:
+    return Pipeline([
+        ('tfidf', TfidfVectorizer(preprocessor=preprocess_text, use_idf=True, stop_words='english')),
+        ('clf', svm.SVC(kernel='linear', verbose=True))
+    ])
