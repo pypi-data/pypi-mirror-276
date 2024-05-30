6d6a0e4f3af2ae6a590ad65b29feb0b0f4974ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:03:48 +0000

    Add package YWP/0.5.0/ywp-0.5.0.tar.gz

---
+++ packages/YWP/ywp-0.5.0.tar.gz/ywp-0.5.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="YWP",
+    version="0.5.0",
+    packages=find_packages(),
+    install_requires=[
+        "SpeechRecognition",
+        "gtts",
+        "pygame",
+        "sounddevice",
+        "pyaudio"
+    ],
+    author="Your Wanted Products",
+    author_email="pbstzidr@ywp.freewebhostmost.com",
+    description="A big Package has a lot of things",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/YourWantedProducts/YWP_Python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
