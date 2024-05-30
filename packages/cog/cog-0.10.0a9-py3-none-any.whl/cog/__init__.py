ec50533773bb267c9854f5ea4e27d6b634ee25db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:41 +0000

    Add package cog/0.10.0a9/cog-0.10.0a9-py3-none-any.whl

---
+++ packages/cog/cog-0.10.0a9-py3-none-any.whl/cog/__init__.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+from .predictor import BasePredictor
+from .server.worker import emit_metric
+from .types import AsyncConcatenateIterator, ConcatenateIterator, File, Input, Path
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+
+
+__all__ = [
+    "__version__",
+    "BaseModel",
+    "BasePredictor",
+    "ConcatenateIterator",
+    "AsyncConcatenateIterator",
+    "File",
+    "Input",
+    "Path",
+    "emit_metric",
+]
