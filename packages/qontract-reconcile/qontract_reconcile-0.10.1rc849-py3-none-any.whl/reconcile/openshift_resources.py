20c67cc87b2a263c19531c1256f7855e7343c5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:59 +0000

    Add package qontract-reconcile/0.10.1rc849/qontract_reconcile-0.10.1rc849-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc849-py3-none-any.whl/reconcile/openshift_resources.py
@@ -0,0 +1,49 @@
+from typing import Any
+
+import reconcile.openshift_base as ob
+import reconcile.openshift_resources_base as orb
+from reconcile.utils.runtime.integration import DesiredStateShardConfig
+from reconcile.utils.semver_helper import make_semver
+
+QONTRACT_INTEGRATION = "openshift_resources"
+QONTRACT_INTEGRATION_VERSION = make_semver(1, 9, 3)
+PROVIDERS = ["resource", "resource-template", "prometheus-rule"]
+
+
+def run(
+    dry_run,
+    thread_pool_size=10,
+    internal=None,
+    use_jump_host=True,
+    cluster_name=None,
+    exclude_cluster=None,
+    namespace_name=None,
+    defer=None,
+):
+    orb.QONTRACT_INTEGRATION = QONTRACT_INTEGRATION
+    orb.QONTRACT_INTEGRATION_VERSION = QONTRACT_INTEGRATION_VERSION
+
+    ri = orb.run(
+        dry_run=dry_run,
+        thread_pool_size=thread_pool_size,
+        internal=internal,
+        use_jump_host=use_jump_host,
+        providers=PROVIDERS,
+        cluster_name=cluster_name,
+        exclude_cluster=exclude_cluster,
+        namespace_name=namespace_name,
+        init_api_resources=True,
+    )
+
+    # check for unused resources types
+    # listed under `managedResourceTypes`
+    if ri:
+        ob.check_unused_resource_types(ri)
+
+
+def early_exit_desired_state(*args, **kwargs) -> dict[str, Any]:
+    return orb.early_exit_desired_state(PROVIDERS)
+
+
+def desired_state_shard_config() -> DesiredStateShardConfig:
+    return orb.desired_state_shard_config()
