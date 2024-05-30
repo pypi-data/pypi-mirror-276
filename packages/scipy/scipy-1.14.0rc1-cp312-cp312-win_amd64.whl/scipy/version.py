328712a5eb77fe3d2c676b1eaab3313319a6fbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 15:04:13 +0000

    Add package scipy/1.14.0rc1/scipy-1.14.0rc1-cp312-cp312-win_amd64.whl

---
+++ packages/scipy/scipy-1.14.0rc1-cp312-cp312-win_amd64.whl/scipy/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS GENERATED DURING THE SCIPY BUILD
+# See tools/version_utils.py for details
+
+short_version = '1.14.0rc1'
+version = '1.14.0rc1'
+full_version = '1.14.0rc1'
+git_revision = '3aa7154'
+commit_count = '1357'
+release = True
+
+if not release:
+    version = full_version
