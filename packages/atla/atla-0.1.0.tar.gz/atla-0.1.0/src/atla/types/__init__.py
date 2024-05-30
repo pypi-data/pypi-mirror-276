a60ff436ddff258082b3fb723d860db47084e99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:04:07 +0000

    Add package atla/0.1.0/atla-0.1.0.tar.gz

---
+++ packages/atla/atla-0.1.0.tar.gz/atla-0.1.0/src/atla/types/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .evaluation import Evaluation as Evaluation
+from .evaluate_create_params import EvaluateCreateParams as EvaluateCreateParams
