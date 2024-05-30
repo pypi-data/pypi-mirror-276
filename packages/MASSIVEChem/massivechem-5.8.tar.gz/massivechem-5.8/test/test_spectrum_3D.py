425168097b4dc120271e9cd86b9299119850d6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:22 +0000

    Add package MASSIVEChem/5.8/massivechem-5.8.tar.gz

---
+++ packages/MASSIVEChem/massivechem-5.8.tar.gz/massivechem-5.8/test/test_spectrum_3D.py
@@ -0,0 +1,31 @@
+import MASSiveChem.MASSiveChem as MC
+import unittest
+
+class TestSpectrum3D(unittest.TestCase):
+    
+    def setUp(self):
+        self.apparatus_resolution = 0.01
+
+    def test_butane_False(self):
+        mol_smi = "CCCC"
+        imprecision_True_False = False
+        result = MC.spectrum_3D(mol_smi, imprecision_True_False, self.apparatus_resolution)
+        self.assertIsNotNone(result)
+    
+    def test_butane_True(self):
+        mol_smi = "CCCC"
+        imprecision_True_False = True
+        result = MC.spectrum_3D(mol_smi, imprecision_True_False, self.apparatus_resolution)
+        self.assertIsNotNone(result)
+
+    def test_invalid_input(self):
+        
+        with self.assertRaises(ValueError):
+            imprecision_True_False = False
+            MC.spectrum_3D("invalid_smiles",imprecision_True_False, self.apparatus_resolution)  
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
+
