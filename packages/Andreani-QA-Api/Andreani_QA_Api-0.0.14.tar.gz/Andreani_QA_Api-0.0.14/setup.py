a93a04be47ddfbd362191326e4cd338d5f3ba9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:43 +0000

    Add package Andreani-QA-Api/0.0.14/Andreani_QA_Api-0.0.14.tar.gz

---
+++ packages/Andreani-QA-Api/Andreani_QA_Api-0.0.14.tar.gz/Andreani_QA_Api-0.0.14/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.14'
+PACKAGE_NAME = 'Andreani_QA_Api'  # Debe coincidir con el nombre de la carpeta
+AUTHOR = 'AndreaniTesting'
+AUTHOR_EMAIL = 'user_appglatest@andreani.com'
+URL = ''
+
+LICENSE = 'MIT'  # Tipo de licencia
+DESCRIPTION = 'Funciones de API para ejecución de casos automatizados'  # Descripción corta
+LONG_DESCRIPTION = ""  # Referencia al documento README con una descripción más elaborada
+LONG_DESC_TYPE = "text/markdown"
+
+# Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
+INSTALL_REQUIRES = ['jsondiff', 'jsonschema'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
