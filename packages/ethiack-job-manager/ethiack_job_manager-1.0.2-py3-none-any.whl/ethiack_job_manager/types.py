3c40218dec81db8ee0877ebbe4a51d3e0177e463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:15 +0000

    Add package ethiack-job-manager/1.0.2/ethiack_job_manager-1.0.2-py3-none-any.whl

---
+++ packages/ethiack-job-manager/ethiack_job_manager-1.0.2-py3-none-any.whl/ethiack_job_manager/types.py
@@ -0,0 +1,61 @@
+"""Types for the Ethiack Job Manager."""
+
+import datetime
+from typing import Annotated
+
+import pydantic
+
+from ethiack_job_manager import utils
+
+
+__all__ = ["Service", "Finding", "Job", "JobFindings"]
+
+
+Url = Annotated[pydantic.AnyUrl,
+                pydantic.AfterValidator(lambda x: str(x).rstrip('/'))]
+
+
+class Service(pydantic.BaseModel):
+    """Service model"""
+
+    url: Url
+    """URL of the service"""
+
+
+class Finding(pydantic.BaseModel):
+    """Finding model"""
+
+    title: str
+    """Title of the finding"""
+
+    severity: utils.Severity
+    """Severity of the finding"""
+
+
+class Job(pydantic.BaseModel):
+    """Job model"""
+
+    uuid: str
+    """UUID of the job"""
+
+    url: str
+    """URL of the target service"""
+
+    status: str
+    """Status of the job"""
+
+    created: datetime.datetime
+    """Timestamp of the job creation"""
+
+    started: datetime.datetime | None = None
+    """Timestamp of the job start"""
+
+    finished: datetime.datetime | None = None
+    """Timestamp of the job finish"""
+
+
+class JobFindings(Job):
+    """JobFindings model"""
+
+    findings: list[Finding]
+    """Findings of the job"""
