29b61bf8e948769690b0cb69b73b8505195e33b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 15:04:21 +0000

    Add package item-matching/0.0.33/item_matching-0.0.33-py3-none-any.whl

---
+++ packages/item-matching/item_matching-0.0.33-py3-none-any.whl/item_matching/__init__.py
@@ -0,0 +1,2 @@
+from .main import Matching
+from .build_index.func_img import PipelineImage
---
+++ packages/item-matching/item_matching-0.0.33-py3-none-any.whl/item_matching/build_index/func.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import polars as pl
+
+
+class PipelineText:
+    @staticmethod
+    def clean_text(data: pl.DataFrame, col: str = 'item_name') -> pl.DataFrame:
+        import re
+
+        regex = "[\(\[\<\"].*?[\)\]\>\"]"
+        return data.with_columns(
+            pl.col(col).map_elements(
+                lambda x: re.sub(regex, "", x).lower().rstrip('.').strip(), return_dtype=pl.String
+            )
+            .alias(f'{col.lower()}_clean')
+        )
+
+
+def ngrams_func(string: str):
+    ngrams = zip(*[string[i:] for i in range(3)])
+    return [''.join(ngram) for ngram in ngrams]
+
+
+def tfidf(lst_item: list, dim: int = 512):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+
+    vectorizer = TfidfVectorizer(analyzer=ngrams_func, max_features=dim)
+    vectorizer.fit(lst_item)
+    return vectorizer
+
+
+def rm_all_folder(path: Path) -> None:
+    for child in path.glob('*'):
+        if child.is_file():
+            child.unlink()
+        else:
+            rm_all_folder(child)
+    path.rmdir()
+
+
+def make_dir(folder_name: Path) -> None:
+    if not folder_name.exists():
+        folder_name.mkdir(parents=True, exist_ok=True)
