36cd567d0c97a0217275c9a4a3699daa267a9eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:04:02 +0000

    Add package excel-normalizer/0.3/excel_normalizer-0.3.tar.gz

---
+++ packages/excel-normalizer/excel_normalizer-0.3.tar.gz/excel_normalizer-0.3/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='excel_normalizer',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.3',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'normalize=excel_normalizer.cli:main',
+        ],
+    },
+    install_requires=[
+        'pandas',
+        'pyarrow',
+        'openpyxl'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
