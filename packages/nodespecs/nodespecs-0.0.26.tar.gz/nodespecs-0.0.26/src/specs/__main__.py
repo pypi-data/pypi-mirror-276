04b5672709abebaa529570ee94e6b1367a9fbe7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:33 +0000

    Add package nodespecs/0.0.26/nodespecs-0.0.26.tar.gz

---
+++ packages/nodespecs/nodespecs-0.0.26.tar.gz/nodespecs-0.0.26/src/specs/__main__.py
@@ -0,0 +1,45 @@
+import sys
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Display hardware information")
+    parser.add_argument('-l','--lite',default=0, help='Display native python platform')
+    parser.add_argument('-u','--utility',default=None, help='run benchmark for single core CPU')
+    parser.add_argument('-i','--iphost',default=None, help='hosthame of the server machine')
+
+    args = parser.parse_args()
+    if args.utility is None:
+        if (args.lite == "1") or (args.lite == "True") :
+            from . import info_plat
+            info_plat()
+        else:
+            from .hardware import (info_cpu,
+                                   info_mem,
+                                   info_disk,
+                                   info_net)
+            info_cpu()
+            info_mem()
+            info_disk()
+            info_net()
+    else:
+        if args.utility == "bcpu":
+            from . import bench_cpu
+            bench_cpu()
+        elif args.utility == "upload":
+            from .communicate import stream_files
+            from . import whoish
+            whoish()
+            stream_files()
+        elif args.utility == "server":
+            from . import server
+            server()
+        elif args.utility == "cld":
+            from . import clientd
+            if args.iphost is not None:
+                clientd(args.iphost)
+
+
+
+if __name__ == "__main__":
+    main()
