f7c50cf10b15d384f9f53fe983417ac1f1bd26b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:39 +0000

    Add package bubblesort-test/1.1/bubblesort_test-1.1.tar.gz

---
+++ packages/bubblesort-test/bubblesort_test-1.1.tar.gz/bubblesort_test-1.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="bubblesort-test",
+    version="1.1",
+    author="Qiyao",
+    author_email="qiyao.xu@eleve.isep.fr",
+    description="This is a bubblesort tool",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
