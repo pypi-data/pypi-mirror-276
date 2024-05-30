2c0defe59f4bf3374a77a98cfde202dde8938b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:44 +0000

    Add package cratedb-toolkit/0.0.11/cratedb_toolkit-0.0.11.tar.gz

---
+++ packages/cratedb-toolkit/cratedb_toolkit-0.0.11.tar.gz/cratedb_toolkit-0.0.11/cratedb_toolkit/util/data.py
@@ -0,0 +1,18 @@
+import json
+import sys
+import typing as t
+
+
+def jd(data: t.Any):
+    """
+    Pretty-print JSON with indentation.
+    """
+    print(json.dumps(data, indent=2), file=sys.stdout)  # noqa: T201
+
+
+def str_contains(haystack, *needles):
+    """
+    Whether haystack contains any of the provided needles.
+    """
+    haystack = str(haystack)
+    return any(needle in haystack for needle in needles)
