e98b5dc8fc532998a29e5702c14b5227043b8501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:04:40 +0000

    Add package r2r/0.2.3/r2r-0.2.3.tar.gz

---
+++ packages/r2r/r2r-0.2.3.tar.gz/r2r-0.2.3/r2r/core/providers/eval_provider.py
@@ -0,0 +1,36 @@
+from abc import abstractmethod
+from typing import Union
+
+from .base_provider import Provider, ProviderConfig
+from .llm_provider import LLMConfig
+
+
+class EvalConfig(ProviderConfig):
+    """A base eval config class"""
+
+    llm: LLMConfig
+
+    def validate(self) -> None:
+        if self.provider not in self.supported_providers:
+            raise ValueError(f"Provider {self.provider} not supported.")
+
+    @property
+    def supported_providers(self) -> list[str]:
+        return ["local"]
+
+
+class EvalProvider(Provider):
+    """An abstract class to provide a common interface for evaluation providers."""
+
+    def __init__(self, config: EvalConfig):
+        if not isinstance(config, EvalConfig):
+            raise ValueError(
+                "EvalProvider must be initialized with a `EvalConfig`."
+            )
+
+        super().__init__(config)
+
+    def evaluate(
+        self, query: str, context: str, completion: str
+    ) -> dict[str, dict[str, Union[str, float]]]:
+        return self._evaluate(query, context, completion)
