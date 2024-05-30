b6fb558356898645d9bdb3d86acb948f9e67d0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:03:53 +0000

    Add package opentnsim/1.3.7/opentnsim-1.3.7.tar.gz

---
+++ packages/opentnsim/opentnsim-1.3.7.tar.gz/opentnsim-1.3.7/opentnsim/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import opentnsim.core as core  # now contains mixin class consumes energy, also the modified vessel properties
+import opentnsim.graph_module as graph_module
+import opentnsim.plot as plot
+import opentnsim.model as model
+import opentnsim.energy as energy
+import opentnsim.strategy as strategy
+
+"""Top-level package for OpenTNSim."""
+
+__author__ = """Mark van Koningsveld"""
+__email__ = "M.vanKoningsveld@tudelft.nl"
+__version__ = "1.3.7"
