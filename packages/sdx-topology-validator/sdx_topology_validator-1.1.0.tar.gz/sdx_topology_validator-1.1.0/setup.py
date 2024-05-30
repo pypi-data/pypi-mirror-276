e24523849bef17a31693bdf1af3a81b9dedf1be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:15 +0000

    Add package sdx-topology-validator/1.1.0/sdx_topology_validator-1.1.0.tar.gz

---
+++ packages/sdx-topology-validator/sdx_topology_validator-1.1.0.tar.gz/sdx_topology_validator-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sdx_topology_validator",
+    version="1.1.0",
+    author="Sai Krishna Voruganti",
+    author_email="saikrishnavoruganti@gmail.com",
+    description="Validates the converted topology against the given OpenAPI specifications",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=["pyyaml", "jsonref", "jsonschema"],
+)
