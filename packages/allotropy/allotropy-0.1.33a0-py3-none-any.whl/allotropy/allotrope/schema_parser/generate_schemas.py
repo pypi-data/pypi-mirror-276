55c57b4a86da6c31237bfceca2c01e0cb2aa0515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:33 +0000

    Add package allotropy/0.1.33a0/allotropy-0.1.33a0-py3-none-any.whl

---
+++ packages/allotropy/allotropy-0.1.33a0-py3-none-any.whl/allotropy/allotrope/schema_parser/generate_schemas.py
@@ -0,0 +1,142 @@
+import os
+from pathlib import Path
+import re
+import subprocess  # noqa: S404, RUF100
+
+from autoflake import fix_file  # type: ignore[import-untyped]
+from datamodel_code_generator import (
+    DataModelType,
+    generate,
+    InputFileType,
+    PythonVersion,
+)
+
+from allotropy.allotrope.schema_parser.backup_manager import (
+    backup,
+    is_backup_file,
+    is_file_changed,
+    restore_backup,
+)
+from allotropy.allotrope.schema_parser.model_class_editor import modify_file
+from allotropy.allotrope.schema_parser.path_util import (
+    CUSTOM_MODELS_PATH,
+    GENERATED_SHARED_PATHS,
+    get_schema_and_model_paths,
+    SCHEMA_DIR_PATH,
+    UNITS_MODELS_PATH,
+)
+from allotropy.allotrope.schema_parser.schema_cleaner import SchemaCleaner
+from allotropy.allotrope.schema_parser.update_units import update_unit_files
+
+
+def lint_file(model_path: str) -> None:
+    # The first run of ruff changes typing annotations and causes unused imports. We catch failure
+    # due to unused imports.
+    try:
+        subprocess.check_call(
+            f"ruff {model_path} --fix",
+            shell=True,  # noqa: S602
+            stdout=subprocess.DEVNULL,
+        )
+    except subprocess.CalledProcessError:
+        pass
+    # The call to autoflake.fix_file removes unused imports.
+    fix_file(
+        model_path,
+        {
+            "in_place": True,
+            "remove_unused_variables": True,
+            "write_to_stdout": False,
+            "ignore_init_module_imports": False,
+            "expand_star_imports": False,
+            "remove_all_unused_imports": True,
+            "remove_duplicate_keys": True,
+            "remove_rhs_for_unused_variables": False,
+            "ignore_pass_statements": False,
+            "ignore_pass_after_docstring": False,
+            "check": False,
+            "check_diff": False,
+        },
+    )
+    # The second call to ruff checks for additional rules.
+    subprocess.check_call(
+        f"ruff {model_path} --fix", shell=True, stdout=subprocess.DEVNULL  # noqa: S602
+    )
+    subprocess.check_call(
+        f"black {model_path}", shell=True, stderr=subprocess.DEVNULL  # noqa: S602
+    )
+
+
+def _generate_schema(model_path: Path, schema_path: Path) -> None:
+    # Generate models
+    generate(
+        input_=schema_path,
+        output=model_path,
+        output_model_type=DataModelType.DataclassesDataclass,
+        input_file_type=InputFileType.JsonSchema,
+        # Specify base_class as empty when using dataclass
+        base_class="",
+        target_python_version=PythonVersion.PY_310,
+        use_union_operator=True,
+    )
+    # Import classes from shared files, remove unused classes, format.
+    modify_file(str(model_path), str(schema_path))
+    lint_file(str(model_path))
+
+
+def _should_generate_schema(schema_path: str, schema_regex: str | None = None) -> bool:
+    # Skip files in the shared directory
+    if schema_path.startswith("shared"):
+        return False
+    if is_backup_file(schema_path):
+        return False
+    if schema_regex:
+        return bool(re.match(schema_regex, str(schema_path)))
+    return True
+
+
+def generate_schemas(
+    root_dir: Path,
+    *,
+    dry_run: bool | None = False,
+    schema_regex: str | None = None,
+) -> list[str]:
+    """Generate schemas from JSON schema files.
+
+    :root_dir: The root directory of the project.
+    :dry_run: If true, does not save changes to any models, but still returns the list of models that would change.
+    :schema_regex: If set, filters schemas to generate using regex.
+    :return: A list of model files that were changed.
+    """
+
+    unit_to_iri: dict[str, str] = {}
+    with backup(GENERATED_SHARED_PATHS, restore=dry_run):
+        os.chdir(os.path.join(root_dir, SCHEMA_DIR_PATH))
+        schema_paths = list(Path(".").rglob("*.json"))
+        os.chdir(os.path.join(root_dir))
+        models_changed = []
+        for rel_schema_path in schema_paths:
+            if not _should_generate_schema(str(rel_schema_path), schema_regex):
+                continue
+
+            print(f"Generating models for schema: {rel_schema_path}...")  # noqa: T201
+            schema_path, model_path = get_schema_and_model_paths(
+                root_dir, rel_schema_path
+            )
+
+            with backup(model_path, restore=dry_run), backup(schema_path, restore=True):
+                schema_cleaner = SchemaCleaner()
+                schema_cleaner.clean_file(str(schema_path))
+                unit_to_iri |= schema_cleaner.get_referenced_units()
+                _generate_schema(model_path, schema_path)
+
+                if is_file_changed(model_path):
+                    models_changed.append(model_path.stem)
+                else:
+                    restore_backup(model_path)
+
+        update_unit_files(unit_to_iri)
+        for path in [UNITS_MODELS_PATH, CUSTOM_MODELS_PATH]:
+            lint_file(path)
+
+    return models_changed
---
+++ packages/allotropy/allotropy-0.1.33a0-py3-none-any.whl/allotropy/parser_factory.py
@@ -0,0 +1,137 @@
+from datetime import tzinfo
+from enum import Enum
+
+from allotropy.exceptions import AllotropeConversionError
+from allotropy.parsers.agilent_gen5.agilent_gen5_parser import AgilentGen5Parser
+from allotropy.parsers.agilent_gen5_image.agilent_gen5_image_parser import (
+    AgilentGen5ImageParser,
+)
+from allotropy.parsers.appbio_absolute_q.appbio_absolute_q_parser import (
+    AppbioAbsoluteQParser,
+)
+from allotropy.parsers.appbio_quantstudio.appbio_quantstudio_parser import (
+    AppBioQuantStudioParser,
+)
+from allotropy.parsers.appbio_quantstudio_designandanalysis.appbio_quantstudio_designandanalysis_parser import (
+    AppBioQuantStudioDesignandanalysisParser,
+)
+from allotropy.parsers.beckman_pharmspec.beckman_pharmspec_parser import PharmSpecParser
+from allotropy.parsers.beckman_vi_cell_blu.vi_cell_blu_parser import ViCellBluParser
+from allotropy.parsers.beckman_vi_cell_xr.vi_cell_xr_parser import ViCellXRParser
+from allotropy.parsers.biorad_bioplex_manager.biorad_bioplex_manager_parser import (
+    BioradBioplexParser,
+)
+from allotropy.parsers.chemometec_nucleoview.nucleoview_parser import (
+    ChemometecNucleoviewParser,
+)
+from allotropy.parsers.ctl_immunospot.ctl_immunospot_parser import CtlImmunospotParser
+from allotropy.parsers.example_weyland_yutani.example_weyland_yutani_parser import (
+    ExampleWeylandYutaniParser,
+)
+from allotropy.parsers.luminex_xponent.luminex_xponent_parser import (
+    LuminexXponentParser,
+)
+from allotropy.parsers.moldev_softmax_pro.softmax_pro_parser import SoftmaxproParser
+from allotropy.parsers.novabio_flex2.novabio_flex2_parser import NovaBioFlexParser
+from allotropy.parsers.perkin_elmer_envision.perkin_elmer_envision_parser import (
+    PerkinElmerEnvisionParser,
+)
+from allotropy.parsers.qiacuity_dpcr.qiacuity_dpcr_parser import QiacuitydPCRParser
+from allotropy.parsers.revvity_kaleido.kaleido_parser import KaleidoParser
+from allotropy.parsers.roche_cedex_bioht.roche_cedex_bioht_parser import (
+    RocheCedexBiohtParser,
+)
+from allotropy.parsers.thermo_fisher_nanodrop_eight.nanodrop_eight_parser import (
+    NanodropEightParser,
+)
+from allotropy.parsers.unchained_labs_lunatic.unchained_labs_lunatic_parser import (
+    UnchainedLabsLunaticParser,
+)
+from allotropy.parsers.utils.timestamp_parser import TimestampParser
+from allotropy.parsers.vendor_parser import VendorParser
+
+
+class Vendor(Enum):
+    AGILENT_GEN5 = "AGILENT_GEN5"
+    AGILENT_GEN5_IMAGE = "AGILENT_GEN5_IMAGE"
+    APPBIO_ABSOLUTE_Q = "APPBIO_ABSOLUTE_Q"
+    APPBIO_QUANTSTUDIO = "APPBIO_QUANTSTUDIO"
+    APPBIO_QUANTSTUDIO_DESIGNANDANALYSIS = "APPBIO_QUANTSTUDIO_DESIGNANDANALYSIS"
+    BECKMAN_PHARMSPEC = "BECKMAN_PHARMSPEC"
+    BECKMAN_VI_CELL_BLU = "BECKMAN_VI_CELL_BLU"
+    BECKMAN_VI_CELL_XR = "BECKMAN_VI_CELL_XR"
+    BIORAD_BIOPLEX = "BIORAD_BIOPLEX"
+    CHEMOMETEC_NUCLEOVIEW = "CHEMOMETEC_NUCLEOVIEW"
+    CTL_IMMUNOSPOT = "CTL_IMMUNOSPOT"
+    EXAMPLE_WEYLAND_YUTANI = "EXAMPLE_WEYLAND_YUTANI"
+    LUMINEX_XPONENT = "LUMINEX_XPONENT"
+    MOLDEV_SOFTMAX_PRO = "MOLDEV_SOFTMAX_PRO"
+    NOVABIO_FLEX2 = "NOVABIO_FLEX2"
+    PERKIN_ELMER_ENVISION = "PERKIN_ELMER_ENVISION"
+    QIACUITY_DPCR = "QIACUITY_DPCR"
+    REVVITY_KALEIDO = "REVVITY_KALEIDO"
+    ROCHE_CEDEX_BIOHT = "ROCHE_CEDEX_BIOHT"
+    THERMO_FISHER_NANODROP_EIGHT = "THERMO_FISHER_NANODROP_EIGHT"
+    UNCHAINED_LABS_LUNATIC = "UNCHAINED_LABS_LUNATIC"
+
+    def get_display_name(self) -> str:
+        return _VENDOR_TO_DISPLAY_NAME.get(self, self.value.title())
+
+
+_VENDOR_TO_DISPLAY_NAME = {
+    Vendor.AGILENT_GEN5: "Agilent Gen5",
+    Vendor.AGILENT_GEN5_IMAGE: "Agilent Gen5 Image",
+    Vendor.APPBIO_ABSOLUTE_Q: "AppBio AbsoluteQ",
+    Vendor.APPBIO_QUANTSTUDIO: "AppBio QuantStudio RT-PCR",
+    Vendor.APPBIO_QUANTSTUDIO_DESIGNANDANALYSIS: "AppBio QuantStudio Design And Analysis",
+    Vendor.BECKMAN_PHARMSPEC: "Beckman PharmSpec",
+    Vendor.BECKMAN_VI_CELL_BLU: "Beckman Vi Cell BLU",
+    Vendor.BECKMAN_VI_CELL_XR: "Beckman Vi Cell XR",
+    Vendor.BIORAD_BIOPLEX: "BioRad BioPlex Manager",
+    Vendor.CHEMOMETEC_NUCLEOVIEW: "Chemometec Nucleoview",
+    Vendor.CTL_IMMUNOSPOT: "CTL ImmunoSpot",
+    Vendor.EXAMPLE_WEYLAND_YUTANI: "Example Weyland Yutani",
+    Vendor.LUMINEX_XPONENT: "Luminex xPONENT",
+    Vendor.MOLDEV_SOFTMAX_PRO: "MolDev SoftMax Pro",
+    Vendor.NOVABIO_FLEX2: "NovaBio Flex2",
+    Vendor.PERKIN_ELMER_ENVISION: "Perkin Elmer Envision",
+    Vendor.QIACUITY_DPCR: "Qiacuity dPCR",
+    Vendor.REVVITY_KALEIDO: "Revvity Kaleiedo",
+    Vendor.ROCHE_CEDEX_BIOHT: "Roche Cedex BioHT",
+    Vendor.THERMO_FISHER_NANODROP_EIGHT: "Thermo Fisher Nanodrop Eight",
+    Vendor.UNCHAINED_LABS_LUNATIC: "Unchained Labs Lunatic",
+}
+
+
+_VENDOR_TO_PARSER: dict[Vendor, type[VendorParser]] = {
+    Vendor.AGILENT_GEN5: AgilentGen5Parser,
+    Vendor.AGILENT_GEN5_IMAGE: AgilentGen5ImageParser,
+    Vendor.APPBIO_ABSOLUTE_Q: AppbioAbsoluteQParser,
+    Vendor.APPBIO_QUANTSTUDIO: AppBioQuantStudioParser,
+    Vendor.APPBIO_QUANTSTUDIO_DESIGNANDANALYSIS: AppBioQuantStudioDesignandanalysisParser,
+    Vendor.BECKMAN_PHARMSPEC: PharmSpecParser,
+    Vendor.BECKMAN_VI_CELL_BLU: ViCellBluParser,
+    Vendor.BECKMAN_VI_CELL_XR: ViCellXRParser,
+    Vendor.BIORAD_BIOPLEX: BioradBioplexParser,
+    Vendor.CHEMOMETEC_NUCLEOVIEW: ChemometecNucleoviewParser,
+    Vendor.CTL_IMMUNOSPOT: CtlImmunospotParser,
+    Vendor.EXAMPLE_WEYLAND_YUTANI: ExampleWeylandYutaniParser,
+    Vendor.LUMINEX_XPONENT: LuminexXponentParser,
+    Vendor.MOLDEV_SOFTMAX_PRO: SoftmaxproParser,
+    Vendor.NOVABIO_FLEX2: NovaBioFlexParser,
+    Vendor.PERKIN_ELMER_ENVISION: PerkinElmerEnvisionParser,
+    Vendor.QIACUITY_DPCR: QiacuitydPCRParser,
+    Vendor.REVVITY_KALEIDO: KaleidoParser,
+    Vendor.ROCHE_CEDEX_BIOHT: RocheCedexBiohtParser,
+    Vendor.THERMO_FISHER_NANODROP_EIGHT: NanodropEightParser,
+    Vendor.UNCHAINED_LABS_LUNATIC: UnchainedLabsLunaticParser,
+}
+
+
+def get_parser(vendor: Vendor, default_timezone: tzinfo | None = None) -> VendorParser:
+    timestamp_parser = TimestampParser(default_timezone)
+    try:
+        return _VENDOR_TO_PARSER[vendor](timestamp_parser)
+    except KeyError as e:
+        error = f"Failed to create parser, unregistered vendor: {vendor}."
+        raise AllotropeConversionError(error) from e
