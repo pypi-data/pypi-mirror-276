0bdc7bdce52e7bec0e95324e5dd62bea847299ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:48 +0000

    Add package myfunctions-viksan17/0.0.4/myfunctions_viksan17-0.0.4-py3-none-any.whl

---
+++ packages/myfunctions-viksan17/myfunctions_viksan17-0.0.4-py3-none-any.whl/myfunctions_viksan17/math.py
@@ -0,0 +1,18 @@
+"""
+    This is how you use this function.
+"""
+
+def addition(NUMBER1,NUMBER2):
+    print(NUMBER1 + NUMBER2)
+    """
+    This is how you use this function.
+    e.g addition(2,3)
+    """
+
+
+def multiplication(NUMBER1, NUMBER2):
+    print(NUMBER1 * NUMBER2)
+    """
+    This is how you use this function.
+    e.g multiplication(2,5)
+    """
