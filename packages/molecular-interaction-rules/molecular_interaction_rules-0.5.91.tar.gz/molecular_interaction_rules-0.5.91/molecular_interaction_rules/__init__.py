8335703e0e01d9f1906490fad6e322aae1d6f792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 15:03:36 +0000

    Add package molecular-interaction-rules/0.5.91/molecular_interaction_rules-0.5.91.tar.gz

---
+++ packages/molecular-interaction-rules/molecular_interaction_rules-0.5.91.tar.gz/molecular_interaction_rules-0.5.91/molecular_interaction_rules/__init__.py
@@ -0,0 +1,8 @@
+# Aromatics
+
+from molecular_interaction_rules.molecules.aromatic.benzene import Benzene
+
+from molecular_interaction_rules.molecular_database import MoleculerDatabase
+
+__all__ = ['MoleculerDatabase']
+__name__ = 'MolecularInteractionRules'
