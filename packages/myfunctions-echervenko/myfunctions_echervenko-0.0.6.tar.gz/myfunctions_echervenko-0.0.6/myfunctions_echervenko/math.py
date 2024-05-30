9fc9d450cc32a5f8bb83971c6435c19f78d26a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:37 +0000

    Add package myfunctions-echervenko/0.0.6/myfunctions_echervenko-0.0.6.tar.gz

---
+++ packages/myfunctions-echervenko/myfunctions_echervenko-0.0.6.tar.gz/myfunctions_echervenko-0.0.6/myfunctions_echervenko/math.py
@@ -0,0 +1,24 @@
+"""
+This is how hyou use this function
+"""
+
+import os
+
+def addition(NUMBER1,NUMBER2):
+"""
+This is how hyou use this function
+"""
+    print(NUMBER1 + NUMBER2)
+
+
+def multiplication(NUMBER1,NUMBER2):
+
+"""
+This is how hyou use this function
+"""
+    print(NUMBER1 * NUMBER2)
+
+def filechecker(FILENAME):
+    FILENAME = os.path.isfile(FILENAME)
+    print("file exists")
+
---
+++ packages/myfunctions-echervenko/myfunctions_echervenko-0.0.6.tar.gz/myfunctions_echervenko-0.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_echervenko',
+    version='0.0.6',
+    packages=find_packages(),
+    install_requires=[""],
+    url='https://github.com/echervenko',
+    license='MIT',
+    author='Elena Chervenko',
+    author_email='chervenkoelena@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
