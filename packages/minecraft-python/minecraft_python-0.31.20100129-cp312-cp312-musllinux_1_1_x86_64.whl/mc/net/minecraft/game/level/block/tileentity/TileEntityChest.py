73c190ab0750d2ac110553a19d3cd44b1526d468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:04:14 +0000

    Add package minecraft-python/0.31.20100129/minecraft_python-0.31.20100129-cp312-cp312-musllinux_1_1_x86_64.whl

---
+++ packages/minecraft-python/minecraft_python-0.31.20100129-cp312-cp312-musllinux_1_1_x86_64.whl/mc/net/minecraft/game/level/block/tileentity/TileEntityChest.py
@@ -0,0 +1,46 @@
+from mc.net.minecraft.game.level.block.tileentity.TileEntity import TileEntity
+from mc.net.minecraft.game.item.ItemStack import ItemStack
+from mc.net.minecraft.game.Inventory import Inventory
+
+from nbtlib.tag import Compound, String, Byte, List
+
+class TileEntityChest(TileEntity, Inventory):
+    CHEST_STACK_LIMIT = 64
+
+    def __init__(self):
+        self.__chestContents = [None] * 36
+
+    def getSizeInventory(self):
+        return 27
+
+    def getStackInSlot(self, slot):
+        return self.__chestContents[slot]
+
+    def setInventorySlotContents(self, slot, stack):
+        self.__chestContents[slot] = stack
+        if stack:
+            stack.stackSize = min(stack.stackSize, TileEntityChest.CHEST_STACK_LIMIT)
+
+    def getInvName(self):
+        return 'Chest'
+
+    def readFromNBT(self, compound):
+        tagList = compound['Items']
+        self.__chestContents = [None] * 64
+        for tag in tagList:
+            slot = tag['Slot'].real & 255
+            self.__chestContents[slot] = ItemStack(tag)
+
+    def writeToNBT(self, compound):
+        compound['id'] = String('Chest')
+        tagList = List[Compound]()
+        for i in range(len(self.__chestContents)):
+            if self.__chestContents[i]:
+                comp = Compound({'Slot': Byte(i)})
+                self.__chestContents[i].writeToNBT(comp)
+                tagList.append(comp)
+
+        compound['Items'] = tagList
+
+    def getInventoryStackLimit(self):
+        return TileEntityChest.CHEST_STACK_LIMIT
