50418321610daf995615db9023350bf9f6f27ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:04:35 +0000

    Add package cash-support-mlm-prefect2/0.0.1/cash_support_mlm_prefect2-0.0.1.tar.gz

---
+++ packages/cash-support-mlm-prefect2/cash_support_mlm_prefect2-0.0.1.tar.gz/cash_support_mlm_prefect2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='cash-support-mlm-prefect2',
+  version="0.0.1",
+  description='This is used as a placeholder to help prevent dependency confusion attacks.',
+  author='SorryNotSorry',
+  author_email='dependency.placeholder@block.xyz',
+  python_requires=">=3.10",
+  packages=find_packages()
+)
