2395269273a5411e408909a843277a6d8739ca39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 06:05:31 +0000

    Add package almax-common/0.9.1/almax_common-0.9.1.tar.gz

---
+++ packages/almax-common/almax_common-0.9.1.tar.gz/almax_common-0.9.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name='almax_common',
+    version='0.9.1',
+    description='A common library with some of my implementations',
+    long_description='test',
+    long_description_content_type='text/markdown',
+    author='AlMax98',
+    author_email='alihaider.maqsood@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+);
