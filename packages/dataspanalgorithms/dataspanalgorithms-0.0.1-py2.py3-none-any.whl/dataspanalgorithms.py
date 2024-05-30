93bc4b36aa9319dcc0ba315dcd96a487b989854a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:40 +0000

    Add package dataspanalgorithms/0.0.1/dataspanalgorithms-0.0.1-py2.py3-none-any.whl

---
+++ packages/dataspanalgorithms/dataspanalgorithms-0.0.1-py2.py3-none-any.whl/dataspanalgorithms.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+if __name__ == "__main__":
+    print('This is a dummy placeholder for the `dataspanalgorithms` module. For further information, please contact pypi@dataspan.ai')
