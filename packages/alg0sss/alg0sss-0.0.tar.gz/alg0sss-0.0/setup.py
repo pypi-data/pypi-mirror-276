33333cfdff59fe3e06033382c10aad3935d8d9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:52 +0000

    Add package alg0sss/0.0/alg0sss-0.0.tar.gz

---
+++ packages/alg0sss/alg0sss-0.0.tar.gz/alg0sss-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='alg0sss',
+      version='0.0',
+      description='alg0sss',
+      packages=['alg0sss'],
+      author_email='applepro285@gmail.com',
+      zip_safe=False)
