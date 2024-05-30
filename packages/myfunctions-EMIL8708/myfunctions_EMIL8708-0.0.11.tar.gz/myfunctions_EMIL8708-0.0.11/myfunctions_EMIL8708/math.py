1b60f86884b0104142aa2ef6ffe846bd9b081128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:45 +0000

    Add package myfunctions-EMIL8708/0.0.11/myfunctions_EMIL8708-0.0.11.tar.gz

---
+++ packages/myfunctions-EMIL8708/myfunctions_EMIL8708-0.0.11.tar.gz/myfunctions_EMIL8708-0.0.11/myfunctions_EMIL8708/math.py
@@ -0,0 +1,17 @@
+"""
+    This is how you use this function
+"""
+
+def addition(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    e.g addition(2,3)
+    """
+    print(NUMBER1 + NUMBER2)
+
+def multiplication(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    e.g addition(2,3)
+    """
+    print(NUMBER1 * NUMBER2)
