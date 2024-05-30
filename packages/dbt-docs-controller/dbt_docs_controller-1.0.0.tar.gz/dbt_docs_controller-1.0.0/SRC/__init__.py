299fa57eb0adf5075fb18f58bea2c59d1b63b824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:40 +0000

    Add package dbt-docs-controller/1.0.0/dbt_docs_controller-1.0.0.tar.gz

---
+++ packages/dbt-docs-controller/dbt_docs_controller-1.0.0.tar.gz/dbt_docs_controller-1.0.0/SRC/__init__.py
@@ -0,0 +1,10 @@
+"""
+dbt-docs-controller package
+
+This package streamlines dbt documentation by generating only the relevant models, sources, and components for concise, targeted user information.
+"""
+
+# __version__ = "0.1.0"
+# from .main import main
+
+from .main import main
