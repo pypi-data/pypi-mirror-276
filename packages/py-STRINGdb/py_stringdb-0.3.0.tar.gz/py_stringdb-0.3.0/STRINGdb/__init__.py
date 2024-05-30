e49ea391b74cb061935ac4e7508df496184db736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:04:05 +0000

    Add package py-STRINGdb/0.3.0/py_stringdb-0.3.0.tar.gz

---
+++ packages/py-STRINGdb/py_stringdb-0.3.0.tar.gz/py_stringdb-0.3.0/STRINGdb/__init__.py
@@ -0,0 +1,10 @@
+
+__version__ = '0.3.0'
+
+from .prep import *
+from . import dataset
+#from . import network
+from .network import Image,Enrichment
+#from . import graph
+from .graph import Graph
+
---
+++ packages/py-STRINGdb/py_stringdb-0.3.0.tar.gz/py_stringdb-0.3.0/STRINGdb/version.py
@@ -0,0 +1,32 @@
+from typing import Union
+from httpx import Client, HTTPTransport
+
+client = Client(
+    base_url="https://string-db.org/api",
+    headers={
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"
+    },
+    follow_redirects=True,
+    timeout=10,
+    transport=HTTPTransport(retries=3),
+)
+
+
+string_versions = client.get("/json/version").json()
+STRINGdb_latest = string_versions[0]
+latest_api: str = STRINGdb_latest["stable_address"]
+latest_ver: str = STRINGdb_latest["string_version"]
+
+
+def choose_version(version: Union[float, str] = latest_ver) -> str:
+    for ver in string_versions:
+        if ver["string_version"] == str(version):
+            return ver["stable_address"]
+    print(
+        f"Warning: Your requested version is not supported now, try using {latest_ver} instead."
+    )
+    old_ver = str(version).replace(".", "-")
+    return f"https://version-{old_ver}.string-db.org"
+
+
+STATIC_ASSET = "https://stringdb-downloads.org/download/"
