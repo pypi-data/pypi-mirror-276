76bc2b8fa3a5d0f31a3fe18b152c21f2410c2fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:04:03 +0000

    Add package kaal/1.0.0/kaal-1.0.0.tar.gz

---
+++ packages/kaal/kaal-1.0.0.tar.gz/kaal-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Time series utility package'
+
+# Setting up
+setup(
+    name="kaal",
+    version=VERSION,
+    author="Debangan Mishra",
+    author_email="debangan.coding@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'time series', 'time analysis'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Data Scientists",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
