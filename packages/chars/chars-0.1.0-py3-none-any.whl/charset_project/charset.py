06d14f3b7fce2c93e256c6fb4bfe46c7ab564e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:03:59 +0000

    Add package chars/0.1.0/chars-0.1.0-py3-none-any.whl

---
+++ packages/chars/chars-0.1.0-py3-none-any.whl/charset_project/charset.py
@@ -0,0 +1,20 @@
+
+def uppercase():
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return chars
+
+def lowercase():
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    return chars
+
+def numbers():
+    chars = "1234567890"
+    return chars
+
+def specialchars():
+    chars = "~`!@#$%^&*()-_=+|[]\{|};':\",./<> ?"
+    return chars
+
+def all():
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~`!@#$%^&*()-_=+|[]\{|};':\",./<>? 1234567890"
+    return chars
