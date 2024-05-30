caf446e726cb55a8fd3ca26bbf6f7c1b2565bdeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:21 +0000

    Add package KassOrm/1.1.5/kassorm-1.1.5-py3-none-any.whl

---
+++ packages/KassOrm/kassorm-1.1.5-py3-none-any.whl/KassOrm/__init__.py
@@ -0,0 +1,7 @@
+from .Conn import Conn
+from .Creator import Creator
+from .Migration import Migration, Migrater
+from .Querier import Querier
+from .Modelr import Modelr
+from .Seeder import Seeder
+from .Factory import Factory
