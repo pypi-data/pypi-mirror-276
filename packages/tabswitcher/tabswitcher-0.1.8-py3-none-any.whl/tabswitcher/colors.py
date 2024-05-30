2ee424b8f226fc582cb4cde5b8cc51b5055b511c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 06:05:23 +0000

    Add package tabswitcher/0.1.8/tabswitcher-0.1.8-py3-none-any.whl

---
+++ packages/tabswitcher/tabswitcher-0.1.8-py3-none-any.whl/tabswitcher/colors.py
@@ -0,0 +1,29 @@
+from .Settings import Settings
+
+settings = Settings()
+def getTextColor():
+    if(settings.get_dark_mode()):
+        return "#ffffff"
+    return "#161618"
+
+def getBackgroundColor():
+    if(settings.get_dark_mode()):
+        return "#161618"
+    return "#ffffff"
+
+def getSelectedColor():
+    if(settings.get_dark_mode()):
+        return "#818181"
+    return "#818181"
+
+def getWindowBackgroundColor():
+    if(settings.get_show_background()):
+        if(settings.get_dark_mode()):
+            return "#121212"
+        return "#dddddd"
+    return "transparent"
+
+def getPlaceholderColor():
+    if(settings.get_dark_mode()):
+        return "#8a8a8b"
+    return "#8a8a8b"
