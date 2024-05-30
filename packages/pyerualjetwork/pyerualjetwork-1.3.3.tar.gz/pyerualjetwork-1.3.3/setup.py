5ee3d3682c1a2873a1a10bbd6f4f231ae26ca639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:52 +0000

    Add package pyerualjetwork/1.3.3/pyerualjetwork-1.3.3.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-1.3.3.tar.gz/pyerualjetwork-1.3.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "1.3.3",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Advanced python deep learning library. New Features: 'SyntheticAugmentation' function added for unbalanced datasets. Changes for variable names to snake_case (Function names are still PascalCase). (Documentation in desc. Examples in GİTHUB: https://github.com/HCB06/PyerualJetwork)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
