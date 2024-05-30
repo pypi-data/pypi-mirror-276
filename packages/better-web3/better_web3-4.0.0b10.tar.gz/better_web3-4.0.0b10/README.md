b414b202791c6ebb2556b56aa5024fef90c41802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:03:59 +0000

    Add package better-web3/4.0.0b10/better_web3-4.0.0b10.tar.gz

---
+++ packages/better-web3/better_web3-4.0.0b10.tar.gz/better_web3-4.0.0b10/README.md
@@ -0,0 +1,13 @@
+# Better Web3.py
+[![Telegram channel](https://img.shields.io/endpoint?url=https://runkit.io/damiankrawczyk/telegram-badge/branches/master?url=https://t.me/cum_insider)](https://t.me/cum_insider)
+[![PyPI version info](https://img.shields.io/pypi/v/better-web3.svg)](https://pypi.python.org/pypi/better-web3)
+[![PyPI supported Python versions](https://img.shields.io/pypi/pyversions/better-web3.svg)](https://pypi.python.org/pypi/better-web3)
+[![PyPI downloads per month](https://img.shields.io/pypi/dm/better-web3.svg)](https://pypi.python.org/pypi/better-web3)
+
+```bash
+pip install better-web3
+```
+
+More libraries of the family:
+- [tweepy-self](https://github.com/alenkimov/tweepy-self)
+- [better-proxy](https://github.com/alenkimov/better_proxy)
---
+++ packages/better-web3/better_web3-4.0.0b10.tar.gz/better_web3-4.0.0b10/better_web3/models.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, Field
+from eth_typing import HexStr
+
+
+class Explorer(BaseModel):
+    name: str
+    url: str
+    standard: str
+    # icon: str | None = None
+
+    def tx_url(self, tx_hash: HexStr | str):
+        return f"{self.url}/tx/0x{tx_hash}"
+
+
+class NativeCurrency(BaseModel):
+    name: str = "Ether"
+    symbol: str = "ETH"
+    decimals: int = 18
+
+
+class Feature(BaseModel):
+    name: str
+
+
+class CAIP2ChainData(BaseModel):
+    name: str
+    chain: str
+    rpc_list: list[str] = Field(alias="rpc")
+    explorers: list[Explorer] = Field(default_factory=list)
+    # faucets: list[str] = Field(default_factory=list)
+    native_currency: NativeCurrency = Field(alias="nativeCurrency")
+    features: list[Feature] | None = None
+    info_url: str = Field(alias="infoURL")
+    short_name: str = Field(alias="shortName")
+    chain_id: int = Field(alias="chainId")
+    # network_id: int = Field(alias="networkId")
+    # icon: str | None = None
+    # slip44: int | None = None
+    # ens: dict | None = None
