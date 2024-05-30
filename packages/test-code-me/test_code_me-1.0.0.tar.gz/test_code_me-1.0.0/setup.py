971d4fa1ec864c5d8676509d8542e86466579200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:55 +0000

    Add package test-code-me/1.0.0/test_code_me-1.0.0.tar.gz

---
+++ packages/test-code-me/test_code_me-1.0.0.tar.gz/test_code_me-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_code_me",
+    version='1.0.0',
+    packages=find_packages(),
+    author='Farbod Parkhooi',
+    description='Hi Bro Whats up',
+    install_requires=[
+        'numpy',
+    ],
+)
