5999e2bf03a0b82f6e4c5a8173504ffe23363865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:04:29 +0000

    Add package casq/1.3.3/casq-1.3.3.tar.gz

---
+++ packages/casq/casq-1.3.3.tar.gz/casq-1.3.3/test/test_casq.py
@@ -0,0 +1,29 @@
+"""Tests for CaSQ."""
+
+from filecmp import cmp
+from glob import glob
+from os import path
+from unittest.mock import patch
+
+import pytest  # type: ignore
+
+from casq.celldesigner2qual import main, map_to_model
+from casq.utils import validate
+
+
+@pytest.mark.parametrize(
+    "infile",
+    glob(path.join(str(path.dirname(path.realpath(__file__))), "*.xml")),
+)
+def test_casq_produces_valid_files(tmp_path, infile):
+    """Check if the files we produce are valid."""
+    outfile = path.join(
+        str(tmp_path), path.splitext(path.basename(infile))[0] + ".sbml"
+    )
+    with patch("sys.argv", ["casq", infile, outfile]):
+        main()
+    assert validate(outfile) == "OK"
+
+    map_to_model(infile, outfile + "_api")
+
+    assert cmp(outfile, outfile + "_api")
