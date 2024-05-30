02f63150a22e659a7d31afa5728e37124333cefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:37 +0000

    Add package pinnacle-cli/0.0.1/pinnacle_cli-0.0.1.tar.gz

---
+++ packages/pinnacle-cli/pinnacle_cli-0.0.1.tar.gz/pinnacle_cli-0.0.1/pinnacle_cli/constants.py
@@ -0,0 +1,5 @@
+import os
+
+HOST = os.getenv("PINNACLE_HOST", default="localhost")
+PORT = int(os.getenv("PINNACLE_PORT", default=8000))
+DIRECTORY = os.getenv("PINNACLE_DIRECTORY", default="./pinnacle")
