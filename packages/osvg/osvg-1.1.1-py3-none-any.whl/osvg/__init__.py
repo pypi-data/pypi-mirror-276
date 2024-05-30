a7bc80adf761d0b493845fe76f1abf554ff22e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:04:20 +0000

    Add package osvg/1.1.1/osvg-1.1.1-py3-none-any.whl

---
+++ packages/osvg/osvg-1.1.1-py3-none-any.whl/osvg/__init__.py
@@ -0,0 +1,59 @@
+"""
+Object-oriented SVG Drawing Tool
+"""
+
+from .elements.svg import SVG, ViewBox
+from .elements.group import Group
+from .positions import (
+    Position,
+    ShiftedPosition,
+    PolarShiftedPosition,
+    RotatedPosition,
+    Distance,
+    AngleDegree,
+)
+from .float import (
+    Const,
+)
+from .float_math import (
+    InvertedSign,
+    Cos,
+    Sin,
+    Cot,
+    Tan,
+    FunctFloat,
+    Prod,
+    Sum,
+    Min,
+    LowestAbs,
+    Max,
+)
+from .elements.elementbase import (
+    Height,
+    Width,
+)
+from .elements.circle import Circle
+from .elements.ellipse import Ellipse
+from .elements.rectangle import Rectangle, SCRectangle
+from .elements.polyline import Polyline
+from .elements.line import Line
+from .elements.line_marker import (
+    CircleMarker,
+    ArrowMarker,
+    LightArrowMarker,
+    SkinnyArrowMarker,
+)
+from .style import (
+    FillColor,
+    FillOpacity,
+    StrokeDashArray,
+    StrokeColor,
+    StrokeLineCap,
+    StrokeLineJoin,
+    StrokeOpacity,
+    StrokeWidth,
+    Style,
+)
+
+__version__ = "1.1.1"
+version_info = (1, 1, 1)
