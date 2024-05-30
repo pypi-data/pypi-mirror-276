b1d8b6b95429fd16e7b84e249e7bce30132f4335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 15:03:42 +0000

    Add package libro/0.1.11/libro-0.1.11.tar.gz

---
+++ packages/libro/libro-0.1.11.tar.gz/libro-0.1.11/src/libro_server/static/4113.async.js
@@ -0,0 +1,17 @@
+(self["webpackChunklibro_lab"] = self["webpackChunklibro_lab"] || []).push([[4113],{
+
+/***/ 69862:
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ 40964:
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
