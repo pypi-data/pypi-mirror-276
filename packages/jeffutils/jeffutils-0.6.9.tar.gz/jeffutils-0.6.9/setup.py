455e8332238c9b577177710de899868cb014df33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:19 +0000

    Add package jeffutils/0.6.9/jeffutils-0.6.9.tar.gz

---
+++ packages/jeffutils/jeffutils-0.6.9.tar.gz/jeffutils-0.6.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name='jeffutils',
+    version='0.6.9',
+    author='Jeff Hansen',
+    author_email='jeffxhansen@gmail.com',
+    description='A series of useful functions and decorators I use in most of my projects. Feel free to use them as well :)',
+    package_dir={"": "src"},
+    packages = find_packages(where="src"),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    url="https://github.com/jeffxhansen/jeffutils",
+    python_requires='>=3.6',
+)
