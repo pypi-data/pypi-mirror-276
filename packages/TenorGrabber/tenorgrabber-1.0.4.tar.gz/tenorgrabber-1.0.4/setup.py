78c12a246bc2c088b0c713f8db95bd7c286565c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:03:56 +0000

    Add package TenorGrabber/1.0.4/tenorgrabber-1.0.4.tar.gz

---
+++ packages/TenorGrabber/tenorgrabber-1.0.4.tar.gz/tenorgrabber-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='TenorGrabber',
+    packages=find_packages(),
+    version='1.0.4',
+    author='user0',
+    description='Get the direct link to a tenor GIF on python3!',
+    install_requires=[
+    'requests'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
