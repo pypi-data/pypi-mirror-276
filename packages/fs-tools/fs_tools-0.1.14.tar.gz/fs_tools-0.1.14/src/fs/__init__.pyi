c497afb4687511852c5ddb4e19779c53015abf94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:03:35 +0000

    Add package fs-tools/0.1.14/fs_tools-0.1.14.tar.gz

---
+++ packages/fs-tools/fs_tools-0.1.14.tar.gz/fs_tools-0.1.14/src/fs/__init__.pyi
@@ -0,0 +1,15 @@
+from .io import append, delete, write, read, update
+from .moving import move, copy
+from .paths import ensure_path, filenames
+from .compression import gzcompress
+from .parallel import chunked_read, parallel_map
+from .reading import concat_lines
+
+__all__ = [
+  'append', 'delete', 'write', 'read', 'update',
+  'move', 'copy',
+  'ensure_path', 'filenames',
+  'gzcompress',
+  'chunked_read', 'parallel_map',
+  'concat_lines',
+]
