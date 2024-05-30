8852d9b2234594d0db5c5c8509e6efb92845ef3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 15:03:52 +0000

    Add package busycat/0.1.1/busycat-0.1.1.tar.gz

---
+++ packages/busycat/busycat-0.1.1.tar.gz/busycat-0.1.1/busycat/__init__.py
@@ -0,0 +1,1 @@
+name = "busycat_pkg"
---
+++ packages/busycat/busycat-0.1.1.tar.gz/busycat-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="busycat",
+    version="0.1.1",
+    author="busycat",
+    author_email="",
+    description="selfmade pkg",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+)
