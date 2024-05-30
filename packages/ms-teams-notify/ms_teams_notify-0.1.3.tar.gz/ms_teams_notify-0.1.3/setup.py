d418676fdb5d14fc75cd20082bd7f0b532ab5ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:03:52 +0000

    Add package ms-teams-notify/0.1.3/ms_teams_notify-0.1.3.tar.gz

---
+++ packages/ms-teams-notify/ms_teams_notify-0.1.3.tar.gz/ms_teams_notify-0.1.3/setup.py
@@ -0,0 +1,24 @@
+import logging
+
+from setuptools import find_packages, setup
+
+with open('README.md', encoding="utf-8") as fp:
+    distribution = setup(
+        name='ms_teams_notify',
+        version='0.1.3',
+        description='Notify to Microsoft Teams with Adaptive Card.',
+        long_description=fp.read(),
+        long_description_content_type='text/markdown',
+        author='rue1-suzuki',
+        author_email='suzuki.ryuichi.1998@gmail.com',
+        url='https://github.com/rue1-suzuki?tab=packages',
+        packages=find_packages(),
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+        ],
+        python_requires='>=3.6',
+    )
+
+logging.debug(distribution)
