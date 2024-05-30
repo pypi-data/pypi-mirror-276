98ef7c041645efa7068a34fda4d7d125f5eee6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:44 +0000

    Add package myfunctions-Andre-Pank/0.0.12/myfunctions_Andre-Pank-0.0.12.tar.gz

---
+++ packages/myfunctions-Andre-Pank/myfunctions_Andre-Pank-0.0.12.tar.gz/myfunctions_Andre-Pank-0.0.12/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_Andre-Pank',
+    version='0.0.12',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/Andre-Pank',
+    license='MIT',
+    author='Andrei Pankevitch ',
+    author_email='panchevici.andrei@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
