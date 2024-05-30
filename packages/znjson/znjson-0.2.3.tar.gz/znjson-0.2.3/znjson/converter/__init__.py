fed9df6ec2d017f487f43f0d2a76763174d9c498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:50 +0000

    Add package znjson/0.2.3/znjson-0.2.3.tar.gz

---
+++ packages/znjson/znjson-0.2.3.tar.gz/znjson-0.2.3/znjson/converter/__init__.py
@@ -0,0 +1,23 @@
+"""Manage Converter Inputs based on available moduls"""
+
+from znjson.converter.class_converter import ClassConverter
+from znjson.converter.pathlib_converter import PathlibConverter
+
+__all__ = ["PathlibConverter", "ClassConverter"]
+
+try:
+    from znjson.converter.numpy_converter_base64 import NumpyConverter
+    from znjson.converter.numpy_converter_small import NumpyConverterSmall
+    from znjson.converter.old_converters import (
+        NumpyConverterBase64,
+        NumpyConverterLatin1,
+    )
+
+    __all__ += [
+        "NumpyConverterSmall",
+        "NumpyConverter",
+        "NumpyConverterBase64",
+        "NumpyConverterLatin1",
+    ]
+except ModuleNotFoundError:
+    pass
