4353f0d3cc3d10bdd7d3ea2f05afac415496b47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:50 +0000

    Add package saxton2006/1.1.0/saxton2006-1.1.0-py3-none-any.whl

---
+++ packages/saxton2006/saxton2006-1.1.0-py3-none-any.whl/saxton2006/__init__.py
@@ -0,0 +1,14 @@
+"""
+Formalisms published in Saxton and Rawls (2006)
+"""
+# {# pkglts, src
+# FYEO
+# #}
+# {# pkglts, version, after src
+from . import version
+
+__version__ = version.__version__
+# #}
+# {# pkglts, glabdata, after version
+from .info import *
+# #}
