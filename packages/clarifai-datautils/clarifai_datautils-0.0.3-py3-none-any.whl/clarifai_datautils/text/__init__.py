f0fb84a4f19790201c68c0e5ed43395eb9001c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:49 +0000

    Add package clarifai-datautils/0.0.3/clarifai_datautils-0.0.3-py3-none-any.whl

---
+++ packages/clarifai-datautils/clarifai_datautils-0.0.3-py3-none-any.whl/clarifai_datautils/text/__init__.py
@@ -0,0 +1,5 @@
+from clarifai_datautils.text.pipeline.base import Pipeline
+from clarifai_datautils.text.pipeline.PDF import PDFPartition
+from clarifai_datautils.text.pipeline.Text import TextPartition
+
+__all__ = ['Pipeline', 'PDFPartition', 'TextPartition']
