ae1b9178183ee6aabf9115dd37cb9f2a36b30112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:04:15 +0000

    Add package gusense/1.1.0/gusense-1.1.0.tar.gz

---
+++ packages/gusense/gusense-1.1.0.tar.gz/gusense-1.1.0/gusense/common/contant.py
@@ -0,0 +1,9 @@
+LOGIN = 'login'
+REQ_SUCCESS_CODE = 200
+BASIC_URL = 'http://14.18.87.235:8081/di'
+LOGIN_URL = 'http://14.18.87.235:8081/uc/api/login'
+TIME_OUT = 65  # 秒
+USER_TOKEN_CN = 'user_tk.csv'
+SEPARATOR = '/'
+
+TOKEN_NULL_MSG = '请先通过auth方法获取接口调用权限'
