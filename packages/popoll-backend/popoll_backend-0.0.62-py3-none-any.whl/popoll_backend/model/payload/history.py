edeef8bfdfbd7b83e50e57cc04ea3f7fc1de4b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:04:01 +0000

    Add package popoll-backend/0.0.62/popoll_backend-0.0.62-py3-none-any.whl

---
+++ packages/popoll-backend/popoll_backend-0.0.62-py3-none-any.whl/popoll_backend/model/payload/history.py
@@ -0,0 +1,21 @@
+import contextlib
+import datetime
+import flask
+import json
+
+from popoll_backend.model import Payload
+
+class History(Payload):
+    
+    def __init__(self, request: flask.Request, response, kwargs):
+        self.date = datetime.datetime.now().isoformat(sep='T', timespec='auto')
+        self.method = request.method
+        self.path = request.path
+        self.matching_path = request.url_rule.rule
+        self.ip = request.headers.get('Host', None)
+        self.sessionId = request.headers.get('SessionId', None)
+        self.queryParams = kwargs
+        self.body = request.data
+        with contextlib.suppress(Exception):
+            self.body_json = json.loads(request.data)
+        self.response = response
