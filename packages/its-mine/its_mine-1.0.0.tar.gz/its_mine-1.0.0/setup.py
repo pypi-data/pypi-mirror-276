8d01e36b10746d0dd7dc3ee2cb48cc4c51cd586e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:51 +0000

    Add package its-mine/1.0.0/its_mine-1.0.0.tar.gz

---
+++ packages/its-mine/its_mine-1.0.0.tar.gz/its_mine-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="its_mine",
+    version='1.0.0',
+    packages=find_packages(),
+    author='Farbod Parkhooi',
+    description='Hi Bro Whats up',
+    install_requires=[
+        'numpy',
+    ],
+)
