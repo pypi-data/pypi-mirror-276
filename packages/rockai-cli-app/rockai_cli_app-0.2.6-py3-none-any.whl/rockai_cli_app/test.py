663dbb407139cf1e0408502f2c5ccfa86f360439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:04:24 +0000

    Add package rockai-cli-app/0.2.6/rockai_cli_app-0.2.6-py3-none-any.whl

---
+++ packages/rockai-cli-app/rockai_cli_app-0.2.6-py3-none-any.whl/rockai_cli_app/test.py
@@ -0,0 +1,13 @@
+# import asyncio
+
+
+# # Function to run the asynchronous function and print the result
+# async def main():
+#     from rockai_cli_app.client import Client
+#     my_client = Client(api_token='b5960fdf801c48008aa2165a43ba64a1')
+#     result = my_client.get('89xme47d31rgg0cfqry8zvsbgr')
+#     print("Result:", result)
+
+# # Run the main function using asyncio.run
+# if __name__ == "__main__":
+#     asyncio.run(main())
