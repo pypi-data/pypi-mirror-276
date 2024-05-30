9c83adff69f8c4f952de7a7fba0513c79dbc34c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:04:21 +0000

    Add package sentiment-analysis-model/0.0.7/sentiment_analysis_model-0.0.7-py3-none-any.whl

---
+++ packages/sentiment-analysis-model/sentiment_analysis_model-0.0.7-py3-none-any.whl/sentiment_analysis_model/processing/data_manager.py
@@ -0,0 +1,13 @@
+import typing as t
+from pathlib import Path
+
+import pandas as pd
+
+from sentiment_analysis_model.config.core import DATASET_DIR
+
+
+def load_dataset(*, file_name: str) -> pd.DataFrame:
+    dataframe = pd.read_csv(Path(f"{DATASET_DIR}/{file_name}"))
+    return dataframe
+
+
