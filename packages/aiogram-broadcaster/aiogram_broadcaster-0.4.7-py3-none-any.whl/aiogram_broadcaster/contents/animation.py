4db1e4eba1cf2f6966f05dc6e23d665c2d558b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:28 +0000

    Add package aiogram-broadcaster/0.4.7/aiogram_broadcaster-0.4.7-py3-none-any.whl

---
+++ packages/aiogram-broadcaster/aiogram_broadcaster-0.4.7-py3-none-any.whl/aiogram_broadcaster/contents/animation.py
@@ -0,0 +1,84 @@
+from typing import TYPE_CHECKING, Any, List, Optional, Union
+
+from aiogram.client.default import Default
+from aiogram.methods import SendAnimation
+from aiogram.types import (
+    ForceReply,
+    InlineKeyboardMarkup,
+    InputFile,
+    MessageEntity,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+
+from .base import BaseContent
+
+
+class AnimationContent(BaseContent):
+    animation: Union[InputFile, str]
+    business_connection_id: Optional[str] = None
+    duration: Optional[int] = None
+    width: Optional[int] = None
+    height: Optional[int] = None
+    thumbnail: Optional[InputFile] = None
+    caption: Optional[str] = None
+    parse_mode: Optional[Union[str, Default]] = Default("parse_mode")
+    caption_entities: Optional[List[MessageEntity]] = None
+    has_spoiler: Optional[bool] = None
+    disable_notification: Optional[bool] = None
+    protect_content: Optional[Union[bool, Default]] = Default("protect_content")
+    reply_markup: Optional[
+        Union[
+            InlineKeyboardMarkup,
+            ReplyKeyboardMarkup,
+            ReplyKeyboardRemove,
+            ForceReply,
+        ]
+    ] = None
+
+    async def __call__(self, chat_id: int) -> SendAnimation:
+        return SendAnimation(
+            chat_id=chat_id,
+            animation=self.animation,
+            business_connection_id=self.business_connection_id,
+            duration=self.duration,
+            width=self.width,
+            height=self.height,
+            thumbnail=self.thumbnail,
+            caption=self.caption,
+            parse_mode=self.parse_mode,
+            caption_entities=self.caption_entities,
+            has_spoiler=self.has_spoiler,
+            disable_notification=self.disable_notification,
+            protect_content=self.protect_content,
+            reply_markup=self.reply_markup,
+            **(self.model_extra or {}),
+        )
+
+    if TYPE_CHECKING:
+
+        def __init__(
+            self,
+            *,
+            animation: Union[InputFile, str],
+            business_connection_id: Optional[str] = ...,
+            duration: Optional[int] = ...,
+            width: Optional[int] = ...,
+            height: Optional[int] = ...,
+            thumbnail: Optional[InputFile] = ...,
+            caption: Optional[str] = ...,
+            parse_mode: Optional[Union[str, Default]] = ...,
+            caption_entities: Optional[List[MessageEntity]] = ...,
+            has_spoiler: Optional[bool] = ...,
+            disable_notification: Optional[bool] = ...,
+            protect_content: Optional[Union[bool, Default]] = ...,
+            reply_markup: Optional[
+                Union[
+                    InlineKeyboardMarkup,
+                    ReplyKeyboardMarkup,
+                    ReplyKeyboardRemove,
+                    ForceReply,
+                ]
+            ] = ...,
+            **kwargs: Any,
+        ) -> None: ...
---
+++ packages/aiogram-broadcaster/aiogram_broadcaster-0.4.7-py3-none-any.whl/aiogram_broadcaster/contents/game.py
@@ -0,0 +1,39 @@
+from typing import TYPE_CHECKING, Any, Optional, Union
+
+from aiogram.client.default import Default
+from aiogram.methods import SendGame
+from aiogram.types import InlineKeyboardMarkup
+
+from .base import BaseContent
+
+
+class GameContent(BaseContent):
+    game_short_name: str
+    business_connection_id: Optional[str] = None
+    disable_notification: Optional[bool] = None
+    protect_content: Optional[Union[bool, Default]] = Default("protect_content")
+    reply_markup: Optional[InlineKeyboardMarkup] = None
+
+    async def __call__(self, chat_id: int) -> SendGame:
+        return SendGame(
+            chat_id=chat_id,
+            game_short_name=self.game_short_name,
+            business_connection_id=self.business_connection_id,
+            disable_notification=self.disable_notification,
+            protect_content=self.protect_content,
+            reply_markup=self.reply_markup,
+            **(self.model_extra or {}),
+        )
+
+    if TYPE_CHECKING:
+
+        def __init__(
+            self,
+            *,
+            game_short_name: str,
+            business_connection_id: Optional[str] = ...,
+            disable_notification: Optional[bool] = ...,
+            protect_content: Optional[Union[bool, Default]] = ...,
+            reply_markup: Optional[InlineKeyboardMarkup] = ...,
+            **kwargs: Any,
+        ) -> None: ...
