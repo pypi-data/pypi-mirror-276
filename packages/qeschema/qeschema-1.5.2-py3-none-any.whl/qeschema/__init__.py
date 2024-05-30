25cb585884fee960a020c2279339963ba0e670b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:22 +0000

    Add package qeschema/1.5.2/qeschema-1.5.2-py3-none-any.whl

---
+++ packages/qeschema/qeschema-1.5.2-py3-none-any.whl/qeschema/__init__.py
@@ -0,0 +1,27 @@
+#
+# Copyright (c), 2015-2022, Quantum Espresso Foundation and SISSA (Scuola
+# Internazionale Superiore di Studi Avanzati). All rights reserved.
+# This file is distributed under the terms of the MIT License. See the
+# file 'LICENSE' in the root directory of the present distribution, or
+# http://opensource.org/licenses/MIT.
+#
+# Authors: Davide Brunato
+#
+from .documents import XmlDocument, QeDocument, PwDocument, PhononDocument, \
+    NebDocument, TdDocument, TdSpectrumDocument, XSpectraDocument, EPWDocument
+from .converters import RawInputConverter, PwInputConverter, \
+    PhononInputConverter, NebInputConverter, TdInputConverter, \
+    TdSpectrumInputConverter, XSpectraInputConverter, EPWInputConverter
+from .exceptions import QESchemaError, XmlDocumentError
+from .utils import set_logger
+
+__version__ = '1.5.2'
+
+__all__ = [
+    'XmlDocument', 'QeDocument', 'PwDocument', 'PhononDocument', 'NebDocument',
+    'TdDocument', 'TdSpectrumDocument', 'EPWDocument', 'RawInputConverter',
+    'PwInputConverter', 'PhononInputConverter', 'TdInputConverter',
+    'TdSpectrumInputConverter', 'NebInputConverter', 'QESchemaError',
+    'XmlDocumentError', 'set_logger', 'hdf5', 'XSpectraDocument',
+    'XSpectraInputConverter', 'EPWInputConverter'
+]
