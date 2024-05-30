f6a9db78b6add7f412961b017f784a44b7b83b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 17:03:37 +0000

    Add package pynumint/0.0.6/pynumint-0.0.6.tar.gz

---
+++ packages/pynumint/pynumint-0.0.6.tar.gz/pynumint-0.0.6/post_install.py
@@ -0,0 +1,7 @@
+from pynumint.src.starpattern import print_word_star_pattern
+
+def main():
+    print_word_star_pattern("pynumint")
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pynumint/pynumint-0.0.6.tar.gz/pynumint-0.0.6/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+# Display a message during installation
+print("Installing pynumint...")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pynumint",
+    version="0.0.6",
+    author="Arjun Jagdale",
+    author_email="arjunjagdale14@gmail.com",
+    description="pynumint Test Package for Numerical Integration Demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/pynumint/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["pynumint"],
+    package_dir={'': 'pynumint/src'},
+    install_requires=[],
+
+    scripts=['post_install.py'],
+)
+
+# Print a completion message
+print("pynumint has been successfully installed!")
