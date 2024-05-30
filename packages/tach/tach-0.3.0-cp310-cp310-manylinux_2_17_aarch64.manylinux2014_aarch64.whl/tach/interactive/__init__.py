0c9027cda5c74088f2711d8ed6b4812584f55bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:59 +0000

    Add package tach/0.3.0/tach-0.3.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/tach/tach-0.3.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/tach/interactive/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from tach.interactive.packages import SelectedPackage, get_selected_packages_interactive
+
+__all__ = ["get_selected_packages_interactive", "SelectedPackage"]
