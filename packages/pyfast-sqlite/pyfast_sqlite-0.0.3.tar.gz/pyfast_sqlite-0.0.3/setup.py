0ba3348584d6b774361968ee29d04d66b96e4bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:04:22 +0000

    Add package pyfast-sqlite/0.0.3/pyfast_sqlite-0.0.3.tar.gz

---
+++ packages/pyfast-sqlite/pyfast_sqlite-0.0.3.tar.gz/pyfast_sqlite-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "pyfast-sqlite",
+    version = "0.0.3",
+    author = "Farbod Parkhooi",
+    author_email = "farbod.p1390@gmail.com",
+    description = "Fast_sqlite is a library to use sqlite database faster and easier.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Unknow-per/Fast_sqlite-lIbrary",
+    project_urls = {
+        "Bug Tracker": "https://github.com/Unknow-per/Fast_sqlite-lIbrary/~/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.8"
+)
