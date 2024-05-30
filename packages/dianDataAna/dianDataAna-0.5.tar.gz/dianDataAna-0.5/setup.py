718fed477dfc69a420d93ee9b71965dc470ca841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:03:41 +0000

    Add package dianDataAna/0.5/dianDataAna-0.5.tar.gz

---
+++ packages/dianDataAna/dianDataAna-0.5.tar.gz/dianDataAna-0.5/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+import setuptools
+packages = ['dianDataAna']
+setup(name='dianDataAna',
+      version='0.5',
+      author='ZhangDian',
+      packages=packages,
+      package_dir={'requsets':'requests'},
+      )
