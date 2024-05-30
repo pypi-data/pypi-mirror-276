ab2f9626aa5af4eb3eed1c04aed929bed6706e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 07:03:39 +0000

    Add package dwd-global-radiation/1.1.0rc3/dwd_global_radiation-1.1.0rc3.tar.gz

---
+++ packages/dwd-global-radiation/dwd_global_radiation-1.1.0rc3.tar.gz/dwd_global_radiation-1.1.0rc3/setup.py
@@ -0,0 +1,13 @@
+"""Setup configuration for the dwd_global_radiation package.
+
+This package provides tools to access and analyze DWD (Deutscher Wetterdienst)
+global radiation data and forecasts. It is designed for applications in weather forecasting,
+climate studies, and solar energy analysis, utilizing libraries like xarray for data handling
+and netCDF4 for managing data formats.
+
+Author: Arno Schmerer
+License: MIT
+"""
+from setuptools import setup
+
+setup()
