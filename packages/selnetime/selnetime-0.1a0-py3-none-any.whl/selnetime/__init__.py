552c3e2b6810643c6cca31b017a50cd1c12dc2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 03:03:24 +0000

    Add package selnetime/0.1a0/selnetime-0.1a0-py3-none-any.whl

---
+++ packages/selnetime/selnetime-0.1a0-py3-none-any.whl/selnetime/__init__.py
@@ -0,0 +1,12 @@
+from enum import Enum
+from collections import namedtuple
+
+VERSION = '0.1a'
+
+class TransModel(Enum):
+    BwS = "BwS"
+    WF = "WF"
+
+TransParam = namedtuple("TransParam", "dt N s h")
+
+Moments = namedtuple("Moments", "mu var pfix0 pfix1 a b")
---
+++ packages/selnetime/selnetime-0.1a0-py3-none-any.whl/selnetime_package/utils.py
@@ -0,0 +1,18 @@
+import time
+
+class Timer(object):
+    def __init__(self, name=None, filename=None):
+        self.name = name
+        self.filename = filename
+
+    def __enter__(self):
+        self.tstart = time.time()
+
+    def __exit__(self, type, value, traceback):
+        message = 'Elapsed: %.2f seconds' % (time.time() - self.tstart)
+        if self.name:
+            message = '[%s] ' % self.name + message
+        print(message)
+        if self.filename:
+            with open(self.filename,'a') as file:
+                print(str(datetime.datetime.now())+": ",message,file=file)
