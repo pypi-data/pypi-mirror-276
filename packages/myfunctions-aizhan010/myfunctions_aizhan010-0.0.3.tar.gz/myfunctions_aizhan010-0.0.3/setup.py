390e097e810e096d73b9848975c0cd5c3f9ce7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:31 +0000

    Add package myfunctions-aizhan010/0.0.3/myfunctions_aizhan010-0.0.3.tar.gz

---
+++ packages/myfunctions-aizhan010/myfunctions_aizhan010-0.0.3.tar.gz/myfunctions_aizhan010-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_aizhan010',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/aizhan010/',
+    license='MIT',
+    author='Aizhan Maratova',
+    author_email='aizhanmaratova6@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
