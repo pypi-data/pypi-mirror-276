63f09dd4ae1d98fb1e770688d5477007ce43675a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:19 +0000

    Add package tach/0.3.0/tach-0.3.0.tar.gz

---
+++ packages/tach/tach-0.3.0.tar.gz/tach-0.3.0/src/lib.rs
@@ -0,0 +1,29 @@
+pub mod filesystem;
+pub mod imports;
+pub mod parsing;
+
+use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
+
+impl From<imports::ImportParseError> for PyErr {
+    fn from(err: imports::ImportParseError) -> Self {
+        PyValueError::new_err(err.message)
+    }
+}
+
+/// Get first-party imports from file_path relative to project_root
+#[pyfunction]
+#[pyo3(signature = (project_root, file_path, ignore_type_checking_imports=false))]
+fn get_project_imports(
+    project_root: String,
+    file_path: String,
+    ignore_type_checking_imports: bool,
+) -> imports::Result<imports::ProjectImports> {
+    imports::get_project_imports(project_root, file_path, ignore_type_checking_imports)
+}
+
+#[pymodule]
+fn extension(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_project_imports, m)?)?;
+    Ok(())
+}
