90c86a52f53a40ffc8e17c393edddf2a264177d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:03:52 +0000

    Add package echoss-query/0.1.2/echoss_query-0.1.2-py3-none-any.whl

---
+++ packages/echoss-query/echoss_query-0.1.2-py3-none-any.whl/echoss_query/__init__.py
@@ -0,0 +1,4 @@
+from .query.echoss_query import MysqlQuery
+from .query.echoss_query import MongoQuery
+from .query.echoss_query import ElasticSearch
+from .query.echoss_query import ElasticSearchV2
