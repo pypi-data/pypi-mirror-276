d27e5ca429b282843dd45a1c385dd706d0d5f69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:57 +0000

    Add package ProjectManagerSdk/110.0.3705/projectmanagersdk-110.0.3705-py3-none-any.whl

---
+++ packages/ProjectManagerSdk/projectmanagersdk-110.0.3705-py3-none-any.whl/ProjectManagerSdk/models/createprojectfielddto.py
@@ -0,0 +1,48 @@
+#
+# ProjectManager API for Python
+#
+# (c) 2023-2024 ProjectManager.com, Inc.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author     ProjectManager.com <support@projectmanager.com>
+# @copyright  2023-2024 ProjectManager.com, Inc.
+# @link       https://github.com/projectmgr/projectmanager-sdk-python
+#
+
+
+from typing import List
+import dataclasses
+
+@dataclasses.dataclass
+class CreateProjectFieldDto:
+    """
+    A ProjectField is a custom field defined within your Workspace. You
+    can define ProjectFields for any integration purpose that is
+    important to your business. Each ProjectField has a data type as
+    well as options in how it is handled. ProjectFields can be edited
+    for each Project within your Workspace.
+    """
+
+    name: str | None = None
+    """
+    The name of this Field
+    """
+
+    type: str | None = None
+    """
+    The type of the Field. Valid types are the following: * Text *
+    Number * Date * Currency * Dropdown Attempting to create a field
+    with any Type other than these will return an error. For Dropdown
+    Field, specify the list of choices in the `Options` field. TODO -
+    This object needs to support a list of options, in case dropdown is
+    selected
+    """
+
+    shortId: str | None = None
+    """
+    The short Id of this field - human readable identity
+    """
+
+
