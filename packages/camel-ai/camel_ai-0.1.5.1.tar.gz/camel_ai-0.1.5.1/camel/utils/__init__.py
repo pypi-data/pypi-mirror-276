d6fe8883bd1ff9521b8513ce811dbd34f046678a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:42 +0000

    Add package camel-ai/0.1.5.1/camel_ai-0.1.5.1.tar.gz

---
+++ packages/camel-ai/camel_ai-0.1.5.1.tar.gz/camel_ai-0.1.5.1/camel/utils/__init__.py
@@ -0,0 +1,54 @@
+# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+from .commons import (
+    PYDANTIC_V2,
+    api_key_required,
+    api_keys_required,
+    check_server_running,
+    dependencies_required,
+    download_tasks,
+    get_first_int,
+    get_prompt_template_key_words,
+    get_system_information,
+    get_task_list,
+    print_text_animated,
+    to_pascal,
+)
+from .token_counting import (
+    AnthropicTokenCounter,
+    BaseTokenCounter,
+    OpenAITokenCounter,
+    OpenSourceTokenCounter,
+    get_model_encoding,
+)
+
+__all__ = [
+    'api_key_required',
+    'print_text_animated',
+    'get_prompt_template_key_words',
+    'get_first_int',
+    'download_tasks',
+    'get_task_list',
+    'check_server_running',
+    'AnthropicTokenCounter',
+    'get_system_information',
+    'to_pascal',
+    'PYDANTIC_V2',
+    'get_model_encoding',
+    'BaseTokenCounter',
+    'OpenAITokenCounter',
+    'OpenSourceTokenCounter',
+    'dependencies_required',
+    'api_keys_required',
+]
