38bb0359b0eec8292a5ff0fbb39f9ee1372ace38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 06:05:53 +0000

    Add package washpy/0.4.0/washpy-0.4.0.tar.gz

---
+++ packages/washpy/washpy-0.4.0.tar.gz/washpy-0.4.0/washpy/__init__.py
@@ -0,0 +1,21 @@
+"""
+washpy implements parts of the Miele Professional IP Profile API.
+
+Start by setting up the admin user, see `washpy.post_new_password`.
+
+Then, create a `washpy.device_user.DeviceUser`, representing an user on a device.
+Most of the functionality is derived from that class.
+"""
+
+import requests
+
+# disables warnings from the unverified HTTPS requests
+requests.packages.urllib3.disable_warnings()
+
+from washpy.device_user import DeviceUser
+from washpy.authenticate import authenticate
+from washpy.post_new_password import post_new_password
+
+
+if __name__ == "__main__":
+    print("Hello World!")
