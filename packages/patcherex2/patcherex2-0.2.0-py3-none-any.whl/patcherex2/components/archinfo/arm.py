470993ce0f14b74314018126c2c915601605184a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:28 +0000

    Add package patcherex2/0.2.0/patcherex2-0.2.0-py3-none-any.whl

---
+++ packages/patcherex2/patcherex2-0.2.0-py3-none-any.whl/patcherex2/components/archinfo/arm.py
@@ -0,0 +1,17 @@
+class ArmInfo:
+    nop_bytes = b"\x00\xf0\x20\xe3"  # TODO: thumb
+    nop_size = 4
+    jmp_asm = "b {dst}"
+    jmp_size = 4
+    alignment = 4
+    bits = 32
+    is_variable_length_isa = False
+    instr_size = 4  # TODO: thumb 2
+    call_asm = "bl {dst}"
+    pc_reg_names = ["pc", "r15"]
+    save_context_asm = """
+        push {r0-r11}
+    """
+    restore_context_asm = """
+        pop {r0-r11}
+    """
