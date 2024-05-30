485639f9d696909078db4d21f47332aff6627857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:50 +0000

    Add package myfunctions-diana11d/0.0.3/myfunctions_diana11d-0.0.3.tar.gz

---
+++ packages/myfunctions-diana11d/myfunctions_diana11d-0.0.3.tar.gz/myfunctions_diana11d-0.0.3/myfunctions_diana11d/math.py
@@ -0,0 +1,20 @@
+"""
+    This is how you use this function
+
+"""
+
+def addition(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    e.g addition(2,3)
+    """
+
+    print(NUMBER1 + NUMBER2)
+
+def multiplication(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    
+    """
+
+    print(NUMBER1 * NUMBER2)
