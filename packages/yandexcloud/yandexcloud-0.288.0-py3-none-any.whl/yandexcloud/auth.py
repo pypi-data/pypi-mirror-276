0fa3be8dde6af44c6ea8f519bd612e95b9722a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 15:03:35 +0000

    Add package yandexcloud/0.288.0/yandexcloud-0.288.0-py3-none-any.whl

---
+++ packages/yandexcloud/yandexcloud-0.288.0-py3-none-any.whl/yandexcloud/auth.py
@@ -0,0 +1,26 @@
+from yandex.cloud.iam.v1.iam_token_service_pb2_grpc import IamTokenServiceStub
+from yandexcloud import SDK
+from yandexcloud._auth_fabric import (
+    YC_API_ENDPOINT,
+    IamTokenAuth,
+    MetadataAuth,
+    get_auth_token_requester,
+)
+
+
+def get_auth_token(token=None, service_account_key=None, iam_token=None, metadata_addr=None, endpoint=None):
+    if endpoint is None:
+        endpoint = YC_API_ENDPOINT
+    requester = get_auth_token_requester(
+        token=token,
+        service_account_key=service_account_key,
+        iam_token=iam_token,
+        metadata_addr=metadata_addr,
+        endpoint=endpoint,
+    )
+    if isinstance(requester, (MetadataAuth, IamTokenAuth)):
+        return requester.get_token()
+
+    sdk = SDK()
+    client = sdk.client(IamTokenServiceStub)
+    return client.Create(requester.get_token_request()).iam_token
