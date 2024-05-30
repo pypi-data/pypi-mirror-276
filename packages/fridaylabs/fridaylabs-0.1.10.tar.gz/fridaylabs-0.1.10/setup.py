714324a6310c4e589de691ccd5d7a50b148dc85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 06:05:58 +0000

    Add package fridaylabs/0.1.10/fridaylabs-0.1.10.tar.gz

---
+++ packages/fridaylabs/fridaylabs-0.1.10.tar.gz/fridaylabs-0.1.10/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="fridaylabs",
+    version="0.1.10",
+    packages=find_packages(),
+    install_requires=["requests"],  # Add any dependencies here
+    author="FridayLabs",
+    author_email="fridaylabs@fridaylabs.ai",
+    description="A package to interact with FridayLabs AI API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ImJustRicky/fridaylabs-package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
