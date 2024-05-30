145e91c6bee61defe72a803966247f8afeefdf94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:40 +0000

    Add package bard-http-srv/0.1.5/bard-http-srv-0.1.5.tar.gz

---
+++ packages/bard-http-srv/bard-http-srv-0.1.5.tar.gz/bard-http-srv-0.1.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bard-http-srv",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        # 在这里列出你的库所需的其他Python包
+        'bardapi==0.1.23a0',
+        'Flask==2.3.2',
+        'configparser==7.0.0',
+    ],
+
+    author="vow1231a",
+    author_email="voidmanwzp@gmail.com",
+    description="bard http server",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/NullpointerW/bard-http-srv",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+     project_urls={
+        'Source': "https://github.com/NullpointerW/bard-http-srv",
+    },
+)
