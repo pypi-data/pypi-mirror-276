984f3cd7169ea2cfb372703c0d0987cb4133d1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:04:03 +0000

    Add package chars/0.1.1/chars-0.1.1.tar.gz

---
+++ packages/chars/chars-0.1.1.tar.gz/chars-0.1.1/README.md
@@ -0,0 +1,20 @@
+# mypackage
+
+chars is a simple Python package that provides functions to get different sets of characters including uppercase letters, lowercase letters, numbers, and special characters.
+
+## Features
+
+- Retrieve uppercase letters
+- Retrieve lowercase letters
+- Retrieve numbers
+- Retrieve special characters
+- Retrieve a combination of all character sets
+
+## Installation
+
+You can install the package using `pip`:
+
+```sh
+pip install chars
+```
+END
---
+++ packages/chars/chars-0.1.1.tar.gz/chars-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chars',
+    version='0.1.1',
+    packages=find_packages(),
+    description='A package for character sets',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='PiyushThePal',
+    author_email='cool_guy2331708@yahoo.com',
+    # url='https://github.com/yourusername/mypackage',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
