bc06d19c4bfb3c58a6808a769c5aad09e5575950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 14:03:44 +0000

    Add package myfunctions-thejondaw/0.0.6/myfunctions_thejondaw-0.0.6.tar.gz

---
+++ packages/myfunctions-thejondaw/myfunctions_thejondaw-0.0.6.tar.gz/myfunctions_thejondaw-0.0.6/myfunctions_thejondaw/math.py
@@ -0,0 +1,15 @@
+"""
+    This is how you use this function
+"""
+
+def addition(NUMBER1,NUMBER2):
+    print(NUMBER1 + NUMBER2)
+    """
+    This is docs
+    """
+    
+def multiplication(NUMBER1,NUMBER2):
+    print(NUMBER1 * NUMBER2)
+    """
+    This docs2
+    """
---
+++ packages/myfunctions-thejondaw/myfunctions_thejondaw-0.0.6.tar.gz/myfunctions_thejondaw-0.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_thejondaw',
+    version='0.0.6',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/thejondaw',
+    license='MIT',
+    author='Jon Daw',
+    author_email='mrjondaw@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
