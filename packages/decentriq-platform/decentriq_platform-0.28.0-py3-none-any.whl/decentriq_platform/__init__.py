f3e193142b24af25d2c5ea103d4c584cf21a1947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 16:03:44 +0000

    Add package decentriq-platform/0.28.0/decentriq_platform-0.28.0-py3-none-any.whl

---
+++ packages/decentriq-platform/decentriq_platform-0.28.0-py3-none-any.whl/decentriq_platform/__init__.py
@@ -0,0 +1,66 @@
+"""
+.. include:: ../../decentriq_platform_docs/gcg_getting_started.md
+___
+"""
+
+from . import (
+    analytics,
+    data_lab,
+    legacy,
+    lookalike_media,
+    session,
+    types,
+    authentication,
+)
+from .attestation import EnclaveSpecifications, enclave_specifications
+from .client import Client, Session, create_client
+from .endorsement import Endorser
+from .keychain import Keychain, KeychainEntry
+from .storage import Key
+
+__docformat__ = "restructuredtext"
+
+__pdoc__ = {
+    "api": False,
+    "attestation": True,
+    "authentication": True,
+    "builders": False,
+    "certs": False,
+    "client": False,
+    "compute": False,
+    "config": False,
+    "graphql": False,
+    "helpers": False,
+    "proto": False,
+    "lookalike_media": True,
+    "session": False,
+    "storage": False,
+    "types": False,
+    "verification": False,
+    "data_lab": True,
+    "legacy": True,
+    "decoder": False,
+    "media": True,
+}
+
+__all__ = [
+    "create_client",
+    "Client",
+    "Session",
+    "enclave_specifications",
+    "EnclaveSpecifications",
+    "Key",
+    "KeychainEntry",
+    "lookalike_media",
+    "storage",
+    "attestation",
+    "types",
+    "authentication",
+    "session",
+    "Endorser",
+    "Keychain",
+    "data_lab",
+    "legacy",
+    "analytics",
+    "media",
+]
---
+++ packages/decentriq-platform/decentriq_platform-0.28.0-py3-none-any.whl/decentriq_platform/legacy/post/proto/__init__.py
@@ -0,0 +1,1 @@
+from .compute_post_pb2 import PostWorkerConfiguration
