dd13005dfafcf97a9c6ae15c56430ef80d632d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 05:03:59 +0000

    Add package tach/0.3.0/tach-0.3.0-pp38-pypy38_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/tach/tach-0.3.0-pp38-pypy38_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/tach/constants/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+PACKAGE_NAME = "tach"
+TOOL_NAME = "tach"
+CONFIG_FILE_NAME = TOOL_NAME
+PACKAGE_FILE_NAME = "package"
+
+__all__ = ["PACKAGE_NAME", "TOOL_NAME", "CONFIG_FILE_NAME", "PACKAGE_FILE_NAME"]
