14aa1eb31e8bb6414d9f5dcb4a795b04ceefb8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:33 +0000

    Add package howso-visuals/1.2.3/howso_visuals-1.2.3.tar.gz

---
+++ packages/howso-visuals/howso_visuals-1.2.3.tar.gz/howso_visuals-1.2.3/howso/visuals/__init__.py
@@ -0,0 +1,21 @@
+from .visuals import (
+    compose_figures,
+    plot_anomalies,
+    plot_dataset,
+    plot_drift,
+    plot_fairness_disparity,
+    plot_feature_importances,
+    plot_interpretable_prediction,
+    plot_kl_divergence,
+)
+
+__all__ = [
+    "compose_figures",
+    "plot_anomalies",
+    "plot_dataset",
+    "plot_drift",
+    "plot_fairness_disparity",
+    "plot_feature_importances",
+    "plot_interpretable_prediction",
+    "plot_kl_divergence",
+]
