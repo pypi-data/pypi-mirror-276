8d7b0444c4f5beab235625a77e90fe3374ff449b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:58 +0000

    Add package RapidanAPI/1.0.25/RapidanAPI-1.0.25-py3-none-any.whl

---
+++ packages/RapidanAPI/RapidanAPI-1.0.25-py3-none-any.whl/RapidanAPI/energy_calendar.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from fastapi import HTTPException
+import asyncio
+from .utils import check_api_key
+
+async def get_data_async(api_keys, api_key: str):
+    check_api_key(api_keys, api_key, "energy_calendar")
+
+    csv_file_path = 'calendar.csv'
+
+    try:
+        df = pd.read_csv(csv_file_path)
+        df.replace([np.inf, -np.inf], np.nan, inplace=True)
+        df.dropna(inplace=True)
+        return df
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
+
+def get_data(api_keys, api_key: str):
+    return asyncio.run(get_data_async(api_keys, api_key))
