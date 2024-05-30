b7c00942367cd4b6f9535d954cdf97110013cc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-30 04:03:37 +0000

    Add package splight-lib/5.7.2/splight_lib-5.7.2.tar.gz

---
+++ packages/splight-lib/splight_lib-5.7.2.tar.gz/splight_lib-5.7.2/splight_lib/models/exceptions.py
@@ -0,0 +1,38 @@
+class MethodNotAllowed(Exception):
+    pass
+
+
+class InvalidObjectInstance(Exception):
+    pass
+
+
+class SecretNotFound(Exception):
+    def __init__(self, name: str):
+        msg = f"Secret {name} not found in database"
+        super().__init__(msg)
+
+
+class SecretDecryptionError(Exception):
+    def __init__(self, name: str):
+        msg = f"Unable to decrypt secret {name}"
+        super().__init__(msg)
+
+
+class InvalidFunctionConfiguration(Exception):
+    pass
+
+
+class MissingFunctionItemExpression(Exception):
+    pass
+
+
+class InvalidAlertConfiguration(Exception):
+    pass
+
+
+class MissingAlertItemExpression(Exception):
+    pass
+
+
+class ForbiddenOperation(Exception):
+    pass
