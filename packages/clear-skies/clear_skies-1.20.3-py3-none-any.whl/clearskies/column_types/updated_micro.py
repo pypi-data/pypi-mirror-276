90139b8d97d0e9958af15c7a3f575b367790fac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:45 +0000

    Add package clear-skies/1.20.3/clear_skies-1.20.3-py3-none-any.whl

---
+++ packages/clear-skies/clear_skies-1.20.3-py3-none-any.whl/clearskies/column_types/updated_micro.py
@@ -0,0 +1,24 @@
+from .datetime_micro import DateTimeMicro
+
+
+class UpdatedMicro(DateTimeMicro):
+    my_configs = [
+        "date_format",
+        "default_date",
+        "utc",
+    ]
+
+    def __init__(self, di, datetime):
+        super().__init__(di)
+        self.datetime = datetime
+
+    @property
+    def is_writeable(self):
+        return False
+
+    def pre_save(self, data, model):
+        if self.config("utc", silent=True):
+            now = self.datetime.datetime.now(self.datetime.timezone.utc)
+        else:
+            now = self.datetime.datetime.now()
+        return {**data, self.name: now}
