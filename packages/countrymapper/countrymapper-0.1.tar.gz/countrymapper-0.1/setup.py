b498945b68a0c7607d98a5eac6bf0164de20717b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:04:16 +0000

    Add package countrymapper/0.1/countrymapper-0.1.tar.gz

---
+++ packages/countrymapper/countrymapper-0.1.tar.gz/countrymapper-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='countrymapper',
+    version='0.1',
+    description='A simple tool to normalise/standardise country names to their official country names or codes',
+    author='Arfat Shaikh',
+    packages=find_packages(),
+)
