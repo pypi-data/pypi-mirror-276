b70bdc0326fc453d3fee8054c8b0f495325a453f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:05:10 +0000

    Add package crowdin-api-client/1.15.2/crowdin_api_client-1.15.2.tar.gz

---
+++ packages/crowdin-api-client/crowdin_api_client-1.15.2.tar.gz/crowdin_api_client-1.15.2/crowdin_api/__init__.py
@@ -0,0 +1,10 @@
+from crowdin_api.client import CrowdinClient
+
+__all__ = ["CrowdinClient"]
+__author__ = "Crowdin"
+__version__ = "1.15.2"
+
+__pdoc__ = {
+    "tests": False,
+    "fixtures": False
+}
