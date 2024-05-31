e980f2e9e1e39be8b48f4362b83a5e3deea6990b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:45 +0000

    Add package pigeon-tem-comms/0.1.0/pigeon_tem_comms-0.1.0.tar.gz

---
+++ packages/pigeon-tem-comms/pigeon_tem_comms-0.1.0.tar.gz/pigeon_tem_comms-0.1.0/TEM_comms/__init__.py
@@ -0,0 +1,37 @@
+from . import buffer
+from . import camera
+from . import scope
+from . import stage
+from . import tile
+
+import importlib.metadata
+
+
+__version__ = importlib.metadata.version("TEM_comms")
+
+topics = {
+    "buffer.status": buffer.Status,
+    "camera.command": camera.Command,
+    "camera.image": camera.Image,
+    "camera.settings": camera.Settings,
+    "camera.status": camera.Status,
+    "scope.command": scope.Command,
+    "scope.status": scope.Status,
+    "stage.aperture.command": stage.aperture.Command,
+    "stage.aperture.status": stage.aperture.Status,
+    "stage.motion.command": stage.motion.Command,
+    "stage.motion.status": stage.motion.Status,
+    "stage.rotation.command": stage.rotation.Command,
+    "stage.rotation.status": stage.rotation.Status,
+    "tile.jpeg": tile.JPEG,
+    "tile.minimap": tile.Minimap,
+    "tile.processed": tile.Processed,
+    "tile.raw": tile.Raw,
+    "tile.statistics.focus": tile.statistics.Focus,
+    "tile.statistics.histogram": tile.statistics.Histogram,
+    "tile.statistics.min_max_mean": tile.statistics.MinMaxMean,
+    "tile.transform": tile.Transform,
+}
+
+
+msgs = (topics, __version__)
---
+++ packages/pigeon-tem-comms/pigeon_tem_comms-0.1.0.tar.gz/pigeon_tem_comms-0.1.0/TEM_comms/stage/aperture.py
@@ -0,0 +1,10 @@
+from emarfarap import BaseMessage
+
+class Command(BaseMessage):
+    aperture_id: int | None = None
+    calibrate: bool = False
+
+class Status(BaseMessage):
+    current_aperture: int
+    calibrated: bool
+    error: str = ""
