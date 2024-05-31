20b5ea3ee6a01e845a03ab39819b20049b360bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:35 +0000

    Add package archerysec-cli/3.1.17/archerysec_cli-3.1.17.tar.gz

---
+++ packages/archerysec-cli/archerysec_cli-3.1.17.tar.gz/archerysec_cli-3.1.17/archerysec_cli/scanners/scanners.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+"""
+======================= START OF LICENSE NOTICE =======================
+  Copyright (C) 2021 ArcherySec. All Rights Reserved
+  NO WARRANTY. THE PRODUCT IS PROVIDED BY DEVELOPER "AS IS" AND ANY
+  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DEVELOPER BE LIABLE FOR
+  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THE PRODUCT, EVEN
+  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+======================== END OF LICENSE NOTICE ========================
+  Primary Author: Anand Tiwari
+"""
+import time
+
+import docker
+
+
+class ScannersRunner(object):
+    def __init__(self, pwd, report_pwd):
+        self.pwd = pwd
+        self.report_pwd = report_pwd
+
+    def bandit_scan(self):
+        print("Scan Running")
+        client = docker.from_env()
+        d = client.containers.run(
+            "archerysec/bandit:latest",
+            volumes={
+                self.pwd: {"bind": "/src"},
+                self.report_pwd: {"bind": "/report"},
+            },
+            detach=True,
+        )
+        c_id = d.id
+        container = client.containers.get(c_id)
+        status = container.status
+        while status == "running":
+            time.sleep(5)
+            container = client.containers.get(c_id)
+            status = container.status
+            # print(status)
+        container.remove()
+        print("Scan Completed")
+
+    def dependency_check_scan(self, data):
+        print("Scan Running")
+        client = docker.from_env()
+        d = client.containers.run(
+            "archerysec/dependency-check:latest",
+            volumes={
+                self.pwd: {"bind": "/src"},
+                self.report_pwd: {"bind": "/report"},
+                data: {"bind": "/usr/share/dependency-check/data"},
+            },
+            command='--scan /src --format "ALL" --project "dependency-check scan: %s" --out /report' % self.pwd,
+            detach=True,
+        )
+        c_id = d.id
+        container = client.containers.get(c_id)
+        status = container.status
+        while status == "running":
+            time.sleep(5)
+            container = client.containers.get(c_id)
+            status = container.status
+            # print(status)
+        container.remove()
+        print("Scan Completed")
+
+    def owasp_zap_baseline_scan(self, target):
+        print("Scan Running")
+        client = docker.from_env()
+        d = client.containers.run(
+            "archerysec/owasp-zap:latest",
+            volumes={
+                self.report_pwd: {"bind": "/zap/wrk"},
+            },
+            command='zap-baseline.py -t %s -x archerysec-owasp-zap-base-line-report.xml' % target,
+            detach=True,
+
+        )
+        c_id = d.id
+        container = client.containers.get(c_id)
+        status = container.status
+        while status == "running":
+            time.sleep(5)
+            container = client.containers.get(c_id)
+            status = container.status
+            # print(status)
+        container.remove()
+        print("Scan Completed")
+
+    def owasp_zap_full_scan(self, target):
+        print("Scan Running")
+        client = docker.from_env()
+        d = client.containers.run(
+            "archerysec/owasp-zap:latest",
+            volumes={
+                self.report_pwd: {"bind": "/zap/wrk"},
+            },
+            command='zap-full-scan.py -t %s -x archerysec-owasp-zap-full-scan-report.xml' % target,
+            detach=True,
+        )
+        c_id = d.id
+        container = client.containers.get(c_id)
+        status = container.status
+        while status == "running":
+            time.sleep(5)
+            container = client.containers.get(c_id)
+            status = container.status
+            # print(status)
+        container.remove()
+        print("Scan Completed")
+
+    def findsecbugs_scan(self):
+        print("Scan Running")
+        client = docker.from_env()
+        d = client.containers.run(
+            "archerysec/findsecbugs:latest",
+            volumes={
+                self.pwd: {"bind": "/src"},
+                self.report_pwd: {"bind": "/report"},
+            },
+            detach=True,
+        )
+        c_id = d.id
+        container = client.containers.get(c_id)
+        status = container.status
+        while status == "running":
+            time.sleep(5)
+            container = client.containers.get(c_id)
+            status = container.status
+            # print(status)
+        container.remove()
+        print("Scan Completed")
---
+++ packages/archerysec-cli/archerysec_cli-3.1.17.tar.gz/archerysec_cli-3.1.17/setup.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__license__ = """
+C#                   _
+#    /\            | |
+#   /  \   _ __ ___| |__   ___ _ __ _   _
+#  / /\ \ | '__/ __| '_ \ / _ \ '__| | | |
+# / ____ \| | | (__| | | |  __/ |  | |_| |
+# /_/    \_\_|  \___|_| |_|\___|_|   \__, |
+#                                    __/ |
+#                                   |___/
+# Copyright (C) 2017-2018 ArcherySec
+# This file is part of ArcherySec Project.
+"""
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+import ast
+import os
+import re
+
+
+from pathlib import Path
+
+def read(rel_path):
+    init = Path(__file__).resolve().parent / rel_path
+    return init.read_text('utf-8', 'ignore')
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            return line.split('\'')[1]
+    raise RuntimeError('Unable to find version string.')
+
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='archerysec_cli',
+    version=get_version('archerysec_cli/__init__.py'),
+    description='A commandline tool that wraps'
+                ' the Archerysec REST API for'
+                ' controlling Archery and executing '
+                'quick, targeted scans.',
+    long_description=long_description,
+    url='https://github.com/archerysec/archerysec-cli.git',
+    author='Anand Tiwari',
+    author_email=' ',
+    license=' ',
+    packages=find_packages(include=[
+        'archerysec_cli', 'archerysec_cli.*',
+    ]),
+    install_requires=[
+        'click',
+        'docker==5.0.0',
+        'idna==2.10',
+        'jmespath==0.10.0',
+        'python-dateutil==2.8.1',
+        'PyYAML',
+        'websocket-client==0.58.0',
+        'wget==3.2',
+    ],
+    extras_require={
+        'dev': [
+            'coverage',
+            'ddt',
+            'mock',
+            'pep8',
+            'pylint',
+            'pytest',
+            'responses',
+        ]
+    },
+
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'archerysec-cli=archerysec_cli.cli.cli:main',
+        ],
+    },
+    test_suite='tests',
+    classifiers=[
+        'Topic :: Security',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+    ],
+)
