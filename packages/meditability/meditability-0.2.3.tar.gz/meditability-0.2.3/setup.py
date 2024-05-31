0e073ddcfe36ccac26a2d9f75184d667d93686a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:05:02 +0000

    Add package meditability/0.2.3/meditability-0.2.3.tar.gz

---
+++ packages/meditability/meditability-0.2.3.tar.gz/meditability-0.2.3/setup.py
@@ -0,0 +1,43 @@
+# == Native Modules
+
+# == Installed Modules
+from setuptools import setup, find_packages
+# == Project Modules
+
+
+setup(
+    name='meditability',
+    version='0.2.3',
+    description='',
+    author='Interventional Genomics Unit',
+    author_email='',
+    entry_points={
+        "console_scripts": [
+            "medit = prog:main",
+            "ncbi_cross_db = prog:cross_db"
+        ]
+    },
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    package_data={"smk.config": ["*.yaml"],
+                  "smk.envs": ["*.yaml"],
+                  "smk.pipelines": ["*.smk"]},
+    include_package_data=True,
+    install_requires=[
+        'snakemake>=7.32.4',
+        'pulp<2.8.0',
+        'wrapt>=1.15.0',
+        'biopython>=1.81',
+        'pyyaml>=6.0',
+        'importlib-resources>=6.1.1',
+        'pytz>=2023.3',
+        'boto3>=1.28.57',
+        'alive-progress>=3.1.5',
+        'xmltodict>=0.13.0'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+    ],
+)
