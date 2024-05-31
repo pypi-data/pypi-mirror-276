cf51c35c768e92a10267b52d617dae48105d7ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:04:53 +0000

    Add package evomip/0.1.5/evomip-0.1.5.tar.gz

---
+++ packages/evomip/evomip-0.1.5.tar.gz/evomip-0.1.5/evomip/Parameter.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+
+@dataclass
+class Parameter:
+    name: str
+    min_val: float
+    max_val: float
+    integer: bool
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def __init__(self, t_name: str = "", t_min: float = 0., t_max: float = 0., t_int:bool = False) -> None:
+        self.name = t_name
+        self.min_val = t_min
+        self.max_val = t_max
+        self.integer = t_int
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def setName(self, t_name: str) -> None:
+        self.name = t_name
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def setRange(self, t_min: float,  t_max: float) -> None:
+        self.min_val = t_min
+        self.max_val = t_max
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def setIsInteger(self, t_int: bool) -> None:
+        self.integer = t_int
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def getName(self) -> str:
+        return self.name
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def getMin(self) -> float:
+        return self.min_val
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def getMax(self) -> float:
+        return self.max_val
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def getWidth(self) -> float:
+        return self.max_val-self.min_val;
+
+    #_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    def getIsInteger(self) -> bool:
+        return self.integer
