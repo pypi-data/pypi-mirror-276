5f1d5d42d977da1d8ae013b5ff55ad7c87b13ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:04:40 +0000

    Add package voxelworldapi/1.0.20240531/voxelworldapi-1.0.20240531.tar.gz

---
+++ packages/voxelworldapi/voxelworldapi-1.0.20240531.tar.gz/voxelworldapi-1.0.20240531/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from io import open
+
+
+
+
+
+ld = open("README1.md",encoding="utf-8").read()
+
+
+setup(
+    name='voxelworldapi',
+    version='1.0.0',
+    packages=['voxelworldapi'],
+    install_requires=['requests'],
+    author='CallFish',
+    author_email='callfish@mail.ru',
+    description='A Python library for interacting with VoxelWorld API',
+    url='https://github.com/callfishxt/voxelworldapi',
+    license='MIT',
+    long_description_content_type='text/markdown',
+    long_description=ld
+)
+
