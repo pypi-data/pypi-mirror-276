9177e5f85c18db5f9b146b2bc21952cf53d4c46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:05:01 +0000

    Add package bpkio-python-sdk/1.16.0/bpkio_python_sdk-1.16.0.tar.gz

---
+++ packages/bpkio-python-sdk/bpkio_python_sdk-1.16.0.tar.gz/bpkio_python_sdk-1.16.0/bpkio_api/helpers/profile/dash.py
@@ -0,0 +1,51 @@
+import logging
+
+from bpkio_api.helpers.handlers.dash import DASHHandler
+from mpd_parser.parser import MPD
+
+from ..codecstrings import CodecStringParser
+
+
+class DashAnalyser:
+    def __init__(self, handler: DASHHandler) -> None:
+        self.handler = handler
+        self.messages = []
+
+    def analyze_renditions(self):
+        dash_obj: MPD = self.handler.document
+
+        if len(dash_obj.periods) > 1:
+            logging.warning(
+                "More than 1 period found. Only the first period will be taken into account"
+            )
+
+        period = dash_obj.periods[0]
+
+        audio_renditions = {}
+        video_renditions = {}
+
+        for adaptation_set in period.adaptation_sets:
+            if adaptation_set.content_type == "audio":
+                for representation in adaptation_set.representations:
+                    codec = CodecStringParser.parse_codec_string(
+                        representation.codecs or adaptation_set.codecs
+                    )
+
+                    codec["bitrate"] = representation.bandwidth
+                    audio_renditions[representation.id] = codec
+
+            if adaptation_set.content_type == "video":
+                for representation in adaptation_set.representations:
+                    codec = CodecStringParser.parse_codec_string(
+                        representation.codecs or adaptation_set.codecs
+                    )
+
+                    codec["resolution"] = (representation.width, representation.height)
+                    codec["bitrate"] = representation.bandwidth
+                    codec["framerate"] = representation.frame_rate
+                    video_renditions[representation.id] = codec
+
+        return [*video_renditions.values(), *audio_renditions.values()]
+
+    def analyze_packaging(self):
+        return {"container": "ISOBMFF"}
