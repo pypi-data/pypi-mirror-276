374275f1e1ce9714775ba010e57b0f05282c2914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:04:40 +0000

    Add package decipher-sdk/0.0.18/decipher-sdk-0.0.18.tar.gz

---
+++ packages/decipher-sdk/decipher-sdk-0.0.18.tar.gz/decipher-sdk-0.0.18/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='decipher-sdk',
+    version='0.0.18',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'Flask',
+        'requests',
+    ],
+    author='Decipher AI, Inc',
+    author_email='michael@getdecipher.com',
+    description='Flask SDK for Decipher AI',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
