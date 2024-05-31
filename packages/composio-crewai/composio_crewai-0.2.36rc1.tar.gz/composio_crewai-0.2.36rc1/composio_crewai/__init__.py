642963ebc641f6a982ea0ea499d9b269b6c4d851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:04:13 +0000

    Add package composio-crewai/0.2.36rc1/composio_crewai-0.2.36rc1.tar.gz

---
+++ packages/composio-crewai/composio_crewai-0.2.36rc1.tar.gz/composio_crewai-0.2.36rc1/composio_crewai/__init__.py
@@ -0,0 +1,11 @@
+from composio_langchain import Action, App, Tag, ComposioSDK, ComposioToolset, client
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "ComposioSDK",
+    "ComposioToolset",
+    "client",
+)
