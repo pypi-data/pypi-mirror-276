0ca895a9be538e75324bc86941634ec2b950e55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:11 +0000

    Add package pyeconomics/0.2.4/pyeconomics-0.2.4.tar.gz

---
+++ packages/pyeconomics/pyeconomics-0.2.4.tar.gz/pyeconomics-0.2.4/tests/conftest.py
@@ -0,0 +1,20 @@
+# test/conftest.py
+
+import pytest
+from unittest.mock import patch, MagicMock
+
+
+@pytest.fixture(autouse=True)
+def mock_keyring():
+    with patch('keyring.get_password') as mock_get_password:
+        mock_get_password.return_value = 'dummy_fred_api_key'
+        yield
+
+
+@pytest.fixture(autouse=True)
+def mock_fred_client():
+    with patch('pyeconomics.api.fred_api.FredClient.__new__') as mock_new:
+        mock_instance = MagicMock()
+        mock_instance.client = MagicMock()
+        mock_new.return_value = mock_instance
+        yield mock_instance
