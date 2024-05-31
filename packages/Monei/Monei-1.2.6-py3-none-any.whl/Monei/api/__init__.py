b25b5bc10bd6d4a075ab41d776b3d0b8d8b0237c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:03:57 +0000

    Add package Monei/1.2.6/Monei-1.2.6-py3-none-any.whl

---
+++ packages/Monei/Monei-1.2.6-py3-none-any.whl/Monei/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from Monei.api.apple_pay_domain_api import ApplePayDomainApi
+from Monei.api.bizum_api import BizumApi
+from Monei.api.payments_api import PaymentsApi
+from Monei.api.subscriptions_api import SubscriptionsApi
