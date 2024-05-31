1a285a4ccf5a1542ff3cd65bb0fc480a3876cd4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:04:51 +0000

    Add package myfunctions-edatop/0.0.2/myfunctions_edatop-0.0.2-py3-none-any.whl

---
+++ packages/myfunctions-edatop/myfunctions_edatop-0.0.2-py3-none-any.whl/myfunctions_edatop/math.py
@@ -0,0 +1,11 @@
+"""
+This is how you use this function
+
+"""
+
+def additions(NUMBER1,NUMBER2):
+    print(NUMBER1 + NUMBER2)
+
+
+def multiplication(NUMBER1,NUMBER2):
+    print(NUMBER1 * NUMBER2)
