e60fe7d98d474f70220c5439a688a1789efe9f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:04:55 +0000

    Add package codedog/0.9.0/codedog-0.9.0-py3-none-any.whl

---
+++ packages/codedog/codedog-0.9.0-py3-none-any.whl/codedog/models/repository.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+from pydantic import BaseModel, Field, validator
+from pydantic.fields import ModelField
+
+
+class Repository(BaseModel):
+    repository_id: int = Field()
+    """Repository id."""
+
+    repository_name: str = Field(default="")
+    """Repository name this pull request belongs to."""
+    repository_full_name: str = Field(default="")
+    """Repository full name this pull request belongs to."""
+    repository_url: str = Field(default="")
+    """Repository url this pull request belongs to."""
+
+    _raw: object = Field(default=None, exclude=True)
+    """git repository raw object"""
+
+    @validator("*", pre=True, allow_reuse=True)
+    def none_to_default(value: Any, field: ModelField):
+        if value is not None or field.type_ not in [str, int, float, bool, list, dict]:
+            return value
+        if field.default:
+            return field.default
+        if field.default_factory:
+            return (field.default_factory)()
+        raise ValueError(f"Field {field.name} is None.")
