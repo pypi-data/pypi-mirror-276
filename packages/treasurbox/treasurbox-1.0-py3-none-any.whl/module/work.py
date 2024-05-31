16630287662dbb4ae7a536a2445d38dc3434e80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:04:32 +0000

    Add package treasurbox/1.0/treasurbox-1.0-py3-none-any.whl

---
+++ packages/treasurbox/treasurbox-1.0-py3-none-any.whl/module/work.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env Python
+# _*_coding:utf-8 _*_
+# @Time: 2024-05-31 14:39:31
+# @Author: Alan
+# @File: work.py
+# @Describe: core code
+
+def hello():
+    print("first make my package...")
