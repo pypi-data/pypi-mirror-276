570fd53ab90951ff5cec996b99041a5af78c0a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:04:34 +0000

    Add package tz-osemosys/0.0.7/tz_osemosys-0.0.7.tar.gz

---
+++ packages/tz-osemosys/tz_osemosys-0.0.7.tar.gz/tz_osemosys-0.0.7/tz/osemosys/model/variables/activity.py
@@ -0,0 +1,40 @@
+import xarray as xr
+from linopy import Model
+from numpy import inf
+
+
+def add_activity_variables(ds: xr.Dataset, m: Model) -> Model:
+    """Add activity variables to the model
+
+    Arguments
+    ---------
+    ds: xarray.Dataset
+        The parameters dataset
+    m: linopy.Model
+        A linopy model
+
+    Returns
+    -------
+    linopy.Model
+    """
+    # Add indices
+    RRTiFY = [
+        ds.coords["REGION"],
+        ds.coords["_REGION"],
+        ds.coords["TIMESLICE"],
+        ds.coords["FUEL"],
+        ds.coords["YEAR"],
+    ]
+    RTeMYTi = [
+        ds.coords["REGION"],
+        ds.coords["TECHNOLOGY"],
+        ds.coords["MODE_OF_OPERATION"],
+        ds.coords["YEAR"],
+        ds.coords["TIMESLICE"],
+    ]
+
+    m.add_variables(lower=0, upper=inf, coords=RTeMYTi, name="RateOfActivity", integer=False)
+
+    m.add_variables(lower=-inf, upper=inf, coords=RRTiFY, name="Trade", integer=False)
+
+    return m
