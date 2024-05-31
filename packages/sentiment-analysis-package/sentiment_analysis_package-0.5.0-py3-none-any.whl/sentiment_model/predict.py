dd6c7f3106c1afaa565f159a9f83c7b863db808c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:05:37 +0000

    Add package sentiment-analysis-package/0.5.0/sentiment_analysis_package-0.5.0-py3-none-any.whl

---
+++ packages/sentiment-analysis-package/sentiment_analysis_package-0.5.0-py3-none-any.whl/sentiment_model/predict.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from joblib import load
+from sentiment_model.config.core import fetch_config_from_yaml
+
+config = fetch_config_from_yaml('config.yml')
+model = load(f"{config['model']['name']}_{config['model']['version']}.pkl")
+
+def predict(texts):
+    predictions = model.predict(texts)
+    return [pred for pred in predictions]
+
+if __name__ == "__main__":
+    sample_texts = ["I love this!", "I hate this...", "I live in London."]
+    preds = predict(sample_texts)
+    print(preds)
