fd80147c45117050f9ffef118fbf4b379790ce28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:56 +0000

    Add package opengamedata-core/0.0.6/opengamedata_core-0.0.6.tar.gz

---
+++ packages/opengamedata-core/opengamedata_core-0.0.6.tar.gz/opengamedata_core-0.0.6/src/ogd/core/__init__.py
@@ -0,0 +1,17 @@
+__all__ = [
+	"models",
+	"utils",
+	"schemas",
+    "generators",
+	"interfaces",
+	"managers",
+	"requests"
+]
+
+from . import models
+from . import utils
+from . import schemas
+from . import generators
+from . import interfaces
+from . import managers
+from . import requests
