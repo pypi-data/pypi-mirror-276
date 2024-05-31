7d1818d763e4bbac9cd8588d6b0f278df5a6f87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:04:59 +0000

    Add package cropimg/0.2.0/cropimg-0.2.0.tar.gz

---
+++ packages/cropimg/cropimg-0.2.0.tar.gz/cropimg-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cropimg',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'pillow',
+        'adb-tool-py>=0.1.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cropimg=crop_image.cropimg:main',
+        ],
+    },
+    author='Shota Iuchi',
+    author_email='shotaiuchi.develop@gmail.com',
+    description='Cropping images',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords='crop, image, adb, screenshot',
+    url='https://github.com/ShotaIuchi/crop-image',
+)
