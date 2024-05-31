ac85a2735560eceec4b6d161ccf0dc3b045ca5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:48 +0000

    Add package nested-pandas/0.1.1/nested_pandas-0.1.1-py3-none-any.whl

---
+++ packages/nested-pandas/nested_pandas-0.1.1-py3-none-any.whl/nested_pandas/__init__.py
@@ -0,0 +1,8 @@
+from .nestedframe import NestedFrame
+from .nestedframe.io import read_parquet
+
+# Import for registering
+from .series.accessor import NestSeriesAccessor  # noqa: F401
+from .series.dtype import NestedDtype
+
+__all__ = ["NestedDtype", "NestedFrame", "read_parquet"]
