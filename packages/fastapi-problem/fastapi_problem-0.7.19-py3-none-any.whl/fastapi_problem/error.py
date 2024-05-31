62e6f060f0069abe53a37c077fedf1bd036d5392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:04:39 +0000

    Add package fastapi-problem/0.7.19/fastapi_problem-0.7.19-py3-none-any.whl

---
+++ packages/fastapi-problem/fastapi_problem-0.7.19-py3-none-any.whl/fastapi_problem/error.py
@@ -0,0 +1,83 @@
+"""Implement RFC9547 compatible exceptions.
+
+https://www.rfc-editor.org/rfc/rfc9457.html
+"""
+
+from __future__ import annotations
+
+import typing as t
+from warnings import warn
+
+from rfc9457 import (
+    BadRequestProblem,
+    ConflictProblem,
+    ForbiddenProblem,
+    NotFoundProblem,
+    Problem,
+    RedirectProblem,
+    ServerProblem,
+    StatusProblem,
+    UnauthorisedProblem,
+    UnprocessableProblem,
+)
+
+
+class HttpException(Problem):
+    def __init__(
+        self: t.Self,
+        title: str,
+        type_: str | None = None,
+        details: str | None = None,
+        status: int = 500,
+        **kwargs,
+    ) -> None:
+        warn(
+            "HttpException use is deprecated, use `Problem` subclasses instead.",
+            FutureWarning,
+            stacklevel=2,
+        )
+        super().__init__(title, type_=type_, details=details, status=status, **kwargs)
+
+
+class HttpCodeException(StatusProblem, HttpException):
+    def __init__(self: t.Self, details: str | None = None, **kwargs) -> None:
+        warn(
+            "HttpCodeException use is deprecated, use `StatusProblem` subclasses instead.",
+            FutureWarning,
+            stacklevel=2,
+        )
+        super().__init__(details=details, **kwargs)
+
+
+class ServerException(HttpCodeException): ...
+
+
+class BadRequestException(HttpCodeException):
+    status = 400
+
+
+class UnauthorisedException(HttpCodeException):
+    status = 401
+
+
+class NotFoundException(HttpCodeException):
+    status = 404
+
+
+__all__ = [
+    "Problem",
+    "StatusProblem",
+    "BadRequestProblem",
+    "ConflictProblem",
+    "ForbiddenProblem",
+    "NotFoundProblem",
+    "RedirectProblem",
+    "ServerProblem",
+    "UnauthorisedProblem",
+    "UnprocessableProblem",
+    "HttpException",
+    "ServerException",
+    "BadRequestException",
+    "UnauthorisedException",
+    "NotFoundException",
+]
