78006c47d9085e1bf855953e4e71b40c75706510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:05:11 +0000

    Add package myfunction-vikmin2022/0.4/myfunction_vikmin2022-0.4.tar.gz

---
+++ packages/myfunction-vikmin2022/myfunction_vikmin2022-0.4.tar.gz/myfunction_vikmin2022-0.4/README.md
@@ -0,0 +1,7 @@
+# Usage
+.........
+# 1. Use function multiplication to do arithmetic tasks with multiplication. Calling the function multiplication specify numbers you want to multiply. Exemple: multiplication(1,2)  
+
+# 2. Use function substruction to do arithmetic tasks with substruction. Calling the function substruction specify numbers you want to substruct. Exemple: substruction(6,3)
+
+# 3. Use function devision to do arithmetic tasks with devisio. Calling the function devision specify numbers you want to devide. Exemple: devision(8,2)
---
+++ packages/myfunction-vikmin2022/myfunction_vikmin2022-0.4.tar.gz/myfunction_vikmin2022-0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunction_vikmin2022',
+    version='0.04',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/vikmin2022',
+    license='MIT',
+    author='Viktoriia Minosian',
+    author_email='viktoriia.minosian@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
