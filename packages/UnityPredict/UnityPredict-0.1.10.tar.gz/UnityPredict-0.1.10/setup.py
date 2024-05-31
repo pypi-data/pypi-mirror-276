593baca04d9bae2fa4b2ecafa476f2b987a0c24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:04:55 +0000

    Add package UnityPredict/0.1.10/UnityPredict-0.1.10.tar.gz

---
+++ packages/UnityPredict/UnityPredict-0.1.10.tar.gz/UnityPredict-0.1.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+description = ""
+with open("README.md", "r") as rdf:
+    description = rdf.read()
+
+print ("Possible packages: {}".format(find_packages()))
+
+setup (
+    name="UnityPredict",
+    version="0.1.10",
+    packages=find_packages(),
+    install_requires=[
+        # Currently no dependencies
+    ],
+
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
