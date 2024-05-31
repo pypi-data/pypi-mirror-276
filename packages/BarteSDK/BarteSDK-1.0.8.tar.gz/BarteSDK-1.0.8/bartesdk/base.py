109c1650c74f257bbf37f82f26a1e9908b91ff49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:34 +0000

    Add package BarteSDK/1.0.8/BarteSDK-1.0.8.tar.gz

---
+++ packages/BarteSDK/BarteSDK-1.0.8.tar.gz/BarteSDK-1.0.8/bartesdk/base.py
@@ -0,0 +1,65 @@
+import requests
+
+class BaseAPI:
+    def __init__(self, api_key, base_url):
+        self.api_key = api_key
+        self.base_url = base_url
+
+    def _get_base_url(self, env, api_version, path):
+        if env == "prd":
+            return f'https://api.barte.com/{api_version}/{path}'
+        elif env == "sandbox":
+            return f'https://sandbox-api.barte.com/{api_version}/{path}'
+        else:
+            raise ValueError("Invalid environment specified")
+
+    def create(self, **kwargs):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+        }
+        payload = kwargs
+        response = requests.post(self.base_url, headers=headers, json=payload)
+        return response.status_code, response.json() if response.ok else response.text
+
+    def get(self, **params):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+        }
+        response = requests.get(self.base_url, headers=headers, params=params)
+        return response.json()
+
+    def getByUuid(self, uuid):
+        headers = {
+            'X-Token-Api': self.api_key
+        }
+        url = f"{self.base_url}/{uuid}"
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+    def update(self, uuid, **kwargs):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'Content-Type': 'application/json',
+            'accept': '*/*'
+        }
+        url = f"{self.base_url}/{uuid}"
+        payload = kwargs
+        response = requests.put(url, headers=headers, json=payload)
+        try:
+            response_data = response.json() if response.ok else response.text
+        except ValueError:
+            response_data = response.text
+        return response.status_code, response_data
+
+    def cancel(self, uuid):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'accept': '*/*'
+        }
+        url = f"{self.base_url}/{uuid}"
+        response = requests.delete(url, headers=headers)
+        return response.status_code, response.json() if response.ok else response.text
