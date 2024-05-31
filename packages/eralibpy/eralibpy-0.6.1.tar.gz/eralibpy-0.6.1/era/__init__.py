f3bbeb3f1714324c88a423026798e640f7901415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:04:31 +0000

    Add package eralibpy/0.6.1/eralibpy-0.6.1.tar.gz

---
+++ packages/eralibpy/eralibpy-0.6.1.tar.gz/eralibpy-0.6.1/era/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from .era import ERa
+except ImportError:
+    from .era_micro import ERa
---
+++ packages/eralibpy/eralibpy-0.6.1.tar.gz/eralibpy-0.6.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="eralibpy",
+    version="0.6.1",
+    description="ERa Python/Micropython library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eoh-jsc/era-lib-py",
+    license="MIT",
+    author="EoH Ltd",
+    author_email="info@eoh.io",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest>=6.2.5", "pytest-mock>=3.6.1"],
+    py_modules=["era.era", "era.era_micro", "era.era_protocol", "era.era_const", "era.era_timer"],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7"
+)
