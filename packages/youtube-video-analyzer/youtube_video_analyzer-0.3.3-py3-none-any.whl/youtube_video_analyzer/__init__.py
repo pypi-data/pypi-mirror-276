d37444bff1b4b0bc818a16031e45f86f9964df67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:04:45 +0000

    Add package youtube-video-analyzer/0.3.3/youtube_video_analyzer-0.3.3-py3-none-any.whl

---
+++ packages/youtube-video-analyzer/youtube_video_analyzer-0.3.3-py3-none-any.whl/youtube_video_analyzer/__init__.py
@@ -0,0 +1,5 @@
+from .video_downloader import VideoDownloader
+from .sound_extract import AudioExtractor
+from .frame_extract import FrameExtractor
+from .sound_intervals import SoundIntervalAnalyzer
+from .face_detection import FaceDetection
