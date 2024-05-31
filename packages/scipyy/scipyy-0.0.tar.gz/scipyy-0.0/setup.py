de7ceeaeb27eb317b0564222993fee464b84e690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:33 +0000

    Add package scipyy/0.0/scipyy-0.0.tar.gz

---
+++ packages/scipyy/scipyy-0.0.tar.gz/scipyy-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='scipyy',
+      version='0.0',
+      description='scipyy',
+      packages=['scipyy'],
+      author_email='ilapronin0700@gmail.com',
+      zip_safe=False)
