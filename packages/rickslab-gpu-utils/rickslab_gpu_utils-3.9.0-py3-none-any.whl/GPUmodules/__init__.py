57cb28a937b65b5837b59344440794fde594da6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:04:00 +0000

    Add package rickslab-gpu-utils/3.9.0/rickslab_gpu_utils-3.9.0-py3-none-any.whl

---
+++ packages/rickslab-gpu-utils/rickslab_gpu_utils-3.9.0-py3-none-any.whl/GPUmodules/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '3.9.0'
+__status__ = 'Development Status :: 5 - Production/Stable'
+#__status__ = 'Development Status :: 4 - Beta'
+__author__ = 'RicksLab'
+__credits__ = ('Craig Echt - Testing, Debug, Verification, and Documentation',
+               'Keith Myers - Testing, Debug, and Verification of NV Capability',
+               '@berturion - Testing and Verification for Arch',
+               '@Marjorie-R - Testing, Debug, and Verification for AMD-APU Capability',
+               'Andrius Merkys - Debian Packaging',
+               'Min Xu - Eliminate Gtk dependency for text gpu-mon option')
+__required_pversion__ = (3, 6)
+__required_kversion__ = (4, 8)
