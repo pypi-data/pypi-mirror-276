7c1ba9f930d09214266efbf13472df70a905608d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:14 +0000

    Add package spotipy/2.24.0/spotipy-2.24.0.tar.gz

---
+++ packages/spotipy/spotipy-2.24.0.tar.gz/spotipy-2.24.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+test_reqs = [
+    'mock==2.0.0'
+]
+
+memcache_cache_reqs = [
+    'pymemcache>=3.5.2'
+]
+
+extra_reqs = {
+    'test': test_reqs,
+    'memcache': memcache_cache_reqs
+}
+
+setup(
+    name='spotipy',
+    version='2.24.0',
+    description='A light weight Python library for the Spotify Web API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="@plamere",
+    author_email="paul@echonest.com",
+    url='https://spotipy.readthedocs.org/',
+    project_urls={
+        'Source': 'https://github.com/plamere/spotipy',
+    },
+    python_requires='>3.8',
+    install_requires=[
+        "redis>=3.5.3",  # TODO: Move to extras_require in v3
+        "requests>=2.25.0",
+        "urllib3>=1.26.0"
+    ],
+    tests_require=test_reqs,
+    extras_require=extra_reqs,
+    license='MIT',
+    packages=['spotipy'])
