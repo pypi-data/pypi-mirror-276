8c181ef08ef75d56316cce052d81683a0c353811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:17 +0000

    Add package birrdawn/0.1/birrdawn-0.1.tar.gz

---
+++ packages/birrdawn/birrdawn-0.1.tar.gz/birrdawn-0.1/setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="birrdawn",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    author="Abuabdirrahman",
+    author_email="abdulbasitdamililo6@gmail.com",
+    description="A simple example package",
+    url="https://github.com/marrwan",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
