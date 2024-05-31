d7c4ead5a97b7a3311b50171e997ffe722b65bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:17 +0000

    Add package onyx-extension/0.4.3/onyx_extension-0.4.3.tar.gz

---
+++ packages/onyx-extension/onyx_extension-0.4.3.tar.gz/onyx_extension-0.4.3/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ReactWidget } from '@jupyterlab/apputils';
+//import App from '../../react-prototype/src/App'
+import Onyx from 'climb-onyx-ui';
+
+export class ReactAppWidget extends ReactWidget {
+  constructor(dom: string, tok: string) {
+    super();
+    this.domain = dom;
+    this.token = tok;
+  }
+
+  domain: string;
+  token: string;
+
+  render(): JSX.Element {
+    return <Onyx domain={this.domain} token={this.token} />;
+  }
+}
