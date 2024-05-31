5ef9ca540d365e4d478c5bd9ce6ff849ffe76888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:04:28 +0000

    Add package yfunc/1.0.0/yfunc-1.0.0.tar.gz

---
+++ packages/yfunc/yfunc-1.0.0.tar.gz/yfunc-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yfunc',
+    version='1.0.0',
+    description='a collection of some productive functions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='yzjsswk',
+    author_email='yzjsswk@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+    install_requires=[],
+)
