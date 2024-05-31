84400051a4a88db60c13292cc58ee7f6a15a1247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:04:44 +0000

    Add package exobject/1.0/exobject-1.0.tar.gz

---
+++ packages/exobject/exobject-1.0.tar.gz/exobject-1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="exobject",
+    version=1.0,
+    description=("Extend Object Code"),
+    long_description=open("README.rst").read(),
+    author="LenShang",
+    author_email="lenshang@qq.com",
+    maintainer="LenShang",
+    maintainer_email="lenshang@qq.com",
+    license="BSD License",
+    packages=find_packages(),
+    platforms=["all"],
+    url="",
+    install_requires=["python-dateutil", "parsel"],
+)
