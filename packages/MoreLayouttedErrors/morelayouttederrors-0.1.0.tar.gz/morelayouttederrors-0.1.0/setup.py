b2e34983515ea927041bca0051ff9b166f694eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:04:00 +0000

    Add package MoreLayouttedErrors/0.1.0/morelayouttederrors-0.1.0.tar.gz

---
+++ packages/MoreLayouttedErrors/morelayouttederrors-0.1.0.tar.gz/morelayouttederrors-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="MoreLayouttedErrors",  # Replace with your package name
+    version="0.1.0",  # Replace with your package version
+    author="Sion",  # Replace with your name
+    author_email="the.rckr@gmail.com",  # Replace with your email
+    description="A module to make errors; and colors!",
+    packages=find_packages(),  # Automatically find packages in the directory
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Replace with your license
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Specify the Python versions you support
+)
