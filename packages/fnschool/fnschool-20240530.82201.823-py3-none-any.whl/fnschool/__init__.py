f0042c7a6a4e8a5807b2fdd48283c1f9d3cd5c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:03:50 +0000

    Add package fnschool/20240530.82201.823/fnschool-20240530.82201.823-py3-none-any.whl

---
+++ packages/fnschool/fnschool-20240530.82201.823-py3-none-any.whl/fnschool/__init__.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import argparse
+import random
+from pathlib import Path
+import tomllib
+
+import pandas as pd
+import numpy as np
+from openpyxl import load_workbook
+from openpyxl.styles import numbers
+from openpyxl.styles import Font
+from fnschool.app import *
+from fnschool.language import _
+from fnschool.fnprint import *
+from fnschool.path import *
+from fnschool.entry import *
+from fnschool.external import *
+from fnschool.user import *
+from fnschool.config import *
+
+
+__version__ = "20240530.82201.823"
+
+
+# The end.
---
+++ packages/fnschool/fnschool-20240530.82201.823-py3-none-any.whl/fnschool/exam/teacher.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from fnschool import *
+from fnschool.exam import *
+
+
+class Teacher(User):
+    def __init__(
+        self,
+    ):
+        super.__init__(user_exam_dpath, teach_name_fpath)
+        self._name = None
+        self._dpath = None
+        pass
