ce1b538604934b6834d94e4c6981a79e0d9fc39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:47 +0000

    Add package langroid/0.1.253/langroid-0.1.253.tar.gz

---
+++ packages/langroid/langroid-0.1.253.tar.gz/langroid-0.1.253/langroid/agent/special/sql/__init__.py
@@ -0,0 +1,10 @@
+from . import sql_chat_agent, utils
+from .sql_chat_agent import SQLChatAgentConfig, SQLChatAgent
+
+
+__all__ = [
+    "SQLChatAgentConfig",
+    "SQLChatAgent",
+    "sql_chat_agent",
+    "utils",
+]
