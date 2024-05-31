ac10b9147c2826a0bb92365666f612e721ec4e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:25 +0000

    Add package sunholo/0.60.2/sunholo-0.60.2.tar.gz

---
+++ packages/sunholo/sunholo-0.60.2.tar.gz/sunholo-0.60.2/sunholo/cli/cli.py
@@ -0,0 +1,60 @@
+import argparse
+import logging
+
+from .configs import setup_list_configs_subparser
+from .deploy import setup_deploy_subparser
+from .cli_init import setup_init_subparser
+from .merge_texts import setup_merge_text_subparser
+from .run_proxy import setup_proxy_subparser
+from .chat_vac import setup_vac_subparser
+
+from ..logging import log
+
+def main(args=None):
+    """
+    Entry point for the sunholo console script. This function parses command line arguments
+    and invokes the appropriate functionality based on the user input.
+
+    Example commands:
+    ```bash
+    sunholo deploy --config_path . --gcs_bucket your-gcs-bucket --lancedb_bucket your-lancedb-bucket
+    ```
+    """
+    parser = argparse.ArgumentParser(description="sunholo CLI tool for deploying GenAI VACs")
+    parser.add_argument('--debug', action='store_true', help='Enable debug output')
+
+    subparsers = parser.add_subparsers(title='commands', 
+                                       description='Valid commands', 
+                                       help='Commands', 
+                                       dest='command', 
+                                       required=True)
+
+    # deploy command
+    setup_deploy_subparser(subparsers)
+    # Setup list-configs command
+    setup_list_configs_subparser(subparsers)
+    # init command
+    setup_init_subparser(subparsers)
+    # merge-text command
+    setup_merge_text_subparser(subparsers)
+    # proxy command
+    setup_proxy_subparser(subparsers)
+    # vac command
+    setup_vac_subparser(subparsers)
+
+    args = parser.parse_args(args)
+
+    if args.debug:
+        log.setLevel(logging.INFO)
+        logging.getLogger().setLevel(logging.INFO)
+    else:
+        log.setLevel(logging.WARNING)
+        logging.getLogger().setLevel(logging.WARNING)
+
+    if hasattr(args, 'func'):
+        args.func(args)
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
