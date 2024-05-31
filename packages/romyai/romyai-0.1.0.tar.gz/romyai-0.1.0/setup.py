8494c6ebb42a393a171348c1daa18c45c3976614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:50 +0000

    Add package romyai/0.1.0/romyai-0.1.0.tar.gz

---
+++ packages/romyai/romyai-0.1.0.tar.gz/romyai-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="romyai",
+    version="0.1.0",
+    author="Md Rehan",
+    author_email="mdrehan4all@gmail.com",
+    description="A simple example package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mdrehan4all/romyai",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
