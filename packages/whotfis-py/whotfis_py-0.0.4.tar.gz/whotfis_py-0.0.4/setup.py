120812835827e5894aeb4e5a8a7b796b3f4808d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:26 +0000

    Add package whotfis-py/0.0.4/whotfis_py-0.0.4.tar.gz

---
+++ packages/whotfis-py/whotfis_py-0.0.4.tar.gz/whotfis_py-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = "Simple package to interact with the whois command line tool"
+
+setup(
+    name="whotfis-py",
+    version=VERSION,
+    author="Valerio Pio De Nicola",
+    author_email="valeriopio02@gmail.com",
+    description=DESCRIPTION,
+    long_description=open('whotfis-py/README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'whois', 'whois command line', 'whois python', 'whois python package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
