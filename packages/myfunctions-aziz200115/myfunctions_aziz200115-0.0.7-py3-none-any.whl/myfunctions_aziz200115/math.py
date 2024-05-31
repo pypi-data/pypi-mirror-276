bda87269e34230e36e553f2556ebe100f4c28d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:29 +0000

    Add package myfunctions-aziz200115/0.0.7/myfunctions_aziz200115-0.0.7-py3-none-any.whl

---
+++ packages/myfunctions-aziz200115/myfunctions_aziz200115-0.0.7-py3-none-any.whl/myfunctions_aziz200115/math.py
@@ -0,0 +1,35 @@
+"""
+    This is how you use this function
+"""
+
+import os
+
+
+def addition(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    e.g. addition(2,3)
+    """
+    print(NUMBER1 + NUMBER2) 
+
+
+def multiplication(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    e.g. multiplication(2,3)
+    """
+    print(NUMBER1 * NUMBER2) 
+
+
+def division(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    e.g. division(2,3)
+    """
+    print(NUMBER1 / NUMBER2) 
+
+
+def filechecker(FILENAME):
+    FILENAME = os.path.isfile(FILENAME)
+    print("File exists")
+
