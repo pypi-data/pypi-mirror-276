4bffe89b33fd632df3356388080dac745683457e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:04:13 +0000

    Add package composio-langchain/0.2.36rc1/composio_langchain-0.2.36rc1-py3-none-any.whl

---
+++ packages/composio-langchain/composio_langchain-0.2.36rc1-py3-none-any.whl/composio_langchain/__init__.py
@@ -0,0 +1,13 @@
+from composio import Action, App, Tag
+
+from .langchain_toolspec import ComposioSDK, ComposioToolset, client
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "ComposioSDK",
+    "ComposioToolset",
+    "client",
+)
