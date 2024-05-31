127ddfdd621a63ae48304684513d58f91e3e89bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:47 +0000

    Add package bcqa/1.0.4/bcqa-1.0.4-py3-none-any.whl

---
+++ packages/bcqa/bcqa-1.0.4-py3-none-any.whl/bcqa/retriever/ColBERT/colbert/modeling/tokenization/__init__.py
@@ -0,0 +1,3 @@
+from retriever.ColBERT.colbert.modeling.tokenization.query_tokenization import *
+from retriever.ColBERT.colbert.modeling.tokenization.doc_tokenization import *
+from retriever.ColBERT.colbert.modeling.tokenization.utils import tensorize_triples
