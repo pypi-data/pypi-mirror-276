88596fd2880cdadf91eb988b5d2b30f5a702117d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:05:02 +0000

    Add package impactlab-tools/0.6.0/impactlab_tools-0.6.0-py3-none-any.whl

---
+++ packages/impactlab-tools/impactlab_tools-0.6.0-py3-none-any.whl/impactlab_tools/__init__.py
@@ -0,0 +1,13 @@
+'''
+'''
+
+
+from impactlab_tools._version import __version__  # noqa: F401
+
+
+__author__ = """Climate Impact Lab"""
+
+_module_imports = (
+)
+
+__all__ = list(map(lambda x: x.__name__, _module_imports))
