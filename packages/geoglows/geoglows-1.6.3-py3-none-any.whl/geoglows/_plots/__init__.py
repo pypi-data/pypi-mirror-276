1b4b4c9913db0615923e653d59751a72ab83c57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:59 +0000

    Add package geoglows/1.6.3/geoglows-1.6.3-py3-none-any.whl

---
+++ packages/geoglows/geoglows-1.6.3-py3-none-any.whl/geoglows/_plots/__init__.py
@@ -0,0 +1,31 @@
+from .plots import (
+    forecast,
+    forecast_stats,
+    forecast_ensembles,
+    forecast_records,
+    retrospective,
+    daily_averages,
+    monthly_averages,
+    annual_averages,
+    flow_duration_curve,
+    corrected_retrospective,
+    corrected_month_average,
+    corrected_day_average,
+    corrected_scatterplots,
+)
+
+__all__ = [
+    'forecast',
+    'forecast_stats',
+    'forecast_ensembles',
+    'forecast_records',
+    'retrospective',
+    'daily_averages',
+    'monthly_averages',
+    'annual_averages',
+    'flow_duration_curve',
+    'corrected_retrospective',
+    'corrected_month_average',
+    'corrected_day_average',
+    'corrected_scatterplots',
+]
