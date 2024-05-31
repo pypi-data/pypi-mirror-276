e71d2c616eaf82abd223234d9517c86f0367e520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:04:37 +0000

    Add package nodeorc/0.1.7/nodeorc-0.1.7.tar.gz

---
+++ packages/nodeorc/nodeorc-0.1.7.tar.gz/nodeorc-0.1.7/nodeorc/__init__.py
@@ -0,0 +1,17 @@
+"""NodeORC: Automated edge and cloud image-based discharge estimation with OpenRiverCam"""
+import os
+__version__ = "0.1.7"
+
+__home__ = os.path.join(os.path.expanduser("~"), ".nodeorc")
+if not(os.path.isdir(__home__)):
+    os.makedirs(__home__)
+
+settings_path = os.path.join(os.getcwd(), "settings")
+
+from . import tasks
+from . import log
+from . import models
+from . import callbacks
+from . import disk_mng
+from . import db
+from . import config
