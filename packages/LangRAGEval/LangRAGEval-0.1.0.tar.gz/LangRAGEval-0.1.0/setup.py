961a3a872add81a98ae13b935a1fe6d6a55ffefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:04:33 +0000

    Add package LangRAGEval/0.1.0/LangRAGEval-0.1.0.tar.gz

---
+++ packages/LangRAGEval/LangRAGEval-0.1.0.tar.gz/LangRAGEval-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LangRAGEval",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "sacrebleu",
+        "rouge-score",
+        "bert-score",
+        "transformers",
+        "nltk",
+        "langchain",
+        "pydantic",
+    ],
+    entry_points={
+        'console_scripts': [
+            'langrageval=langrageval:main',
+        ],
+    },
+    author="Prashant Verma",
+    author_email="prashant27050@gmail.com",
+    description="LangRAGEval is a library for evaluating responses based on faithfulness, context recall, answer relevancy, and context relevancy.",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Vprashant/LangGPTEval",
+)
+
