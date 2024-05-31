4480bc67c0a66aff85be5c6b5aab5c175543d35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:43 +0000

    Add package polars-xdt/0.14.13/polars_xdt-0.14.13.tar.gz

---
+++ packages/polars-xdt/polars_xdt-0.14.13.tar.gz/polars_xdt-0.14.13/tests/test_ewma_by_time.py
@@ -0,0 +1,49 @@
+import polars as pl
+from polars.testing import assert_frame_equal
+import polars_xdt as xdt
+import pytest
+from datetime import datetime, timedelta
+
+import os
+
+os.environ["POLARS_VERBOSE"] = "1"
+
+@pytest.mark.parametrize("start_null", [True, False])
+def test_ewma_by_time(start_null):
+    if start_null:
+        values = [None]
+        times = [datetime(2020, 1, 1)]
+    else:
+        values = []
+        times = []
+        
+    df = pl.DataFrame(
+        {
+            "values": values + [0.0, 1., 2., None, 4.],
+            "times": times + [
+                datetime(2020, 1, 2),
+                datetime(2020, 1, 4),
+                datetime(2020, 1, 11),
+                datetime(2020, 1, 16),
+                datetime(2020, 1, 18),
+            ],
+        }
+    )
+    with pytest.deprecated_call():
+        result = df.select(
+            xdt.ewma_by_time("values", times="times", half_life=timedelta(days=2)),
+        )
+        
+    expected = pl.DataFrame(
+        {
+            "values": values + [
+                0.0,
+                0.5,
+                1.8674174785275222,
+                None,
+                3.811504554703363,
+            ]
+        }
+    )
+
+    assert_frame_equal(result, expected)
