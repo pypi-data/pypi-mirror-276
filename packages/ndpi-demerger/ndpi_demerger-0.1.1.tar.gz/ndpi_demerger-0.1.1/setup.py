fd86c5d0647a7c1ed4c2e65f5871019e3d5afd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:06 +0000

    Add package ndpi-demerger/0.1.1/ndpi_demerger-0.1.1.tar.gz

---
+++ packages/ndpi-demerger/ndpi_demerger-0.1.1.tar.gz/ndpi_demerger-0.1.1/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ndpi_demerger',
+    version='0.1.1',
+    description='A package for extracting patches from NDPI images',
+    author='Thirteen',
+    author_email='506607814@qq.com',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'openslide-python',
+        'Pillow',
+        'numpy',
+        'scikit-image'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+
