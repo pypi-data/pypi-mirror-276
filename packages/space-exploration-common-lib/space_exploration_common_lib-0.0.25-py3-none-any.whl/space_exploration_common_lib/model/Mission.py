84ff0928bff7fe02c9bf096f667754863bb79085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:04:25 +0000

    Add package space-exploration-common-lib/0.0.25/space_exploration_common_lib-0.0.25-py3-none-any.whl

---
+++ packages/space-exploration-common-lib/space_exploration_common_lib-0.0.25-py3-none-any.whl/space_exploration_common_lib/model/Mission.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import List
+from decimal import Decimal
+from bson import Decimal128
+from ..model.Duration import Duration
+
+
+class Mission(BaseModel):
+    mission_name: str
+    astronauts: List[str]
+    programs: str
+    call_sign: str
+    image_url: str
+    spacecraft_number: int
+    launch_time: str
+    launch_site: str
+    duration: Duration
+    orbits: int
+    apogee_mi: int
+    perigee_mi: int
+    velocity_max_mph: int
+    miss_mi: Decimal
+
+    def __init__(self, **data):
+        if 'miss_mi' in data and isinstance(data['miss_mi'], Decimal128):
+            data['miss_mi'] = Decimal(str(data['miss_mi']))
+        super().__init__(**data)
