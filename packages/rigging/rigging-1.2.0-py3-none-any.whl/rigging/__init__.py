fcba39e7d4a1b6af76f703107f2a183404c1dda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:15 +0000

    Add package rigging/1.2.0/rigging-1.2.0-py3-none-any.whl

---
+++ packages/rigging/rigging-1.2.0-py3-none-any.whl/rigging/__init__.py
@@ -0,0 +1,48 @@
+from rigging.chat import Chat, PendingChat
+from rigging.completion import Completion, PendingCompletion
+from rigging.data import chats_to_df, df_to_chats
+from rigging.generator import (
+    GeneratedMessage,
+    GeneratedText,
+    GenerateParams,
+    Generator,
+    chat,
+    complete,
+    get_generator,
+    register_generator,
+)
+from rigging.message import Message, MessageDict, Messages
+from rigging.model import Model, attr, element, make_primitive, wrapped
+from rigging.tool import Tool
+
+__version__ = "1.1.1"
+
+__all__ = [
+    "get_generator",
+    "Message",
+    "MessageDict",
+    "Messages",
+    "Tool",
+    "Model",
+    "attr",
+    "element",
+    "wrapped",
+    "Chat",
+    "PendingChat",
+    "Generator",
+    "GenerateParams",
+    "GeneratedMessage",
+    "GeneratedText",
+    "chat",
+    "complete",
+    "Completion",
+    "PendingCompletion",
+    "register_generator",
+    "chats_to_df",
+    "df_to_chats",
+    "make_primitive",
+]
+
+from loguru import logger
+
+logger.disable("rigging")
