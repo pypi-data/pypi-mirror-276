b124b0ec7737521d546cc8121411d81ad4d7107b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:03:58 +0000

    Add package rrelu/0.0.5/rrelu-0.0.5-py3-none-any.whl

---
+++ packages/rrelu/rrelu-0.0.5-py3-none-any.whl/rrelu/search_bound/fitact.py
@@ -0,0 +1,324 @@
+import sys
+import torch.nn as nn
+import torch
+import copy
+import numpy as np
+import warnings
+import sys; 
+import setup 
+from rrelu.relu_bound.bound_fitact import bounded_relu_fitact
+import os
+import argparse
+from rrelu.utils.metric import accuracy,AverageMeter
+from rrelu.utils.lr_scheduler import CosineLRwithWarmup
+from rrelu.utils.distributed import DistributedMetric
+from rrelu.search_bound.ranger import Ranger_bounds
+import random
+import time
+from tqdm import tqdm
+from torchpack import distributed as dist
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", type=str, metavar="DIR", help="run directory",default="./pretrained_models/teachers/vgg16_bound_layer_relu_float/checkpoint")
+parser.add_argument("--base_batch_size", type=int, default=128)
+parser.add_argument("--manual_seed", type=int, default=0)
+parser.add_argument(
+    "--gpu", type=str, default=None
+)  # used in single machine experiments
+
+
+def eval(model: nn.Module, data_loader_dict) :
+
+    test_criterion = nn.CrossEntropyLoss().cuda()
+
+    val_loss = DistributedMetric()
+    val_top1 = DistributedMetric()
+    val_top5 = DistributedMetric()
+
+    model.eval()
+    with torch.no_grad():
+        with tqdm(
+            total=len(data_loader_dict["val"]),
+            desc="Eval",
+            disable=not dist.is_master(),
+        ) as t:
+            for images, labels in data_loader_dict["val"]:
+                images, labels = images.cuda(), labels.cuda()
+                # compute output
+                output = model(images)
+                loss = test_criterion(output, labels)
+                val_loss.update(loss, images.shape[0])
+                acc1, acc5 = accuracy(output, labels, topk=(1, 5))
+                val_top5.update(acc5[0], images.shape[0])
+                val_top1.update(acc1[0], images.shape[0])
+
+                t.set_postfix(
+                    {
+                        "loss": val_loss.avg.item(),
+                        "top1": val_top1.avg.item(),
+                        "top5": val_top5.avg.item(),
+                        "#samples": val_top1.count.item(),
+                        "batch_size": images.shape[0],
+                        "img_size": images.shape[2],
+                    }
+                )
+                t.update()
+
+    val_results = {
+        "val_top1": val_top1.avg.item(),
+        "val_top5": val_top5.avg.item(),
+        "val_loss": val_loss.avg.item(),
+    }
+    return val_results
+
+
+activation={}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output
+    return hook
+
+def relu_hooks(model:nn.Module,name=''):
+    for name1,layer in model.named_children():
+        if list(layer.children()) == []:
+            if isinstance(layer,nn.ReLU):
+                name_ = name1 + name
+                layer.register_forward_hook(get_activation(name_)) 
+        else:
+            name+=name1
+            relu_hooks(layer,name)
+
+def replace_act(model:nn.Module,bounds,tresh,name='')->nn.Module:
+    for name1,layer in model.named_children():
+        if list(layer.children()) == []:
+            if isinstance(layer,nn.ReLU) and 'last' not in name1:
+                name_ = name1 + name
+                model._modules[name1] = bounded_relu_fitact(bounds[name_].detach(),tresh[name_],-20)
+            elif  isinstance(layer,nn.ReLU) and 'last' in name1:
+                print("last relu layer")
+                name_ = name1 + name
+                model._modules[name1] = bounded_relu_fitact(bounds[name_].detach(),tresh[name_],-20)
+        else:
+            name+=name1
+            replace_act(layer,bounds,tresh,name)               
+    return model  
+  
+
+
+
+
+def fitact_bounds(model:nn.Module,train_loader, device="cuda", bound_type='layer',bitflip='float'):
+    model.eval()
+    results,tresh,_ = Ranger_bounds(copy.deepcopy(model),train_loader,device,bound_type,bitflip)
+    # print(results['relu1'])
+    model = replace_act(model,results,tresh)
+    # print(model)
+    warnings.filterwarnings("ignore")
+    args, opt = parser.parse_known_args()
+    if args.gpu is not None:
+        os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    if not torch.distributed.is_initialized():
+        dist.init()
+    torch.backends.cudnn.benchmark = True
+    torch.cuda.set_device(dist.local_rank())
+    os.makedirs(args.path, exist_ok=True)
+    torch.manual_seed(args.manual_seed)
+    torch.cuda.manual_seed_all(args.manual_seed)
+    random.seed(args.manual_seed)
+    np.random.seed(args.manual_seed)
+    for name, param in model.named_parameters():
+        if np.any([key in name for key in ["weight", "norm","bias"]]):
+            param.requires_grad=False
+        else:
+            param.requires_grad=True
+    model = nn.parallel.DistributedDataParallel(
+        model.cuda(), device_ids=[dist.local_rank()]
+    )
+    for name, param in model.named_parameters():
+        if np.any([key in name for key in ["weight", "norm","bias"]]):
+            param.requires_grad=False
+        else:
+            param.requires_grad=True
+
+    model = train(model, train_loader, args.path)
+    # print(eval(model,train_loader))
+    bounds_dict = {}
+    keys=[]
+    i=0
+   
+    for key,val in results.items():
+        keys.append(key)
+    # print(keys)    
+    for name, param in model.module.named_parameters():
+        if param.requires_grad:
+            if np.any([key in name for key in ["bounds_param"]]):
+                # print(param)
+                bounds_dict[keys[i]]=param
+                i+=1
+    return bounds_dict,bounds_dict,None
+
+def train(
+    model: nn.Module,
+    data_provider,
+    path: str,
+    base_lr=0.001,
+    warmup_epochs = 0 ,
+    n_epochs = 150,
+    weight_decay = 4e-9
+
+):
+    
+    params_without_wd = []
+    params_with_wd = []
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+
+            if np.any([key in name for key in ["bias", "norm"]]):
+                params_without_wd.append(param)
+            else:
+                # print(name)
+                params_with_wd.append(param)
+    net_params = [
+        {"params": params_without_wd, "weight_decay": 0},
+        {
+            "params": params_with_wd,
+            "weight_decay": weight_decay,
+        },
+    ]
+    # build optimizer
+    optimizer = torch.optim.Adam(
+        net_params,
+        lr=base_lr * dist.size(),
+    )
+    # build lr scheduler
+    lr_scheduler = CosineLRwithWarmup(
+        optimizer,
+        warmup_epochs * len(data_provider['train']),
+        base_lr,
+        n_epochs * len(data_provider['train']),
+    )
+    # train criterion
+    train_criterion = nn.CrossEntropyLoss()
+    # init
+    best_val = 0.0
+    start_epoch = 0
+    checkpoint_path = os.path.join(path, "checkpoint")
+    log_path = os.path.join(path, "logs")
+    os.makedirs(checkpoint_path, exist_ok=True)
+    os.makedirs(log_path, exist_ok=True)
+    logs_writer = open(os.path.join(log_path, "exp.log"), "a")
+
+
+    # start training
+    for epoch in range(
+        start_epoch,
+        n_epochs
+        + warmup_epochs,
+    ):
+        train_info_dict = train_one_epoch(
+            model,
+            data_provider,
+            epoch,
+            optimizer,
+            train_criterion,
+            lr_scheduler,
+        )
+    
+        val_info_dict = eval(model, data_provider)
+        is_best = val_info_dict["val_top1"] > best_val
+        best_val = max(best_val, val_info_dict["val_top1"])
+        # log
+        epoch_log = f"[{epoch + 1 - warmup_epochs}/{n_epochs}]"
+        epoch_log += f"\tval_top1={val_info_dict['val_top1']:.2f} ({best_val:.2f})"
+        epoch_log += f"\ttrain_top1={train_info_dict['train_top1']:.2f}\tlr={optimizer.param_groups[0]['lr']:.2E}"
+        if dist.is_master():
+            logs_writer.write(epoch_log + "\n")
+            logs_writer.flush()
+        # save checkpoint
+        checkpoint = {
+            "state_dict": model.module.state_dict(),
+            "epoch": epoch,
+            "best_val": best_val,
+            "optimizer": optimizer.state_dict(),
+            "lr_scheduler": lr_scheduler.state_dict(),
+        }
+        if dist.is_master():
+            torch.save(
+                checkpoint,
+                os.path.join(checkpoint_path, "checkpoint.pt"),
+                _use_new_zipfile_serialization=False,
+            )
+            if is_best:
+                torch.save(
+                    checkpoint,
+                    os.path.join(checkpoint_path, "best.pt"),
+                    _use_new_zipfile_serialization=False,
+                )
+    
+         
+    return model
+
+
+
+
+def train_one_epoch(
+    model: nn.Module,
+    data_provider,
+    epoch: int,
+    optimizer,
+    criterion,
+    lr_scheduler,
+
+):
+    train_loss = DistributedMetric()
+    train_top1 = DistributedMetric()
+
+    model.train()
+    data_provider['train'].sampler.set_epoch(epoch)
+
+    data_time = AverageMeter()
+    with tqdm(
+        total=len(data_provider["train"]),
+        desc="Train Epoch #{}".format(epoch + 1),
+        disable=not dist.is_master(),
+    ) as t:
+        end = time.time()
+        for _, (images, labels) in enumerate(data_provider['train']):
+            data_time.update(time.time() - end)
+            images, labels = images.cuda(), labels.cuda()
+            l2_bounds = 0.0
+            for name, param in model.named_parameters():
+                if param.requires_grad==True:
+                    if "bounds" in name:
+                        l2_bounds += torch.mean(torch.pow(param, 2))
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, labels) + 4e-9 * l2_bounds
+            loss.backward()
+            # for par in model.parameters():
+            #     print(par.grad)
+            top1 = accuracy(output, labels, topk=(1,))[0][0]
+            optimizer.step()
+            lr_scheduler.step()
+
+            train_loss.update(loss, images.shape[0])
+            train_top1.update(top1, images.shape[0])
+
+            t.set_postfix(
+                {
+                    "loss": train_loss.avg.item(),
+                    "top1": train_top1.avg.item(),
+                    "batch_size": images.shape[0],
+                    "img_size": images.shape[2],
+                    "lr": optimizer.param_groups[0]["lr"],
+                    "data_time": data_time.avg,
+                }
+            )
+            t.update()
+
+            end = time.time()
+    return {
+        "train_top1": train_top1.avg.item(),
+        "train_loss": train_loss.avg.item(),
+    }
+
+
---
+++ packages/rrelu/rrelu-0.0.5-py3-none-any.whl/rrelu/setup.py
@@ -0,0 +1,349 @@
+import copy
+import math
+import os.path
+from typing import Dict, Optional, Tuple, Type
+
+import torch
+import torch.nn as nn
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from rrelu.models.vgg import make_layers,cfg
+from rrelu.models import (
+   Lenet,
+   VGG,
+   AlexNet_model,
+   ResNet50,
+   VGG16,
+   LeNet_cifar,
+   AlexNet_cifar100,
+   VGG16_cifar100,
+   ResNet50_cifar100,
+)
+from rrelu.relu_bound import (
+    bounded_relu_zero,
+    bounded_relu_tresh,
+    bounded_relu_fitact,
+    bounded_hyrelu_proact
+
+)
+
+from rrelu.search_bound import (
+    FtClipAct_bounds,
+    Ranger_bounds,
+    fitact_bounds,
+    proact_bounds
+
+)
+__all__ = ["build_data_loader", "build_model", "replace_act","find_bounds"]
+
+
+def build_data_loader(
+    dataset: str,
+    image_size: int,
+    batch_size: int,
+    n_worker: int = 8,
+    data_path: Optional[str] = None,
+    num_replica: Optional[int] = None,
+    rank: Optional[int] = None,
+) -> Tuple[Dict, int]:
+    # build dataset
+    dataset_info_dict = {
+        "imagenet21k_winter_p": (
+            os.path.expanduser("./dataset/imagenet21k_winter_p"),
+            10450,
+        ),
+        "imagenet": (os.path.expanduser("./dataset/imagenet"), 1000),
+        "car": (os.path.expanduser("./dataset/fgvc/stanford_car"), 196),
+        "flowers102": (os.path.expanduser("./dataset/fgvc/flowers102"), 102),
+        "food101": (os.path.expanduser("./dataset/fgvc/food101"), 101),
+        "cub200": (os.path.expanduser("./dataset/fgvc/cub200"), 200),
+        "pets": (os.path.expanduser("./dataset/fgvc/pets"), 37),
+        "cifar10": (os.path.expanduser("./dataset/cifar10"), 10),
+         "cifar100": (os.path.expanduser("./dataset/cifar100"), 100),
+         "mnist":(os.path.expanduser("./dataset/mnist"), 10)
+    }
+    assert dataset in dataset_info_dict, f"Do not support {dataset}"
+
+    data_path = data_path or dataset_info_dict[dataset][0]
+    n_classes = dataset_info_dict[dataset][1]
+
+    # build datasets
+    if dataset=="mnist":
+        train_dataset  =datasets.MNIST(
+            os.path.join(data_path, "MNIST"),
+            train=True,
+            download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                
+                
+            ]
+            )
+        )
+        val_dataset = datasets.MNIST(
+            os.path.join(data_path, "MNIST"),
+            train=False,
+            download=True,
+            transform=transforms.Compose([
+                 transforms.ToTensor(),
+            ]     
+            )
+        )
+    elif dataset=="cifar10":
+        train_dataset  =datasets.CIFAR10(
+            os.path.join(data_path, "CIFAR10"),
+            train=True,
+            download=True,
+            transform=transforms.Compose([
+                transforms.RandomCrop(32,padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                
+                
+            ]
+            )
+        )
+        val_dataset = datasets.CIFAR10(
+            os.path.join(data_path, "CIFAR10"),
+            train=False,
+            download=True,
+            transform=transforms.Compose([
+                 transforms.ToTensor(),
+            ]     
+            )
+        )
+    elif dataset=="cifar100":
+        train_dataset  =datasets.CIFAR100(
+            os.path.join(data_path, "CIFAR100"),
+            train=True,
+            download=True,
+            transform=transforms.Compose([
+                transforms.RandomCrop(32,padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                
+            ]
+            )
+        )
+        val_dataset = datasets.CIFAR100(
+            os.path.join(data_path, "CIFAR100"),
+            train=False,
+            download=True,
+            transform=transforms.Compose([
+                 transforms.ToTensor(),
+            ]     
+            )
+        )
+    else:    
+        train_dataset = datasets.ImageFolder(
+            os.path.join(data_path, "train"),
+            transforms.Compose(
+                [
+                    transforms.RandomResizedCrop(image_size),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ColorJitter(brightness=32.0 / 255.0, saturation=0.5),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                    ),
+                ]
+            ),
+        )
+
+        val_dataset = datasets.ImageFolder(
+            os.path.join(data_path, "val"),
+            transforms.Compose(
+                [
+                    transforms.Resize(int(math.ceil(image_size / 0.875))),
+                    transforms.CenterCrop(image_size),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                    ),
+                ]
+            ),
+        )
+
+    sub_train_dataset = copy.deepcopy(train_dataset)  # used for resetting bn statistics
+    sub_train_dataset.transform = val_dataset.transform
+    if len(sub_train_dataset) > 16000:
+        g = torch.Generator()
+        g.manual_seed(937162211)
+        rand_indexes = torch.randperm(len(sub_train_dataset), generator=g).tolist()
+        rand_indexes = rand_indexes[:3000] # for alexnet and  vgg use 3000 for ftclip 1000
+        if  dataset=="cifar10":
+            sub_train_dataset.data = [
+                sub_train_dataset.data[idx] for idx in rand_indexes
+            ]
+            sub_train_dataset.targets = [
+                sub_train_dataset.targets[idx] for idx in rand_indexes
+            ]
+        elif  dataset=="cifar100":
+            sub_train_dataset.data = [
+                sub_train_dataset.data[idx] for idx in rand_indexes
+            ]    
+            sub_train_dataset.targets = [
+                sub_train_dataset.targets[idx] for idx in rand_indexes
+            ]
+        elif  dataset=="mnist":
+            sub_train_dataset.data = [
+                sub_train_dataset.data[idx] for idx in rand_indexes
+            ]    
+            sub_train_dataset.targets = [
+                sub_train_dataset.targets[idx] for idx in rand_indexes
+            ]    
+        else:   
+            sub_train_dataset.samples = [
+                sub_train_dataset.samples[idx] for idx in rand_indexes
+            ]
+            
+
+        # build data loader
+    if num_replica is None:
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=n_worker,
+            pin_memory=True,
+            drop_last=True,
+        )
+        sub_train_loader = torch.utils.data.DataLoader(
+            dataset=sub_train_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=n_worker,
+            pin_memory=True,
+            drop_last=True,
+        )
+        valid_loader = torch.utils.data.DataLoader(
+            dataset=val_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=n_worker,
+            pin_memory=True,
+            drop_last=False,
+        )
+    else:
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_dataset,
+            batch_size=batch_size,
+            sampler=torch.utils.data.distributed.DistributedSampler(
+                train_dataset, num_replica, rank
+            ),
+            num_workers=n_worker,
+            pin_memory=False,
+            drop_last=True,
+        )
+        sub_train_loader = torch.utils.data.DataLoader(
+            dataset=sub_train_dataset,
+            batch_size=batch_size,
+            sampler=torch.utils.data.distributed.DistributedSampler(
+                sub_train_dataset, num_replica, rank
+            ),
+            num_workers=n_worker,
+            pin_memory=False,
+            drop_last=True,
+            shuffle=False,
+        )
+        valid_loader = torch.utils.data.DataLoader(
+            dataset=val_dataset,
+            batch_size=batch_size,
+            sampler=torch.utils.data.distributed.DistributedSampler(
+                val_dataset, num_replica, rank
+            ),
+            num_workers=n_worker,
+            pin_memory=False,
+            drop_last=False,
+            shuffle=False,
+        )
+
+    # prefetch sub_train
+    sub_train_loader = [data for data in sub_train_loader]
+
+    data_loader_dict = {
+        "train": train_loader,
+        "val": valid_loader,
+        "sub_train": sub_train_loader,
+    }
+
+    return data_loader_dict, n_classes
+
+
+def build_model(
+    name: str,
+    n_classes=10,
+    dropout_rate=0.0,
+    **kwargs,
+) -> nn.Module:
+
+    model_dict = {
+        "lenet": Lenet,
+        "lenet_cifar10": LeNet_cifar,
+        "vgg16": VGG16,
+        "resnet50": ResNet50,
+        "alexnet": AlexNet_model,
+        "alexnet_cifar100" : AlexNet_cifar100, 
+        "vgg16_cifar100":VGG16_cifar100,
+        "resnet50_cifar100": ResNet50_cifar100,
+    }
+
+    name = name.split("-")
+    if len(name) > 1:
+        kwargs["width_mult"] = float(name[1])
+    name = name[0]
+
+    return model_dict[name](n_classes=n_classes, dropout_rate=dropout_rate, **kwargs)
+
+
+def find_bounds(model:nn.Module, data_loader, name:str,bound_type:str,bitflip:str):
+    search_bounds_dict={
+        "ranger" : Ranger_bounds,
+        "ftclip" : FtClipAct_bounds,
+        'fitact' : fitact_bounds,
+        'proact'  : proact_bounds
+    }
+    return search_bounds_dict[name](model,data_loader,bound_type=bound_type,bitflip=bitflip)
+
+def replace_act_all(model:nn.Module,relu_bound,bounds,tresh,alpha=None, name='')->nn.Module:
+    for name1,layer in model.named_children():
+        if list(layer.children()) == []:
+            if isinstance(layer,nn.ReLU) and 'last' not in name1:
+                name_ = name1 + name
+                if alpha == None:
+                    # print(name_)
+                    model._modules[name1] = relu_bound(bounds[name_].detach(),tresh[name_].detach(),alpha)
+                else:
+                    model._modules[name1] = relu_bound(bounds[name_].detach(),tresh[name_].detach(),alpha[name_].detach())    
+            elif isinstance(layer,nn.ReLU) and 'last' in name1:
+                name_ = name1 + name
+                if alpha == None:
+                    # print(name_)
+                    model._modules[name1] = relu_bound(bounds[name_].detach(),tresh[name_].detach(),alpha,k=-20.0)
+                else:
+                    model._modules[name1] = relu_bound(bounds[name_].detach(),tresh[name_].detach(),alpha[name_].detach(),k=-20.0) 
+                    
+        else:
+            name+=name1
+            replace_act_all(layer,relu_bound,bounds,tresh,alpha,name)               
+    return model  
+def replace_act(model:nn.Module, name_relu_bound:str, name_serach_bound:str,data_loader,bound_type:str,bitflip:str)->nn.Module:
+    replace_act_dict={
+        'zero' : bounded_relu_zero,
+        'tresh': bounded_relu_tresh,
+        'fitact': bounded_relu_fitact,
+        'proact' : bounded_hyrelu_proact
+    }
+    bounds,tresh,alpha = find_bounds(copy.deepcopy(model),data_loader,name_serach_bound,bound_type,bitflip) 
+    print(bounds)
+    model = replace_act_all(model,replace_act_dict[name_relu_bound],bounds,tresh,alpha,name='')
+    return model                
+                    
+
+if __name__=="__main__":
+    model = build_model("lenet") 
+    replace_act(model,"a","a")       
+    
+
+
+
