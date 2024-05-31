15d89771c082343b8d3168b0b058f7f0eb5834f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:12 +0000

    Add package micrOSDevToolKit/2.2.0/micrOSDevToolKit-2.2.0.tar.gz

---
+++ packages/micrOSDevToolKit/micrOSDevToolKit-2.2.0.tar.gz/micrOSDevToolKit-2.2.0/micrOS/source/Types.py
@@ -0,0 +1,106 @@
+"""
+micrOS Frontend element types - 'This is the frontend perspective of micrOS functions'
+- Can be attached for Load Module functions in module help tuple
+
+USAGE:
+    <TAG> function params*
+    BUTTON toggle
+
+    <range> int
+    SLIDER brightness br
+    SLIDER brightness br=<0-100>
+    SLIDER brightness br=<0-100-5>
+"""
+from json import dumps
+from Debug import errlog_add
+
+########################################################
+#                 HELP TUPLE RESOLVER                  #
+########################################################
+__TEMPLATE = {'type': 'n/a', 'lm_call': ''}
+__RANGE_100 = {'range': (0, 100, 2)}
+__RANGE_255 = {'range': (0, 255, 2)}
+__RANGE_OPTS = {'range': ("True", "False")}
+
+
+# WIDGET TYPES - STRUCTURE (DYNAMIC)
+BUTTON = __TEMPLATE | {'type': 'button'}                                    # Mandatory func params: n/a
+TOGGLE = __TEMPLATE | {'type': 'toggle'} | __RANGE_OPTS                     # Mandatory func params: state
+SLIDER = __TEMPLATE | {'type': 'slider'} | __RANGE_100                      # Mandatory func params: br
+TEXTBOX = __TEMPLATE | {'type': 'textbox', 'refresh': 10000}                # Mandatory func params: n/a
+COLOR = __TEMPLATE | {'type': 'color'} | __RANGE_255                        # Mandatory func params: r, g, b
+WHITE = __TEMPLATE | {'type': 'white'} | __RANGE_255                        # Mandatory func params: wc, ww
+
+
+########################################################
+#                       FUNCTIONS                      #
+########################################################
+
+def _placeholder(var, value, type_dict):
+    def _is_int(data):
+        try:
+            int(data)
+            return True
+        except:
+            return False
+
+    new_range = None
+    if value.startswith('<') and value.endswith('>'):
+        if '-' in value:
+            _range = value[1:-1].split('-')
+            # Range param int check
+            if len([r for r in _range if not _is_int(r)]) == 0:
+                # Get custom range values
+                _min, _max, _step = (_range[0], _range[1], _range[2]) if len(_range) > 2 else (_range[0], _range[1], None)
+                if _step is None:
+                    _step = type_dict['range'][2]
+                new_range = (int(_min), int(_max), int(_step))
+                #print(f"[i] Range overwrite[{var}]: {_range}")
+                return f"{var}=:range:", new_range
+            # Ignore param
+            return "", new_range
+    # Keep param value
+    return f"{var}={value}", new_range
+
+
+def _generate(type_dict, help_msg):
+    func = help_msg.split()[1]
+    params = help_msg.split()[2:] if len(help_msg.split()) > 2 else []
+    valid_params = []
+    for p in params:
+        if '=' in p:
+            var, value = p.split("=")
+            try:
+                p, new_range = _placeholder(var, value, type_dict)
+                if new_range is not None:
+                    type_dict['range'] = new_range
+            except Exception as e:
+                errlog_add(f"[ERR] Type.resolve._placeholder: {e}")
+        else:
+            # Empty param fallback
+            p = f'{p}=:range:'
+        valid_params.append(p)
+    type_dict['lm_call'] = f"{func} " + " ".join(valid_params)
+    return dumps(type_dict)
+
+def resolve(help_data, widgets=False):
+    help_msg = []
+    for msg in help_data:
+        tag = msg.split()[0].strip()
+        # TYPE DECORATION detect in help strings
+        if tag.isupper():
+            try:
+                resolved_tag = eval(tag)
+            except:
+                resolved_tag = tag
+            if widgets:
+                if isinstance(resolved_tag, dict):
+                    # Create json string type + extract widgets from help message
+                    help_msg.append(_generate(resolved_tag, msg))
+                continue
+            # Human readable mode (decorated functions)
+            help_msg.append(msg.replace(tag, '').strip())
+        elif not widgets:
+            # Human readable mode (non decorated functions)
+            help_msg.append(f"{msg}")
+    return tuple(help_msg)
---
+++ packages/micrOSDevToolKit/micrOSDevToolKit-2.2.0.tar.gz/micrOSDevToolKit-2.2.0/micrOS/source/uapi.js
@@ -0,0 +1,72 @@
+// Get the current hostname and create the REST API URL
+const currentHostname = window.location.hostname;
+const port = window.location.port ? `:${window.location.port}` : "";
+
+
+function restAPICore(cmd) {
+    // Core micrOS rest API handler
+    cmd = cmd.trim().replace(/\s+/g, '/');
+    let query = `http://${currentHostname}${port}/rest/${cmd}`;
+    const startTime = performance.now();
+    let endTime;
+    // Call rest api with command parameter and return the promise
+    return fetch(query).then(response => {
+        if (!response.ok) {
+            throw new Error('restAPICore code NOK: ' + response.status);
+        }
+        endTime = performance.now();
+        return response.json();
+    }).then(response => {
+        // return cmd result
+        const delta = (endTime - startTime).toFixed(0);
+        return { response, delta, query };                  // RETURN DATA
+    }).catch(error => {
+        console.error('Error in restAPICore:', error);
+        throw error;
+    });
+}
+
+function restAPI(cmd='', console=true) {
+    // micrOS rest API handler with built-in restCmdInput
+    // UPDATES: restConsole(...)
+    if (cmd == '') {
+        cmd = document.getElementById('restCmdInput').value;
+    }
+    return restAPICore(cmd)
+            .then(({ response, delta, query }) => {
+                if (console) {
+                    restConsole(query, response, delta);
+                }
+            return response
+        }).catch(error => {
+            console.error('Error in restAPI:', error);
+        });
+}
+
+function restConsole(apiUrl, data, delta) {
+    // UPDATES: restConsoleUrl, restConsoleResponse, restConsoleTime
+    document.getElementById('restConsoleUrl').innerHTML = `<strong>Generated URL:</strong><br> <a href="${apiUrl}" target="_blank" style="color: white;">${apiUrl}</a>`;
+    document.getElementById('restConsoleResponse').innerHTML = JSON.stringify(data, null, 4).replace(/\\n/g, "<br/>" + "&nbsp;".repeat(15));
+    document.getElementById('restConsoleTime').innerHTML = `⏱ Response time: ${delta} ms`;
+}
+
+function restInfo() {
+    // UPDATES: 'restInfo' and restConsole(...)
+    restAPICore(cmd='').then(({ response, delta, query }) => {
+        // Update API Console
+        restConsole(query, response, delta)
+        // Update 'SysApiInfo' tag
+        const result = response['result'];
+        const auth = result.auth ? "🔑" : "";
+        let infoHeader = `micrOS: ${result.micrOS} ❖ node: ${result.node}${auth}`;
+        let infoSubpages = (response['result']['usr_endpoints'] ? "<br><br>📎 " + Object.entries(response['result']['usr_endpoints'])
+            .map(([key, value]) => `<a href="${value}" target="_blank" style="color: white;">${value} </a>`)
+            .join(' | ') : '');
+        document.getElementById('restInfo').innerHTML = infoHeader + infoSubpages;
+    }).catch(error => {
+        console.error('Error in restAPI:', error);
+    });
+}
+
+// Init basic info from board
+// restInfo();
