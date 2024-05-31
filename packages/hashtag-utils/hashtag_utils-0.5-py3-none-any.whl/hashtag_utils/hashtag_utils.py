761cfaad887cc23dd32840614a4058511e5ae1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:04:52 +0000

    Add package hashtag-utils/0.5/hashtag_utils-0.5-py3-none-any.whl

---
+++ packages/hashtag-utils/hashtag_utils-0.5-py3-none-any.whl/hashtag_utils/hashtag_utils.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+import os
+import json
+
+
+class HashtagUtils:
+    def __init__(self):
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    def get_hashtags(self, content, temperature=0.5, num_tags=5):
+        completion = self.client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "system",
+                    "content": f"You are an AI agent that generates hashtags for a given piece of content.\
+                                           The content will be supplied to your in the form of a prompt.\
+                                           Return the result as a JSON-array of the hashtags. \
+                                           Generate {num_tags} hashtags for the following content.",
+                },
+                {"role": "user", "content": f"Content: {content}"},
+            ],
+            temperature=temperature,
+            response_format={"type": "json_object"},
+        )
+
+        hashtags_json = completion.choices[0].message.content
+        hashtags = json.loads(hashtags_json)
+
+        return hashtags
