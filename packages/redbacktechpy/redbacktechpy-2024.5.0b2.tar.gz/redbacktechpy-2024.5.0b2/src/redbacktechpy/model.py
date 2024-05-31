225212b2c2703ba0b79195bd39bf8dbbc7338718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:03:59 +0000

    Add package redbacktechpy/2024.5.0b2/redbacktechpy-2024.5.0b2.tar.gz

---
+++ packages/redbacktechpy/redbacktechpy-2024.5.0b2.tar.gz/redbacktechpy-2024.5.0b2/src/redbacktechpy/model.py
@@ -0,0 +1,50 @@
+""" Data classes for Redback Tech API """
+from __future__ import annotations
+
+from dataclasses import dataclass
+import datetime
+from typing import Any, Optional
+
+
+@dataclass
+class RedbackTechData:
+    """Dataclass for all RedbackTech Data."""
+
+    user_id: str
+    inverters: Optional[dict[int, Any]] = None
+    batterys: Optional[dict[int, Any]] = None
+
+@dataclass
+class Site:
+    """Dataclass for Redback Sites."""
+
+    id: str
+    data: dict[str, Any]
+    type: str
+    
+@dataclass
+class Inverters:
+    """Dataclass for Redback Inverters."""
+
+    id: str
+    device_serial_number: str
+    data: dict[str, Any]
+    type: str
+
+@dataclass    
+class Batterys:
+    """Dataclass for RedBack Batteries."""
+
+    id: str
+    device_serial_number: str
+    data: dict[str, Any]
+    type: str
+
+@dataclass
+class DeviceInfo:
+    """Dataclass for Device Info."""
+
+    id: str
+    device_serial_number: str
+    data: dict[str, Any]
+    type: str
