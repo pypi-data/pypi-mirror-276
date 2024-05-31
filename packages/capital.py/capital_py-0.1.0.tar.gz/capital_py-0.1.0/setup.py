6512924f6d1a1f4c0f1d6a71a12f317a4a11fe49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:04:33 +0000

    Add package capital.py/0.1.0/capital_py-0.1.0.tar.gz

---
+++ packages/capital.py/capital_py-0.1.0.tar.gz/capital_py-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='capital.py',
+version='0.1.0',
+author='Malakia',
+author_email='someguyagain1@gmail.com',
+description='This is a easy to use python module for capital.com API',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
