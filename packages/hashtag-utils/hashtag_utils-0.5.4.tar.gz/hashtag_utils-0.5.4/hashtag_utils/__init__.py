89aae2abe3851a0788a65b0e0107cb84b7f018f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:04:41 +0000

    Add package hashtag-utils/0.5.4/hashtag_utils-0.5.4.tar.gz

---
+++ packages/hashtag-utils/hashtag_utils-0.5.4.tar.gz/hashtag_utils-0.5.4/hashtag_utils/__init__.py
@@ -0,0 +1,19 @@
+import dotenv
+import os
+import sys
+from .hashtag_utils import HashtagUtils
+
+# Get the current script's directory
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Get the parent directory
+parent_directory = os.path.dirname(current_directory)
+
+# Add the parent directory to the Python path
+sys.path.append(parent_directory)
+
+env_path = os.path.join(os.getcwd(), ".env")
+
+print("env path", env_path)
+
+dotenv.load_dotenv(env_path)
