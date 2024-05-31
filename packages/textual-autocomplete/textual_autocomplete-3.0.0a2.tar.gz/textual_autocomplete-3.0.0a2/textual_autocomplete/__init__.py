d88d351ecffade52cec52bbcb1bc9f8c396c09b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:52 +0000

    Add package textual-autocomplete/3.0.0a2/textual_autocomplete-3.0.0a2.tar.gz

---
+++ packages/textual-autocomplete/textual_autocomplete-3.0.0a2.tar.gz/textual_autocomplete-3.0.0a2/textual_autocomplete/__init__.py
@@ -0,0 +1,15 @@
+from textual_autocomplete._autocomplete import (
+    CompletionStrategy,
+    Dropdown,
+    InputState,
+)
+
+from textual_autocomplete._autocomplete2 import AutoComplete, DropdownItem
+
+__all__ = [
+    "AutoComplete",
+    "CompletionStrategy",
+    "Dropdown",
+    "DropdownItem",
+    "InputState",
+]
