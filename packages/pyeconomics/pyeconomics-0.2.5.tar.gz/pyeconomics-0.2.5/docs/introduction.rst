22831bc64dbe1529769136a376b81e45c9a54785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:52 +0000

    Add package pyeconomics/0.2.5/pyeconomics-0.2.5.tar.gz

---
+++ packages/pyeconomics/pyeconomics-0.2.5.tar.gz/pyeconomics-0.2.5/docs/introduction.rst
@@ -0,0 +1,56 @@
+Introduction
+============
+
+.. image:: _static/logo.png
+   :width: 800px
+   :align: center
+
+.. raw:: html
+
+   <br><br>
+
+.. list-table::
+   :widths: auto
+   :header-rows: 1
+
+   * - Category
+     - Badges
+   * - **Testing**
+     - .. image:: https://github.com/nathanramoscfa/pyeconomics/actions/workflows/tests.yml/badge.svg
+          :target: https://github.com/nathanramoscfa/pyeconomics/actions/workflows/tests.yml
+       .. image:: https://codecov.io/gh/nathanramoscfa/pyeconomics/graph/badge.svg?token=I1CRHDN73S
+          :target: https://codecov.io/gh/nathanramoscfa/pyeconomics
+   * - **Package**
+     - .. image:: https://img.shields.io/pypi/v/pyeconomics.svg
+          :target: https://pypi.org/project/pyeconomics/
+       .. image:: https://img.shields.io/pypi/pyversions/pyeconomics
+       .. image:: https://static.pepy.tech/badge/pyeconomics
+          :target: https://pepy.tech/project/pyeconomics
+       .. image:: https://img.shields.io/badge/License-MIT-brightgreen.svg
+          :target: https://opensource.org/licenses/MIT
+   * - **Documentation**
+     - .. image:: https://readthedocs.org/projects/pyeconomics/badge/?version=latest
+          :target: https://pyeconomics.readthedocs.io/en/latest/?badge=latest
+   * - **Release**
+     - .. image:: https://github.com/nathanramoscfa/pyeconomics/actions/workflows/release.yml/badge.svg
+          :target: https://github.com/nathanramoscfa/pyeconomics/actions/workflows/release.yml
+   * - **Build Status**
+     - .. image:: https://github.com/nathanramoscfa/pyeconomics/actions/workflows/tests.yml/badge.svg
+          :target: https://github.com/nathanramoscfa/pyeconomics/actions/workflows/tests.yml
+   * - **Maintainability**
+     - .. image:: https://api.codeclimate.com/v1/badges/8d0d8b5a5bed8fe94814/maintainability
+          :target: https://codeclimate.com/github/nathanramoscfa/pyeconomics/maintainability
+   * - **Code Style**
+     - .. image:: https://img.shields.io/badge/code%20style-flake8-brightgreen.svg
+          :target: https://github.com/PyCQA/flake8
+   * - **Dependencies**
+     - .. image:: https://img.shields.io/librariesio/github/nathanramoscfa/pyeconomics
+          :target: https://libraries.io/github/nathanramoscfa/pyeconomics
+   * - **Security**
+     - .. image:: https://snyk.io/test/github/nathanramoscfa/pyeconomics/badge.svg
+          :target: https://snyk.io/test/github/nathanramoscfa/pyeconomics
+
+PyEconomics is a versatile and comprehensive library that integrates various
+economic and financial models. It is designed to support the development and
+analysis of monetary policy rules, exchange rate models, macroeconomic and
+microeconomic models, discounted cash flow models, and much more.
---
+++ packages/pyeconomics/pyeconomics-0.2.5.tar.gz/pyeconomics-0.2.5/tests/test_tr_utils.py
@@ -0,0 +1,252 @@
+# tests/test_tr_utils.py
+
+import pytest
+from datetime import datetime
+
+from pyeconomics.utils.tr_utils import verbose_taylor_rule
+
+
+@pytest.fixture
+def mock_data():
+    return {
+        'current_inflation_rate': 2.5,
+        'inflation_target': 2.0,
+        'current_unemployment_rate': 4.0,
+        'natural_unemployment_rate': 4.5,
+        'long_term_real_interest_rate': 2.0,
+        'current_fed_rate': 0.5,
+        'inflation_gap': 0.5,
+        'unemployment_gap': 0.5,
+        'alpha': 0.5,
+        'beta': 0.5,
+        'okun_factor': 2.0,
+        'unadjusted_taylor_rule': 1.0,
+        'adjusted_taylor_rule_after_elb': 1.0,
+        'apply_elb': False,
+        'elb': 0.0,
+        'rho': 0.5,
+        'adjusted_taylor_rule_after_inertia': 0.75
+    }
+
+
+def test_verbose_taylor_rule(capsys, mock_data):
+    verbose_taylor_rule(mock_data)
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    as_of_date = datetime.now().strftime("%B %d, %Y")
+    expected_output = (
+        "==== Economic Indicators "
+        "=================================================\n"
+        "Current Inflation:                               2.50%\n"
+        "Target Inflation:                                2.00%\n"
+        "Current Unemployment Rate:                       4.00%\n"
+        "Natural Unemployment Rate:                       4.50%\n"
+        "Long-Term Real Interest Rate:                    2.00%\n"
+        "Current Fed Rate:                                0.50%\n"
+        f"As of Date:                                      {as_of_date}\n"
+        "\n==== Gaps "
+        "================================================================\n"
+        "Inflation Gap:                                   0.50%\n"
+        "Unemployment Gap:                                0.50%\n"
+        "\n==== Taylor Rule "
+        "=========================================================\n"
+        "  Long-Term Real Interest Rate:                  2.00%\n"
+        "  Current Inflation:                             + 2.50%\n"
+        "  Alpha * Inflation Gap:                         + 0.50 * 0.50%\n"
+        "  Beta * Okun Factor * Unemployment Gap:         "
+        "+ 0.50 * 2.00 * 0.50%\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Unadjusted Taylor Rule Estimate:               1.00%\n"
+        "\n==== Adjusted Taylor Rule "
+        "================================================\n"
+        "\n  Policy Inertia Adjustment:              \n"
+        "  Policy Inertia Coefficient (rho):              0.50\n"
+        "  Current Fed Rate:                              * 0.50%\n"
+        "  Adjustment Coefficient (1 - rho):              + (1 - 0.50)\n"
+        "  Taylor Rule Adjusted for ELB:                  * 1.00%\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Adjusted Taylor Rule Estimate:                 0.75%\n"
+        "\n==== Policy Prescription "
+        "=================================================\n"
+        "  The Adjusted Taylor Rule Estimate is 0.25% higher than the "
+        "Current \n  Fed Rate. The Fed should consider raising the interest "
+        "rate by 0.25%.\n"
+    )
+
+    # Comparing the captured output with the expected output
+    assert output.strip() == expected_output.strip()
+
+
+def test_verbose_taylor_rule_apply_elb(capsys, mock_data):
+    mock_data['apply_elb'] = True
+    mock_data['adjusted_taylor_rule_after_elb'] = 1.25
+    mock_data['unadjusted_taylor_rule'] = 1.0
+    mock_data['elb'] = 0.5
+    mock_data['adjusted_taylor_rule_after_inertia'] = 0.88
+    verbose_taylor_rule(mock_data)
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    as_of_date = datetime.now().strftime("%B %d, %Y")
+    expected_output = (
+        "==== Economic Indicators "
+        "=================================================\n"
+        "Current Inflation:                               2.50%\n"
+        "Target Inflation:                                2.00%\n"
+        "Current Unemployment Rate:                       4.00%\n"
+        "Natural Unemployment Rate:                       4.50%\n"
+        "Long-Term Real Interest Rate:                    2.00%\n"
+        "Current Fed Rate:                                0.50%\n"
+        f"As of Date:                                      {as_of_date}\n"
+        "\n==== Gaps "
+        "================================================================\n"
+        "Inflation Gap:                                   0.50%\n"
+        "Unemployment Gap:                                0.50%\n"
+        "\n==== Taylor Rule "
+        "=========================================================\n"
+        "  Long-Term Real Interest Rate:                  2.00%\n"
+        "  Current Inflation:                             + 2.50%\n"
+        "  Alpha * Inflation Gap:                         + 0.50 * 0.50%\n"
+        "  Beta * Okun Factor * Unemployment Gap:         "
+        "+ 0.50 * 2.00 * 0.50%\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Unadjusted Taylor Rule Estimate:               1.00%\n"
+        "\n==== Adjusted Taylor Rule "
+        "================================================\n"
+        "  Effective Lower Bound (ELB) Adjustment: \n"
+        "  Maximum of Taylor Rule or ELB:                 max(1.00%, 0.50%)\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Taylor Rule Adjusted for ELB:                  1.25%\n"
+        "\n  Policy Inertia Adjustment:              \n"
+        "  Policy Inertia Coefficient (rho):              0.50\n"
+        "  Current Fed Rate:                              * 0.50%\n"
+        "  Adjustment Coefficient (1 - rho):              + (1 - 0.50)\n"
+        "  Taylor Rule Adjusted for ELB:                  * 1.25%\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Adjusted Taylor Rule Estimate:                 0.88%\n"
+        "\n==== Policy Prescription "
+        "=================================================\n"
+        "  The Adjusted Taylor Rule Estimate is 0.38% higher than the "
+        "Current \n  Fed Rate. The Fed should consider raising the interest "
+        "rate by 0.50%.\n"
+    )
+
+    # Comparing the captured output with the expected output
+    assert output.strip() == expected_output.strip()
+
+
+def test_verbose_taylor_rule_lower_rate(capsys, mock_data):
+    mock_data['adjusted_taylor_rule_after_inertia'] = 0.25
+    verbose_taylor_rule(mock_data)
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    as_of_date = datetime.now().strftime("%B %d, %Y")
+    expected_output = (
+        "==== Economic Indicators "
+        "=================================================\n"
+        "Current Inflation:                               2.50%\n"
+        "Target Inflation:                                2.00%\n"
+        "Current Unemployment Rate:                       4.00%\n"
+        "Natural Unemployment Rate:                       4.50%\n"
+        "Long-Term Real Interest Rate:                    2.00%\n"
+        "Current Fed Rate:                                0.50%\n"
+        f"As of Date:                                      {as_of_date}\n"
+        "\n==== Gaps "
+        "================================================================\n"
+        "Inflation Gap:                                   0.50%\n"
+        "Unemployment Gap:                                0.50%\n"
+        "\n==== Taylor Rule "
+        "=========================================================\n"
+        "  Long-Term Real Interest Rate:                  2.00%\n"
+        "  Current Inflation:                             + 2.50%\n"
+        "  Alpha * Inflation Gap:                         + 0.50 * 0.50%\n"
+        "  Beta * Okun Factor * Unemployment Gap:         "
+        "+ 0.50 * 2.00 * 0.50%\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Unadjusted Taylor Rule Estimate:               1.00%\n"
+        "\n==== Adjusted Taylor Rule "
+        "================================================\n"
+        "\n  Policy Inertia Adjustment:              \n"
+        "  Policy Inertia Coefficient (rho):              0.50\n"
+        "  Current Fed Rate:                              * 0.50%\n"
+        "  Adjustment Coefficient (1 - rho):              + (1 - 0.50)\n"
+        "  Taylor Rule Adjusted for ELB:                  * 1.00%\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Adjusted Taylor Rule Estimate:                 0.25%\n"
+        "\n==== Policy Prescription "
+        "=================================================\n"
+        "  The Adjusted Taylor Rule Estimate is 0.25% lower than the "
+        "Current \n  Fed Rate. The Fed should consider lowering the interest "
+        "rate by -0.25%.\n"
+    )
+
+    # Comparing the captured output with the expected output
+    assert output.strip() == expected_output.strip()
+
+
+def test_verbose_taylor_rule_maintain_rate(capsys, mock_data):
+    mock_data['adjusted_taylor_rule_after_inertia'] = 0.5
+    verbose_taylor_rule(mock_data)
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    as_of_date = datetime.now().strftime("%B %d, %Y")
+    expected_output = (
+        "==== Economic Indicators "
+        "=================================================\n"
+        "Current Inflation:                               2.50%\n"
+        "Target Inflation:                                2.00%\n"
+        "Current Unemployment Rate:                       4.00%\n"
+        "Natural Unemployment Rate:                       4.50%\n"
+        "Long-Term Real Interest Rate:                    2.00%\n"
+        "Current Fed Rate:                                0.50%\n"
+        f"As of Date:                                      {as_of_date}\n"
+        "\n==== Gaps "
+        "================================================================\n"
+        "Inflation Gap:                                   0.50%\n"
+        "Unemployment Gap:                                0.50%\n"
+        "\n==== Taylor Rule "
+        "=========================================================\n"
+        "  Long-Term Real Interest Rate:                  2.00%\n"
+        "  Current Inflation:                             + 2.50%\n"
+        "  Alpha * Inflation Gap:                         + 0.50 * 0.50%\n"
+        "  Beta * Okun Factor * Unemployment Gap:         "
+        "+ 0.50 * 2.00 * 0.50%\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Unadjusted Taylor Rule Estimate:               1.00%\n"
+        "\n==== Adjusted Taylor Rule "
+        "================================================\n"
+        "\n  Policy Inertia Adjustment:              \n"
+        "  Policy Inertia Coefficient (rho):              0.50\n"
+        "  Current Fed Rate:                              * 0.50%\n"
+        "  Adjustment Coefficient (1 - rho):              + (1 - 0.50)\n"
+        "  Taylor Rule Adjusted for ELB:                  * 1.00%\n"
+        "-------------------------------------"
+        "-------------------------------------\n"
+        "  Adjusted Taylor Rule Estimate:                 0.50%\n"
+        "\n==== Policy Prescription "
+        "=================================================\n"
+        "  The Adjusted Taylor Rule Estimate is equal to the Current "
+        "Fed Rate.\n  The Fed should maintain the current interest rate.\n"
+    )
+
+    # Comparing the captured output with the expected output
+    assert output.strip() == expected_output.strip()
+
+
+if __name__ == '__main__':
+    pytest.main()
