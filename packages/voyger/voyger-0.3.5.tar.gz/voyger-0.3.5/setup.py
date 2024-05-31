fe2de77049b315f4b8bc0b2fc6ee1fddac51a2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 04:04:01 +0000

    Add package voyger/0.3.5/voyger-0.3.5.tar.gz

---
+++ packages/voyger/voyger-0.3.5.tar.gz/voyger-0.3.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='voyger',
+    version='0.3.5',  # Updated version number
+    description='A Python library for space exploration enthusiasts.',
+    author='Coataoc',
+    author_email='coataoccreater@gmail.com',
+    py_modules=['voyger'],  # Name of your module without the .py extension
+)
