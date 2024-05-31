44c7eb7ee4c0c0c7a70b24f548761621071413b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:04:13 +0000

    Add package countrymapper/0.1/countrymapper-0.1-py3-none-any.whl

---
+++ packages/countrymapper/countrymapper-0.1-py3-none-any.whl/countrymapper/__init__.py
@@ -0,0 +1,6 @@
+from countrymapper.countrymapper import *
+
+from .version import __version__
+
+__author__ = "Arfat Shaikh"
+__all__ = ["agg_conc", "match", "convert", "CountryConverter", "cli_output"]
