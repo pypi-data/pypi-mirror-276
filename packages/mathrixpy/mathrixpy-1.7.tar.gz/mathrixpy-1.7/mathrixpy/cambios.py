bf05402c60ba8b4f577ccf756b0a3f2a1475a9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:45 +0000

    Add package mathrixpy/1.7/mathrixpy-1.7.tar.gz

---
+++ packages/mathrixpy/mathrixpy-1.7.tar.gz/mathrixpy-1.7/mathrixpy/cambios.py
@@ -0,0 +1,26 @@
+class matrizop:
+    def __init__(self,datos:list)-> object:   #Incializa la clase    
+        '''
+        Convierte una lista de lista a matriz
+        '''
+        
+        self.datos=datos #Se ingresa una lista de listas
+        self.filas=len(datos) #El número de elementos de las lista de datos es igual a el numero de filas
+        self.columnas=len(datos[0]) #El número de elementos de cada fila es igual al número de columnas
+
+#Cambios  
+    def extractColumna(self,numColumna)->list:
+        resultado=[]
+        
+        for i in self.datos:
+            resultado.append(i[numColumna-1])
+        return resultado
+    
+    def apply_function(self, func) -> object:
+        '''
+        Aplica una función a cada elemento de la matriz
+        '''
+        matrix = [[func(self.datos[i][j]) for j in range(self.columnas)] for i in range(self.filas)]
+        return matrizop(matrix)
+
+    
