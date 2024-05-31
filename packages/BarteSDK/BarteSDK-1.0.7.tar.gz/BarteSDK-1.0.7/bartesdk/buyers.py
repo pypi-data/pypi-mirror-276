502f6b39d33030c3634f19a5eae713df59a0a170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:07 +0000

    Add package BarteSDK/1.0.7/BarteSDK-1.0.7.tar.gz

---
+++ packages/BarteSDK/BarteSDK-1.0.7.tar.gz/BarteSDK-1.0.7/bartesdk/buyers.py
@@ -0,0 +1,14 @@
+from .base import BaseAPI
+
+class buyersAPI(BaseAPI):
+    def __init__(self, api_key, env="prd", api_version="v1"):
+        base_url = self._get_base_url(env, api_version)
+        super().__init__(api_key, base_url)
+
+    def _get_base_url(self, env, api_version):
+        if env == "prd":
+            return f'https://api.barte.com/{api_version}/buyers'
+        elif env == "sandbox":
+            return f'https://sandbox-api.barte.com/{api_version}/buyers'
+        else:
+            raise ValueError("Invalid environment specified")
