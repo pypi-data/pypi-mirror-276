89e6bf5e730eac24797b851f890046dd522abf2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:54 +0000

    Add package electronAppGeneratorMaker/0.1.0/electronappgeneratormaker-0.1.0.tar.gz

---
+++ packages/electronAppGeneratorMaker/electronappgeneratormaker-0.1.0.tar.gz/electronappgeneratormaker-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# Define project metadata
+NAME = 'electronAppGeneratorMaker'
+DESCRIPTION = 'JavaScript electron app generator with Python'
+AUTHOR = 'Sion'
+AUTHOR_EMAIL = 'the.rckr@gmail.com'
+VERSION = '0.1.0'
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    packages=find_packages(),
+classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: MacOS',
+],
+
+)
