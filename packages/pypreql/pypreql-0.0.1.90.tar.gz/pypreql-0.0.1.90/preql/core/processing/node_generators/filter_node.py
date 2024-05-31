ff863f79e7f7dd2c269529c36a6e18052c2841f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:05:30 +0000

    Add package pypreql/0.0.1.90/pypreql-0.0.1.90.tar.gz

---
+++ packages/pypreql/pypreql-0.0.1.90.tar.gz/pypreql-0.0.1.90/preql/core/processing/node_generators/filter_node.py
@@ -0,0 +1,78 @@
+from typing import List
+
+
+from preql.core.enums import JoinType
+from preql.core.models import (
+    Concept,
+    Environment,
+    FilterItem,
+)
+from preql.core.processing.nodes import (
+    FilterNode,
+    MergeNode,
+    NodeJoin,
+)
+from preql.core.processing.node_generators.common import (
+    resolve_filter_parent_concepts,
+)
+
+
+def gen_filter_node(
+    concept: Concept,
+    local_optional: List[Concept],
+    environment: Environment,
+    g,
+    depth: int,
+    source_concepts,
+) -> MergeNode:
+    immediate_parent, parent_concepts = resolve_filter_parent_concepts(concept)
+
+    filter_node = FilterNode(
+        input_concepts=[immediate_parent] + parent_concepts,
+        output_concepts=[concept, immediate_parent],
+        environment=environment,
+        g=g,
+        parents=[
+            source_concepts(
+                mandatory_list=parent_concepts,
+                environment=environment,
+                g=g,
+                depth=depth + 1,
+            )
+        ],
+        conditions=(
+            concept.lineage.where.conditional
+            if isinstance(concept.lineage, FilterItem)
+            else None
+        ),
+        partial_concepts=[immediate_parent],
+    )
+    enrich_node = source_concepts(  # this fetches the parent + join keys
+        # to then connect to the rest of the query
+        mandatory_list=[immediate_parent] + local_optional,
+        environment=environment,
+        g=g,
+        depth=depth + 1,
+    )
+    return MergeNode(
+        input_concepts=[concept, immediate_parent] + local_optional,
+        output_concepts=[concept, immediate_parent] + local_optional,
+        environment=environment,
+        g=g,
+        parents=[
+            # this node fetches only what we need to filter
+            filter_node,
+            enrich_node,
+        ],
+        node_joins=[
+            NodeJoin(
+                left_node=filter_node,
+                right_node=enrich_node,
+                concepts=[immediate_parent],
+                join_type=JoinType.INNER,
+                filter_to_mutual=False,
+            )
+        ],
+        # all of these concepts only count as partial
+        partial_concepts=[immediate_parent] + local_optional,
+    )
