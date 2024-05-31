439fc33b65920277d818def63095af771f038154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:50 +0000

    Add package algophon/0.1.1/algophon-0.1.1.tar.gz

---
+++ packages/algophon/algophon-0.1.1.tar.gz/algophon-0.1.1/tests/tester.py
@@ -0,0 +1,35 @@
+import unittest
+from test_seg import TestSeg
+from test_seginv import TestSegInv
+from test_segstr import TestSegStr
+from test_natclass import TestNatClass
+from test_distance import TestDistance
+from test_utils import TestUtils
+from test_data_structures import TestDataStructures
+from test_d2l import TestD2L
+
+'''
+A script to run all the test cases.
+'''
+# load test suites
+test_seg_suite = unittest.TestLoader().loadTestsFromTestCase(TestSeg)
+test_seginv_suite = unittest.TestLoader().loadTestsFromTestCase(TestSegInv)
+test_segstr_suite = unittest.TestLoader().loadTestsFromTestCase(TestSegStr)
+test_natclass_suite = unittest.TestLoader().loadTestsFromTestCase(TestNatClass)
+test_distance_suite = unittest.TestLoader().loadTestsFromTestCase(TestDistance)
+test_utils_suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+test_datastructures_suite = unittest.TestLoader().loadTestsFromTestCase(TestDataStructures)
+test_d2l_suite = unittest.TestLoader().loadTestsFromTestCase(TestD2L)
+# combine the test suites
+suites = unittest.TestSuite([
+    test_seg_suite,
+    test_seginv_suite,
+    test_segstr_suite,
+    test_natclass_suite,
+    test_distance_suite,
+    test_utils_suite,
+    test_datastructures_suite,
+    test_d2l_suite
+])
+# run the test suites
+unittest.TextTestRunner(verbosity=2).run(suites)
