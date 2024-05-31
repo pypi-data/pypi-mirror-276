e70bdd345789ac0736a3c9163b714f46e40262d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:05:03 +0000

    Add package bees-challenge/0.14.0/bees-challenge-0.14.0.tar.gz

---
+++ packages/bees-challenge/bees-challenge-0.14.0.tar.gz/bees-challenge-0.14.0/fuel_efficency/entities/plateau.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from functools import total_ordering
+
+from fuel_efficency.entities.node import Node
+from fuel_efficency.entities.position import Position
+
+
+@total_ordering
+@dataclass(slots=True)
+class Plateau:
+    weight: float = float(1)
+    position: Position = field(default_factory=Position)
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            raise NotImplementedError("Missing `position` or `weight` attribute")
+
+    def __gt__(self, other):
+        if not isinstance(other, Node):
+            raise NotImplementedError("Missing `weight` attribute")
