1788991b92ecf512fe46b95b5acb2b5c7be55404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:54 +0000

    Add package torch-timeseries/0.1.1/torch-timeseries-0.1.1.tar.gz

---
+++ packages/torch-timeseries/torch-timeseries-0.1.1.tar.gz/torch-timeseries-0.1.1/torch_timeseries/experiments/__init__.py
@@ -0,0 +1,10 @@
+from .forecast import ForecastExp
+from .uea_classification import UEAClassificationExp
+from .anomaly_detection import AnomalyDetectionExp
+from .imputation import ImputationExp
+from .DLinear import (
+    DLinearAnomalyDetection,
+    DLinearForecast,
+    DLinearImputation,
+    DLinearUEAClassification,
+)
