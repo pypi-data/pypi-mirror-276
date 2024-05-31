3b4bcf8de8b09af3ab922f336ababe42138bdf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:15 +0000

    Add package connect-four-tobiasocula/1.0.4/connect_four_tobiasocula-1.0.4.tar.gz

---
+++ packages/connect-four-tobiasocula/connect_four_tobiasocula-1.0.4.tar.gz/connect_four_tobiasocula-1.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='connect_four_tobiasocula',
+    version='1.0.4',
+    description='Your package description',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        # Your dependencies
+    ],
+)
