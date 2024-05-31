fabdad2d24c9a7ca6b0a1295f8373cc411011d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 16:04:58 +0000

    Add package hooks-toolkit/1.0.5/hooks_toolkit-1.0.5.tar.gz

---
+++ packages/hooks-toolkit/hooks_toolkit-1.0.5.tar.gz/hooks_toolkit-1.0.5/hooks_toolkit/libs/xrpl_helpers/constants.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from xrpl.wallet import Wallet
+
+# **DO NOT EDIT THESE SEEDS**
+# They match the seeds in the rippled source test env.
+# Where the secret that is used to generate the seed is the name
+
+# wallet secret == "mastersecret"
+# rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh
+MASTER_ACCOUNT_WALLET = Wallet("snoPBrXtMeMyMHUVTgbuqAfg1SUTb", 0)
+# wallet secret == "gw"
+# rExKpRKXNz25UAjbckCRtQsJFcSfjL9Er3
+GW_ACCOUNT_WALLET = Wallet("safmpBLsy2paxybRMpvXqFqSrV5HG", 0)
+# wallet secret == "notactive"
+# rMqTbPY6rzTspuBGBcS7po7FFTPwitrKHX
+NOT_ACTIVE_WALLET = Wallet("snqPCkCnfAbK4p981HZZGMj8SnhZ7", 0)
+# wallet secret == "alice"
+# rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn
+ALICE_ACCOUNT_WALLET = Wallet("ssbTMHrmEJP7QEQjWJH3a72LQipBM", 0)
+# wallet secret == "bob"
+# rPMh7Pi9ct699iZUTWaytJUoHcJ7cgyziK
+BOB_ACCOUNT_WALLET = Wallet("spkcsko6Ag3RbCSVXV2FJ8Pd4Zac1", 0)
+# wallet secret == "carol"
+# rH4KEcG9dEwGwpn6AyoWK9cZPLL4RLSmWW
+CAROL_ACCOUNT_WALLET = Wallet("snzb83cV8zpLPTE4nQamoLP9pbhB7", 0)
+# wallet secret == "dave"
+#  rGWC14yT3U8bkuAx2c8tA1ajLUJCandp59
+DAVE_ACCOUNT_WALLET = Wallet("sh2Q7wDfjdvyVaVHEE8JT3C9osGFD", 0)
+# wallet secret == "elsa"
+# rL8xeQSGxTmKXXyLUFRuAAKsWMY1BMpQbe
+ELSA_ACCOUNT_WALLET = Wallet("sspu32LMDPU9V5NCUb584FqbdPsZ6", 0)
+# wallet secret == "frank"
+# rDvJdyJXEMcLByY7y8fPXKnK6g9BpAyhb9
+FRANK_ACCOUNT_WALLET = Wallet("sshnbebSnLqrwyh8rorMcYgH18fXR", 0)
+# wallet secret == "grace"
+# rKwXpiHGcQSzKEGXcwej5Eq2CQtN6HoTDr
+GRACE_ACCOUNT_WALLET = Wallet("saNMoZ8vo5xHhTkdY591ZM5tzxC4J", 0)
+# wallet secret == "heidi"
+# rMcXuYsQ3MjFeRRqi1GvasJzTdL7wuL3hN
+HEIDI_ACCOUNT_WALLET = Wallet("shQ9J4uun5BFQ5coEeUorTPipssR8", 0)
+# wallet secret == "ivan"
+# rhW5hg2xE7w2ewepbhWtgZWW8SuN6uW97S
+IVAN_ACCOUNT_WALLET = Wallet("ssUL8VwUfDNzttN7AsimLPyiP7RXQ", 0)
+# wallet secret == "judy"
+# rpggxGs4yFaFEGThxBEbhir7T9Fb5LQKLC
+JUDY_ACCOUNT_WALLET = Wallet("shaiav41HpCvyHQhbSJDWS2pAqD77", 0)
+
+# wallet secret == "hook1"
+# rBpVrkKc8QnxsCGsngMJgmDKqxJKoWHfKt
+HOOK1_ACCOUNT_WALLET = Wallet("ssM2MJpZchJhEWYCP1wMqpD9MWbcd", 0)
+# wallet secret == "hook2"
+# rpeWVvqpm2EPzfFiWrV421AMHKUQBk5aDt
+HOOK2_ACCOUNT_WALLET = Wallet("ss7Ec7bReBqsYpLY2cVsyJsgeLJLq", 0)
+# wallet secret == "hook3"
+# raNiq8ztgmvSXuKrD8iEn2QL6MEf72wSXz
+HOOK3_ACCOUNT_WALLET = Wallet("shgqrTAn8TkqKSfVHhDU4kMthzHeJ", 0)
+# wallet secret == "hook4"
+# rHbizjpTJsqKh4WBcJnJr99uP6H1chRcTd
+HOOK4_ACCOUNT_WALLET = Wallet("snZ1P5gkmuJYUi2LDtSSzYQnxcdp8", 0)
+# wallet secret == "hook4"
+# rUPJACGdNu2rNRiWYpjTbr9BGYz8caXEQG
+HOOK5_ACCOUNT_WALLET = Wallet("ssUcdBBRiCendP9TenSr2tfKhFw7Z", 0)
