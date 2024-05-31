d52771b47ca338ae838f9d0c3eb89f7ff7ff70c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:14 +0000

    Add package libschrodinger/1.0.4rc2/libschrodinger-1.0.4rc2.tar.gz

---
+++ packages/libschrodinger/libschrodinger-1.0.4rc2.tar.gz/libschrodinger-1.0.4rc2/libschrodinger/figlocation.py
@@ -0,0 +1,34 @@
+import matplotlib
+import matplotlib.gridspec
+
+
+class FigureLocation:
+    """
+    Describes the location of a subplot in a matplotlib figure
+    """
+
+    x: int
+    """
+    the width of the figure grid
+    """
+    y: int
+    """
+    the height of the figure grid
+    """
+    num: int
+    """
+    which graph is this
+    """
+
+    def __init__(self, x, y, num):
+        self.x = x
+        self.y = y
+        self.num = num
+
+    def spec(self) -> matplotlib.gridspec.SubplotSpec:
+        """
+        Creates a SubplotSpec which can then be passed to fig.add_subplot
+        """
+        return matplotlib.gridspec.SubplotSpec(
+            matplotlib.gridspec.GridSpec(self.x, self.y), self.num
+        )
