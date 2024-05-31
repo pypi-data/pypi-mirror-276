4b2e9376fbe9e5f9fcc9a4bb9cb1253624853322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:04 +0000

    Add package uugai-python-kerberos-vault/1.0.0/uugai_python_kerberos_vault-1.0.0.tar.gz

---
+++ packages/uugai-python-kerberos-vault/uugai_python_kerberos_vault-1.0.0.tar.gz/uugai_python_kerberos_vault-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+   name='uugai_python_kerberos_vault',
+   version='1.0.0',
+   description='Kerberos Vault Python library for interacting with the Kerberos Vault.', 
+   author='uug.ai',
+   author_email='support@uug.ai',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   packages=find_packages(),
+   install_requires=requirements
+)
