9d7911f309c33929e869874fd73623d79edcd70e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:16 +0000

    Add package shil/2024.5.30.18.30/shil-2024.5.30.18.30-py3-none-any.whl

---
+++ packages/shil/shil-2024.5.30.18.30-py3-none-any.whl/shil/bin.py
@@ -0,0 +1,7 @@
+""" shil.bin
+"""
+
+from shil.__main__ import entry
+
+if __name__ == "__main__":
+    entry()
---
+++ packages/shil/shil-2024.5.30.18.30-py3-none-any.whl/shil/models/base.py
@@ -0,0 +1,15 @@
+""" shil.models.base
+"""
+
+import pydantic
+
+
+class BaseModel(pydantic.BaseModel):
+    """ """
+
+    class Config:
+        arbitrary_types_allowed = True
+        # frozen = True
+
+    def update(self, **kwargs):
+        return self.__dict__.update(**kwargs)
