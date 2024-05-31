0d144a3462740775f10e62f2266b21a954b367bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 15:05:24 +0000

    Add package punkweb-bb/0.4.1/punkweb_bb-0.4.1.tar.gz

---
+++ packages/punkweb-bb/punkweb_bb-0.4.1.tar.gz/punkweb_bb-0.4.1/punkweb_bb/templatetags/render.py
@@ -0,0 +1,35 @@
+import html
+
+import markdown as md
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+
+from punkweb_bb.bbcode import get_parser, get_shoutbox_parser
+from punkweb_bb.settings import PARSER
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def render(value):
+    if PARSER == "bbcode":
+        parser = get_parser()
+        rendered = parser.format(value)
+        return mark_safe(rendered)
+    elif PARSER == "markdown":
+        escaped = html.escape(value, quote=False)
+        rendered = md.markdown(escaped, extensions=["markdown.extensions.fenced_code"])
+        return mark_safe(rendered)
+    return value
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def render_shout(value):
+    if PARSER == "bbcode":
+        parser = get_shoutbox_parser()
+        rendered = parser.format(value)
+        return mark_safe(rendered)
+    return value
