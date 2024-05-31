4412ca3df06b6f8a8b1e07753322ca58fe790e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:04:50 +0000

    Add package SerialDBPy/0.1.1/SerialDBPy-0.1.1.tar.gz

---
+++ packages/SerialDBPy/SerialDBPy-0.1.1.tar.gz/SerialDBPy-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name='SerialDBPy',
+    version='0.1.1',
+    author='G',
+    author_email='serialdbpy@swimhdr.com',
+    description='Lightweight Python ORM for basic CRUD operations.',
+    license='MIT',
+    url = 'https://github.com/publicsignal',
+    download_url='https://github.com/publicsignal/SerialDBPy/blob/main/dist/SerialDBPy-0.1.1.tar.gz',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
