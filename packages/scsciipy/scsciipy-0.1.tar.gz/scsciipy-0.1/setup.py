db32065eeccfb7caed19fd64b410316065773fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:04:10 +0000

    Add package scsciipy/0.1/scsciipy-0.1.tar.gz

---
+++ packages/scsciipy/scsciipy-0.1.tar.gz/scsciipy-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='scsciipy',
+      version='0.1',
+      description='scsciipy',
+      packages=['scsciipy'],
+      author_email='ilapronin0700@gmail.com',
+      zip_safe=False)
