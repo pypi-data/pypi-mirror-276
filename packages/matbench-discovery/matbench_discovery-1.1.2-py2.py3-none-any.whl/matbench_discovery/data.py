82853eab82178841b87744f41cd1fdb02e4122fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 05:03:55 +0000

    Add package matbench-discovery/1.1.2/matbench_discovery-1.1.2-py2.py3-none-any.whl

---
+++ packages/matbench-discovery/matbench_discovery-1.1.2-py2.py3-none-any.whl/matbench_discovery/data.py
@@ -0,0 +1,277 @@
+"""Download, cache and hydrate data files from the Matbench Discovery Figshare article.
+
+https://figshare.com/articles/dataset/22715158
+"""
+
+import gzip
+import json
+import os
+import pickle
+import sys
+import urllib.error
+import urllib.request
+from collections.abc import Callable
+from glob import glob
+from pathlib import Path
+from typing import Any
+
+import pandas as pd
+from monty.json import MontyDecoder
+from pymatgen.analysis.phase_diagram import PatchedPhaseDiagram
+from tqdm import tqdm
+
+from matbench_discovery import FIGSHARE_DIR
+from matbench_discovery.enums import Key
+
+# ruff: noqa: T201
+
+# repo URL to raw files on GitHub
+RAW_REPO_URL = "https://github.com/janosh/matbench-discovery/raw"
+figshare_versions = sorted(
+    x.split(os.path.sep)[-1].split(".json")[0] for x in glob(f"{FIGSHARE_DIR}/*.json")
+)
+# directory to cache downloaded data files
+default_cache_dir = os.getenv(
+    "MATBENCH_DISCOVERY_CACHE_DIR",
+    f"{os.path.expanduser('~/.cache/matbench-discovery')}/{figshare_versions[-1]}",
+)
+
+
+def as_dict_handler(obj: Any) -> dict[str, Any] | None:
+    """Pass this to json.dump(default=) or as pandas.to_json(default_handler=) to
+    serialize Python classes with as_dict(). Warning: Objects without a as_dict() method
+    are replaced with None in the serialized data.
+    """
+    try:
+        return obj.as_dict()  # all MSONable objects implement as_dict()
+    except AttributeError:
+        return None  # replace unhandled objects with None in serialized data
+        # removes e.g. non-serializable AseAtoms from M3GNet relaxation trajectories
+
+
+def load(
+    key: str,
+    *,
+    version: str = figshare_versions[-1],
+    cache_dir: str | Path = default_cache_dir,
+    hydrate: bool = False,
+    **kwargs: Any,
+) -> pd.DataFrame | PatchedPhaseDiagram:
+    """Download parts of or the full MP training data and WBM test data as pandas
+    DataFrames. The full training and test sets are each about ~500 MB as compressed
+    JSON which will be cached locally to cache_dir for faster re-loading unless
+    cache_dir is set to None.
+
+    See matbench_discovery.data.DATA_FILES for recognized data keys. See [here]
+    (https://janosh.github.io/matbench-discovery/contribute#--direct-download) for file
+    descriptions.
+
+    Args:
+        key (str): Which parts of the MP/WBM data to load. Must be one of
+            list(DATA_FILES).
+        version (str, optional): Which version of the dataset to load. Defaults to
+            latest version of data files published to Figshare. Pass any invalid version
+            to see valid options.
+        cache_dir (str, optional): Where to cache data files on local drive. Defaults to
+            '~/.cache/matbench-discovery'. Set to None to disable caching.
+        hydrate (bool, optional): Whether to hydrate pymatgen objects. If False,
+            Structures and ComputedStructureEntries are returned as dictionaries which
+            can be hydrated on-demand with df.col.map(Structure.from_dict). Defaults to
+            False as it noticeably increases load time.
+        **kwargs: Additional keyword arguments passed to pandas.read_json or read_csv,
+            depending on which file is loaded.
+
+    Raises:
+        ValueError: On bad version number or bad data_key.
+
+    Returns:
+        pd.DataFrame: Single dataframe or dictionary of dfs if multiple data requested.
+    """
+    if version not in figshare_versions:
+        raise ValueError(f"Unexpected {version=}. Must be one of {figshare_versions}.")
+
+    if not isinstance(key, str) or key not in DATA_FILES:
+        raise ValueError(f"Unknown {key=}, must be one of {list(DATA_FILES)}.")
+
+    with open(f"{FIGSHARE_DIR}/{version}.json") as json_file:
+        file_urls = json.load(json_file)["files"]
+
+    file_path = DataFiles.__dict__[key]
+
+    cache_path = f"{cache_dir}/{file_path}"
+    if not os.path.isfile(cache_path):  # download from Figshare URL
+        url = file_urls[key][0]
+        print(f"Downloading {key!r} from {url}")
+        try:
+            # ensure directory exists
+            os.makedirs(os.path.dirname(cache_path), exist_ok=True)
+            # download and save to disk
+            urllib.request.urlretrieve(url, cache_path)
+            print(f"Cached {key!r} to {cache_path!r}")
+        except urllib.error.HTTPError as exc:
+            raise ValueError(f"Bad {url=}") from exc
+        except Exception:
+            print(f"\n\nvariable dump:\n{file_path=},\n{url=}")
+            raise
+
+    print(f"Loading {key!r} from cached file at {cache_path!r}")
+    if ".pkl" in file_path:  # handle key='mp_patched_phase_diagram' separately
+        with gzip.open(cache_path, "rb") as zip_file:
+            return pickle.load(zip_file)
+    if ".pth" in file_path:  # handle model checkpoints (e.g. key='alignn_checkpoint')
+        return cache_path
+
+    csv_ext = (".csv", ".csv.gz", ".csv.bz2")
+    reader = pd.read_csv if file_path.endswith(csv_ext) else pd.read_json
+    try:
+        df = reader(cache_path, **kwargs)
+    except Exception:
+        print(f"\n\nvariable dump:\n{file_path=},\n{reader=}\n{kwargs=}")
+        raise
+
+    if Key.mat_id in df:
+        df = df.set_index(Key.mat_id)
+    if hydrate:
+        for col in df:
+            if not isinstance(df[col].iloc[0], dict):
+                continue
+            try:
+                # convert dicts to pymatgen Structures and ComputedStructureEntries
+                df[col] = [
+                    MontyDecoder().process_decoded(dct)
+                    for dct in tqdm(df[col], desc=col)
+                ]
+            except Exception:
+                print(f"\n\nvariable dump:\n{col=},\n{df[col]=}")
+                raise
+
+    return df
+
+
+def glob_to_df(
+    pattern: str,
+    *,
+    reader: Callable[[Any], pd.DataFrame] | None = None,
+    pbar: bool = True,
+    **kwargs: Any,
+) -> pd.DataFrame:
+    """Combine data files matching a glob pattern into a single dataframe.
+
+    Args:
+        pattern (str): Glob file pattern.
+        reader (Callable[[Any], pd.DataFrame], optional): Function that loads data from
+            disk. Defaults to pd.read_csv if ".csv" in pattern else pd.read_json.
+        pbar (bool, optional): Whether to show progress bar. Defaults to True.
+        **kwargs: Keyword arguments passed to reader (i.e. pd.read_csv or pd.read_json).
+
+    Returns:
+        pd.DataFrame: Combined dataframe.
+    """
+    reader = reader or pd.read_csv if ".csv" in pattern.lower() else pd.read_json
+
+    # prefix pattern with ROOT if not absolute path
+    files = glob(pattern)
+    if len(files) == 0:
+        raise FileNotFoundError(f"No files matching glob {pattern=}")
+
+    sub_dfs = {}  # used to join slurm job array results into single df
+    for file in tqdm(files, disable=not pbar):
+        df = reader(file, **kwargs)
+        sub_dfs[file] = df
+
+    return pd.concat(sub_dfs.values())
+
+
+class Files(dict):  # type: ignore[type-arg]
+    """Files instance inherits from dict so that .values(), items(), etc. are supported
+    but also allows accessing attributes by dot notation. E.g. FILES.some_file_key.
+    This enables tab completion in IDEs and auto-updating attribute names across the
+    code base when changing the key of a file.
+    """
+
+    def __init__(
+        self, root: str = default_cache_dir, key_map: dict[str, str] | None = None
+    ) -> None:
+        """Create a Files instance.
+
+        Args:
+            root (str, optional): Root directory used to absolufy every file path.
+            Defaults to '~/.cache/matbench-discovery/[latest_figshare_release]' where
+                latest_figshare_release is e.g. 1.0.0. Can also be set through env var
+                MATBENCH_DISCOVERY_CACHE_DIR.
+            key_map (dict[str, str], optional): Mapping from attribute names to keys in
+                the dict. Useful if you want to have keys like 'foo+bar' that are not
+                valid Python identifiers. Defaults to None.
+        """
+        # callback that's triggered when a file corresponding to a class attribute does
+        # not exist. gets passed key and missing filepath. set to None to disable
+        self._on_not_found: Callable[[str, str], None] | None = lambda key, path: print(
+            f"Warning: {path!r} associated with {key=} does not exist.",
+            file=sys.stderr,
+        )
+
+        rel_paths = {
+            (key_map or {}).get(key, key): file
+            for key, file in type(self).__dict__.items()
+            if not key.startswith("_")
+        }
+        abs_paths = {key: f"{root}/{file}" for key, file in rel_paths.items()}
+        self.__dict__ = abs_paths
+        super().__init__(abs_paths)
+
+    def __getattribute__(self, key: str) -> str:
+        """Override __getattr__ to check if key matches a file attribute."""
+        filepath = super().__getattribute__(key)
+        if key in self and not os.path.isfile(filepath) and self._on_not_found:
+            self._on_not_found(key, filepath)
+        return filepath
+
+
+class DataFiles(Files):
+    """Data files provided by Matbench Discovery.
+    See https://janosh.github.io/matbench-discovery/contribute for data descriptions.
+    """
+
+    def _on_not_found(self, key: str, path: str) -> None:  # type: ignore[override]
+        msg = (
+            f"{path!r} associated with {key=} does not exist. Would you like to "
+            "download it now? This will cache the file for future use."
+        )
+
+        # default to 'y' if not in interactive session, and user can't answer
+        answer = "" if sys.stdin.isatty() else "y"
+        while answer not in ("y", "n"):
+            answer = input(f"{msg} [y/n] ").lower().strip()
+        if answer == "y":
+            load(key)  # download and cache data file
+
+    # TODO maybe set attrs to None and load file names from Figshare json
+    mp_computed_structure_entries = (
+        "mp/2023-02-07-mp-computed-structure-entries.json.gz"
+    )
+    mp_elemental_ref_entries = "mp/2023-02-07-mp-elemental-reference-entries.json.gz"
+    mp_energies = "mp/2023-01-10-mp-energies.csv.gz"
+    mp_patched_phase_diagram = "mp/2023-02-07-ppd-mp.pkl.gz"
+    mp_trj_extxyz = "mp/2023-11-22-mp-trj-extxyz-by-yuan.zip"
+    # snapshot of every task (calculation) in MP as of 2023-03-16 (14 GB)
+    all_mp_tasks = "mp/2023-03-16-all-mp-tasks.zip"
+
+    wbm_computed_structure_entries = (
+        "wbm/2022-10-19-wbm-computed-structure-entries.json.bz2"
+    )
+    wbm_initial_structures = "wbm/2022-10-19-wbm-init-structs.json.bz2"
+    wbm_cses_plus_init_structs = (
+        "wbm/2022-10-19-wbm-computed-structure-entries+init-structs.json.bz2"
+    )
+    wbm_summary = "wbm/2023-12-13-wbm-summary.csv.gz"
+
+    alignn_checkpoint = "2023-06-02-pbenner-best-alignn-model.pth.zip"
+
+
+# data files can be downloaded and cached with matbench_discovery.data.load()
+DATA_FILES = DataFiles()
+
+
+df_wbm = load("wbm_summary")
+# str() around Key.mat_id added for https://github.com/janosh/matbench-discovery/issues/81
+df_wbm[str(Key.mat_id)] = df_wbm.index
---
+++ packages/matbench-discovery/matbench_discovery-1.1.2-py2.py3-none-any.whl/matbench_discovery/enums.py
@@ -0,0 +1,217 @@
+"""Enums used as keys/accessors for dicts and dataframes across Matbench Discovery."""
+
+from enum import StrEnum, unique
+from typing import Self
+
+from pymatviz.utils import styled_html_tag
+
+
+class LabelEnum(StrEnum):
+    """StrEnum with optional label and description attributes plus dict() methods."""
+
+    def __new__(
+        cls, val: str, label: str | None = None, desc: str | None = None
+    ) -> Self:
+        """Create a new class."""
+        member = str.__new__(cls, val)
+        member._value_ = val
+        member.__dict__ |= dict(label=label, desc=desc)
+        return member
+
+    @property
+    def label(self) -> str:
+        """Make label read-only."""
+        return self.__dict__["label"]
+
+    @property
+    def description(self) -> str:
+        """Make description read-only."""
+        return self.__dict__["desc"]
+
+    @classmethod
+    def key_val_dict(cls) -> dict[str, str]:
+        """Map of keys to values."""
+        return {key: str(val) for key, val in cls.__members__.items()}
+
+    @classmethod
+    def val_label_dict(cls) -> dict[str, str | None]:
+        """Map of values to labels."""
+        return {str(val): val.label for val in cls.__members__.values()}
+
+    @classmethod
+    def val_desc_dict(cls) -> dict[str, str | None]:
+        """Map of values to descriptions."""
+        return {str(val): val.description for val in cls.__members__.values()}
+
+    @classmethod
+    def label_desc_dict(cls) -> dict[str | None, str | None]:
+        """Map of labels to descriptions."""
+        return {str(val.label): val.description for val in cls.__members__.values()}
+
+
+@unique
+class Key(LabelEnum):
+    """Keys used to access dataframes columns."""
+
+    arity = "arity", "Arity"
+    bandgap_pbe = "bandgap_pbe", "PBE Band Gap"
+    chem_sys = "chemical_system", "Chemical System"
+    composition = "composition", "Composition"
+    cse = "computed_structure_entry", "Computed Structure Entry"
+    daf = "DAF", "Discovery Acceleration Factor"
+    dft_energy = "uncorrected_energy", "DFT Energy"
+    e_form = "e_form_per_atom_mp2020_corrected", "DFT E_form"
+    e_form_pred = "e_form_per_atom_pred", "Predicted E_form"
+    e_form_raw = "e_form_per_atom_uncorrected", "DFT E_form raw"
+    e_form_wbm = "e_form_per_atom_wbm", "WBM E_form"
+    each = "energy_above_hull", "E<sub>hull dist</sub>"
+    each_pred = "e_above_hull_pred", "Predicted E<sub>hull dist</sub>"
+    each_true = "e_above_hull_mp2020_corrected_ppd_mp", "E<sub>MP hull dist</sub>"
+    each_wbm = "e_above_hull_wbm", "E<sub>WBM hull dist</sub>"
+    final_struct = "relaxed_structure", "Relaxed Structure"
+    forces = "forces", "Forces"
+    form_energy = "formation_energy_per_atom", "Formation Energy (eV/atom)"
+    formula = "formula", "Formula"
+    init_struct = "initial_structure", "Initial Structure"
+    magmoms = "magmoms", "Magnetic Moments"
+    mat_id = "material_id", "Material ID"
+    each_mean_models = "each_mean_models", "E<sub>hull dist</sub> mean of models"
+    each_err_models = "each_err_models", "E<sub>hull dist</sub> mean error of models"
+    model_std_each = "each_std_models", "Std. dev. over models"
+    n_sites = "n_sites", "Number of Sites"
+    site_nums = "site_nums", "Site Numbers", "Atomic numbers for each crystal site"
+    spacegroup = "spacegroup", "Spacegroup"
+    stress = "stress", "Stress"
+    stress_trace = "stress_trace", "Stress Trace"
+    struct = "structure", "Structure"
+    task_id = "task_id", "Task ID"
+    task_type = "task_type", "Task Type"
+    train_task = "train_task", "Training Task"
+    test_task = "test_task", "Test Task"
+    targets = "targets", "Targets"
+    # lowest WBM structures for a given prototype that isn't already in MP
+    uniq_proto = "unique_prototype", "Unique Prototype"
+    volume = "volume", "Volume (Å³)"
+    wyckoff = "wyckoff_spglib", "Aflow-Wyckoff Label"  # relaxed structure Aflow label
+    init_wyckoff = (
+        "wyckoff_spglib_initial_structure",
+        "Aflow-Wyckoff Label Initial Structure",
+    )
+    # number of structures in a model's training set
+    train_set = "train_set", "Training Set"
+    model_params = "model_params", "Model Params"  # model's parameter count
+    model_type = "model_type", "Model Type"  # number of parameters in the model
+    openness = "openness", "Openness"  # openness of data and code for a model
+
+
+@unique
+class Task(LabelEnum):
+    """Thermodynamic stability prediction task types."""
+
+    IS2RE = "IS2RE", "initial structure to relaxed energy"
+    RS2RE = "RS2RE", "relaxed structure to relaxed energy"
+    S2EFSM = "S2EFSM", "structure to energy force stress magmom"
+    S2EFS = "S2EFS", "structure to energy force stress"
+    # S2RE is for models that learned a discrete version of PES like CGCNN+P
+    S2RE = "S2RE", "structure to relaxed energy"
+    RP2RE = "RP2RE", "relaxed prototype to relaxed energy"
+    IP2RE = "IP2RE", "initial prototype to relaxed energy"
+    IS2E = "IS2E", "initial structure to energy"
+    IS2RE_SR = "IS2RE-SR", "initial structure to relaxed energy after ML relaxation"
+
+
+@unique
+class Targets(LabelEnum):
+    """Thermodynamic stability prediction task types."""
+
+    E = "E", "energy"
+    EFS = "EFS", "energy forces stress"
+    EFSM = "EFSM", "energy forces stress magmoms"
+
+
+@unique
+class ModelType(LabelEnum):
+    """Model types."""
+
+    GNN = "GNN", "graph neural network"
+    UIP = "UIP-GNN", "universal interatomic potential"
+    BO_GNN = "BO-GNN", "GNN in a Bayesian optimization loop"
+    Fingerprint = "Fingerprint", "models with structural fingerprint features"  # ex. RF
+    Transformer = "Transformer", "transformer-based models"  # ex. Wrenformer
+    RF = "RF", "random forest"
+
+
+@unique
+class Open(LabelEnum):
+    """Openness of data and code for a model."""
+
+    OSOD = "OSOD", "open source, open data"
+    CSOD = "CSOD", "closed source, open data"
+    OSCD = "OSCD", "open source, closed data"
+    CSCD = "CSCD", "closed source, closed data"
+
+
+@unique
+class TestSubset(LabelEnum):
+    """Which subset of the test data to use for evaluation."""
+
+    uniq_protos = "uniq_protos", "Unique Structure Prototypes"
+    ten_k_most_stable = "10k_most_stable", "10k Most Stable"
+    full = "full", "Full Test Set"
+
+
+eV_per_atom = styled_html_tag(  # noqa: N816
+    "(eV/atom)", tag="span", style="font-size: 0.8em; font-weight: lighter;"
+)
+
+
+class Quantity(LabelEnum):
+    """Quantity labels for plots."""
+
+    n_atoms = "Atom Count"
+    n_elems = "Element Count"
+    crystal_sys = "Crystal system"
+    spg_num = "Space group"
+    n_wyckoff = "Number of Wyckoff positions"
+    n_sites = "Number of atoms"
+    energy_per_atom = f"Energy {eV_per_atom}"
+    e_form = f"DFT E<sub>form</sub> {eV_per_atom}"
+    e_above_hull = f"E<sub>hull dist</sub> {eV_per_atom}"
+    e_above_hull_mp2020_corrected_ppd_mp = f"DFT E<sub>hull dist</sub> {eV_per_atom}"
+    e_above_hull_pred = f"Predicted E<sub>hull dist</sub> {eV_per_atom}"
+    e_above_hull_mp = f"E<sub>above MP hull</sub> {eV_per_atom}"
+    e_above_hull_error = f"Error in E<sub>hull dist</sub> {eV_per_atom}"
+    vol_diff = "Volume difference (A^3)"
+    e_form_per_atom_mp2020_corrected = f"DFT E<sub>form</sub> {eV_per_atom}"
+    e_form_per_atom_pred = f"Predicted E<sub>form</sub> {eV_per_atom}"
+    material_id = "Material ID"
+    band_gap = "Band gap (eV)"
+    formula = "Formula"
+    stress = "σ (eV/Å³)"  # noqa: RUF001
+    stress_trace = "1/3 Tr(σ) (eV/Å³)"  # noqa: RUF001
+
+
+class Model(LabelEnum):
+    """Model labels for plots."""
+
+    alignn_ff = "ALIGNN FF"
+    alignn_pretrained = "ALIGNN Pretrained"
+    alignn = "ALIGNN"
+    bowsr_megnet = "BOWSR"
+    cgcnn_p = "CGCNN+P"
+    cgcnn = "CGCNN"
+    chgnet_megnet = "CHGNet→MEGNet"
+    chgnet = "CHGNet"
+    dft = "DFT"
+    gnome = "GNoME"
+    m3gnet_direct = "M3GNet DIRECT"
+    m3gnet_megnet = "M3GNet→MEGNet"
+    m3gnet_ms = "M3GNet MS"
+    m3gnet = "M3GNet"
+    mace = "MACE"
+    megnet_rs2re = "MEGNet RS2RE"
+    megnet = "MEGNet"
+    pfp = "PFP"
+    voronoi_rf = "Voronoi RF"
+    wbm = "WBM"
+    wrenformer = "Wrenformer"
