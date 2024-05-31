ec64945556a17c12d6f72e2258ce9f1f9c43ed5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:05:24 +0000

    Add package rocketflightsim/0.1.2/rocketflightsim-0.1.2-py3-none-any.whl

---
+++ packages/rocketflightsim/rocketflightsim-0.1.2-py3-none-any.whl/rocketflightsim/flight_simulation.py
@@ -0,0 +1,396 @@
+# Import libraries
+import sys
+import os
+
+import pandas as pd
+import numpy as np
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'examples')))
+from . import helper_functions as hfunc
+from . import constants as con
+
+# Default timestep for the simulation
+default_timestep = 0.02
+""" Notes on timesteps:
+
+The default for OpenRocket sims is 0.01s for the first while, and then somewhere between 0.02 and 0.05 for a while, and then 0.05 for most of the rest of the ascent. It simulates more complicated dynamics than we do
+
+A timestep of 0.02s gives apogees a difference of a few feet for a 10k launch compared to using 0.001s. 0.001s can still be used for one-off sims, but when running many sims, 0.02s is better.
+"""
+
+# TODO consider splitting simulator into stages. Could be better for readability, but also for allowing more complex simulations with multiple stages, and going beyond the troposphere with different lapse rates, a different gravity model, and a different wind model. Could even use it for educational purposes like showing the importance of having a launch rail
+# Flight simulation function
+def simulate_flight(rocket, launch_conditions, timestep=default_timestep):
+    """
+    Simulate the flight of a rocket until its apogee given its specifications and launch conditions.
+
+    Args:
+    - rocket (Rocket): An instance of the Rocket class.
+    - launch_conditions (LaunchConditions): An instance of the LaunchConditions class.
+    - timestep (float, optional): The time increment for the simulation in seconds.
+
+    Returns:
+    - tuple: A tuple containing the dataset of simulation results and indices of key flight events.
+    """
+
+    # Initialize rocket parameters and launch conditions
+    dry_mass = rocket.dry_mass
+    fuel_mass_lookup = rocket.motor.fuel_mass_curve
+    engine_thrust_lookup = rocket.motor.thrust_curve
+    Cd_A_rocket_fn = rocket.Cd_A_rocket
+    burnout_time = rocket.motor.burn_time
+
+    # Extract launch condition parameters
+    launchpad_pressure = launch_conditions.launchpad_pressure
+    launchpad_temp = launch_conditions.launchpad_temp
+    L_launch_rail = launch_conditions.L_launch_rail
+    launch_angle = launch_conditions.launch_angle
+    
+    T_lapse_rate = launch_conditions.local_T_lapse_rate
+    F_gravity = launch_conditions.local_gravity
+
+    # Initialize simulation variables
+    time, height, speed, a_y, a_x, v_y, v_x, Ma, q = 0, 0, 0, 0, 0, 0, 0, 0, 0
+    angle_to_vertical = np.deg2rad(90 - launch_angle)
+
+    # Calculate constants to be used in air density function, set initial air density
+    multiplier = launchpad_pressure / (con.R_specific_air * pow(launchpad_temp, - F_gravity / (con.R_specific_air * T_lapse_rate)))
+    exponent = - F_gravity / (con.R_specific_air * T_lapse_rate) - 1
+    air_density = hfunc.air_density_optimized(launchpad_temp, multiplier, exponent)
+
+    # Store the initial state of the rocket
+    simulated_values = [
+        [
+            time,
+            height,
+            speed,
+            a_y,
+            a_x,
+            v_y,
+            v_x,
+            launchpad_temp,
+            air_density,
+            q,
+            Ma,
+            angle_to_vertical,
+        ]
+    ]
+
+    # Calculate trigonometric ratios on the launch rail
+    cos_rail_angle_to_vertical = np.cos(angle_to_vertical)
+    sin_rail_angle_to_vertical = np.sin(angle_to_vertical)
+
+    # Simulate motor burn until liftoff
+    takeoff_thrust = F_gravity * cos_rail_angle_to_vertical
+    while (
+        hfunc.thrust_at_time(time, engine_thrust_lookup)
+        / hfunc.mass_at_time(time, dry_mass, fuel_mass_lookup)
+        <= takeoff_thrust
+    ):
+        time += timestep
+        # Append simulation values for each timestep
+        simulated_values.append(
+            [
+                time,
+                height, # 0
+                speed, # 0
+                a_y, # 0
+                a_x, # 0
+                v_y, # 0
+                v_x, # 0
+                launchpad_temp,
+                air_density,
+                q, # 0
+                Ma, # 0
+                angle_to_vertical,
+            ]
+        )
+
+    liftoff_index = len(simulated_values)
+
+    # Liftoff until launch rail cleared
+    time += timestep
+    effective_L_launch_rail = L_launch_rail - rocket.h_second_rail_button
+    effective_h_launch_rail = effective_L_launch_rail * cos_rail_angle_to_vertical
+
+    # Simulate flight from liftoff until the launch rail is cleared
+    while height < effective_h_launch_rail:
+        # Update environmental conditions based on height
+        temperature = hfunc.temp_at_height(height, launchpad_temp, lapse_rate = T_lapse_rate)
+        air_density = hfunc.air_density_optimized(temperature, multiplier,exponent)
+
+        # Calculate Mach number, drag coefficient, and forces
+        Ma = hfunc.mach_number_fn(speed, temperature)
+        Cd_A_rocket = Cd_A_rocket_fn(Ma)
+        q = hfunc.calculate_dynamic_pressure(air_density, speed)
+        F_drag = q * Cd_A_rocket
+
+        # Update rocket's motion parameters
+        mass = hfunc.mass_at_time(time, dry_mass, fuel_mass_lookup)
+        thrust = hfunc.thrust_at_time(time, engine_thrust_lookup)
+        a_rail = (thrust - F_drag) / mass - F_gravity * cos_rail_angle_to_vertical
+        a_y = a_rail * cos_rail_angle_to_vertical
+        a_x = a_rail * sin_rail_angle_to_vertical
+        v_y += a_y * timestep
+        v_x += a_x * timestep
+        speed = np.sqrt(v_y**2 + v_x**2)
+        height += v_y * timestep
+
+        # Append updated simulation values
+        simulated_values.append(
+            [
+                time,
+                height,
+                speed,
+                a_y,
+                a_x,
+                v_y,
+                v_x,
+                temperature,
+                air_density,
+                q,
+                Ma,
+                angle_to_vertical,
+            ]
+        )
+        time += timestep
+
+    launch_rail_cleared_index = len(simulated_values)
+
+    # Flight from launch rail cleared until burnout
+    while time < burnout_time:
+        # Update environmental conditions based on height
+        temperature = hfunc.temp_at_height(height, launchpad_temp, lapse_rate = T_lapse_rate)
+        air_density = hfunc.air_density_optimized(temperature, multiplier,exponent)
+
+        # Calculate Mach number, Drag coefficient, and Forces
+        Ma = hfunc.mach_number_fn(speed, temperature)
+        Cd_A_rocket = Cd_A_rocket_fn(Ma)
+        q = hfunc.calculate_dynamic_pressure(air_density, speed)
+        F_drag = q * Cd_A_rocket
+
+        # Update rocket's motion parameters
+        mass = hfunc.mass_at_time(time, dry_mass, fuel_mass_lookup)
+        thrust = hfunc.thrust_at_time(time, engine_thrust_lookup)
+        a_y = (thrust - F_drag) * np.cos(angle_to_vertical) / mass - F_gravity
+        a_x = (thrust - F_drag) * np.sin(angle_to_vertical) / mass
+        v_y += a_y * timestep
+        v_x += a_x * timestep
+        speed = np.sqrt(v_y**2 + v_x**2)
+        height += v_y * timestep
+
+        # Recalculate the angle to the veritcal
+        angle_to_vertical = np.arctan(v_x / v_y)
+
+        # Append updated simulation values
+        simulated_values.append(
+            [
+                time,
+                height,
+                speed,
+                a_y,
+                a_x,
+                v_y,
+                v_x,
+                temperature,
+                air_density,
+                q,
+                Ma,
+                angle_to_vertical,
+            ]
+        )
+
+        time += timestep
+
+    burnout_index = len(simulated_values)
+
+    # Flight from burnout to apogee
+    mass = dry_mass
+
+    while v_y > 0:
+        temperature = hfunc.temp_at_height(height, launchpad_temp, lapse_rate = T_lapse_rate)
+        air_density = hfunc.air_density_optimized(temperature, multiplier,exponent)
+
+        # Calculate Mach number, Drag coefficient, and Forces
+        Ma = hfunc.mach_number_fn(speed, temperature)
+        Cd_A_rocket = Cd_A_rocket_fn(Ma)
+        q = hfunc.calculate_dynamic_pressure(air_density, speed)
+        F_drag = q * Cd_A_rocket
+
+        # Update rocket's motion parameters
+        a_y = -F_drag * np.cos(angle_to_vertical) / mass - F_gravity
+        a_x = -F_drag * np.sin(angle_to_vertical) / mass
+        v_y += a_y * timestep
+        v_x += a_x * timestep
+        speed = np.sqrt(v_y**2 + v_x**2)
+        height += v_y * timestep
+
+        # Recalculate the angle to the veritcal
+        angle_to_vertical = np.arctan(v_x / v_y)
+
+        # Append updated simulation values
+        simulated_values.append(
+            [
+                time,
+                height,
+                speed,
+                a_y,
+                a_x,
+                v_y,
+                v_x,
+                temperature,
+                air_density,
+                q,
+                Ma,
+                angle_to_vertical,
+            ]
+        )
+
+        time += timestep
+
+    # Mark the index at apogee
+    apogee_index = len(simulated_values)
+    simulated_values[-1][11] = simulated_values[-2][11]
+
+    # Convert the list of simulation values to a DataFrame for easier analysis and visualization
+    dataset = pd.DataFrame(
+        simulated_values,
+        columns=[
+            "time",
+            "height",
+            "speed",
+            "a_y",
+            "a_x",
+            "v_y",
+            "v_x",
+            "temperature",
+            "air_density",
+            "q",
+            "Ma",
+            "angle_to_vertical",
+        ],
+    )
+
+    return (
+        dataset,
+        liftoff_index,
+        launch_rail_cleared_index,
+        burnout_index,
+        apogee_index
+    )
+
+
+# Flight with airbrakes simulation function
+def simulate_airbrakes_flight(pre_brake_flight, rocket, launch_conditions, airbrakes, timestep = default_timestep):
+    """
+    Simulate the flight of a rocket during its post-burnout ascent with airbrakes deployed until apogee, given its specifications and environmental conditions.
+
+    Args:
+    - pre_brake_flight (DataFrame): A DataFrame containing the simulation results from the rocket's ascent until burnout. # TODO: maybe change to a tuple of the dataset at the time to start the simulation?
+    - rocket (Rocket): An instance of the Rocket class.
+    - launch_conditions (LaunchConditions): An instance of the LaunchConditions class.
+    - airbrakes (Airbrakes): An instance of the Airbrakes class.
+    - timestep (float, optional): The time increment for the simulation in seconds.
+
+    Returns:
+    - DataFrame: A DataFrame containing the simulation results from the rocket's post-burnout ascent with airbrakes deployed until apogee.
+    """
+    # Extract rocket parameters
+    mass = rocket.dry_mass
+    Cd_A_rocket_fn = rocket.Cd_A_rocket
+
+    # Extract launch condition parameters
+    launchpad_temp = pre_brake_flight.temperature.iloc[0]
+    launchpad_pressure = pre_brake_flight.air_density.iloc[0] * con.R_specific_air * launchpad_temp
+    
+    T_lapse_rate = launch_conditions.local_T_lapse_rate    
+    F_gravity = launch_conditions.local_gravity
+
+    # Calculate constants to be used in air density function
+    multiplier = launchpad_pressure / (con.R_specific_air * pow(launchpad_temp, - F_gravity / (con.R_specific_air * T_lapse_rate)))
+    exponent = - F_gravity / (con.R_specific_air * T_lapse_rate) - 1
+
+    # Extract airbrakes parameters
+    Cd_brakes = airbrakes.Cd_brakes
+    max_deployment_rate = np.deg2rad(airbrakes.max_deployment_rate)
+    max_deployment_angle = np.deg2rad(airbrakes.max_deployment_angle)
+    A_brakes = airbrakes.num_flaps * airbrakes.A_flap
+
+    pre_brake_flight["deployment_angle"] = 0
+
+    # Initialize simulation variables
+    height = pre_brake_flight["height"].iloc[-1]
+    speed = pre_brake_flight["speed"].iloc[-1]
+    v_y = pre_brake_flight["v_y"].iloc[-1]
+    v_x = pre_brake_flight["v_x"].iloc[-1]
+    time = pre_brake_flight["time"].iloc[-1]
+    angle_to_vertical = np.arctan(v_x / v_y)
+
+    deployment_angle = 0
+
+    # for efficiency, may be removed if/when the simulation is made more accurate by the cd of the brakes changing during the sim:
+    A_Cd_brakes = A_brakes * Cd_brakes
+
+    simulated_values = []
+    while v_y > 0:
+        time += timestep
+
+        temperature = hfunc.temp_at_height(height, launchpad_temp, lapse_rate = T_lapse_rate)
+        air_density = hfunc.air_density_optimized(temperature, multiplier,exponent)
+        Ma = hfunc.mach_number_fn(speed, temperature)
+        Cd_A_rocket = Cd_A_rocket_fn(Ma)
+        q = hfunc.calculate_dynamic_pressure(air_density, speed)
+
+        deployment_angle = min(max_deployment_angle, deployment_angle + max_deployment_rate * timestep)
+
+        F_drag = q * (np.sin(deployment_angle) * A_Cd_brakes + Cd_A_rocket)
+        a_y = -F_drag * np.cos(angle_to_vertical) / mass - F_gravity
+        a_x = -F_drag * np.sin(angle_to_vertical) / mass
+        v_y += a_y * timestep
+        v_x += a_x * timestep
+        speed = np.sqrt(v_y**2 + v_x**2)
+        height += v_y * timestep
+
+        angle_to_vertical = np.arctan(v_x / v_y)
+
+        simulated_values.append(
+            [
+                time,
+                height,
+                speed,
+                a_y,
+                a_x,
+                v_y,
+                v_x,
+                temperature,
+                air_density,
+                q,
+                Ma,
+                angle_to_vertical,
+                deployment_angle,
+            ]
+        )
+
+    simulated_values[-1][11] = simulated_values[-2][11]
+
+    airbrakes_ascent = pd.DataFrame(
+        simulated_values,
+        columns=[
+            "time",
+            "height",
+            "speed",
+            "a_y",
+            "a_x",
+            "v_y",
+            "v_x",
+            "temperature",
+            "air_density",
+            "q",
+            "Ma",
+            "angle_to_vertical",
+            "deployment_angle",
+        ],
+    )
+
+    return airbrakes_ascent
+
---
+++ packages/rocketflightsim/rocketflightsim-0.1.2-py3-none-any.whl/rocketflightsim/rocket_classes.py
@@ -0,0 +1,241 @@
+# Define Motor, Rocket, LaunchConditions, and Airbrakes classes
+
+import numpy as np
+
+from . import constants as con
+from . import helper_functions as hfunc
+
+class Motor:
+    """
+    The Motor class is used to store the properties of a rocket motor. The properties are:
+
+    - dry_mass: mass of the motor without fuel (kg)
+    - thrust_curve: dictionary of thrust (N) at time (s after ignition)
+    - total_impulse: total impulse of the motor (Ns)
+    - burn_time: time it takes for the motor to burn all of its fuel (s)
+    - fuel_mass_curve: dictionary of mass (kg) at time (s after ignition)
+    - fuel_mass: total mass of fuel in the motor before ignition (kg)
+
+    If fuel_mass_curve is not provided but feul_mass is, fuel_mass_curve is calculated from the thrust_curve and fuel_mass (assuming fuel burn is proportional to thrust). If fuel_mass_curve is provided, fuel_mass is set to the initial mass in fuel_mass_curve. If neither are provided, fuel_mass and fuel_mass_curve are set to 0.
+    """
+    
+    def __init__(self, dry_mass, thrust_curve, fuel_mass_curve=None, fuel_mass=None):
+        self.dry_mass = dry_mass
+        self.thrust_curve = thrust_curve
+
+        self.total_impulse = np.trapz(list(thrust_curve.values()), list(thrust_curve.keys()))
+        self.burn_time = max(thrust_curve.keys())
+        
+        if fuel_mass_curve:
+            self.fuel_mass_curve = fuel_mass_curve
+            self.fuel_mass = fuel_mass_curve[0]
+        elif fuel_mass:
+            self.fuel_mass = fuel_mass
+            self.fuel_mass_curve = {0: fuel_mass}
+            times = list(self.thrust_curve.keys())
+            for t in range(1, len(times)):
+                self.fuel_mass_curve[times[t]] = self.fuel_mass_curve[times[t-1]] - (thrust_curve[times[t]] + thrust_curve[times[t-1]])/2 * (times[t] - times[t-1]) / self.total_impulse * fuel_mass
+        else:
+            self.fuel_mass = 0
+            self.fuel_mass_curve = {
+                0: 0,
+                self.burn_time: 0
+                }
+
+
+class Rocket:
+    """
+    The Rocket class is used to store the properties of a rocket.
+
+    Attributes
+    ----------
+    rocket_mass : float
+        Dry mass of the rocket without the motor (kg).
+    motor : Motor object
+        The rocket's motor.
+    A_rocket : float
+        Cross-sectional area of the rocket (m^2) used when Cd_rocket_at_Ma was calculated.
+    Cd_rocket_at_Ma : float or function
+        Coefficient of drag of the rocket. May be given as a function of Mach number or as a constant.
+    h_second_rail_button : float
+        Height of the second rail button from the bottom of the rocket (m). This is the upper button if there are only 2.
+    dry_mass : float
+        Total mass of the rocket without fuel (kg).
+    Cd_A_rocket : function
+        Coefficient of drag of the rocket multiplied by the cross-sectional area of the rocket (m^2).
+    """
+
+    def __init__(
+        self,
+        rocket_mass : float,
+        motor : Motor,
+        A_rocket : float,
+        Cd_rocket_at_Ma = 0.45,
+        h_second_rail_button : float = 0.69,
+    ):
+        """Initializes the Rocket object.
+
+        Parameters
+        ----------
+        rocket_mass : float
+            Dry mass of the rocket without the motor (kg).
+        motor : Motor object
+            The rocket's motor.
+        A_rocket : float
+            Cross-sectional area of the rocket (m^2). Must be the same used when the Cd_rocket_at_Ma was calculated.
+        Cd_rocket_at_Ma : float or function, optional
+            Coefficient of drag of the rocket. May be given as a function of Mach number or as a constant. Defaults to a constant 0.45, which is in the ballpark of what most student team competition rockets our size have.
+        h_second_rail_button : float, optional
+            Height of the second rail button from the bottom of the rocket (m). This is the upper button if there are only 2. Defaults to 0.7m, which is reasonable for most student team competition rockets. Doesn't matter much if it's not set as it changes apogee by less than 10ft on a 10k ft launch when set to 0.
+        """
+
+        self.rocket_mass = rocket_mass
+        self.motor = motor
+        self.A_rocket = A_rocket
+        self.Cd_rocket_at_Ma = Cd_rocket_at_Ma
+        self.h_second_rail_button = h_second_rail_button
+
+        self.dry_mass = rocket_mass + motor.dry_mass
+        
+        if callable(Cd_rocket_at_Ma):
+            def Cd_A_rocket_fn(Ma):
+                return Cd_rocket_at_Ma(Ma) * A_rocket
+        else:
+            def Cd_A_rocket_fn(Ma): return Cd_rocket_at_Ma * A_rocket
+            # TODO: make it actually operate as a constant if it's not a function
+        self.Cd_A_rocket = Cd_A_rocket_fn
+
+
+class LaunchConditions:
+    """The LaunchConditions class is used to store the properties of the launch conditions. 
+
+    Attributes
+    ----------
+    launchpad_pressure : float
+        Pressure at the launchpad (Pa).
+    launchpad_temp : float
+        Temperature at the launchpad (°C).
+    L_launch_rail : float
+        Length of the launch rail (m).
+    launch_angle : float
+        Launch angle from horizontal (deg).
+    local_gravity : float
+        Acceleration due to gravity at the launch site (m/s^2).
+    local_T_lapse_rate : float
+        Temperature lapse rate at the launch site (°C/m, K/m).
+    """
+
+    def __init__(
+        self, 
+        launchpad_pressure: float,
+        launchpad_temp: float,
+        L_launch_rail: float,
+        launch_angle: float,
+        local_gravity: float = None,
+        local_T_lapse_rate: float = con.T_lapse_rate,
+        latitude: float = None,
+        altitude: float = 0,
+    ):
+        """Initializes the LaunchConditions object. 
+        
+        Parameters
+        ----------
+        launchpad_pressure : float
+            Pressure at the launchpad (Pa).
+        launchpad_temp : float
+            Temperature at the launchpad (°C).
+        L_launch_rail : float
+            Length of the launch rail (m).
+        launch_angle : float
+            Launch angle from horizontal (deg).
+        local_gravity : float, optional
+            Acceleration due to gravity at the launch site (m/s^2). Defaults to 9.80665.
+        local_T_lapse_rate : float, optional
+            Temperature lapse rate at the launch site (°C/m, K/m). Defaults to -0.0065.
+        latitude : float, optional
+            Latitude of the launch site (deg). Used along with altitude to calculate local gravity if local_gravity is not provided. If neither local_gravity nor latitude are provided, local gravity defaults to 9.80665. Defaults to None.
+        altitude : float, optional
+            Altitude of the launch site (m ASL). Used along with latitude to calculate local gravity if local_gravity is not provided. Defaults to 0.
+        """
+        self.launchpad_pressure = launchpad_pressure
+        self.launchpad_temp = launchpad_temp + 273.15
+        self.L_launch_rail = L_launch_rail
+        self.launch_angle = launch_angle
+
+        self.local_T_lapse_rate = local_T_lapse_rate
+        
+        if local_gravity:
+            self.local_gravity = local_gravity
+        elif latitude:
+            self.local_gravity = hfunc.get_local_gravity(latitude, altitude)
+        else:
+            self.local_gravity = con.F_gravity
+        
+
+class Airbrakes:
+    """
+    The Airbrakes class is used to store the properties of the airbrakes.
+
+    Attributes
+    ----------
+    num_flaps : int
+        Number of airbrake flaps.
+    A_flap : float
+        Cross-sectional area of each flap (m^2).
+    Cd_brakes : float
+        Coefficient of drag of the airbrakes.
+    max_deployment_angle : float
+        Maximum angle that the flaps can deploy to (deg).    
+    max_deployment_rate : float
+        Maximum rate at which the airbrakes can be deployed (deg/s).
+    max_retraction_rate : float
+        Maximum rate at which the airbrakes can be retracted (deg/s).
+    """
+
+    def __init__(
+        self, 
+        num_flaps : int,
+        A_flap : float,
+        Cd_brakes : float,
+        max_deployment_angle : float,
+        max_deployment_rate : float,
+        max_retraction_rate : float = None,
+    ):
+        """Initializes the Airbrakes object.
+
+        Parameters
+        ----------
+        num_flaps : int
+            Number of airbrake flaps.
+        A_flap : float
+            Cross-sectional area of each flap (m^2).
+        Cd_brakes : float
+            Coefficient of drag of the airbrakes.
+        max_deployment_angle : float
+            Maximum angle that the flaps can deploy to (deg).
+        max_deployment_rate : float
+            Maximum rate at which the airbrakes can be deployed (deg/s).
+        max_retraction_rate : float, optional
+            Maximum rate at which the airbrakes can be retracted (deg/s). Defaults to max_deployment_rate.
+        """
+        self.num_flaps = num_flaps
+        self.A_flap = A_flap
+        self.Cd_brakes = Cd_brakes
+        self.max_deployment_rate = max_deployment_rate
+        self.max_deployment_angle = max_deployment_angle
+        if max_retraction_rate:
+            self.max_retraction_rate = max_retraction_rate
+        else:
+            self.max_retraction_rate = max_deployment_rate
+
+class PastFlight ():
+    """
+    Stores the rocket, launch conditions, and apogee of a past flight
+    likely add more things like max speed, max acceleration later
+    """
+    
+    def __init__(self, rocket, launch_conditions, apogee, name = None):
+        self.rocket = rocket
+        self.launch_conditions = launch_conditions
+        self.apogee = apogee
+        self.name = name
