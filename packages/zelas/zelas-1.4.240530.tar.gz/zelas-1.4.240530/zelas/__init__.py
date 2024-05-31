2af3a803c302f260fd8ea275574df1644fb26071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:05:14 +0000

    Add package zelas/1.4.240530/zelas-1.4.240530.tar.gz

---
+++ packages/zelas/zelas-1.4.240530.tar.gz/zelas-1.4.240530/zelas/__init__.py
@@ -0,0 +1,8 @@
+import zelas.Multispectral
+import zelas.voxel
+import zelas.TheHeartOfTheMilitaryGod
+import zelas.RedundancyElimination
+import zelas.Ellipse
+import zelas.ransac
+import zelas.shield
+
