dda80b753c269b5aee179307db23c3cef72f8add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:41 +0000

    Add package pycustomchatbot/0.1.0/pycustomchatbot-0.1.0.tar.gz

---
+++ packages/pycustomchatbot/pycustomchatbot-0.1.0.tar.gz/pycustomchatbot-0.1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pycustomchatbot",
+    version="0.1.0",
+    author="José Ícaro",
+    author_email="",
+    description="A simple chatbot creation library",
+    url="https://github.com/icarogamer2441/pycustomchatbot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
