b22d790ca9b33f09c33fff8138f6b5ec4ac10569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:45 +0000

    Add package allgraph/0.2/allgraph-0.2.tar.gz

---
+++ packages/allgraph/allgraph-0.2.tar.gz/allgraph-0.2/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='allgraph',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'squarify'
+    ],
+    author='Arnav',
+    author_email='arnav.singh7418@gmail.com',
+    description='A library for creating various types of graphs',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Arnav7418/AllGraph.py',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    
+)
+
