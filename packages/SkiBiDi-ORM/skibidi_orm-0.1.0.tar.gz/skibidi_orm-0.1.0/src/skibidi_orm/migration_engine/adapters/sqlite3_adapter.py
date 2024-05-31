3d1139368a7c68fbf0d428c94c968e4ce617d4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:05:05 +0000

    Add package SkiBiDi-ORM/0.1.0/skibidi_orm-0.1.0.tar.gz

---
+++ packages/SkiBiDi-ORM/skibidi_orm-0.1.0.tar.gz/skibidi_orm-0.1.0/src/skibidi_orm/migration_engine/adapters/sqlite3_adapter.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+from skibidi_orm.migration_engine.adapters.base_adapter import BaseAdapter
+from skibidi_orm.migration_engine.adapters.database_objects.migration_element import (
+    MigrationElement,
+)
+from skibidi_orm.migration_engine.adapters.database_objects.sqlite3_typing import (
+    SQLite3Typing,
+)
+from skibidi_orm.migration_engine.db_inspectors.sqlite3_inspector import (
+    SQLite3Inspector,
+)
+from skibidi_orm.migration_engine.state_manager.state_manager import StateManager
+
+from skibidi_orm.migration_engine.sql_executor.sqlite3_executor import SQLite3Executor
+
+
+class SQLite3Adapter(BaseAdapter):
+
+    tables: list[SQLite3Typing.Table]
+
+    def __init__(self) -> None:
+        self.tables = []
+
+    def create_table(self, table: SQLite3Typing.Table):
+        """Informs the adapter about Table creation."""
+
+        self.tables.append(table)
+
+    def execute_migration(self, preview: bool = False):
+        """Execute the migration process on a full adapter."""
+
+        self.inspector = SQLite3Inspector()
+
+        db_tables = self.inspector.get_tables()
+
+        state_manager = StateManager[SQLite3Typing.Table](
+            db_tables=db_tables,
+            schema_tables=self.tables,
+        )
+
+        MigrationElement.operations = state_manager.get_operations()
+
+        if not preview:
+            SQLite3Executor.execute_operations(MigrationElement.operations)
---
+++ packages/SkiBiDi-ORM/skibidi_orm-0.1.0.tar.gz/skibidi_orm-0.1.0/tests/skibidi-orm/migration_engine/test_module_cooperation.py
@@ -0,0 +1,504 @@
+from skibidi_orm.migration_engine.adapters.database_objects.sqlite3_typing import (
+    SQLite3Typing,
+)
+from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
+from skibidi_orm.migration_engine.db_inspectors.sqlite3_inspector import (
+    SQLite3Inspector,
+)
+from skibidi_orm.migration_engine.sql_executor.sqlite3_executor import SQLite3Executor
+from skibidi_orm.migration_engine.adapters.sqlite3_adapter import SQLite3Adapter
+from skibidi_orm.migration_engine.adapters.database_objects.migration_element import (
+    MigrationElement,
+)
+from skibidi_orm.migration_engine.adapters.database_objects import constraints as c
+from .sql_data import SQLite3TablesData
+
+
+def test_adding_table_to_database(make_database: str):
+    SQLite3Config(make_database)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_user)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_post)
+
+    class Table(MigrationElement):
+        def __init__(self) -> None:
+            self.adapter = SQLite3Adapter()
+
+            models = Table.__subclasses__()
+            if self.__class__ == Table:
+                for cls in models:
+                    self.adapter.create_table(cls.__dict__["table"])
+
+    class User(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="user_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("User", "user_id")],
+            ),
+            SQLite3Typing.Column(
+                name="user_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("User", "user_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="User", columns=columns)
+
+    class Post(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="post_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("Post", "post_id")],
+            ),
+            SQLite3Typing.Column(
+                name="post_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("Post", "post_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="Post", columns=columns)
+
+    class Comment(Table):  # type: ignore
+
+        columns = [
+            SQLite3Typing.Column(
+                name="comment_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("Comment", "comment_id")],
+            ),
+            SQLite3Typing.Column(
+                name="comment_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("Comment", "comment_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="Comment", columns=columns)
+
+    m = MigrationElement()
+    m.migrate()
+
+    tables = SQLite3Inspector().get_tables()
+
+    assert len(tables) == 3
+    assert tables[2].name == "Comment"
+    assert tables[2].columns[0].name == "comment_id"
+    assert tables[2].columns[0].data_type == "INTEGER"
+    assert tables[2].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("Comment", "comment_id")
+    ]
+    assert tables[2].columns[1].name == "comment_name"
+    assert tables[2].columns[1].data_type == "TEXT"
+    assert tables[2].columns[1].column_constraints == [
+        c.NotNullConstraint("Comment", "comment_name")
+    ]
+
+
+def test_removing_table_from_database(make_database: str):
+    SQLite3Config(make_database)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_user)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_post)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_comment)
+
+    class Table(MigrationElement):
+
+        def __init__(self) -> None:
+            self.adapter = SQLite3Adapter()
+
+            models = Table.__subclasses__()
+            if self.__class__ == Table:
+                for cls in models:
+                    self.adapter.create_table(cls.__dict__["table"])
+
+    class User(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="user_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("User", "user_id")],
+            ),
+            SQLite3Typing.Column(
+                name="user_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("User", "user_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="User", columns=columns)
+
+    class Post(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="post_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("Post", "post_id")],
+            ),
+            SQLite3Typing.Column(
+                name="post_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("Post", "post_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="Post", columns=columns)
+
+    m = MigrationElement()
+    m.migrate()
+
+    tables = SQLite3Inspector().get_tables()
+
+    tables.sort(key=lambda table: table.name, reverse=True)
+
+    assert len(tables) == 2
+    assert tables[0].name == "User"
+    assert tables[0].columns[0].name == "user_id"
+    assert tables[0].columns[0].data_type == "INTEGER"
+    assert tables[0].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("User", "user_id")
+    ]
+    assert tables[0].columns[1].name == "user_name"
+    assert tables[0].columns[1].data_type == "TEXT"
+    assert tables[0].columns[1].column_constraints == [
+        c.NotNullConstraint("User", "user_name")
+    ]
+
+    assert tables[1].name == "Post"
+    assert tables[1].columns[0].name == "post_id"
+    assert tables[1].columns[0].data_type == "INTEGER"
+    assert tables[1].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("Post", "post_id")
+    ]
+    assert tables[1].columns[1].name == "post_name"
+    assert tables[1].columns[1].data_type == "TEXT"
+    assert tables[1].columns[1].column_constraints == [
+        c.NotNullConstraint("Post", "post_name")
+    ]
+
+
+def test_add_column_to_database(make_database: str):
+    SQLite3Config(make_database)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_user)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_post)
+
+    class Table(MigrationElement):
+
+        def __init__(self) -> None:
+            self.adapter = SQLite3Adapter()
+
+            models = Table.__subclasses__()
+            if self.__class__ == Table:
+                for cls in models:
+                    self.adapter.create_table(cls.__dict__["table"])
+
+    class User(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="user_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("User", "user_id")],
+            ),
+            SQLite3Typing.Column(
+                name="user_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("User", "user_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="User", columns=columns)
+
+    class Post(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="post_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("Post", "post_id")],
+            ),
+            SQLite3Typing.Column(
+                name="post_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("Post", "post_name")],
+            ),
+            SQLite3Typing.Column(
+                name="post_content",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("Post", "post_content")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="Post", columns=columns)
+
+    m = MigrationElement()
+    m.migrate()
+
+    tables = SQLite3Inspector().get_tables()
+
+    tables.sort(key=lambda table: table.name, reverse=True)
+
+    assert len(tables) == 2
+    assert tables[0].name == "User"
+    assert tables[0].columns[0].name == "user_id"
+    assert tables[0].columns[0].data_type == "INTEGER"
+    assert tables[0].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("User", "user_id")
+    ]
+    assert tables[0].columns[1].name == "user_name"
+    assert tables[0].columns[1].data_type == "TEXT"
+    assert tables[0].columns[1].column_constraints == [
+        c.NotNullConstraint("User", "user_name")
+    ]
+
+    assert tables[1].name == "Post"
+    assert tables[1].columns[0].name == "post_id"
+    assert tables[1].columns[0].data_type == "INTEGER"
+    assert tables[1].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("Post", "post_id")
+    ]
+    assert tables[1].columns[1].name == "post_name"
+    assert tables[1].columns[1].data_type == "TEXT"
+    assert tables[1].columns[1].column_constraints == [
+        c.NotNullConstraint("Post", "post_name")
+    ]
+    assert tables[1].columns[2].name == "post_content"
+    assert tables[1].columns[2].data_type == "TEXT"
+    assert tables[1].columns[2].column_constraints == [
+        c.NotNullConstraint("Post", "post_content")
+    ]
+
+
+def test_removing_column_from_database(make_database: str):
+    SQLite3Config(make_database)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_user)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_post)
+
+    class Table(MigrationElement):
+        def __init__(self) -> None:
+            self.adapter = SQLite3Adapter()
+
+            models = Table.__subclasses__()
+            if self.__class__ == Table:
+                for cls in models:
+                    self.adapter.create_table(cls.__dict__["table"])
+
+    class User(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="user_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("User", "user_id")],
+            ),
+            SQLite3Typing.Column(
+                name="user_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("User", "user_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="User", columns=columns)
+
+    class Post(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="post_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("Post", "post_id")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="Post", columns=columns)
+
+    m = MigrationElement()
+    m.migrate()
+
+    tables = SQLite3Inspector().get_tables()
+
+    tables.sort(key=lambda table: table.name, reverse=True)
+
+    assert len(tables) == 2
+    assert tables[0].name == "User"
+    assert tables[0].columns[0].name == "user_id"
+    assert tables[0].columns[0].data_type == "INTEGER"
+    assert tables[0].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("User", "user_id")
+    ]
+    assert tables[0].columns[1].name == "user_name"
+    assert tables[0].columns[1].data_type == "TEXT"
+    assert tables[0].columns[1].column_constraints == [
+        c.NotNullConstraint("User", "user_name")
+    ]
+
+    assert tables[1].name == "Post"
+    assert len(tables[1].columns) == 1
+    assert tables[1].columns[0].name == "post_id"
+    assert tables[1].columns[0].data_type == "INTEGER"
+    assert tables[1].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("Post", "post_id")
+    ]
+
+
+def test_renaming_table_in_database(make_database: str):
+    SQLite3Config(make_database)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_user)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_post)
+
+    class Table(MigrationElement):
+
+        def __init__(self) -> None:
+            self.adapter = SQLite3Adapter()
+
+            models = Table.__subclasses__()
+            if self.__class__ == Table:
+                for cls in models:
+                    self.adapter.create_table(cls.__dict__["table"])
+
+    class User(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="user_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("User", "user_id")],
+            ),
+            SQLite3Typing.Column(
+                name="user_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("User", "user_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="User", columns=columns)
+
+    class NewNamePost(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="post_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("NewNamePost", "post_id")],
+            ),
+            SQLite3Typing.Column(
+                name="post_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("NewNamePost", "post_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="NewNamePost", columns=columns)
+
+    m = MigrationElement()
+    m.migrate()
+
+    tables = SQLite3Inspector().get_tables()
+
+    assert len(tables) == 2
+    assert tables[0].name == "User"
+    assert tables[0].columns[0].name == "user_id"
+    assert tables[0].columns[0].data_type == "INTEGER"
+    assert tables[0].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("User", "user_id")
+    ]
+    assert tables[0].columns[1].name == "user_name"
+    assert tables[0].columns[1].data_type == "TEXT"
+    assert tables[0].columns[1].column_constraints == [
+        c.NotNullConstraint("User", "user_name")
+    ]
+
+    assert tables[1].name == "NewNamePost"
+    assert tables[1].columns[0].name == "post_id"
+    assert tables[1].columns[0].data_type == "INTEGER"
+    assert tables[1].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("NewNamePost", "post_id")
+    ]
+    assert tables[1].columns[1].name == "post_name"
+    assert tables[1].columns[1].data_type == "TEXT"
+    assert tables[1].columns[1].column_constraints == [
+        c.NotNullConstraint("NewNamePost", "post_name")
+    ]
+
+
+def test_add_column_to_database_and_remove_table_from_database(make_database: str):
+    SQLite3Config(make_database)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_user)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_post)
+    SQLite3Executor.execute_sql(SQLite3TablesData.sql_table_comment)
+
+    class Table(MigrationElement):
+
+        def __init__(self) -> None:
+            self.adapter = SQLite3Adapter()
+
+            models = Table.__subclasses__()
+            if self.__class__ == Table:
+                for cls in models:
+                    self.adapter.create_table(cls.__dict__["table"])
+
+    class User(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="user_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("User", "user_id")],
+            ),
+            SQLite3Typing.Column(
+                name="user_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("User", "user_name")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="User", columns=columns)
+
+    class Post(Table):  # type: ignore
+        columns = [
+            SQLite3Typing.Column(
+                name="post_id",
+                data_type="INTEGER",
+                column_constraints=[c.PrimaryKeyConstraint("Post", "post_id")],
+            ),
+            SQLite3Typing.Column(
+                name="post_name",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("Post", "post_name")],
+            ),
+            SQLite3Typing.Column(
+                name="post_content",
+                data_type="TEXT",
+                column_constraints=[c.NotNullConstraint("Post", "post_content")],
+            ),
+        ]
+
+        table = SQLite3Typing.Table(name="Post", columns=columns)
+
+    m = MigrationElement()
+    m.migrate()
+
+    tables = SQLite3Inspector().get_tables()
+
+    assert len(tables) == 2
+    assert tables[0].name == "User"
+    assert tables[0].columns[0].name == "user_id"
+    assert tables[0].columns[0].data_type == "INTEGER"
+    assert tables[0].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("User", "user_id")
+    ]
+    assert tables[0].columns[1].name == "user_name"
+    assert tables[0].columns[1].data_type == "TEXT"
+    assert tables[0].columns[1].column_constraints == [
+        c.NotNullConstraint("User", "user_name")
+    ]
+
+    assert tables[1].name == "Post"
+    assert tables[1].columns[0].name == "post_id"
+    assert tables[1].columns[0].data_type == "INTEGER"
+    assert tables[1].columns[0].column_constraints == [
+        c.PrimaryKeyConstraint("Post", "post_id")
+    ]
+    assert tables[1].columns[1].name == "post_name"
+    assert tables[1].columns[1].data_type == "TEXT"
+    assert tables[1].columns[1].column_constraints == [
+        c.NotNullConstraint("Post", "post_name")
+    ]
+    assert tables[1].columns[2].name == "post_content"
+    assert tables[1].columns[2].data_type == "TEXT"
+    assert tables[1].columns[2].column_constraints == [
+        c.NotNullConstraint("Post", "post_content")
+    ]
