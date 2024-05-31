ed501bbbeecca27cf7bdda4947f65271640058d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 06:05:49 +0000

    Add package patho-pix/0.1.0/patho_pix-0.1.0.tar.gz

---
+++ packages/patho-pix/patho_pix-0.1.0.tar.gz/patho_pix-0.1.0/src/patho_pix/utils/__init__.py
@@ -0,0 +1,7 @@
+from .custom_tiler import AwesomeTiler
+from .jpeg_to_tiff import convert_jpeg_to_tiff
+
+__all__ = [
+    'convert_jpeg_to_tiff',
+    'AwesomeTiler'
+]
---
+++ packages/patho-pix/patho_pix-0.1.0.tar.gz/patho_pix-0.1.0/tests/test_io.py
@@ -0,0 +1,87 @@
+# =========================================================== #
+#  Author:       Hackathon UKSH Team 14 - Patho-Pix           #
+#  Copyright:    Patho-Pix Team - 2024                        #
+# =========================================================== #
+# -----------------------------------------------------#
+#                   Library imports                    #
+# -----------------------------------------------------#
+# External libraries
+# Standard Library
+import os
+import tempfile
+import unittest
+
+# Third Party
+import numpy as np
+import requests
+
+# patho_pix
+# Internal libraries
+from patho_pix.io import load_mask, load_wsi
+from patho_pix.utils import convert_jpeg_to_tiff
+
+# ---------------------------------------------------- #
+#                    Configuration                     #
+# ---------------------------------------------------- #
+# Download links
+url_img = "http://glioblastoma.alleninstitute.org/cgi-bin/imageservice?path=" + \
+          "/external/gbm/prod0/0534338827/0534338827_annotation.aff&mime=1" + \
+          "&fileout=100122048_1.jpg&zoom=9&top=20224&left=57888&width=15040&height=18048"
+url_mask = "http://glioblastoma.alleninstitute.org/cgi-bin/imageservice?path=" + \
+           "/external/gbm/prod0/0534338971/0534338971.aff&mime=1&fileout=100125374_2." + \
+           "jpg&zoom=9&top=20608&left=55168&width=15040&height=18048"
+
+path_img = None
+path_mask = None
+
+# Internal debugging links
+# debug_path_img = "../data/dummy_wsi_01.image.jpg"
+# debug_path_mask = "../data/dummy_wsi_01.mask.jpg"
+
+
+# ---------------------------------------------------- #
+#          Unittest: Input/Output Interface            #
+# ---------------------------------------------------- #
+class IOTEST(unittest.TestCase):
+    # Download dummy data
+    @classmethod
+    def setUpClass(self):
+        # Create temporary directory for dummy data
+        self.tmp_data = tempfile.TemporaryDirectory(prefix="tmp.patho-pix.")
+        # Download dummy image
+        print("Downloading dummy image")
+        response = requests.get(url_img)
+        if response.status_code == 200:
+            self.path_img = os.path.join(self.tmp_data.name, "image.jpg")
+            with open(self.path_img, "wb") as fd:
+                fd.write(response.content)
+        # Download dummy mask
+        print("Downloading dummy mask")
+        response = requests.get(url_mask)
+        if response.status_code == 200:
+            self.path_mask = os.path.join(self.tmp_data.name, "mask.jpg")
+            with open(self.path_mask, "wb") as fd:
+                fd.write(response.content)
+        # convert to tiff
+        convert_jpeg_to_tiff(self.path_img, self.path_img.replace(".jpg", ".tiff"))
+        convert_jpeg_to_tiff(self.path_mask, self.path_mask.replace(".jpg", ".tiff"))
+        self.path_img = self.path_img.replace(".jpg", ".tiff")
+        self.path_mask = self.path_mask.replace(".jpg", ".tiff")
+
+    # ------------------------------------------------ #
+    #               Test: Image Loading                #
+    # ------------------------------------------------ #
+    def test_load_image(self):
+        tile_dir = tempfile.TemporaryDirectory(prefix="tmp.patho-pix.")
+        wsi = load_wsi(self.path_img, tile_dir.name)
+        self.assertTrue(np.array_equal(wsi.level_dimensions(level=0),
+                                      (15040, 18048)))
+
+    # ------------------------------------------------ #
+    #                Test: Mask Loading                #
+    # ------------------------------------------------ #
+    def test_load_mask(self):
+        tile_dir = tempfile.TemporaryDirectory(prefix="tmp.patho-pix.")
+        mask = load_mask(self.path_mask, tile_dir.name)
+        self.assertTrue(np.array_equal(mask.level_dimensions(level=0),
+                                      (15040, 18048)))
