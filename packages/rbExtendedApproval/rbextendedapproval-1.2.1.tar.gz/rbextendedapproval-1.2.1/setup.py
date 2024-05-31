a39c7184293ba778721bc6a644526c28473195c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:39 +0000

    Add package rbExtendedApproval/1.2.1/rbextendedapproval-1.2.1.tar.gz

---
+++ packages/rbExtendedApproval/rbextendedapproval-1.2.1.tar.gz/rbextendedapproval-1.2.1/setup.py
@@ -0,0 +1,32 @@
+from reviewboard.extensions.packaging import setup
+
+
+PACKAGE = "rbExtendedApproval"
+VERSION = "1.2.1"
+
+setup(
+    name=PACKAGE,
+    version=VERSION,
+    description='Review Board extension: ExtendedApproval',
+    author='Andre Klitzing',
+    author_email='aklitzing@gmail.com',
+    url='https://github.com/misery/ExtendedApproval',
+    packages=[str('extended_approval')],
+    install_requires=[
+        'reviewboard>=6',
+    ],
+    entry_points={
+        'reviewboard.extensions':
+            '%s = extended_approval.extension:ExtendedApproval' % PACKAGE,
+    },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Review Board',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
