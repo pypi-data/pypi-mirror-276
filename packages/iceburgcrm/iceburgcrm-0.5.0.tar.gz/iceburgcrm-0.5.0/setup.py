6913ea453f1e2eb3b5f362d1a5bc82a96ca5278d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 14:04:20 +0000

    Add package iceburgcrm/0.5.0/iceburgcrm-0.5.0.tar.gz

---
+++ packages/iceburgcrm/iceburgcrm-0.5.0.tar.gz/iceburgcrm-0.5.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='iceburgcrm',
+    version='0.5.0',
+    packages=find_packages(),
+    install_requires=[],
+    # Add more parameters as needed
+)
