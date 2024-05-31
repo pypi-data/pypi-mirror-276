b9b9b56ef2c535ab1b1c75371c5cfc600b4a840b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-31 03:04:15 +0000

    Add package protein-aa-analyze/1.0.0/protein_aa_analyze-1.0.0.tar.gz

---
+++ packages/protein-aa-analyze/protein_aa_analyze-1.0.0.tar.gz/protein_aa_analyze-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="protein_aa_analyze",
+    version="1.0.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'protein_aa_analyze=protein_aa_analyze.analyzer:main',
+        ],
+    },
+    author="Iman Jouiad",
+    author_email="iman.jouiad@gmail.com",
+    description="A simple Protein Amino-Acid Analyzer CLI application",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Imanjdd/protein_aa_analyze.git",
+    license= "OSI Approved :: MIT License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
