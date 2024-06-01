435772239baf37a12a3d0f7f5a9f63cddf36a4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:44 +0000

    Add package schemathesis/3.29.2/schemathesis-3.29.2-py3-none-any.whl

---
+++ packages/schemathesis/schemathesis-3.29.2-py3-none-any.whl/schemathesis/cli/output/short.py
@@ -0,0 +1,48 @@
+import click
+
+from ...runner import events
+from ..context import ExecutionContext
+from ..handlers import EventHandler
+from . import default
+
+
+def handle_before_execution(context: ExecutionContext, event: events.BeforeExecution) -> None:
+    if event.recursion_level > 0:
+        context.operations_count += 1  # type: ignore
+
+
+def handle_after_execution(context: ExecutionContext, event: events.AfterExecution) -> None:
+    context.operations_processed += 1
+    context.results.append(event.result)
+    context.hypothesis_output.extend(event.hypothesis_output)
+    default.display_execution_result(context, event)
+
+
+class ShortOutputStyleHandler(EventHandler):
+    def handle_event(self, context: ExecutionContext, event: events.ExecutionEvent) -> None:
+        """Short output style shows single symbols in the progress bar.
+
+        Otherwise, identical to the default output style.
+        """
+        if isinstance(event, events.Initialized):
+            default.handle_initialized(context, event)
+        if isinstance(event, events.BeforeProbing):
+            default.handle_before_probing(context, event)
+        if isinstance(event, events.AfterProbing):
+            default.handle_after_probing(context, event)
+        if isinstance(event, events.BeforeAnalysis):
+            default.handle_before_analysis(context, event)
+        if isinstance(event, events.AfterAnalysis):
+            default.handle_after_analysis(context, event)
+        if isinstance(event, events.BeforeExecution):
+            handle_before_execution(context, event)
+        if isinstance(event, events.AfterExecution):
+            handle_after_execution(context, event)
+        if isinstance(event, events.Finished):
+            if context.operations_count == context.operations_processed:
+                click.echo()
+            default.handle_finished(context, event)
+        if isinstance(event, events.Interrupted):
+            default.handle_interrupted(context, event)
+        if isinstance(event, events.InternalError):
+            default.handle_internal_error(context, event)
---
+++ packages/schemathesis/schemathesis-3.29.2-py3-none-any.whl/schemathesis/experimental/__init__.py
@@ -0,0 +1,81 @@
+import os
+from dataclasses import dataclass, field
+
+from ..constants import TRUE_VALUES
+
+
+@dataclass(eq=False)
+class Experiment:
+    name: str
+    verbose_name: str
+    env_var: str
+    description: str
+    discussion_url: str
+    _storage: "ExperimentSet" = field(repr=False)
+
+    def enable(self) -> None:
+        self._storage.enable(self)
+
+    def disable(self) -> None:
+        self._storage.disable(self)
+
+    @property
+    def is_enabled(self) -> bool:
+        return self._storage.is_enabled(self)
+
+    @property
+    def is_env_var_set(self) -> bool:
+        return os.getenv(self.env_var, "").lower() in TRUE_VALUES
+
+
+@dataclass
+class ExperimentSet:
+    available: set = field(default_factory=set)
+    enabled: set = field(default_factory=set)
+
+    def create_experiment(
+        self, name: str, verbose_name: str, env_var: str, description: str, discussion_url: str
+    ) -> Experiment:
+        instance = Experiment(
+            name=name,
+            verbose_name=verbose_name,
+            env_var=f"{ENV_PREFIX}_{env_var}",
+            description=description,
+            discussion_url=discussion_url,
+            _storage=self,
+        )
+        self.available.add(instance)
+        if instance.is_env_var_set:
+            self.enable(instance)
+        return instance
+
+    def enable(self, feature: Experiment) -> None:
+        self.enabled.add(feature)
+
+    def disable(self, feature: Experiment) -> None:
+        self.enabled.discard(feature)
+
+    def disable_all(self) -> None:
+        self.enabled.clear()
+
+    def is_enabled(self, feature: Experiment) -> bool:
+        return feature in self.enabled
+
+
+ENV_PREFIX = "SCHEMATHESIS_EXPERIMENTAL"
+GLOBAL_EXPERIMENTS = ExperimentSet()
+
+OPEN_API_3_1 = GLOBAL_EXPERIMENTS.create_experiment(
+    name="openapi-3.1",
+    verbose_name="OpenAPI 3.1",
+    env_var="OPENAPI_3_1",
+    description="Support for response validation",
+    discussion_url="https://github.com/schemathesis/schemathesis/discussions/1822",
+)
+SCHEMA_ANALYSIS = GLOBAL_EXPERIMENTS.create_experiment(
+    name="schema-analysis",
+    verbose_name="Schema Analysis",
+    env_var="SCHEMA_ANALYSIS",
+    description="Analyzing API schemas via Schemathesis.io",
+    discussion_url="https://github.com/schemathesis/schemathesis/discussions/2056",
+)
