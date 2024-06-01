48001a2d4fb520bb9f5733264af1b29a639ff08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:04:51 +0000

    Add package wf-airtable-api-schema/0.6.0/wf_airtable_api_schema-0.6.0.tar.gz

---
+++ packages/wf-airtable-api-schema/wf_airtable_api_schema-0.6.0.tar.gz/wf_airtable_api_schema-0.6.0/wf_airtable_api_schema/models/hubs.py
@@ -0,0 +1,32 @@
+from typing import Optional, Union
+
+from .base_model import BaseModel, RootModel
+from . import response as response_models
+
+MODEL_TYPE = "hub"
+
+
+class APIHubFields(BaseModel):
+    name: Optional[str] = None
+
+
+class APIHubRelationships(BaseModel):
+    regional_site_entrepreneurs: Optional[response_models.APILinksAndData] = None
+    pods: Optional[response_models.APILinksAndData] = None
+    schools: Optional[response_models.APILinksAndData] = None
+
+
+class APIHubData(response_models.APIData):
+    fields: APIHubFields
+
+
+class ListAPIHubData(RootModel):
+    root: list[APIHubData]
+
+
+class APIHubResponse(response_models.APIResponse):
+    data: APIHubData
+
+
+class ListAPIHubResponse(response_models.ListAPIResponse):
+    data: list[Union[APIHubData, dict]]
---
+++ packages/wf-airtable-api-schema/wf_airtable_api_schema-0.6.0.tar.gz/wf_airtable_api_schema-0.6.0/wf_airtable_api_schema/models/pods.py
@@ -0,0 +1,32 @@
+from typing import Optional, Union
+
+from .base_model import BaseModel, RootModel
+from . import response as response_models
+
+MODEL_TYPE = "pod"
+
+
+class APIPodFields(BaseModel):
+    name: Optional[str] = None
+
+
+class APIPodRelationships(BaseModel):
+    hub: Optional[response_models.APILinksAndData] = None
+    pod_contacts: Optional[response_models.APILinksAndData] = None
+    schools: Optional[response_models.APILinksAndData] = None
+
+
+class APIPodData(response_models.APIData):
+    fields: APIPodFields
+
+
+class ListAPIPodData(RootModel):
+    root: list[APIPodData]
+
+
+class APIPodResponse(response_models.APIResponse):
+    data: APIPodData
+
+
+class ListAPIPodResponse(response_models.ListAPIResponse):
+    data: list[Union[APIPodData, dict]]
