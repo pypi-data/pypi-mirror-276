77fe5b8bf776c8d2b7f9f974eed65af53e2dd1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 06:04:07 +0000

    Add package gdmty-django-users/24.5.1/gdmty_django_users-24.5.1-py3-none-any.whl

---
+++ packages/gdmty-django-users/gdmty_django_users-24.5.1-py3-none-any.whl/gdmty_django_users/permissions.py
@@ -0,0 +1,45 @@
+#!-*- coding: utf-8 -*-
+"""
+Package that is an extension of Django's AbstractBaseUser for user management, using email as user.
+This packages adds features such as group permissions and reCaptcha enterprise token verification.
+
+This package is published as free software under the terms of the Apache License, Version 2.0. Is developed by
+Dirección de Gobierno Digital of the Secretaría de Innovación y Gobierno Abierto of Municipality of Monterrey.
+
+Authors: ['César Benjamín García Martínez <mathereall@gmail.com>', 'Miguel Angel Hernández Cortés
+<miguelhdezc12@gmail.com>', 'César Guillermo Vázquez Álvarez <cdgva@outlook.com>']
+Email: gobiernodigital@monterrey.gob.mx
+GitHub: https://github.com/gobiernodigitalmonterrey/gdmty-django-users
+Package: gdmty_django_users
+PyPi: https://pypi.org/project/gdmty-django-users/
+License: AGPL-3.0
+"""
+
+from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
+
+#
+
+
+class IsAuthenticatedAndSelfOrIsStaff(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and (request.user == obj or request.user.is_staff)
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and (request.user == obj.user or request.user.is_staff)
