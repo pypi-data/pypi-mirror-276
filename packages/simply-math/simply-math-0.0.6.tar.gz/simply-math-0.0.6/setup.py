520bb4ddcb8fd8b67b0807136fa51ac8d0557d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 14:04:13 +0000

    Add package simply-math/0.0.6/simply-math-0.0.6.tar.gz

---
+++ packages/simply-math/simply-math-0.0.6.tar.gz/simply-math-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 11',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='simply-math',
+  version='0.0.6',
+  description='A very simple math module',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='AlexDEV',
+  author_email='',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator, math', 
+  packages=find_packages(),
+)
---
+++ packages/simply-math/simply-math-0.0.6.tar.gz/simply-math-0.0.6/simplymath/__init__.py
@@ -0,0 +1,19 @@
+import random
+
+def random(num1, num2):
+    return random(num1, num2)
+
+def round(num):
+    return round(num)
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    if num1 > num2:
+        return num1 - num2
+    else:
+        return num2 - num1
+
+def multiply(num1, num2):
+    return num1 * num2
