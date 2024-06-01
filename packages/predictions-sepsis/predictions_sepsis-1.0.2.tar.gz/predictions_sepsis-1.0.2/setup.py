6c2e5856a27b23b269279ea79c5a19eef97e6b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:55 +0000

    Add package predictions-sepsis/1.0.2/predictions_sepsis-1.0.2.tar.gz

---
+++ packages/predictions-sepsis/predictions_sepsis-1.0.2.tar.gz/predictions_sepsis-1.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+setup(
+    name='predictions_sepsis',
+    version='1.0.2',
+    author='@Margo78, @akp1n',
+    author_email='timtimk30@yandex.ru',
+    description='Module for sepsis predictions',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sslavian812/sepsis-predictions.git',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.1',
+        'pandas>=1.3.3',
+        'tqdm>=4.62.3',
+        'numpy>=1.21.2',
+        'scikit-learn>=0.24.2',
+        'imbalanced-learn>=0.8.0',
+        'pytorch-tabnet>=3.1.1',
+        'torch>=1.9.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    keywords='sepsis, predictions, python',
+    python_requires='>=3.7'
+)
