17db385678e7b794a992b13e1517d8c1989b72d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:04:51 +0000

    Add package ape-optimism/0.8.0/ape_optimism-0.8.0-py3-none-any.whl

---
+++ packages/ape-optimism/ape_optimism-0.8.0-py3-none-any.whl/ape_optimism/__init__.py
@@ -0,0 +1,34 @@
+from ape import plugins
+from ape.api.networks import LOCAL_NETWORK_NAME, ForkedNetworkAPI, NetworkAPI, create_network_type
+from ape_node import Node
+from ape_test import LocalProvider
+
+from .ecosystem import NETWORKS, Optimism, OptimismConfig
+
+
+@plugins.register(plugins.Config)
+def config_class():
+    return OptimismConfig
+
+
+@plugins.register(plugins.EcosystemPlugin)
+def ecosystems():
+    yield Optimism
+
+
+@plugins.register(plugins.NetworkPlugin)
+def networks():
+    for network_name, network_params in NETWORKS.items():
+        yield "optimism", network_name, create_network_type(*network_params)
+        yield "optimism", f"{network_name}-fork", ForkedNetworkAPI
+
+    # NOTE: This works for local providers, as they get chain_id from themselves
+    yield "optimism", LOCAL_NETWORK_NAME, NetworkAPI
+
+
+@plugins.register(plugins.ProviderPlugin)
+def providers():
+    for network_name in NETWORKS:
+        yield "optimism", network_name, Node
+
+    yield "optimism", LOCAL_NETWORK_NAME, LocalProvider
