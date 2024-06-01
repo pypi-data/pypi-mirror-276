3ef82d505c08dea42894b4755564bac497f163e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:04:52 +0000

    Add package myfunctions-pinar15/0.0.9/myfunctions_pinar15-0.0.9.tar.gz

---
+++ packages/myfunctions-pinar15/myfunctions_pinar15-0.0.9.tar.gz/myfunctions_pinar15-0.0.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_pinar15',
+    version='0.0.9',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/pinar15/class4',
+    license='MIT',
+    author='Pinar Dasdemir',
+    author_email='pdasdemir49@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
