1cb524c1bc91bed0f18dcba49c77adb1cc3a162d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 03:04:55 +0000

    Add package MatplotLibAPI/3.1.0/matplotlibapi-3.1.0.tar.gz

---
+++ packages/MatplotLibAPI/matplotlibapi-3.1.0.tar.gz/matplotlibapi-3.1.0/MatplotLibAPI/__init__.py
@@ -0,0 +1,14 @@
+
+
+from .Table import plot_table
+from .Timeserie import plot_timeserie
+from .Bubble import plot_bubble
+from .Network import plot_network
+from .Pivot import plot_pivotbar
+from .Treemap import plot_treemap
+from .Composite import plot_composite_bubble
+from .pdAccessor import MatPlotLibAccessor
+from .Style import StyleTemplate
+
+__all__ = ["plot_bubble", "plot_timeserie", "plot_table", "plot_network",
+           "plot_pivotbar", "plot_treemap","plot_composite_bubble", "StyleTemplate", "MatPlotLibAccessor"]
