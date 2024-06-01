3ff7068fe58ef154087d8d1be51f725f1c93d00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:23 +0000

    Add package subspy/1.1.1/subspy-1.1.1-py3-none-any.whl

---
+++ packages/subspy/subspy-1.1.1-py3-none-any.whl/subspy/__init__.py
@@ -0,0 +1,4 @@
+#Copyright and Disclaimer
+#© 2024 Fidal. All rights reserved.
+
+#This tool is provided for educational and informational purposes only. The author and contributors of Subspy are not responsible for any misuse or illegal activities performed using this tool.
