f7d425e6eb2ac54b94c95c3e3c1d5727e8b25a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:04:34 +0000

    Add package ultralytics-thop/0.0.1/ultralytics_thop-0.0.1.tar.gz

---
+++ packages/ultralytics-thop/ultralytics_thop-0.0.1.tar.gz/ultralytics_thop-0.0.1/tests/test_utils.py
@@ -0,0 +1,17 @@
+import pytest
+
+from thop import utils
+
+
+class TestUtils:
+    def test_clever_format_returns_formatted_number(self):
+        nums = 1
+        format = "%.2f"
+        clever_nums = utils.clever_format(nums, format)
+        assert clever_nums == "1.00B"
+
+    def test_clever_format_returns_formatted_numbers(self):
+        nums = [1, 2]
+        format = "%.2f"
+        clever_nums = utils.clever_format(nums, format)
+        assert clever_nums == ("1.00B", "2.00B")
