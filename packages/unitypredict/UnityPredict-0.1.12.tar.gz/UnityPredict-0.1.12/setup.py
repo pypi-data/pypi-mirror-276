cdc2fdbb92fa571e2d3bfba8314fd6c6a32b0700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:05:17 +0000

    Add package unitypredict/0.1.12/UnityPredict-0.1.12.tar.gz

---
+++ packages/unitypredict/UnityPredict-0.1.12.tar.gz/UnityPredict-0.1.12/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+description = ""
+with open("README.md", "r") as rdf:
+    description = rdf.read()
+
+print ("Possible packages: {}".format(find_packages()))
+
+setup (
+    name="UnityPredict",
+    version="0.1.12",
+    packages=find_packages(),
+    install_requires=[
+        # Currently no dependencies
+    ],
+    entry_points = {          # this here is the magic that binds your function into a callable script
+        'console_scripts': 
+        [
+            'tools=unitypredict.PlatformTools:deploy'
+        ],
+    },
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
