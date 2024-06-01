28ad755d91105e38e314f8f8a19615d6ddcfb0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 14:04:49 +0000

    Add package snowflake.core/0.8.1/snowflake_core-0.8.1-py3-none-any.whl

---
+++ packages/snowflake.core/snowflake_core-0.8.1-py3-none-any.whl/snowflake/core/function/__init__.py
@@ -0,0 +1,15 @@
+from ..function._generated.models import (
+    Function,
+    FunctionArgument,
+    ServiceFunction,
+)
+from ._function import FunctionCollection, FunctionResource
+
+
+__all__ = [
+    "Function",
+    "FunctionArgument",
+    "FunctionCollection",
+    "FunctionResource",
+    "ServiceFunction"
+]
