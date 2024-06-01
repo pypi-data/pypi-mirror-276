5ec44dd2af1dadaf09a0593d7da2b3a241920b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:52 +0000

    Add package jabutiutils/0.0.8/jabutiutils-0.0.8.tar.gz

---
+++ packages/jabutiutils/jabutiutils-0.0.8.tar.gz/jabutiutils-0.0.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jabutiutils',
+    description=u'Jabuti Technologies - Utils',
+    version='0.0.8',
+    license='MIT License',
+    author='Thales Araújo',
+    install_requires=['msal>=1.6.0,<2', 'django'],
+)
