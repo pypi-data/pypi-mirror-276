6292dad5c2a7f5e09aeda265e65ef59d173ff12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:32 +0000

    Add package aind-data-schema-models/0.1.7/aind_data_schema_models-0.1.7-py3-none-any.whl

---
+++ packages/aind-data-schema-models/aind_data_schema_models-0.1.7-py3-none-any.whl/aind_data_schema_models/specimen_procedure_types.py
@@ -0,0 +1,25 @@
+"""Script for the SpecimenProcedureType enum class."""
+
+from enum import Enum
+
+
+class SpecimenProcedureType(str, Enum):
+    """Names for general specimen procedures"""
+
+    DELIPIDATION = "Delipidation"
+    CLEARING = "Clearing"
+    EMBEDDING = "Embedding"
+    FIXATION = "Fixation"
+    FIXATION_PERMEABILIZATION = "Fixation and permeabilization"
+    GELATION = "Gelation"
+    HYBRIDIZATION_AMPLIFICATION = "Hybridication and amplification"
+    HCR = "Hybridization Chain Reaction"
+    IMMUNOLABELING = "Immunolabeling"
+    MOUNTING = "Mounting"
+    REFRACTIVE_INDEX_MATCHING = "Refractive index matching"
+    SECTIONING = "Sectioning"
+    SOAK = "Soak"
+    STORAGE = "Storage"
+    STRIPPING = "Stripping"
+    TAMOXIFEN_INDUCTION = "Tamoxifen induction"
+    OTHER = "Other - see notes"
