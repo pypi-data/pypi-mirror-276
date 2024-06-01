b5d34daf0b3513108d31b9f64bd30e256896c09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 06:04:12 +0000

    Add package opengamedata-core/0.0.8/opengamedata_core-0.0.8.tar.gz

---
+++ packages/opengamedata-core/opengamedata_core-0.0.8.tar.gz/opengamedata_core-0.0.8/src/ogd/core/schemas/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "Schema",
+    "configs",
+    "datasets",
+    "games",
+    "tables"
+]
+
+from . import Schema
+from . import configs
+from . import datasets
+from . import games
+from . import tables
