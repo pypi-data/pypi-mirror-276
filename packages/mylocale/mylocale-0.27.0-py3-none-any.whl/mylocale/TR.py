68d5d9a7ac4591dd8b32a03e63b87873c0d7c95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 16:04:59 +0000

    Add package mylocale/0.27.0/mylocale-0.27.0-py3-none-any.whl

---
+++ packages/mylocale/mylocale-0.27.0-py3-none-any.whl/mylocale/TR.py
@@ -0,0 +1,25 @@
+import csv
+import locale
+
+
+def tr(
+    csv_file,
+    target_key,
+    varnames: list = [],
+    vars: list = [],
+    langcode=locale.getlocale(),
+):
+    f = open(csv_file, newline="")
+    locale_csv = csv.DictReader(f=f, delimiter=",")
+    for item in locale_csv:
+        if item["stringname"] == target_key:
+            try:
+                if item[langcode] == "":
+                    f.close()
+                    return item["en_EN"]
+                else:
+                    f.close()
+                    return item[langcode]
+            except:
+                f.close()
+                return item["en_EN"]
