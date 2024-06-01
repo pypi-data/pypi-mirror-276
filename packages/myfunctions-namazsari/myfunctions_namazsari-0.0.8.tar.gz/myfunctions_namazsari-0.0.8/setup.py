0811a14afa06f9016ff9800ec84d704bc5a7b94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:56 +0000

    Add package myfunctions-namazsari/0.0.8/myfunctions_namazsari-0.0.8.tar.gz

---
+++ packages/myfunctions-namazsari/myfunctions_namazsari-0.0.8.tar.gz/myfunctions_namazsari-0.0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_namazsari',
+    version='0.0.8',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/namazsari/',
+    license='MIT',
+    author='Namaz Sari',
+    author_email='namazsariyev@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
