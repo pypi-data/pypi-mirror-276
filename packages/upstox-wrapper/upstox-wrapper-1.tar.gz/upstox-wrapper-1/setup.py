3bcf364b4bc0a13a7463da3c2ee0c7a6ee94a2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 16:05:09 +0000

    Add package upstox-wrapper/1/upstox-wrapper-1.tar.gz

---
+++ packages/upstox-wrapper/upstox-wrapper-1.tar.gz/upstox-wrapper-1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'upstox-wrapper',
+    packages=setuptools.find_packages(),
+    version = '1',
+    include_package_data=True,
+    description = 'Python library for upstox APIs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  author = 'Zachbot',
+    author_email = 'zachbot006@gmail.com',
+    url = 'https://github.com/azachbot/upstox_api_wrapper',
+    install_requires=['requests', 'pandas','upstox-python-sdk'],
+    keywords = ['upstox', 'nse', 'python', 'sdk', 'trading', 'stock markets', 'wrapper'],
+    classifiers=[
+      'Intended Audience :: Developers',
+      'Natural Language :: English',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: Implementation :: PyPy',
+      'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
