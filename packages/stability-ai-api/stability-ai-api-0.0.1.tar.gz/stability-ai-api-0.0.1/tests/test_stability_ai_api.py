e4f460b5f9ba2dd3e4d8e89f16a9fd3eb5181f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:41 +0000

    Add package stability-ai-api/0.0.1/stability-ai-api-0.0.1.tar.gz

---
+++ packages/stability-ai-api/stability-ai-api-0.0.1.tar.gz/stability-ai-api-0.0.1/tests/test_stability_ai_api.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""Tests for `stability_ai_api` package."""
+
+
+import unittest
+# from stability_ai_api import stability_ai_api
+
+
+class TestStabilityAiAPI(unittest.TestCase):
+    """Tests for `stability_ai_api` package."""
+    pass
