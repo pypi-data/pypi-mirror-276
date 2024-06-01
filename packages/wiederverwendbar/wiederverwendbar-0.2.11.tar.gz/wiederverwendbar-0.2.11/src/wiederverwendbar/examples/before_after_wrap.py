69ba3adf952198719dcda1d1d3dbef24a4c0d62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:04:27 +0000

    Add package wiederverwendbar/0.2.11/wiederverwendbar-0.2.11.tar.gz

---
+++ packages/wiederverwendbar/wiederverwendbar-0.2.11.tar.gz/wiederverwendbar-0.2.11/src/wiederverwendbar/examples/before_after_wrap.py
@@ -0,0 +1,72 @@
+from wiederverwendbar.before_after_wrap import WrappedClass, wrap
+
+
+class BaseClass(metaclass=WrappedClass):
+    def before_cls_test1(self, __ba_before_result__):
+        print("before_cls_test1")
+
+    @wrap(before=before_cls_test1, after="after_cls_test1")
+    def cls_test1(self, __ba_before_result__):
+        print("cls_test1")
+
+    def after_cls_test1(self, __ba_result__):
+        print("after_cls_test1")
+
+    def before_cls_test2(self):
+        print("before_cls_test2")
+
+    @wrap(before=before_cls_test2, after="after_cls_test2")
+    def cls_test2(self):
+        print("cls_test2")
+
+    def after_cls_test2(self):
+        print("after_cls_test2")
+
+
+class ChildClass(BaseClass):
+    def before_cls_test1A(self):
+        print("before_cls_test1A")
+
+    @wrap(before=before_cls_test1A, after="after_cls_test1A", include_base=False)
+    def cls_test1(self, __ba_before_result__):
+        super().cls_test1(__ba_use__=False)
+        print("cls_test1A")
+
+    def after_cls_test1A(self):
+        print("after_cls_test1A")
+
+    def before_cls_test3(self):
+        print("before_cls_test3")
+
+    @wrap(before=before_cls_test3, after="after_cls_test3")
+    def cls_test3(self):
+        print("cls_test3")
+
+    def after_cls_test3(self):
+        print("after_cls_test3")
+
+
+def before_test1():
+    print("before_test1")
+
+
+@wrap(before=before_test1, after="after_test1")
+def test1():
+    print("test1")
+
+
+def after_test1():
+    print("after_test1")
+
+
+if __name__ == "__main__":
+    base_class = BaseClass()
+    base_class.cls_test1()
+    base_class.cls_test2()
+
+    child_class = ChildClass()
+    child_class.cls_test1()
+    child_class.cls_test2()
+    child_class.cls_test3()
+
+    test1(__ba_use_before__=False, __ba_use_after__=False)
