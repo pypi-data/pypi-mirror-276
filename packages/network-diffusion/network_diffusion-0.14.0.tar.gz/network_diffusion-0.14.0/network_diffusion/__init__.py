39b65f0f7548a340d1a395e82cac2134a6fb4dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:27 +0000

    Add package network-diffusion/0.14.0/network_diffusion-0.14.0.tar.gz

---
+++ packages/network-diffusion/network_diffusion-0.14.0.tar.gz/network_diffusion-0.14.0/network_diffusion/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 by Michał Czuba, Piotr Bródka. All Rights Reserved.
+#
+# This file is part of Network Diffusion.
+#
+# Network Diffusion is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.
+#
+# Network Diffusion is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the  GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Network Diffusion. If not, see <http://www.gnu.org/licenses/>.
+# =============================================================================
+
+"""Network Diffusion is package for simulating spreading phenomenas."""
+
+# flake8: noqa
+
+
+from network_diffusion import models, seeding
+from network_diffusion.logger import Logger
+from network_diffusion.mln.actor import MLNetworkActor
+from network_diffusion.mln.mlnetwork import MultilayerNetwork
+from network_diffusion.simulator import Simulator
+from network_diffusion.tpn.tpnetwork import TemporalNetwork
+
+__version__ = "0.14.0"
