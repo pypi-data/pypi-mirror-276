f3ef54780e14a523c2894b5ad29b0e245ed1ac14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:50 +0000

    Add package speakleash/0.3.51/speakleash-0.3.51-py3-none-any.whl

---
+++ packages/speakleash/speakleash-0.3.51-py3-none-any.whl/speakleash/__init__.py
@@ -0,0 +1,13 @@
+"""
+Speakleash Package
+
+This package provides the core functionality for managing and accessing Speakleash datasets.
+
+Modules:
+- core: Provides the Speakleash class for managing and accessing datasets.
+- category_manager: Provides the CategoryManager class for managing categories in Speakleash.
+- dataset: Provides the SpeakleashDataset class for handling datasets in Speakleash.
+- structure_downloader: Provides the StructureDownloader class for downloading dataset structures in Speakleash.
+"""
+from .core import Speakleash
+from .category_manager import CategoryManager
