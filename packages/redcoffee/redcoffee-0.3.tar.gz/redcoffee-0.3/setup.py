49c0bf1d81b6eaa08fb70162036e8c09dbb71700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 16:05:22 +0000

    Add package redcoffee/0.3/redcoffee-0.3.tar.gz

---
+++ packages/redcoffee/redcoffee-0.3.tar.gz/redcoffee-0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='redcoffee',
+    version='0.3', 
+    description='A command-line tool to generate PDF for SonarQube Reports',
+    long_description=README,
+    long_description_content_type='text/markdown',  # Change this if your README is not markdown
+    py_modules=['redcoffee'],
+    install_requires=[
+        'click',
+        'reportlab',
+        'pytest'
+    ],
+    entry_points='''
+        [console_scripts]
+        redcoffee=redcoffee:cli
+    ''',
+)
