221d0c84676471121676ed366604fb013ecabd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:22 +0000

    Add package ffsim/0.0.35/ffsim-0.0.35-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ffsim/ffsim-0.0.35-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ffsim/variational/__init__.py
@@ -0,0 +1,32 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Variational ansatzes."""
+
+from ffsim.variational.givens import GivensAnsatzOperator
+from ffsim.variational.hopgate import HopGateAnsatzOperator
+from ffsim.variational.multireference import (
+    multireference_state,
+    multireference_state_prod,
+)
+from ffsim.variational.ucj import RealUCJOperator, UCJOperator
+from ffsim.variational.ucj_spin_balanced import UCJOpSpinBalanced
+from ffsim.variational.ucj_spin_unbalanced import UCJOpSpinUnbalanced
+
+__all__ = [
+    "GivensAnsatzOperator",
+    "HopGateAnsatzOperator",
+    "RealUCJOperator",
+    "UCJOperator",
+    "UCJOpSpinBalanced",
+    "UCJOpSpinUnbalanced",
+    "multireference_state",
+    "multireference_state_prod",
+]
