41c9ab86425f6044695ce7921cb90381a9334bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 03:04:46 +0000

    Add package litetts/0.2.0/litetts-0.2.0.tar.gz

---
+++ packages/litetts/litetts-0.2.0.tar.gz/litetts-0.2.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="litetts",
+    version="0.2.0",
+    author="Md Rehan",
+    author_email="mdrehan4all@gmail.com",
+    description="A package to convert text into speech",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mdrehan4all/litetts",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
