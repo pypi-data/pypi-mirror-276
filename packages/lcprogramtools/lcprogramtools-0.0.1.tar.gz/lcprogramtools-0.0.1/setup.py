dffbdd8401ebb923ef70ff355d4b7c76339d089e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 03:04:16 +0000

    Add package lcprogramtools/0.0.1/lcprogramtools-0.0.1.tar.gz

---
+++ packages/lcprogramtools/lcprogramtools-0.0.1.tar.gz/lcprogramtools-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lcprogramtools',
+    version='0.0.1',
+    description='A Python package which does exactly what the name says',
+    author='Subham Sarangi',
+    author_email='ivan.sarangi@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[],
+)
