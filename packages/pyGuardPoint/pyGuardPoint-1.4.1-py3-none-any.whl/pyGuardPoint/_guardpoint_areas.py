54be5473fe6521f47c551368395b355f00ec7dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 03:04:31 +0000

    Add package pyGuardPoint/1.4.1/pyGuardPoint-1.4.1-py3-none-any.whl

---
+++ packages/pyGuardPoint/pyGuardPoint-1.4.1-py3-none-any.whl/pyGuardPoint/_guardpoint_areas.py
@@ -0,0 +1,44 @@
+from .guardpoint_utils import GuardPointResponse
+from .guardpoint_dataclasses import Area
+from .guardpoint_error import GuardPointError, GuardPointUnauthorized
+
+
+class AreasAPI:
+    """
+    A class to interact with the Areas API.
+
+    Methods
+    -------
+    get_areas():
+        Fetches a list of areas from the API and returns them as a list of Area objects.
+    """
+    def get_areas(self):
+        url = "/odata/API_Areas"
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        code, json_body = self.gp_json_query("GET", headers=headers, url=url)
+
+        if code != 200:
+            error_msg = GuardPointResponse.extract_error_msg(json_body)
+
+            if code == 401:
+                raise GuardPointUnauthorized(f"Unauthorized - ({error_msg})")
+            elif code == 404:  # Not Found
+                raise GuardPointError(f"Cardholder Not Found")
+            else:
+                raise GuardPointError(f"{error_msg}")
+
+        if not isinstance(json_body, dict):
+            raise GuardPointError("Badly formatted response.")
+        if 'value' not in json_body:
+            raise GuardPointError("Badly formatted response.")
+        if not isinstance(json_body['value'], list):
+            raise GuardPointError("Badly formatted response.")
+
+        areas = []
+        for x in json_body['value']:
+            areas.append(Area(x))
+        return areas
