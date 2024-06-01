d49527af0c3704c1668174394f0a31353092a5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:06:25 +0000

    Add package jobbergate-api/5.2.0a2/jobbergate_api-5.2.0a2-py3-none-any.whl

---
+++ packages/jobbergate-api/jobbergate_api-5.2.0a2-py3-none-any.whl/jobbergate_api/apps/job_script_templates/schemas.py
@@ -0,0 +1,251 @@
+"""
+Provide schemas for the job script templates component.
+"""
+
+import json
+from datetime import datetime
+from typing import Any, Optional
+
+import pydantic
+from pydantic import BaseModel, validator
+
+from jobbergate_api.apps.constants import FileType
+from jobbergate_api.apps.schemas import LengthLimitedStr, TableResource
+from jobbergate_api.meta_mapper import MetaField, MetaMapper
+
+job_template_meta_mapper = MetaMapper(
+    id=MetaField(
+        description="The unique database identifier for the instance",
+        example=101,
+    ),
+    parent_id=MetaField(
+        description="The unique database identifier for the parent of this instance",
+        example=101,
+    ),
+    created_at=MetaField(
+        description="The timestamp for when the instance was created",
+        example="2023-08-18T13:55:37.172285",
+    ),
+    updated_at=MetaField(
+        description="The timestamp for when the instance was last updated",
+        example="2023-08-18T13:55:37.172285",
+    ),
+    name=MetaField(
+        description="The unique name of the instance",
+        example="test-job-script-88",
+    ),
+    identifier=MetaField(
+        description="A human-friendly label used for lookup on frequently accessed applications",
+        example="App88",
+    ),
+    description=MetaField(
+        description="A text field providing a human-friendly description of the job_script",
+        example="This job_scripts runs an Foo job using the bar variant",
+    ),
+    owner_email=MetaField(
+        description="The email of the owner/creator of the instance",
+        example="tucker@omnivector.solutions",
+    ),
+    template_vars=MetaField(
+        description="The template variables of the job script template",
+        example={"param1": 7, "param2": 13},
+    ),
+    runtime_config=MetaField(
+        description="The runtime configuration of the workflow",
+        example={"param1": 7, "param2": 13},
+    ),
+    filename=MetaField(
+        description="The name of the file",
+        example="job-script.py",
+    ),
+    file_type=MetaField(
+        description="The type of the file",
+        example=FileType.ENTRYPOINT.value,
+    ),
+    template_files=MetaField(
+        description="The template files attached to a job script template",
+    ),
+    workflow_file=MetaField(
+        description="The workflow file attached to a job script template",
+    ),
+    is_archived=MetaField(
+        description="Indicates if the job script template has been archived.",
+        example=False,
+    ),
+    cloned_from_id=MetaField(
+        description="Indicates the id this entry has been cloned from, if any.",
+        example=101,
+    ),
+)
+
+
+class RunTimeConfig(BaseModel):
+    """
+    Schema for the runtime config of a job template.
+
+    Notice this includes user supplied variables, so it has no predefined field.
+    It also loads the contend directly from the json at the request payload.
+    """
+
+    @classmethod
+    def __get_validators__(cls):
+        """
+        Get the validators.
+        """
+        yield cls.validate_to_json
+
+    @classmethod
+    def validate_to_json(cls, value):
+        """
+        Validate the produced json.
+        """
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
+
+    class Config:
+        extra = pydantic.Extra.allow
+        schema_extra = job_template_meta_mapper
+
+
+class JobTemplateCreateRequest(BaseModel):
+    """Schema for the request to create a job template."""
+
+    name: LengthLimitedStr
+    identifier: LengthLimitedStr | None
+    description: LengthLimitedStr | None
+    template_vars: dict[LengthLimitedStr, Any] | None
+
+    @validator("name")
+    def not_empty_str(cls, value):
+        """
+        Do not allow a string value to be empty.
+        """
+        if value == "":
+            raise ValueError("Cannot be an empty string")
+        return value
+
+    @validator("identifier")
+    def empty_str_to_none(cls, value):
+        """
+        Coerce an empty string value to None.
+        """
+        if value == "":
+            return None
+        return value
+
+    class Config:
+        schema_extra = job_template_meta_mapper
+
+
+class JobTemplateCloneRequest(BaseModel):
+    """Schema for the request to clone a job template."""
+
+    name: LengthLimitedStr | None
+    identifier: LengthLimitedStr | None
+    description: LengthLimitedStr | None
+    template_vars: dict[LengthLimitedStr, Any] | None
+
+    @validator("name")
+    def not_empty_str(cls, value):
+        """
+        Do not allow a string value to be empty.
+        """
+        if value == "":
+            raise ValueError("Cannot be an empty string")
+        return value
+
+    @validator("identifier")
+    def empty_str_to_none(cls, value):
+        """
+        Coerce an empty string value to None.
+        """
+        if value == "":
+            return None
+        return value
+
+    class Config:
+        schema_extra = job_template_meta_mapper
+
+
+class JobTemplateUpdateRequest(BaseModel):
+    """Schema for the request to update a job template."""
+
+    name: LengthLimitedStr | None
+    identifier: LengthLimitedStr | None
+    description: LengthLimitedStr | None
+    template_vars: dict[LengthLimitedStr, Any] | None
+    is_archived: bool | None
+
+    @validator("name")
+    def not_empty_str(cls, value):
+        """
+        Do not allow a string value to be empty.
+        """
+        if value == "":
+            raise ValueError("Cannot be an empty string")
+        return value
+
+    @validator("identifier")
+    def empty_str_to_none(cls, value):
+        """
+        Coerce an empty string value to None.
+        """
+        if value == "":
+            return None
+        return value
+
+    class Config:
+        schema_extra = job_template_meta_mapper
+
+
+class TemplateFileDetailedView(BaseModel):
+    """Schema for the response to get a template file."""
+
+    parent_id: int
+    filename: str
+    file_type: FileType
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
+        schema_extra = job_template_meta_mapper
+
+
+class WorkflowFileDetailedView(BaseModel):
+    """Schema for the response to get a workflow file."""
+
+    parent_id: int
+    filename: str
+    runtime_config: Optional[dict[str, Any]] = {}
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+    class Config:
+        orm_mode = True
+        schema_extra = job_template_meta_mapper
+
+
+class JobTemplateListView(TableResource):
+    """Schema for the response to get a list of entries."""
+
+    identifier: Optional[str]
+    cloned_from_id: Optional[int]
+
+    class Config:
+        schema_extra = job_template_meta_mapper
+
+
+class JobTemplateDetailedView(JobTemplateListView):
+    """
+    Schema for the request to an entry.
+
+    Notice the files default to None, as they are not always requested, to differentiate between
+    an empty list when they are requested, but no file is found.
+    """
+
+    template_vars: dict[str, Any] | None
+
+    template_files: list[TemplateFileDetailedView] | None
+    workflow_files: list[WorkflowFileDetailedView] | None
---
+++ packages/jobbergate-api/jobbergate_api-5.2.0a2-py3-none-any.whl/jobbergate_api/apps/schemas.py
@@ -0,0 +1,76 @@
+"""Define app-wide, reusable pydantic schemas."""
+
+from typing import Annotated, Any
+
+import sqlalchemy
+from pendulum.datetime import DateTime
+from pydantic import BaseModel, Field
+from pydantic.utils import GetterDict
+
+# Make both Pydantic and mypy happy:
+LengthLimitedStr = Annotated[str, Field(max_length=255)]
+
+
+class IgnoreLazyGetterDict(GetterDict):
+    """
+    A custom GetterDict to avoid triggering lazy-loads when accessing attributes.
+
+    In this way, only explicitly joined relationships will be loaded and included in the response.
+
+    References:
+        https://github.com/tiangolo/fastapi/discussions/5942
+    """
+
+    def __getitem__(self, key: str) -> Any:
+        """
+        Customize __getitem__ to avoid triggering lazy-loads when accessing attributes.
+        """
+        if self._is_lazy_loaded(key):
+            raise KeyError(f"The attribute '{key}' is not loaded.")
+        super().__getitem__(key)
+
+    def get(self, key: Any, default: Any = None) -> Any:
+        """
+        Get an attribute value from the object, or return a default value if the attribute does not exist.
+        """
+        if self._is_relationship(key) and self._is_lazy_loaded(key):
+            return default
+        return getattr(self._obj, key, default)
+
+    def _is_lazy_loaded(self, key: Any) -> bool:
+        return key in sqlalchemy.orm.attributes.instance_state(self._obj).unloaded  # type: ignore
+
+    def _is_relationship(self, key: Any):
+        relationship_keys = [r.key for r in sqlalchemy.inspect(self._obj.__class__).relationships]
+        return key in relationship_keys
+
+
+class TableResource(BaseModel):
+    """
+    Describes a base for table models that include basic, common info.
+    """
+
+    id: int
+    name: str
+    owner_email: str
+    created_at: DateTime
+    updated_at: DateTime
+    is_archived: bool
+    description: str | None
+
+    class Config:
+        orm_mode = True
+        getter_dict = IgnoreLazyGetterDict
+
+
+class ListParams(BaseModel):
+    """
+    Describe the shared parameters for a list request.
+    """
+
+    sort_ascending: bool = True
+    user_only: bool = False
+    search: LengthLimitedStr | None
+    sort_field: LengthLimitedStr | None
+    include_archived: bool = False
+    include_parent: bool = False
