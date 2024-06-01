21ea9e2ef1e498df81d730c494475c44a1fc9983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:05:04 +0000

    Add package sc-file/3.4.0/sc_file-3.4.0-py3-none-any.whl

---
+++ packages/sc-file/sc_file-3.4.0-py3-none-any.whl/scfile/file/formats/mcsa/__init__.py
@@ -0,0 +1,6 @@
+from .decoder import McsaDecoder
+from .flags import Flag, McsaFlags
+from .versions import SUPPORTED_VERSIONS
+
+
+__all__ = ("McsaDecoder", "McsaFlags", "Flag", "SUPPORTED_VERSIONS")
