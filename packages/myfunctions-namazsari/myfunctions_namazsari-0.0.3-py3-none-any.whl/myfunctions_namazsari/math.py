c80941a2715af8521686c541c3697af05e7be070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:41 +0000

    Add package myfunctions-namazsari/0.0.3/myfunctions_namazsari-0.0.3-py3-none-any.whl

---
+++ packages/myfunctions-namazsari/myfunctions_namazsari-0.0.3-py3-none-any.whl/myfunctions_namazsari/math.py
@@ -0,0 +1,12 @@
+"""
+    this is how you use this function
+"""
+
+
+
+def addition(NUMBER1,NUMBER2):
+    print(NUMBER1 + NUMBER2)
+
+
+def multiplication(NUMBER1,NUMBER2):
+    print(NUMBER1 * NUMBER2)
