2d13f2c82add8d08bfd4255196e48e59f99a2297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 16:04:58 +0000

    Add package nftpy/1.1.5/nftpy-1.1.5.tar.gz

---
+++ packages/nftpy/nftpy-1.1.5.tar.gz/nftpy-1.1.5/nftpy/__init__.py
@@ -0,0 +1,7 @@
+from .opensea import OpenSea, OpenSeaChain
+from .EVM import NFT, Chains, ABI, NFTWallet, Chain
+
+__all__ = ["EVM", "OpenSea", "NFT", "OpenSeaChain", "NFTWallet", "Chains", "ABI", "Chain"]
+__version__ = '1.1.5'
+__author__ = 'Coulter C. Stutz'
+__email__ = 'coulterstutz@gmail.com'
