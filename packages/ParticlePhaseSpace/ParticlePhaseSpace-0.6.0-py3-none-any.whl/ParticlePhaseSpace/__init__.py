b2be4990740c3bb2cf4f880505d355a6a844fde1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:43 +0000

    Add package ParticlePhaseSpace/0.6.0/ParticlePhaseSpace-0.6.0-py3-none-any.whl

---
+++ packages/ParticlePhaseSpace/ParticlePhaseSpace-0.6.0-py3-none-any.whl/ParticlePhaseSpace/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.6.0'
+
+# from ParticlePhaseSpace._ParticlePhaseSpace import PhaseSpace
+from ParticlePhaseSpace.__unit_config__ import ParticlePhaseSpaceUnits, UnitSet
+from ParticlePhaseSpace._ParticlePhaseSpace import PhaseSpace
