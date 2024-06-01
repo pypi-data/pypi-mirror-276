aca04e87585eaf100ade4b60eb4225efc46613fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 03:04:16 +0000

    Add package chzzkpy/0.0.5a1/chzzkpy-0.0.5a1-py3-none-any.whl

---
+++ packages/chzzkpy/chzzkpy-0.0.5a1-py3-none-any.whl/chzzkpy/chat/http.py
@@ -0,0 +1,18 @@
+from ahttp_client import get, Query
+from ahttp_client.extension import get_pydantic_response_model
+from typing import Annotated
+
+from .access_token import AccessToken
+from ..base_model import Content
+from ..http import ChzzkSession, NaverGameAPISession
+
+
+class ChzzkChatSession(NaverGameAPISession):
+    @get_pydantic_response_model()
+    @get("/nng_main/v1/chats/access-token", directly_response=True)
+    @ChzzkSession.configuration(login_able=True)
+    @Query.default_query("chatType", "STREAMING")
+    async def chat_access_token(
+        self, channel_id: Annotated[str, Query.to_camel()]
+    ) -> Content[AccessToken]:
+        pass
---
+++ packages/chzzkpy/chzzkpy-0.0.5a1-py3-none-any.whl/chzzkpy/client.py
@@ -0,0 +1,95 @@
+"""MIT License
+
+Copyright (c) 2024 gunyu1019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from __future__ import annotations
+
+import asyncio
+
+from typing import Optional, TYPE_CHECKING
+from .http import ChzzkAPISession, NaverGameAPISession
+from .live import LiveStatus, LiveDetail
+from .user import User
+
+if TYPE_CHECKING:
+    from types import TracebackType
+    from typing_extensions import Self
+
+
+class Client:
+    def __init__(
+        self,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+        authorization_key: Optional[str] = None,
+        session_key: Optional[str] = None,
+    ):
+        self.loop = loop or asyncio.get_event_loop()
+        self._closed = False
+        self._api_session = None
+        self._game_session = None
+
+        if authorization_key is not None and session_key is not None:
+            self.login(authorization_key, session_key)
+
+        self._session_initial_set()
+
+    def _session_initial_set(self):
+        self._api_session = ChzzkAPISession(loop=self.loop)
+        self._game_session = NaverGameAPISession(loop=self.loop)
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        if not self.is_closed:
+            await self.close()
+
+    @property
+    def is_closed(self) -> bool:
+        return self._closed
+
+    async def close(self):
+        self._closed = True
+        await self._api_session.close()
+        await self._game_session.close()
+        return
+
+    def login(self, authorization_key: str, session_key: str):
+        self._api_session.login(authorization_key, session_key)
+        self._game_session.login(authorization_key, session_key)
+
+    async def live_status(self, channel_id: str) -> Optional[LiveStatus]:
+        res = await self._api_session.live_status(channel_id=channel_id)
+        return res.content
+
+    async def live_detail(self, channel_id: str) -> Optional[LiveDetail]:
+        res = await self._api_session.live_detail(channel_id=channel_id)
+        return res.content
+
+    async def user(self) -> User:
+        res = await self._game_session.user()
+        return res.content
