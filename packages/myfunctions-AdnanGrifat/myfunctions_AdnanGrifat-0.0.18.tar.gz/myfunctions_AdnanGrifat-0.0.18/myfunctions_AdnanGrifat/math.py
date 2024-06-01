18816be7fcbe844a86e991762f97784d6f331890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:45 +0000

    Add package myfunctions-AdnanGrifat/0.0.18/myfunctions_AdnanGrifat-0.0.18.tar.gz

---
+++ packages/myfunctions-AdnanGrifat/myfunctions_AdnanGrifat-0.0.18.tar.gz/myfunctions_AdnanGrifat-0.0.18/myfunctions_AdnanGrifat/math.py
@@ -0,0 +1,31 @@
+"""
+This is how you use function
+"""
+import os
+
+def addition(NUMBER1,NUMBER2):
+    """
+    This is how you use function
+    e.g addition(2,3)
+    """
+    print(NUMBER1 + NUMBER2)
+
+def multiplication(NUMBER1,NUMBER2):
+    """
+    This is how you use function
+    e.g multiplication(2,5)
+    """
+    print(NUMBER1 * NUMBER2)
+
+def division(NUMBER1,NUMBER2):
+    """
+    This is how you use function
+    e.g division(2,5)
+    """
+    print(NUMBER1 / NUMBER2)
+
+
+
+def filechecker(FILENAME):
+    FILENAME = os.path.isfile(FILENAME)
+    print("File Exiests")
