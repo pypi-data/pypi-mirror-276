7c8bb9e533bc105e462bb7d635e1383101dce307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 16:05:30 +0000

    Add package lapa-authentication/0.0.15/lapa_authentication-0.0.15-py3-none-any.whl

---
+++ packages/lapa-authentication/lapa_authentication-0.0.15-py3-none-any.whl/lapa_authentication/configuration.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from lapa_commons.main import read_configuration_from_file_path
+from square_logger.main import SquareLogger
+
+try:
+    config_file_path = (
+            os.path.dirname(os.path.abspath(__file__))
+            + os.sep
+            + "data"
+            + os.sep
+            + "config.ini"
+    )
+    ldict_configuration = read_configuration_from_file_path(config_file_path)
+
+    # get all vars and typecast
+    config_str_module_name = ldict_configuration["GENERAL"]["MODULE_NAME"]
+
+    config_str_host_ip = ldict_configuration["ENVIRONMENT"]["HOST_IP"]
+    config_int_host_port = int(ldict_configuration["ENVIRONMENT"]["HOST_PORT"])
+    config_str_log_file_name = ldict_configuration["ENVIRONMENT"]["LOG_FILE_NAME"]
+    config_str_secret_key_for_access_token = ldict_configuration["ENVIRONMENT"]["SECRET_KEY_FOR_ACCESS_TOKEN"]
+    config_str_secret_key_for_refresh_token = ldict_configuration["ENVIRONMENT"]["SECRET_KEY_FOR_REFRESH_TOKEN"]
+    config_str_secret_key_for_mac_address_encryption = ldict_configuration["ENVIRONMENT"][
+        "SECRET_KEY_FOR_MAC_ADDRESS_ENCRYPTION"]
+    config_int_access_token_valid_minutes = int(ldict_configuration["ENVIRONMENT"]["ACCESS_TOKEN_VALID_MINUTES"])
+    config_int_refresh_token_valid_minutes = int(ldict_configuration["ENVIRONMENT"]["REFRESH_TOKEN_VALID_MINUTES"])
+
+    # Initialize logger
+    global_object_square_logger = SquareLogger(config_str_log_file_name)
+except Exception as e:
+    print(
+        "\033[91mMissing or incorrect config.ini file.\n"
+        "Error details: " + str(e) + "\033[0m"
+    )
+    sys.exit()
