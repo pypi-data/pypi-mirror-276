2dde0dde2cbbddf272ca4bd209a243b0db1c0e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:55 +0000

    Add package space-exploration-common-lib/0.0.32/space_exploration_common_lib-0.0.32-py3-none-any.whl

---
+++ packages/space-exploration-common-lib/space_exploration_common_lib-0.0.32-py3-none-any.whl/space_exploration_common_lib/model/Mission.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import List
+from ..model.Duration import Duration
+
+
+class Mission(BaseModel):
+    mission: str
+    astronauts: List[str]
+    program: str
+    call_sign: str
+    image_url: str
+    launch_mass_lbs: int
+    launch_site: str
+    launch_time: str
+    launch_vehicle: str
+    orbits: int
+    apogee_mi: int
+    perigee_mi: int
+    velocity_max_mph: int
+    landing_date: str
+    landing_site: str
+    recovery_ship: str
+    duration: Duration
+    distance_mi: int
