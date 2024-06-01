250fdfd952f71e85f51925ef97d03ea5956658e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:21 +0000

    Add package adbutils-async/0.0.5/adbutils_async-0.0.5.tar.gz

---
+++ packages/adbutils-async/adbutils_async-0.0.5.tar.gz/adbutils_async-0.0.5/adbutils_async/_version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Created on Fri May 06 2022 10:54:04 by codeskyblue"""
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("adbutils_async")
+except PackageNotFoundError:
+    __version__ = "unknown"
