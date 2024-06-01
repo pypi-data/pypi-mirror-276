d5c2728e82ea2638d525aae8a7bfee8714c8a316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:05:31 +0000

    Add package lsdb/0.2.5/lsdb-0.2.5.tar.gz

---
+++ packages/lsdb/lsdb-0.2.5.tar.gz/lsdb-0.2.5/benchmarks/benchmarks.py
@@ -0,0 +1,75 @@
+"""Two sample benchmarks to compute runtime and memory usage.
+
+For more information on writing benchmarks:
+https://asv.readthedocs.io/en/stable/writing_benchmarks.html."""
+
+import os
+
+import numpy as np
+import pandas as pd
+
+import lsdb
+from benchmarks.utils import upsample_array
+from lsdb.core.search.box_search import box_filter
+from lsdb.core.search.polygon_search import get_cartesian_polygon
+
+TEST_DIR = os.path.join(os.path.dirname(__file__), "..", "tests")
+DATA_DIR_NAME = "data"
+SMALL_SKY_DIR_NAME = "small_sky"
+SMALL_SKY_ORDER1 = "small_sky_order1"
+SMALL_SKY_XMATCH_NAME = "small_sky_xmatch"
+BENCH_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+
+
+def load_small_sky():
+    path = os.path.join(TEST_DIR, DATA_DIR_NAME, SMALL_SKY_DIR_NAME)
+    return lsdb.read_hipscat(path, catalog_type=lsdb.Catalog)
+
+
+def load_small_sky_order1():
+    path = os.path.join(TEST_DIR, DATA_DIR_NAME, SMALL_SKY_ORDER1)
+    return lsdb.read_hipscat(path, catalog_type=lsdb.Catalog)
+
+
+def load_small_sky_xmatch():
+    path = os.path.join(TEST_DIR, DATA_DIR_NAME, SMALL_SKY_XMATCH_NAME)
+    return lsdb.read_hipscat(path, catalog_type=lsdb.Catalog)
+
+
+def time_kdtree_crossmatch():
+    """Time computations are prefixed with 'time'."""
+    small_sky = load_small_sky()
+    small_sky_xmatch = load_small_sky_xmatch()
+    small_sky.crossmatch(small_sky_xmatch, require_right_margin=False).compute()
+
+
+def time_polygon_search():
+    """Time polygonal search using sphgeom"""
+    small_sky_order1 = load_small_sky_order1().compute()
+    # Upsample test catalog to 10,000 points
+    catalog_ra = upsample_array(small_sky_order1["ra"].to_numpy(), 10_000)
+    catalog_dec = upsample_array(small_sky_order1["dec"].to_numpy(), 10_000)
+    # Define sky polygon to use in search
+    vertices = [(300, -50), (300, -55), (272, -55), (272, -50)]
+    polygon, _ = get_cartesian_polygon(vertices)
+    # Apply vectorized filtering on the catalog points
+    polygon.contains(np.radians(catalog_ra), np.radians(catalog_dec))
+
+
+def time_box_filter_on_partition():
+    """Time box search on a single partition"""
+    metadata = load_small_sky_order1().hc_structure
+    mock_partition_df = pd.DataFrame(
+        np.linspace(-1000, 1000, 100_000), columns=[metadata.catalog_info.ra_column]
+    )
+    box_filter(mock_partition_df, ra=(-20, 40), dec=None, metadata=metadata.catalog_info).compute()
+
+
+def time_create_midsize_catalog():
+    path = os.path.join(BENCH_DATA_DIR, "midsize_catalog")
+    return lsdb.read_hipscat(path)
+
+
+def time_create_large_catalog():
+    path = os.path.join(BENCH_DATA_DIR, "large_catalog")
+    return lsdb.read_hipscat(path)
---
+++ packages/lsdb/lsdb-0.2.5.tar.gz/lsdb-0.2.5/src/lsdb/core/crossmatch/kdtree_match.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+from typing import Tuple
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+import pyarrow as pa
+from hipscat.pixel_math.hipscat_id import HIPSCAT_ID_COLUMN
+from hipscat.pixel_math.validators import validate_radius
+
+from lsdb.core.crossmatch.abstract_crossmatch_algorithm import AbstractCrossmatchAlgorithm
+from lsdb.core.crossmatch.kdtree_utils import _find_crossmatch_indices, _get_chord_distance, _lon_lat_to_xyz
+
+
+class KdTreeCrossmatch(AbstractCrossmatchAlgorithm):
+    """Nearest neighbor crossmatch using a 3D k-D tree"""
+
+    extra_columns = pd.DataFrame({"_dist_arcsec": pd.Series(dtype=pd.ArrowDtype(pa.float64()))})
+
+    def validate(
+        self,
+        n_neighbors: int = 1,
+        radius_arcsec: float = 1,
+        require_right_margin: bool = False,
+    ):
+        super().validate()
+        # Validate radius
+        validate_radius(radius_arcsec)
+        # Validate number of neighbors
+        if n_neighbors < 1:
+            raise ValueError("n_neighbors must be greater than 1")
+        # Check that the margin exists and has a compatible radius.
+        if self.right_margin_catalog_info is None:
+            if require_right_margin:
+                raise ValueError("Right catalog margin cache is required for cross-match.")
+        else:
+            if self.right_margin_catalog_info.margin_threshold < radius_arcsec:
+                raise ValueError("Cross match radius is greater than margin threshold")
+
+    def crossmatch(
+        self,
+        n_neighbors: int = 1,
+        radius_arcsec: float = 1,
+        # We need it here because the signature is shared with .validate()
+        require_right_margin: bool = False,  # pylint: disable=unused-argument
+    ) -> pd.DataFrame:
+        """Perform a cross-match between the data from two HEALPix pixels
+
+        Finds the n closest neighbors in the right catalog for each point in the left catalog that
+        are within a threshold distance by using a K-D Tree.
+
+        Args:
+            n_neighbors (int): The number of neighbors to find within each point.
+            radius_arcsec (float): The threshold distance in arcseconds beyond which neighbors are not added
+
+        Returns:
+            A DataFrame from the left and right tables merged with one row for each pair of
+            neighbors found from cross-matching. The resulting table contains the columns from the
+            left table with the first suffix appended, the right columns with the second suffix, and
+            a "_dist_arcsec" column with the great circle separation between the points.
+        """
+        # Distance in 3-D space for unit sphere
+        max_d_chord = _get_chord_distance(radius_arcsec)
+        # calculate the cartesian coordinates of the points
+        left_xyz, right_xyz = self._get_point_coordinates()
+        # get matching indices for cross-matched rows
+        chord_distances, left_idx, right_idx = _find_crossmatch_indices(
+            left_xyz, right_xyz, n_neighbors=n_neighbors, max_distance=max_d_chord
+        )
+        arc_distances = np.degrees(2.0 * np.arcsin(0.5 * chord_distances)) * 3600
+        return self._create_crossmatch_df(left_idx, right_idx, arc_distances)
+
+    def _get_point_coordinates(self) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        left_xyz = _lon_lat_to_xyz(
+            lon=self.left[self.left_catalog_info.ra_column].to_numpy(),
+            lat=self.left[self.left_catalog_info.dec_column].to_numpy(),
+        )
+        right_xyz = _lon_lat_to_xyz(
+            lon=self.right[self.right_catalog_info.ra_column].to_numpy(),
+            lat=self.right[self.right_catalog_info.dec_column].to_numpy(),
+        )
+        return left_xyz, right_xyz
+
+    def _create_crossmatch_df(
+        self,
+        left_idx: npt.NDArray[np.int64],
+        right_idx: npt.NDArray[np.int64],
+        arc_distances: npt.NDArray[np.float64],
+    ) -> pd.DataFrame:
+        # rename columns so no same names during merging
+        self._rename_columns_with_suffix(self.left, self.suffixes[0])
+        self._rename_columns_with_suffix(self.right, self.suffixes[1])
+        # concat dataframes together
+        self.left.index.name = HIPSCAT_ID_COLUMN
+        left_join_part = self.left.iloc[left_idx].reset_index()
+        right_join_part = self.right.iloc[right_idx].reset_index(drop=True)
+        out = pd.concat(
+            [
+                left_join_part,
+                right_join_part,
+            ],
+            axis=1,
+        )
+        out.set_index(HIPSCAT_ID_COLUMN, inplace=True)
+        extra_columns = pd.DataFrame(
+            {"_dist_arcsec": pd.Series(arc_distances, dtype=pd.ArrowDtype(pa.float64()), index=out.index)}
+        )
+        self._append_extra_columns(out, extra_columns)
+        return out
