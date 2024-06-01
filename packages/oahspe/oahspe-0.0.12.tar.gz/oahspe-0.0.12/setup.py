6018143208874663608ce03fb98ef933c54f1e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 16:05:29 +0000

    Add package oahspe/0.0.12/oahspe-0.0.12.tar.gz

---
+++ packages/oahspe/oahspe-0.0.12.tar.gz/oahspe-0.0.12/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+with open('version.txt', 'r') as f: version = f.read()
+
+setup(
+  name = 'oahspe',
+  packages = ['oahspe'],
+  keywords = ['oahspe'],
+  version = version,
+  author = 'quangproo',
+  license='MIT',
+  description = 'Cloud DevOps',
+  author_email = 'contact@quang.pro',
+  url = 'https://github.com/quangproo/oahspe',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  install_requires = [],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
