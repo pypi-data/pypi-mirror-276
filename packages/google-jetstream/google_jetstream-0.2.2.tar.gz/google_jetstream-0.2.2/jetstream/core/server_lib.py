6f4333abc76ec78d2a127ee368650a6b5b79a6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:04:30 +0000

    Add package google-jetstream/0.2.2/google_jetstream-0.2.2.tar.gz

---
+++ packages/google-jetstream/google_jetstream-0.2.2.tar.gz/google_jetstream-0.2.2/jetstream/core/server_lib.py
@@ -0,0 +1,173 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Contains common functions for a JetStream core server.
+
+See implementations/*/sever.py for examples.
+"""
+
+import asyncio
+from concurrent import futures
+import logging
+import threading
+from typing import Any, Type
+
+import grpc
+import jax
+from jetstream.core import config_lib
+from jetstream.core import orchestrator
+from jetstream.core.metrics.prometheus import JetstreamMetricsCollector
+from jetstream.core.proto import jetstream_pb2_grpc
+
+from prometheus_client import start_http_server
+
+_HOST = "[::]"
+
+
+class JetStreamServer:
+  """JetStream grpc server."""
+
+  def __init__(
+      self, driver: orchestrator.Driver, threads: int, port, credentials
+  ):
+    self._executor = futures.ThreadPoolExecutor(max_workers=threads)
+
+    self._loop = asyncio.new_event_loop()
+    self._loop.set_default_executor(self._executor)
+    self._loop_thread = threading.Thread(target=self._loop.run_forever)
+    self._loop_thread.start()
+
+    async def do_init():
+      self._grpc_server = grpc.aio.server(
+          self._executor,
+      )
+
+    asyncio.run_coroutine_threadsafe(do_init(), loop=self._loop).result()
+    self._driver = driver
+    jetstream_pb2_grpc.add_OrchestratorServicer_to_server(
+        orchestrator.LLMOrchestrator(driver=self._driver), self._grpc_server
+    )
+    self._grpc_server.add_secure_port(f"{_HOST}:{port}", credentials)
+
+  async def _async_start(self) -> None:
+    await self._grpc_server.start()
+
+  def start(self) -> None:
+    asyncio.run_coroutine_threadsafe(
+        self._async_start(), loop=self._loop
+    ).result()
+
+  async def _async_stop(self) -> None:
+    await self._grpc_server.stop(grace=10)
+
+  def stop(self) -> None:
+    # Gracefully clean up threads in the orchestrator.
+    self._driver.stop()
+    asyncio.run_coroutine_threadsafe(self._async_stop(), self._loop).result()
+    self._loop.call_soon_threadsafe(self._loop.stop)
+    self._loop_thread.join()
+
+  def wait_for_termination(self) -> None:
+    try:
+      asyncio.run_coroutine_threadsafe(
+          self._grpc_server.wait_for_termination(), self._loop
+      ).result()
+    finally:
+      self.stop()
+
+
+def run(
+    port: int,
+    config: Type[config_lib.ServerConfig],
+    devices: Any,
+    credentials: Any = grpc.insecure_server_credentials(),
+    threads: int | None = None,
+    jax_padding: bool = True,
+    metrics_server_config: config_lib.MetricsServerConfig | None = None,
+    enable_jax_profiler: bool = False,
+    jax_profiler_port: int = 9999,
+) -> JetStreamServer:
+  """Runs a server with a specified config.
+
+  Args:
+    port: Port on which the server will be made available.
+    config: A ServerConfig to config engine, model, device slices, etc.
+    devices: Device objects, will be used to get engine with proper slicing.
+    credentials: Should use grpc credentials by default.
+    threads: Number of RPC handlers worker threads. This should be at least
+      equal to the decoding batch size to fully saturate the decoding queue.
+    jax_padding: The flag to enable JAX padding during tokenization.
+    metrics_server_config: The config to enable Promethus metric server.
+    enable_jax_profiler: The flag to enable JAX profiler server.
+    jax_profiler_port: The port JAX profiler server (default to 9999).
+
+  Returns:
+    JetStreamServer that wraps the grpc server and orchestrator driver.
+  """
+  logging.info("Kicking off gRPC server.")
+  engines = config_lib.get_engines(config, devices=devices)
+  prefill_params = [pe.load_params() for pe in engines.prefill_engines]
+  generate_params = [ge.load_params() for ge in engines.generate_engines]
+  shared_params = [ie.load_params() for ie in engines.interleaved_engines]
+  logging.info("Loaded all weights.")
+  interleaved_mode = (
+      len(config.prefill_slices) + len(config.generate_slices) == 0
+  )
+
+  # Setup Prometheus server
+  metrics_collector: JetstreamMetricsCollector = None
+  if metrics_server_config and metrics_server_config.port:
+    logging.info(
+        "Starting Prometheus server on port %d", metrics_server_config.port
+    )
+    start_http_server(metrics_server_config.port)
+    metrics_collector = JetstreamMetricsCollector()
+  else:
+    logging.info(
+        "Not starting Prometheus server: --prometheus_port flag not set"
+    )
+
+  driver = orchestrator.Driver(
+      prefill_engines=engines.prefill_engines + engines.interleaved_engines,
+      generate_engines=engines.generate_engines + engines.interleaved_engines,
+      prefill_params=prefill_params + shared_params,
+      generate_params=generate_params + shared_params,
+      interleaved_mode=interleaved_mode,
+      jax_padding=jax_padding,
+      metrics_collector=metrics_collector,
+      is_ray_backend=config.is_ray_backend,
+  )
+  # We default threads to the total number of concurrent allowed decodes,
+  # to make sure we can fully saturate the model. Set default minimum to 64.
+  threads = threads or max(driver.get_total_concurrent_requests(), 64)
+  jetstream_server = JetStreamServer(driver, threads, port, credentials)
+  logging.info("Starting server on port %d with %d threads", port, threads)
+
+  jetstream_server.start()
+
+  # Setup Jax Profiler
+  if enable_jax_profiler:
+    logging.info("Starting JAX profiler server on port %s", jax_profiler_port)
+    jax.profiler.start_server(jax_profiler_port)
+  else:
+    logging.info("Not starting JAX profiler server: %s", enable_jax_profiler)
+  return jetstream_server
+
+
+def get_devices() -> Any:
+  """Gets devices."""
+  # TODO: Add more logs for the devices.
+  devices = jax.devices()
+  logging.info("Using devices: %d", len(devices))
+  return devices
---
+++ packages/google-jetstream/google_jetstream-0.2.2.tar.gz/google_jetstream-0.2.2/jetstream/tests/core/test_server.py
@@ -0,0 +1,151 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests gRPC server end-to-end.
+
+See orchestrator test for why these characters specifically will be the
+response.
+"""
+
+from typing import Any, Type
+import unittest
+
+
+import requests
+from parameterized import parameterized
+import grpc
+from jetstream.core import config_lib
+from jetstream.core import server_lib
+from jetstream.core.proto import jetstream_pb2
+from jetstream.core.proto import jetstream_pb2_grpc
+import portpicker
+
+
+class ServerTest(unittest.IsolatedAsyncioTestCase):
+
+  @parameterized.expand(
+      [
+          # Uses weight 2 for prefill, 4 for decode.
+          (
+              config_lib.CPUTestServer,
+              ["Ċ", "Ō", "Ɵ", ""],
+              [266, 332, 415, None],
+              [None, None],
+          ),
+          # Uses the same prefill / generate weights (2).
+          (
+              config_lib.InterleavedCPUTestServer,
+              ["Ċ", "Ə", "ɖ", ""],
+              [266, 399, 598, None],
+              [None],
+          ),
+      ]
+  )
+  async def test_server(
+      self,
+      config: Type[config_lib.ServerConfig],
+      expected_text: list[str],
+      expected_token_ids: list[int | None],
+      devices: list[Any],
+  ):
+    """Sets up a server and requests token responses."""
+    ######################### Server side ######################################
+    port = portpicker.pick_unused_port()
+
+    print("port: " + str(port))
+    credentials = grpc.local_server_credentials()
+
+    server = server_lib.run(
+        port=port,
+        config=config,
+        devices=devices,
+        credentials=credentials,
+    )
+    ###################### Requester side ######################################
+
+    # prometheus not configured, assert no metrics collector on Driver
+    assert server._driver._metrics_collector is None  # pylint: disable=protected-access
+
+    async with grpc.aio.secure_channel(
+        f"localhost:{port}", grpc.local_channel_credentials()
+    ) as channel:
+      stub = jetstream_pb2_grpc.OrchestratorStub(channel)
+
+      healthcheck_request = jetstream_pb2.HealthCheckRequest()
+      healthcheck_response = stub.HealthCheck(healthcheck_request)
+      healthcheck_response = await healthcheck_response
+
+      assert healthcheck_response.is_live is True
+
+      # The string representation of np.array([[65, 66]]), [2] will be prepended
+      # as BOS
+      text = "AB"
+      request = jetstream_pb2.DecodeRequest(
+          session_cache="",
+          text_content=jetstream_pb2.DecodeRequest.TextContent(text=text),
+          priority=1,
+          max_tokens=3,
+      )
+      iterator = stub.Decode(request)
+      counter = 0
+      async for resp in iterator:
+        output_text = resp.stream_content.samples[0].text
+        token_ids = resp.stream_content.samples[0].token_ids
+        output_token_id = token_ids[0] if len(token_ids) > 0 else None
+        print(f"actual output: {output_text=} {output_token_id=}")
+        assert output_text == expected_text[counter]
+        assert output_token_id == expected_token_ids[counter]
+        counter += 1
+      server.stop()
+
+  def test_prometheus_server(self):
+    port = portpicker.pick_unused_port()
+    metrics_port = portpicker.pick_unused_port()
+
+    print("port: " + str(port))
+    print("metrics port: " + str(metrics_port))
+    credentials = grpc.local_server_credentials()
+    # Now test server with prometheus config
+    server = server_lib.run(
+        port=port,
+        config=config_lib.InterleavedCPUTestServer,
+        devices=[None],
+        credentials=credentials,
+        metrics_server_config=config_lib.MetricsServerConfig(port=metrics_port),
+    )
+    # assert prometheus server is running and responding
+    assert server._driver._metrics_collector is not None  # pylint: disable=protected-access
+    assert (
+        requests.get(f"http://localhost:{metrics_port}", timeout=5).status_code
+        == requests.status_codes.codes["ok"]
+    )
+    server.stop()
+
+  def test_jax_profiler_server(self):
+    port = portpicker.pick_unused_port()
+    print("port: " + str(port))
+    credentials = grpc.local_server_credentials()
+    # Now test server with prometheus config
+    server = server_lib.run(
+        port=port,
+        config=config_lib.InterleavedCPUTestServer,
+        devices=[None],
+        credentials=credentials,
+        enable_jax_profiler=True,
+    )
+    assert server
+    server.stop()
+
+  def test_get_devices(self):
+    assert len(server_lib.get_devices()) == 1
