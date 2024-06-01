9bb40d9aa78419f87d32cc1c220627c152bf0349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:05:05 +0000

    Add package ape-polygon/0.8.0/ape-polygon-0.8.0.tar.gz

---
+++ packages/ape-polygon/ape-polygon-0.8.0.tar.gz/ape-polygon-0.8.0/ape_polygon/ecosystem.py
@@ -0,0 +1,30 @@
+from typing import ClassVar, Dict, Tuple, cast
+
+from ape_ethereum.ecosystem import (
+    BaseEthereumConfig,
+    Ethereum,
+    NetworkConfig,
+    create_network_config,
+)
+
+NETWORKS = {
+    # chain_id, network_id
+    "mainnet": (137, 137),
+    "mumbai": (80001, 80001),
+    "amoy": (80002, 80002),
+}
+
+
+class PolygonConfig(BaseEthereumConfig):
+    NETWORKS: ClassVar[Dict[str, Tuple[int, int]]] = NETWORKS
+    mainnet: NetworkConfig = create_network_config(block_time=2, required_confirmations=1)
+    mumbai: NetworkConfig = create_network_config(block_time=2, required_confirmations=1)
+    amoy: NetworkConfig = create_network_config(block_time=2, required_confirmations=1)
+
+
+class Polygon(Ethereum):
+    fee_token_symbol: str = "MATIC"
+
+    @property
+    def config(self) -> PolygonConfig:  # type: ignore[override]
+        return cast(PolygonConfig, self.config_manager.get_config("polygon"))
