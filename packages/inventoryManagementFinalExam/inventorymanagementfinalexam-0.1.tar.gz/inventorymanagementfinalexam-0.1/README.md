3a18b3b9a0aee539112efcc647f0f7bdd57e3568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:28 +0000

    Add package inventoryManagementFinalExam/0.1/inventorymanagementfinalexam-0.1.tar.gz

---
+++ packages/inventoryManagementFinalExam/inventorymanagementfinalexam-0.1.tar.gz/inventorymanagementfinalexam-0.1/README.md
@@ -0,0 +1,24 @@
+# Inventory Management Library
+
+This is a simple inventory management system written in Python.
+
+```sh
+## Commands
+addPart()
+searchPart()
+changePartDescription()
+printInventory()
+deletePart()
+
+### Installation
+
+
+pip install inventory_management
+# Inventory Management Library
+
+This is a simple inventory management system written in Python.
+
+## Installation
+pip install inventoryManagement
+
+```
---
+++ packages/inventoryManagementFinalExam/inventorymanagementfinalexam-0.1.tar.gz/inventorymanagementfinalexam-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inventoryManagementFinalExam',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple inventory management system',
+    author='Kyle Tiongson, Hans Neil Emnacin',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
