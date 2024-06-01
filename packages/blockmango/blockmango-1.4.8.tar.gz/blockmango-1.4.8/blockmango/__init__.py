5f2234f8071d687d1928a66f36ad653c411248e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 06:04:16 +0000

    Add package blockmango/1.4.8/blockmango-1.4.8.tar.gz

---
+++ packages/blockmango/blockmango-1.4.8.tar.gz/blockmango-1.4.8/blockmango/__init__.py
@@ -0,0 +1,8 @@
+# Inside blockmango/__init__.py
+
+from .clan import Clan
+from .user import User
+from .groupchat import Group
+from .friends import Friends
+from .decoration import Decoration
+from .http import HTTPMixin
