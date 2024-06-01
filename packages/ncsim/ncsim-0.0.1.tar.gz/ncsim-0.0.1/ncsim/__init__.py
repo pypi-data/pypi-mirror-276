3cc3b11a892f350465e7792eae10445624ce11af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 14:04:35 +0000

    Add package ncsim/0.0.1/ncsim-0.0.1.tar.gz

---
+++ packages/ncsim/ncsim-0.0.1.tar.gz/ncsim-0.0.1/ncsim/__init__.py
@@ -0,0 +1,2 @@
+from ncsim.sims import sim
+from ncsim.utlis import norm
---
+++ packages/ncsim/ncsim-0.0.1.tar.gz/ncsim-0.0.1/ncsim/sims.py
@@ -0,0 +1,12 @@
+"""
+This file will contain all the simulation methods.
+"""
+
+# Import packages
+import numpy as np
+
+# ------------ METHODS ------------
+
+def sim(a: float, b: float) -> float:
+    #: Return the mean
+    return np.mean([a, b])
