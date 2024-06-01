dc808a76b6589e87f11e7561e5e8828a4e00774d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 16:05:40 +0000

    Add package nftpy/1.2.0a0/nftpy-1.2.0a0-py3-none-any.whl

---
+++ packages/nftpy/nftpy-1.2.0a0-py3-none-any.whl/nftpy/__init__.py
@@ -0,0 +1,9 @@
+from .opensea import OpenSea, OpenSeaChain
+from .rarible import Rarible, RaribleChain
+from .EVM import NFT, NFTWallet, ABI, Chains, Chain
+
+__all__ = ["EVM", "NFT", "NFTWallet", "ABI", "Chains", "Chain", "OpenSea", "OpenSeaChain", "Rarible", "RaribleChain"]
+
+__version__ = '1.2.0a'
+__author__ = 'Coulter C. Stutz'
+__email__ = 'coulterstutz@gmail.com'
