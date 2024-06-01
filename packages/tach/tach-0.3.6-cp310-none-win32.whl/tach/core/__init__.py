c73a1b60ea41fb2b92899b5ae2633c59e72d6072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:31 +0000

    Add package tach/0.3.6/tach-0.3.6-cp310-none-win32.whl

---
+++ packages/tach/tach-0.3.6-cp310-none-win32.whl/tach/core/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from tach.core.config import PackageConfig, ProjectConfig, TagDependencyRules
+from tach.core.package import PackageNode, PackageTrie
+
+__all__ = [
+    "PackageConfig",
+    "TagDependencyRules",
+    "ProjectConfig",
+    "PackageNode",
+    "PackageTrie",
+]
