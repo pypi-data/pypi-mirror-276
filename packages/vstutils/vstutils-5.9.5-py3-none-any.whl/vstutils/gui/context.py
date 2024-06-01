18f72789020223f3b39587a31e4a8c78b6bfe47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:22 +0000

    Add package vstutils/5.9.5/vstutils-5.9.5-py3-none-any.whl

---
+++ packages/vstutils/vstutils-5.9.5-py3-none-any.whl/vstutils/gui/context.py
@@ -0,0 +1,97 @@
+from typing import Dict
+
+from django.urls import reverse
+from django.conf import settings
+from django.http.request import HttpRequest
+from django.utils.functional import lazy, SimpleLazyObject
+from django.utils.module_loading import import_string
+
+from ..utils import raise_context_decorator_with_default
+from ..static_files import SPA_STATIC_FILES_PROVIDERS
+
+
+manifest_object = import_string(settings.MANIFEST_CLASS)()
+project_lib_version = getattr(settings, 'PROJECT_LIB_VERSION', '')
+project_version = getattr(settings, 'PROJECT_VERSION', '')
+vstutils_version = settings.VSTUTILS_VERSION
+gui_version = "_".join(map(str, [project_version, project_lib_version, vstutils_version]))
+static_path = getattr(settings, 'STATIC_URL', '/static/')
+static_list = lazy(SPA_STATIC_FILES_PROVIDERS.get_sorted_list, tuple)()
+debug_enabled = getattr(settings, 'DEBUG', False)
+ignore_errors_decorator = raise_context_decorator_with_default(default={})
+
+
+def lazy_decorator(func):
+    return lazy(ignore_errors_decorator(func), dict)
+
+
+def lazy_value(func):
+    return SimpleLazyObject(func)
+
+
+def static_file_set_version_to_name(files_list_object: dict):
+    new_object = files_list_object.copy()
+    version_source = new_object.get('source', 'gui') or 'gui'
+    new_object['version'] = globals().get(version_source + '_version', gui_version)
+    return new_object
+
+
+@lazy_decorator
+def settings_constants(request: HttpRequest) -> Dict:
+    # pylint: disable=unused-argument
+    return {
+        "login_url": reverse('login'),
+        "logout_url": reverse('logout'),
+        "docs_url": settings.DOC_URL if getattr(settings, 'HAS_DOCS', False) else '',
+        "has_docs": getattr(settings, 'HAS_DOCS', False),
+        "timezone": getattr(settings, 'TIME_ZONE', 'UTC'),
+        "debug": debug_enabled,
+        "languages": settings.LANGUAGES,
+        "name": '',
+    }
+
+
+@lazy_decorator
+def project_args(request: HttpRequest) -> Dict:
+    host_url = request.build_absolute_uri('/')[:-1]
+    ver_key = f'{getattr(settings, "VST_PROJECT", "vstutils")}_version'
+    request_gui_version = f'{gui_version}_{str(request.user.id or 0)}'
+    return {
+        "host_url": host_url,
+        "gui_version": gui_version,
+        "gui_named_version": f'{settings.VST_PROJECT}_{gui_version}',
+        "gui_user_version": request_gui_version,
+        "vstutils_version": settings.VSTUTILS_VERSION,
+        "project_lib_version": project_lib_version,
+        "project_version": project_version,
+        ver_key: project_version,
+        "project_gui_name": getattr(settings, 'PROJECT_GUI_NAME', None),
+        "project_menu": getattr(settings, 'PROJECT_GUI_MENU', []),
+        "openapi_url": reverse('endpoint'),
+        "endpoint_path": reverse('endpoint'),
+        "api_version": settings.VST_API_VERSION,
+        "api_url": f'{host_url}{reverse("api-root")}{settings.VST_API_VERSION}/',
+        "enable_gravatar": settings.ENABLE_GRAVATAR,
+        "registration_enabled": settings.REGISTRATION_ENABLED,
+    }
+
+
+@lazy_decorator
+def pwa_context(request: HttpRequest) -> Dict:
+    return {
+        "manifest_object": manifest_object,
+        "static_files_list": lazy_value(
+            lambda: tuple(map(
+                static_file_set_version_to_name,
+                static_list
+            ))
+        ),
+        "block_timeout": 86400 if not debug_enabled else 0,
+    }
+
+
+@lazy_decorator
+def headers_context(request: HttpRequest) -> Dict:
+    result = dict(request.META)
+    result['HTTP_X_APP'] = result.get('HTTP_X_APP', 'browser')
+    return {'metadata': result}
---
+++ packages/vstutils/vstutils-5.9.5-py3-none-any.whl/vstutils/gui/views.py
@@ -0,0 +1,159 @@
+#  pylint: disable=bad-super-call,unused-argument
+import typing as _t
+from pathlib import Path
+
+from markdown import markdown
+from django.http.response import Http404
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import FormView
+from django.views.generic import TemplateView
+from django.template.response import TemplateResponse
+from django.contrib.auth import views as auth, login
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from jsmin import jsmin
+
+from .forms import RegistrationForm, TwoFaForm
+from ..utils import lazy_translate as __
+
+
+UserModel = get_user_model()
+
+
+class BaseView(TemplateView):
+    login_required = False
+    minify_response = True
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        if not self.minify_response:
+            response.minify_response = False  # type: ignore
+        return response
+
+    @classmethod
+    def as_view(cls, *args, **kwargs):
+        view = super().as_view(*args, **kwargs)
+        return cls.login_required and login_required(view, login_url=reverse_lazy('login')) or view  # type: ignore
+
+
+class GUIView(BaseView):
+    login_required = True
+    template_name = "gui/gui.html"
+
+
+class OfflineView(BaseView):
+    login_required = False
+    template_name = "gui/offline.html"
+
+
+class ManifestView(BaseView):
+    minify_response = False
+    login_required = False
+    template_name = "gui/manifest.json"
+    content_type = 'application/json'
+
+
+class JSMinTemplateResponse(TemplateResponse):
+    @property
+    def rendered_content(self):
+        content = super().rendered_content
+        return content.__class__(jsmin(content, quote_chars="'\"`"))
+
+
+class SWView(BaseView):
+    login_required = False
+    minify_response = False
+    content_type = 'text/javascript'
+    template_name = "gui/service-worker.js"
+    response_class = JSMinTemplateResponse
+
+
+class Login(auth.LoginView):
+    template_name = 'auth/login.html'
+    redirect_authenticated_user = True
+
+    def _is_need_tfa(self, request):
+        return request.user.is_authenticated and getattr(request.user, 'need_twofa', False)
+
+    def dispatch(self, request, *args, **kwargs):
+        if self._is_need_tfa(request):
+            self.redirect_authenticated_user = False
+        return super(Login, self).dispatch(request, *args, **kwargs)
+
+    def get_form_class(self):
+        if self._is_need_tfa(self.request):
+            return TwoFaForm
+        return super().get_form_class()
+
+    def get_template_names(self):
+        if self._is_need_tfa(self.request):
+            return ['auth/tfa.html']
+        return super().get_template_names()
+
+    def get_context_data(self, **kwargs):
+        context = super(Login, self).get_context_data(**kwargs)
+        context['wait_confirmation'] = self.request.GET.get('confirmation', '') == 'true'
+        return context
+
+
+class Logout(auth.LogoutView):
+    next_page = reverse_lazy('login')
+
+
+class Registration(FormView, BaseView):
+    template_name = 'registration/user_registration.html'
+    success_url = reverse_lazy('login')
+    form_class = RegistrationForm
+
+    def form_valid(self, form):
+        form.request = self.request
+        user = form.save()
+        if settings.AUTHENTICATE_AFTER_REGISTRATION and user.id is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if not settings.SEND_CONFIRMATION_EMAIL or 'uid' in context['form'].errors:
+            return context
+        context['uid'] = self.request.GET.get('uid', '')
+        return context
+
+    def get_success_url(self):
+        url = super().get_success_url()
+        if settings.SEND_CONFIRMATION_EMAIL and not self.request.POST.get('uid', False):
+            url += '?confirmation=true'
+        return url
+
+
+class BaseAgreementsView(TemplateView):
+    template_name = 'registration/base_agreements.html'
+    title_message = __('Terms')
+    path_in_settings: str
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['header_message'] = self.title_message
+        path = Path(_t.cast(str, getattr(settings, self.path_in_settings, None)))
+        translated_file_path = path.with_name(path.name + f'.{self.request.language.code}')  # type: ignore
+
+        if translated_file_path.exists():
+            path = translated_file_path
+
+        try:
+            context['terms'] = markdown(path.read_text(encoding='utf8'))
+        except FileNotFoundError as e:
+            raise Http404 from e
+
+        return context
+
+
+class ConsentToProcessingView(BaseAgreementsView):
+    path_in_settings = 'CONSENT_TO_PROCESSING_PATH'
+    title_message = __('personal data processing policy')
+
+
+class TermsView(BaseAgreementsView):
+    path_in_settings = 'AGREEMENT_TERMS_PATH'
+    title_message = __('terms of agreement')
