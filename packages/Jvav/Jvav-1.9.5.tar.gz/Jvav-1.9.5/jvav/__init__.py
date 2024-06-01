629ed3b56645f8833a6dc9cb5c9233bcfba34050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:59 +0000

    Add package Jvav/1.9.5/Jvav-1.9.5.tar.gz

---
+++ packages/Jvav/Jvav-1.9.5.tar.gz/Jvav-1.9.5/jvav/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+from jvav.utils import (
+    BaseUtil,
+    JavLibUtil,
+    DmmUtil,
+    JavBusUtil,
+    AvgleUtil,
+    MagnetUtil,
+    SukebeiUtil,
+    WikiUtil,
+    TransUtil,
+    JavDbUtil,
+    SgpUtil,
+    RankUtil,
+)
+
+__version__ = "1.9.5"
+
+VERSION = __version__
+
+__all__ = [
+    VERSION,
+    BaseUtil,
+    JavLibUtil,
+    DmmUtil,
+    JavBusUtil,
+    AvgleUtil,
+    MagnetUtil,
+    SukebeiUtil,
+    WikiUtil,
+    TransUtil,
+    JavDbUtil,
+    SgpUtil,
+    RankUtil,
+]
