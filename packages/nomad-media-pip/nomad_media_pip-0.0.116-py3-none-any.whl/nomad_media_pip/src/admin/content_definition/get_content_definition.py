b4188756f8d290ad6f4ba3ec5a039e90c68e0774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 14:04:47 +0000

    Add package nomad-media-pip/0.0.116/nomad_media_pip-0.0.116-py3-none-any.whl

---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.116-py3-none-any.whl/nomad_media_pip/src/admin/content_definition/get_content_definition.py
@@ -0,0 +1,31 @@
+from nomad_media_pip.src.exceptions.api_exception_handler import _api_exception_handler
+
+import requests, json
+
+def _get_content_definition(self, AUTH_TOKEN, URL, ID, DEBUG):
+
+    API_URL = f"{URL}/contentDefinition/{ID}"
+
+    HEADERS = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {AUTH_TOKEN}"
+    }
+
+    if DEBUG:
+        print(f"API_URL: {API_URL}\nMETHOD: GET")
+
+    while True:
+        try:
+            RESPONSE = requests.get(API_URL, headers=HEADERS)
+
+            if RESPONSE.ok:
+                break
+
+            if RESPONSE.status_code == 403:
+                self.refresh_token()
+            else:
+                raise Exception()
+        except:
+            _api_exception_handler(RESPONSE, "Get Content Definition failed")
+
+    return RESPONSE.json()
---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.116-py3-none-any.whl/nomad_media_pip/src/admin/live_channel/clip_live_channel.py
@@ -0,0 +1,45 @@
+from nomad_media_pip.src.exceptions.api_exception_handler import _api_exception_handler
+
+import requests, json
+
+def _clip_live_channel(self, AUTH_TOKEN, URL, CHANNEL_ID, START_TIME_CODE, END_TIME_CODE, TITLE, 
+                       OUTPUT_FOLDER_ID, TAGS, COLLECTIONS, RELATED_CONTENTS, VIDEO_BITRATE, 
+                       AUDIO_TRACKS, DEBUG):
+        
+    API_URL = f"{URL}/api/liveChannel/{CHANNEL_ID}/clip"
+    
+    HEADERS = {
+        "Authorization": "Bearer " + AUTH_TOKEN,
+        "Content-Type": "application/json"
+    }
+    
+    BODY = {
+        "startTimeCode": START_TIME_CODE,
+        "endTimeCode": END_TIME_CODE,
+        "title": TITLE,
+        "outputFolderId": OUTPUT_FOLDER_ID,
+        "tags": TAGS,
+        "collections": COLLECTIONS,
+        "relatedContent": RELATED_CONTENTS,
+        "videoBitrate": VIDEO_BITRATE,
+        "audioTracks": AUDIO_TRACKS
+    }
+    
+    if DEBUG:
+        print(f"URL: {API_URL},\nMETHOD: POST,\nBODY: {json.dumps(BODY, indent= 4)}")
+    
+    while True:
+        try:
+            RESPONSE = requests.post(API_URL, headers= HEADERS, data= json.dumps(BODY))
+            
+            if RESPONSE.ok:
+                break
+
+            if RESPONSE.status_code == 403:
+                self.refresh_token()
+            else:
+                raise Exception()
+        except:
+            _api_exception_handler(RESPONSE, "Clip Live Channel Failed")
+        
+        return RESPONSE.json()
