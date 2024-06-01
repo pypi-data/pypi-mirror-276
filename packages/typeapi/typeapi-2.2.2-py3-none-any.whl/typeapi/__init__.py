650ab7d3af3252d97e3b881bec0c4fa1b9ced041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 03:04:37 +0000

    Add package typeapi/2.2.2/typeapi-2.2.2-py3-none-any.whl

---
+++ packages/typeapi/typeapi-2.2.2-py3-none-any.whl/typeapi/__init__.py
@@ -0,0 +1,32 @@
+__version__ = "2.2.2"
+
+from .typehint import (
+    AnnotatedTypeHint,
+    ClassTypeHint,
+    ForwardRefTypeHint,
+    LiteralTypeHint,
+    TupleTypeHint,
+    TypeHint,
+    TypeVarTypeHint,
+    UnionTypeHint,
+)
+from .utils import TypedDictProtocol, get_annotations, is_typed_dict, type_repr
+
+__all__ = [
+    # .typehint
+    "AnnotatedTypeHint",
+    "ClassTypeHint",
+    "ClassVarTypeHint",
+    "ForwardRefTypeHint",
+    "LiteralTypeHint",
+    "TupleTypeHint",
+    "TypeAliasTypeHint",
+    "TypeHint",
+    "TypeVarTypeHint",
+    "UnionTypeHint",
+    # .utils
+    "get_annotations",
+    "is_typed_dict",
+    "type_repr",
+    "TypedDictProtocol",
+]
