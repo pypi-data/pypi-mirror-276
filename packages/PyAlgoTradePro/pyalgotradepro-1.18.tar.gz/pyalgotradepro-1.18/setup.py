966d033c3827ca60c9346f5c339b9c2fe38c9e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 16:05:39 +0000

    Add package PyAlgoTradePro/1.18/pyalgotradepro-1.18.tar.gz

---
+++ packages/PyAlgoTradePro/pyalgotradepro-1.18.tar.gz/pyalgotradepro-1.18/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# PyAlgoTrade
+#
+# Copyright 2011-2015 Gabriel Martin Becedillas Ruiz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='PyAlgoTradePro',
+    version='1.18',
+    description='Python Algorithmic Trading',
+    long_description='Python library for backtesting stock trading strategies.',
+    author='yanjlee',
+    author_email='yanjlee@163.com',
+    url='https://github.com/yanjlee/pyalgotrade/',
+    packages=[
+        'pyalgotrade',
+        'pyalgotrade.barfeed',
+        'pyalgotrade.bitcoincharts',
+        'pyalgotrade.bitstamp',
+        'pyalgotrade.broker',
+        'pyalgotrade.dataseries',
+        'pyalgotrade.feed',
+        'pyalgotrade.optimizer',
+        'pyalgotrade.stratanalyzer',
+        'pyalgotrade.strategy',
+        'pyalgotrade.talibext',
+        'pyalgotrade.technical',
+        'pyalgotrade.tools',
+        'pyalgotrade.twitter',
+        'pyalgotrade.utils',
+        'pyalgotrade.websocket',
+    ],
+    install_requires=[
+        "numpy",
+        "pytz",
+        "python-dateutil",
+        "requests",
+    ],
+    extras_require={
+        'Scipy':  ["scipy"],
+        'TALib':  ["Cython", "TA-Lib"],
+        'Plotting':  ["matplotlib"],
+        'Bitstamp':  ["ws4py>=0.3.4", "tornado"],
+        'Twitter':  ["tweepy"],
+    },
+)
