ce95dd72e3d99c336e78bd57b94dfd1e8db11e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:28 +0000

    Add package myfunctions-AdnanGrifat/0.0.11/myfunctions_AdnanGrifat-0.0.11.tar.gz

---
+++ packages/myfunctions-AdnanGrifat/myfunctions_AdnanGrifat-0.0.11.tar.gz/myfunctions_AdnanGrifat-0.0.11/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_AdnanGrifat',
+    version='0.0.11',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/AdnanGrifat/',
+    license='MIT',
+    author='Adnan Grifat',
+    author_email='grifat1978@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
