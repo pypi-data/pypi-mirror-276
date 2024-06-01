65b0cab71295bd132ce58d4a67ab410b9616d6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:04:46 +0000

    Add package todd-ai/0.5.1/todd_ai-0.5.1.tar.gz

---
+++ packages/todd-ai/todd_ai-0.5.1.tar.gz/todd_ai-0.5.1/todd/tasks/registries.py
@@ -0,0 +1,29 @@
+__all__ = [
+    'IGRegistry',
+    'KDRegistry',
+    'ODRegistry',
+    'ODKDRegistry',
+    'OFERegistry',
+]
+
+from ..registries import TaskRegistry
+
+
+class IGRegistry(TaskRegistry):
+    pass
+
+
+class KDRegistry(TaskRegistry):
+    pass
+
+
+class ODRegistry(TaskRegistry):
+    pass
+
+
+class ODKDRegistry(TaskRegistry):
+    pass
+
+
+class OFERegistry(TaskRegistry):
+    pass
