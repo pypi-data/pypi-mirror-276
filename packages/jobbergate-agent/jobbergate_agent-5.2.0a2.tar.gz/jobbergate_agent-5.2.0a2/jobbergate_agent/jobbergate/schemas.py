5e8e9acff566513885b5b31b7c6c26edf6f8ad79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:05:32 +0000

    Add package jobbergate-agent/5.2.0a2/jobbergate_agent-5.2.0a2.tar.gz

---
+++ packages/jobbergate-agent/jobbergate_agent-5.2.0a2.tar.gz/jobbergate_agent-5.2.0a2/jobbergate_agent/jobbergate/schemas.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+from typing import List, Optional
+
+import pydantic
+
+from jobbergate_agent.jobbergate.constants import FileType
+
+
+class JobScriptFile(pydantic.BaseModel, extra=pydantic.Extra.ignore):
+    """Model for the job_script_files field of the JobScript resource."""
+
+    parent_id: int
+    filename: str
+    file_type: FileType
+
+    @property
+    def path(self) -> str:
+        return f"/jobbergate/job-scripts/{self.parent_id}/upload/{self.filename}"
+
+
+class JobScript(pydantic.BaseModel, extra=pydantic.Extra.ignore):
+    """Model to match database for the JobScript resource."""
+
+    files: List[JobScriptFile] = pydantic.Field(default_factory=list)
+
+
+class PendingJobSubmission(pydantic.BaseModel, extra=pydantic.Extra.ignore):
+    """
+    Specialized model for the cluster-agent to pull a pending job_submission along with
+    data from its job_script and application sources.
+    """
+
+    id: int
+    name: str
+    owner_email: str
+    execution_directory: Optional[Path]
+    sbatch_arguments: List[str] = pydantic.Field(default_factory=list)
+    job_script: JobScript
+
+
+class ActiveJobSubmission(pydantic.BaseModel, extra=pydantic.Extra.ignore):
+    """
+    Specialized model for the cluster-agent to pull an active job_submission.
+    """
+
+    id: int
+    slurm_job_id: int
+
+
+class SlurmSubmitError(pydantic.BaseModel):
+    """
+    Specialized model for error content in a SlurmSubmitResponse.
+    """
+
+    error: Optional[str]
+    error_code: Optional[int] = pydantic.Field(alias="errno")
+
+    class Config:
+        allow_population_by_field_name = True
+        extra = pydantic.Extra.ignore
+
+
+class SlurmSubmitResponse(pydantic.BaseModel, extra=pydantic.Extra.ignore):
+    """
+    Specialized model for the cluster-agent to pull a pending job_submission along with
+    data from its job_script and application sources.
+    """
+
+    errors: List[SlurmSubmitError] = []
+    job_id: Optional[int]
+
+
+class SlurmJobData(pydantic.BaseModel, extra=pydantic.Extra.ignore):
+    """
+    Specialized model for the cluster-agent to pull job state information from slurm and post the data as an update
+    to the Jobbergate API.
+    """
+
+    job_id: Optional[int]
+    job_state: Optional[str]
+    job_info: Optional[str]
+    state_reason: Optional[str]
