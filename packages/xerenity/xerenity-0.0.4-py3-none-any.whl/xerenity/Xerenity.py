00de13075046228d07f1ae0528acbf4b765ce977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:45 +0000

    Add package xerenity/0.0.4/xerenity-0.0.4-py3-none-any.whl

---
+++ packages/xerenity/xerenity-0.0.4-py3-none-any.whl/xerenity/Xerenity.py
@@ -0,0 +1,12 @@
+from xerenity.connection.db import Connection
+from xerenity.search.series import Series
+
+
+class Xerenity:
+
+    def __init__(self, username, password):
+        self.username: username
+        self.password = password
+        self.conn = Connection()
+        self.conn.login(username=username, password=password)
+        self.series: Series = Series(connection=self.conn)
