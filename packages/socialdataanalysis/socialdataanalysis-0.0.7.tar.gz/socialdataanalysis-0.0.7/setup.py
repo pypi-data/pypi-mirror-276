ab64722416bb57816b3cccd6a772fe065b4fcd96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 03:04:45 +0000

    Add package socialdataanalysis/0.0.7/socialdataanalysis-0.0.7.tar.gz

---
+++ packages/socialdataanalysis/socialdataanalysis-0.0.7.tar.gz/socialdataanalysis-0.0.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='socialdataanalysis',
+    version='0.0.7',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'socialdataanalysis': ['notebooks/*.ipynb'],
+    },
+    install_requires=[
+        'pandas',
+        'numpy',
+        'statsmodels',
+        'scipy',
+        'matplotlib',
+        'sympy',
+        'prince',
+        'altair'
+    ],
+    author='Ricardo Mergulhao',
+    author_email='ricardomergulhao@gmail.com',
+    description='Funções personalizadas para análise de dados nas ciências sociais, complementando o uso do SPSS.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rcmergulhao/socialdataanalysis',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
