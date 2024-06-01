69174626d685a794cdb3a772bb7d98c6fe5c269c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:55 +0000

    Add package cubecoders-amp-api-wrapper/0.1.11/cubecoders_amp_api_wrapper-0.1.11.tar.gz

---
+++ packages/cubecoders-amp-api-wrapper/cubecoders_amp_api_wrapper-0.1.11.tar.gz/cubecoders_amp_api_wrapper-0.1.11/ampapi/emailsender.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from .base import Base
+from .types import *
+
+__all__ = ("EmailSenderPlugin",)
+
+
+class EmailSenderPlugin(Base):
+    """
+    Contains the base functions for any `/API/EmailSenderPlugin/` AMP API endpoints.
+
+    """
+    pass
