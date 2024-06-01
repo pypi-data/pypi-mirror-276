36089eaaa1d8510148343ee1e14c626ba6eb0958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 04:04:45 +0000

    Add package schemathesis/3.29.2/schemathesis-3.29.2.tar.gz

---
+++ packages/schemathesis/schemathesis-3.29.2.tar.gz/schemathesis-3.29.2/src/schemathesis/checks.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import json
+
+from . import failures
+from .exceptions import get_server_error, get_response_parsing_error
+from .specs.openapi.checks import (
+    content_type_conformance,
+    response_headers_conformance,
+    response_schema_conformance,
+    status_code_conformance,
+)
+
+if TYPE_CHECKING:
+    from .transports.responses import GenericResponse
+    from .models import Case, CheckFunction
+
+
+def not_a_server_error(response: GenericResponse, case: Case) -> bool | None:
+    """A check to verify that the response is not a server-side error."""
+    from .specs.graphql.schemas import GraphQLCase
+    from .specs.graphql.validation import validate_graphql_response
+    from .transports.responses import get_json
+
+    status_code = response.status_code
+    if status_code >= 500:
+        exc_class = get_server_error(status_code)
+        raise exc_class(failures.ServerError.title, context=failures.ServerError(status_code=status_code))
+    if isinstance(case, GraphQLCase):
+        try:
+            data = get_json(response)
+            validate_graphql_response(data)
+        except json.JSONDecodeError as exc:
+            exc_class = get_response_parsing_error(exc)
+            context = failures.JSONDecodeErrorContext.from_exception(exc)
+            raise exc_class(context.title, context=context) from exc
+    return None
+
+
+DEFAULT_CHECKS: tuple[CheckFunction, ...] = (not_a_server_error,)
+OPTIONAL_CHECKS = (
+    status_code_conformance,
+    content_type_conformance,
+    response_headers_conformance,
+    response_schema_conformance,
+)
+ALL_CHECKS: tuple[CheckFunction, ...] = DEFAULT_CHECKS + OPTIONAL_CHECKS
+
+
+def register(check: CheckFunction) -> CheckFunction:
+    """Register a new check for schemathesis CLI.
+
+    :param check: A function to validate API responses.
+
+    .. code-block:: python
+
+        @schemathesis.check
+        def new_check(response, case):
+            # some awesome assertions!
+            ...
+    """
+    from . import cli
+
+    global ALL_CHECKS
+
+    ALL_CHECKS += (check,)
+    cli.CHECKS_TYPE.choices += (check.__name__,)  # type: ignore
+    return check
---
+++ packages/schemathesis/schemathesis-3.29.2.tar.gz/schemathesis-3.29.2/test/specs/openapi/test_expressions.py
@@ -0,0 +1,166 @@
+import json
+
+import pytest
+import requests
+from hypothesis import given, settings
+from hypothesis import strategies as st
+
+from schemathesis.models import Case
+from schemathesis.models import APIOperation
+from schemathesis.specs.openapi import expressions
+from schemathesis.specs.openapi.expressions.errors import RuntimeExpressionError
+from schemathesis.specs.openapi.expressions.lexer import Token
+from schemathesis.specs.openapi.references import resolve_pointer, UNRESOLVABLE
+
+DOCUMENT = {"foo": ["bar", "baz"], "": 0, "a/b": 1, "c%d": 2, "e^f": 3, "g|h": 4, "i\\j": 5, 'k"l': 6, " ": 7, "m~n": 8}
+
+
+@pytest.fixture(scope="module")
+def operation():
+    return APIOperation(
+        "/users/{user_id}", "GET", None, None, verbose_name="GET /users/{user_id}", base_url="http://127.0.0.1:8080/api"
+    )
+
+
+@pytest.fixture(scope="module")
+def case(operation):
+    return Case(
+        operation,
+        generation_time=0.0,
+        path_parameters={"user_id": 5},
+        query={"username": "foo"},
+        headers={"X-Token": "secret"},
+        body={"a": 1},
+    )
+
+
+@pytest.fixture(scope="module")
+def response():
+    response = requests.Response()
+    response._content = json.dumps(DOCUMENT).encode()
+    response.status_code = 200
+    response.headers["Content-Type"] = "application/json"
+    response.headers["X-Response"] = "Y"
+    return response
+
+
+@pytest.fixture(scope="module")
+def context(case, response):
+    return expressions.ExpressionContext(response=response, case=case)
+
+
+@pytest.mark.parametrize(
+    "expr, expected",
+    (
+        ("", ""),
+        ("foo", "foo"),
+        ("$url", "http://127.0.0.1:8080/api/users/5?username=foo"),
+        ("$method", "GET"),
+        ("$statusCode", "200"),
+        ("ID_{$method}", "ID_GET"),
+        ("$request.query.username", "foo"),
+        ("spam_{$request.query.username}_baz_{$request.query.username}", "spam_foo_baz_foo"),
+        ("spam_{$request.path.user_id}", "spam_5"),
+        ("spam_{$request.header.X-Token}", "spam_secret"),
+        ("spam_{$request.header.x-token}", "spam_secret"),
+        ("$request.path.user_id", 5),
+        ("$request.body", {"a": 1}),
+        ("$request.body#/a", 1),
+        ("spam_{$response.header.X-Response}", "spam_Y"),
+        ("spam_{$response.header.x-response}", "spam_Y"),
+        ("$response.body#/foo/0", "bar"),
+        ("$response.body#/g|h", 4),
+        (42, 42),
+        ("$response.body", DOCUMENT),
+        ("ID_{$response.body#/g|h}", "ID_4"),
+        ("ID_{$response.body#/g|h}_{$response.body#/a~1b}", "ID_4_1"),
+        ("eq.{$response.body#/g|h}", "eq.4"),
+    ),
+)
+def test_evaluate(context, expr, expected):
+    assert expressions.evaluate(expr, context) == expected
+
+
+@pytest.mark.parametrize(
+    "expr",
+    (
+        "$u",
+        "$urlfoo",
+        "{{$foo.$bar}}",
+        "{$foo",
+        "$foo}",
+        "$request..",
+        "$request.unknown",
+        "$request.body.unknown",
+        "$response..",
+        "$response.unknown",
+        "$response.body.something",
+        "$response.header..",
+        "$response}",
+    ),
+)
+def test_invalid_expression(context, expr):
+    with pytest.raises(RuntimeExpressionError):
+        expressions.evaluate(expr, context)
+
+
+@given(expr=(st.text() | (st.lists(st.sampled_from([".", "}", "{", "$"]) | st.text()).map("".join))))
+@settings(deadline=None)
+def test_random_expression(expr):
+    try:
+        expressions.evaluate(expr, context)
+    except RuntimeExpressionError:
+        pass
+
+
+@pytest.mark.parametrize(
+    "expr, expected",
+    (
+        ("$url", [Token.variable("$url")]),
+        ("foo", [Token.string("foo")]),
+        ("foo1", [Token.string("foo1")]),
+        ("{}", [Token.lbracket(), Token.rbracket()]),
+        ("{foo}", [Token.lbracket(), Token.string("foo"), Token.rbracket()]),
+        ("{$foo}", [Token.lbracket(), Token.variable("$foo"), Token.rbracket()]),
+        (
+            "foo{$bar}spam",
+            [Token.string("foo"), Token.lbracket(), Token.variable("$bar"), Token.rbracket(), Token.string("spam")],
+        ),
+        ("$foo.bar", [Token.variable("$foo"), Token.dot(), Token.string("bar")]),
+        ("$foo.$bar", [Token.variable("$foo"), Token.dot(), Token.variable("$bar")]),
+        (
+            "{$foo.$bar}",
+            [Token.lbracket(), Token.variable("$foo"), Token.dot(), Token.variable("$bar"), Token.rbracket()],
+        ),
+        (
+            "$request.body#/foo/bar",
+            [Token.variable("$request"), Token.dot(), Token.string("body"), Token.pointer("#/foo/bar")],
+        ),
+    ),
+)
+def test_lexer(expr, expected):
+    assert list(expressions.lexer.tokenize(expr)) == expected
+
+
+@pytest.mark.parametrize(
+    "pointer, expected",
+    (
+        ("", DOCUMENT),
+        ("abc", UNRESOLVABLE),
+        ("/foo/123", UNRESOLVABLE),
+        ("/foo", ["bar", "baz"]),
+        ("/foo/0", "bar"),
+        ("/", 0),
+        ("/a~1b", 1),
+        ("/c%d", 2),
+        ("/c%d/foo", UNRESOLVABLE),
+        ("/e^f", 3),
+        ("/g|h", 4),
+        ("/i\\j", 5),
+        ('/k"l', 6),
+        ("/ ", 7),
+        ("/m~0n", 8),
+    ),
+)
+def test_pointer(pointer, expected):
+    assert resolve_pointer(DOCUMENT, pointer) == expected
