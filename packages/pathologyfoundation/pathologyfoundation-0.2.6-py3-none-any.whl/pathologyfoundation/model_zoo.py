43a141649c3e2f889fa6f5c5377669f31788d00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 05:05:05 +0000

    Add package pathologyfoundation/0.2.6/pathologyfoundation-0.2.6-py3-none-any.whl

---
+++ packages/pathologyfoundation/pathologyfoundation-0.2.6-py3-none-any.whl/pathologyfoundation/model_zoo.py
@@ -0,0 +1,14 @@
+#from .models import PLIP_ViT
+from .models import PLIP_ViT
+from .utils import get_default_cache_dir
+
+
+def model_zoo(model_name, device=None, cache_dir=None):
+    if cache_dir is None:
+        cache_dir = get_default_cache_dir()
+        print(f"Use default cache dir: {cache_dir}")
+
+    if model_name == "PLIP-ViT-B-32":
+        return PLIP_ViT(model_name, device, cache_dir)
+    else:
+        raise ValueError(f"Model {model_name} not found in the model zoo.")
