de724d7ed79093a4ca3294cee754be5990090321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 14:04:45 +0000

    Add package myfunctions-khikmatillo1/0.0.10/myfunctions_khikmatillo1-0.0.10-py3-none-any.whl

---
+++ packages/myfunctions-khikmatillo1/myfunctions_khikmatillo1-0.0.10-py3-none-any.whl/myfunctions_khikmatillo1/math.py
@@ -0,0 +1,30 @@
+"""
+    This is how you use this function
+"""
+import os 
+
+def addition(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    e.g addition(2,3)
+    """
+    print(NUMBER1 + NUMBER2)
+
+def multiplication(NUMBER1,NUMBER2):
+    """
+    This is how you use this function
+    e.g multiplication(2,5)
+    """
+    print(NUMBER1 * NUMBER2) 
+
+def filecheker(FILENAME):
+    FILENAME = os.path.isfile(FILENAME)
+    print("File exists")
+
+def division(numerator):
+    FILENAME = os.path.isfile(numerator)
+    print("File exists")
+
+def substraction(minuted):
+    FILENAME = os.path.isfile(minuted)
+    print("File exists")
