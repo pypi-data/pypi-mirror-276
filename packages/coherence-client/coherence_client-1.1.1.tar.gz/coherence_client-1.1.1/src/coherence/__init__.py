319c219aa1d1fe674d873ceb674323befd84427f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 14:05:01 +0000

    Add package coherence-client/1.1.1/coherence_client-1.1.1.tar.gz

---
+++ packages/coherence-client/coherence_client-1.1.1.tar.gz/coherence_client-1.1.1/src/coherence/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2022, 2024, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# https://oss.oracle.com/licenses/upl.
+
+from __future__ import annotations
+
+__version__ = "1.1.1"
+
+import logging
+
+# expose these symbols in top-level namespace
+from .aggregator import Aggregators as Aggregators
+from .client import MapEntry as MapEntry
+from .client import NamedCache as NamedCache
+from .client import NamedMap as NamedMap
+from .client import Options as Options
+from .client import Session as Session
+from .client import TlsOptions as TlsOptions
+from .comparator import Comparator as Comparator
+from .extractor import Extractors as Extractors
+from .filter import Filters as Filters
+from .processor import Processors as Processors
+
+# default logging configuration for coherence
+handler: logging.StreamHandler = logging.StreamHandler()  # type: ignore
+handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+
+COH_LOG = logging.getLogger("coherence")
+COH_LOG.setLevel(logging.INFO)
+COH_LOG.addHandler(handler)
