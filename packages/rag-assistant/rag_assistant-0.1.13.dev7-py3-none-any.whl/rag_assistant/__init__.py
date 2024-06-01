6d92c299a94ff885aa73146779c412d80de452e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 15:04:47 +0000

    Add package rag-assistant/0.1.13.dev7/rag_assistant-0.1.13.dev7-py3-none-any.whl

---
+++ packages/rag-assistant/rag_assistant-0.1.13.dev7-py3-none-any.whl/rag_assistant/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.1.13.dev7"
+__short_desc__ = """The Swarmauri Rag Assistant is part of the swarmaURI framework."""
+__long_desc__ = """# Swarmauri Rag Assistant
+
+## Overview
+The Swarmauri Rag Assistant
+
+## Installation
+```bash
+pip install rag_assistant==0.1.12 --user
+```
+
+## Execution
+```bash
+! rag_assistant --api_key "YOUR API KEY" --db_path "E:\\swarmauri_github/prompt_responses.db" --system_context "You are a helpful assistant."
+```
+"""
