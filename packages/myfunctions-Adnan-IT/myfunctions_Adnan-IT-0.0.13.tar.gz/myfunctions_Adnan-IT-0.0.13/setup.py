dfb68a981077873c234be14083149c52f8f703d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-01 14:04:49 +0000

    Add package myfunctions-Adnan-IT/0.0.13/myfunctions_Adnan-IT-0.0.13.tar.gz

---
+++ packages/myfunctions-Adnan-IT/myfunctions_Adnan-IT-0.0.13.tar.gz/myfunctions_Adnan-IT-0.0.13/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_Adnan-IT',
+    version='0.0.13',
+    packages=find_packages(),
+    install_requires=["os"],
+    url='https://github.com/Adnan-IT',
+    license='MIT',
+    author='Adnan-IT',
+    author_email='adnan123it@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
