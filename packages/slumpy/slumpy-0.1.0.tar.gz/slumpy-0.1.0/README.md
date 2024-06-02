fb3c3afe440c3139b8b6a8b27ab62fed8b8c4d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:06:22 +0000

    Add package slumpy/0.1.0/slumpy-0.1.0.tar.gz

---
+++ packages/slumpy/slumpy-0.1.0.tar.gz/slumpy-0.1.0/README.md
@@ -0,0 +1,10 @@
+# Slumpy v0.1.0
+
+Slumpy is a Python module for supporting Slum standard in the language.
+
+---
+## Instalación:
+
+```bash
+pip3 install slumpy
+```
---
+++ packages/slumpy/slumpy-0.1.0.tar.gz/slumpy-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup (
+	name = "slumpy",
+	version = "0.1.0",
+	author = "gloackong",
+	description = "Python Package for Slum Gloack Standard for Code",
+	url = "https://github.com/gloacking/slumpy",
+	packages = find_packages (),
+	license = "MIT",
+	python_requires = ">=3.8",
+	project_urls = {
+		"Source" : "https://github.com/gloacking/slumpy"
+	}
+)
