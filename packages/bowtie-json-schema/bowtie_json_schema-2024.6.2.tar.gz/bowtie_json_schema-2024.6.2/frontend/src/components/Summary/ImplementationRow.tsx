36c894ec7d594c71fd53f480ff328d6ca7d97416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:20 +0000

    Add package bowtie-json-schema/2024.6.2/bowtie_json_schema-2024.6.2.tar.gz

---
+++ packages/bowtie-json-schema/bowtie_json_schema-2024.6.2.tar.gz/bowtie_json_schema-2024.6.2/frontend/src/components/Summary/ImplementationRow.tsx
@@ -0,0 +1,98 @@
+import "./ImplementationRow.css";
+import { useContext, useState } from "react";
+import { DetailsButtonModal } from "../Modals/DetailsButtonModal";
+import { mapLanguage } from "../../data/mapLanguage";
+import { useNavigate } from "react-router-dom";
+import {
+  Case,
+  Implementation,
+  ImplementationResults,
+} from "../../data/parseReportData";
+import { ThemeContext } from "../../context/ThemeContext";
+
+const ImplementationRow = ({
+  cases,
+  id,
+  implementation,
+  implementationResults,
+}: {
+  cases: Map<number, Case>;
+  id: string;
+  implementation: Implementation;
+  implementationResults: ImplementationResults;
+  key: number;
+  index: number;
+}) => {
+  const { isDarkMode } = useContext(ThemeContext);
+  const [showDetails, setShowDetails] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <tr>
+      <th
+        className="table-implementation-name align-middle p-0"
+        onClick={() => navigate(`/implementations/${id}`)}
+        scope="row"
+      >
+        <span
+          className={`text-decoration-underline ${
+            isDarkMode ? "text-primary-emphasis" : "text-primary"
+          }`}
+        >
+          {implementation.name}
+        </span>
+        <small className="text-muted ps-1">
+          {mapLanguage(implementation.language)}
+        </small>
+      </th>
+      <td className="align-middle d-none d-sm-table-cell">
+        <small className="font-monospace text-muted">
+          {implementation.version ?? ""}
+        </small>
+      </td>
+
+      <td className="text-center align-middle">
+        {implementationResults.totals.erroredCases}
+      </td>
+      <td className="text-center align-middle">
+        {implementationResults.totals.skippedTests}
+      </td>
+      <td className="text-center align-middle details-required">
+        {implementationResults.totals.failedTests! +
+          implementationResults.totals.erroredTests!}
+        <div className="hover-details text-center">
+          <p>
+            <b>failed</b>: &nbsp;{implementationResults.totals.failedTests}
+          </p>
+          <p>
+            <b>errored</b>: &nbsp;{implementationResults.totals.erroredTests}
+          </p>
+        </div>
+      </td>
+
+      <td className="align-middle p-0">
+        {implementationResults.totals.failedTests! +
+          implementationResults.totals.erroredTests! +
+          implementationResults.totals.skippedTests! >
+          0 && (
+          <button
+            type="button"
+            className="btn btn-sm btn-primary"
+            onClick={() => setShowDetails(true)}
+          >
+            Details
+          </button>
+        )}
+      </td>
+      <DetailsButtonModal
+        show={showDetails}
+        handleClose={() => setShowDetails(false)}
+        cases={cases}
+        implementationResults={implementationResults}
+        implementation={implementation}
+      />
+    </tr>
+  );
+};
+
+export default ImplementationRow;
---
+++ packages/bowtie-json-schema/bowtie_json_schema-2024.6.2.tar.gz/bowtie_json_schema-2024.6.2/frontend/src/components/Summary/SummaryTable.tsx
@@ -0,0 +1,108 @@
+import { useMemo } from "react";
+import Table from "react-bootstrap/Table";
+
+import ImplementationRow from "./ImplementationRow";
+import { ReportData, calculateTotals } from "../../data/parseReportData";
+
+const SummaryTable = ({ reportData }: { reportData: ReportData }) => {
+  const totals = useMemo(() => calculateTotals(reportData), [reportData]);
+  return (
+    <Table hover responsive>
+      <thead>
+        <tr>
+          <th
+            colSpan={2}
+            rowSpan={2}
+            scope="col"
+            className="text-center align-middle"
+          >
+            implementation
+          </th>
+          <th colSpan={1} className="text-center">
+            <span className="text-muted">cases ({reportData.cases.size})</span>
+          </th>
+          <th colSpan={3} className="text-center">
+            <span className="text-muted">tests ({totals.totalTests})</span>
+          </th>
+          <th colSpan={1}></th>
+        </tr>
+        <tr>
+          <th scope="col" className="text-center">
+            errors
+          </th>
+          <th scope="col" className="table-bordered text-center">
+            skipped
+          </th>
+          <th
+            scope="col"
+            className="table-bordered text-center details-required"
+          >
+            <div className="hover-details details-desc text-center">
+              <p>
+                failed
+                <br />
+                <span>
+                  implementation worked successfully but got the wrong answer
+                </span>
+              </p>
+              <p>
+                errored
+                <br />
+                <span>
+                  implementation crashed when trying to calculate an answer
+                </span>
+              </p>
+            </div>
+            unsuccessful
+          </th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody className="table-group-divider">
+        {Array.from(reportData.implementationsResults.entries())
+          .sort(
+            ([, a], [, b]) =>
+              a.totals.failedTests! +
+              a.totals.erroredTests! +
+              a.totals.skippedTests! -
+              b.totals.failedTests! -
+              b.totals.erroredTests! -
+              b.totals.skippedTests!,
+          )
+          .map(([id, implResults], index) => (
+            <ImplementationRow
+              cases={reportData.cases}
+              id={id}
+              implementation={reportData.runMetadata.implementations.get(id)!}
+              implementationResults={implResults}
+              key={index}
+              index={index}
+            />
+          ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <th scope="row" colSpan={2}>
+            total
+          </th>
+          <td className="text-center">{totals.erroredCases}</td>
+          <td className="text-center">{totals.skippedTests}</td>
+          <td className="text-center details-required">
+            {totals.failedTests + totals.erroredTests}
+            <div className="hover-details text-center">
+              <p>
+                <b>failed</b>: {totals.failedTests}
+              </p>
+              <p>
+                <b>errored</b>: {totals.erroredTests}
+              </p>
+            </div>
+          </td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </Table>
+  );
+};
+
+export default SummaryTable;
