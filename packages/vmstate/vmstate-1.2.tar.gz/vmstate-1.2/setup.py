9994f2e4649b030c5f4b91120307c0a9ba9e5e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:31 +0000

    Add package vmstate/1.2/vmstate-1.2.tar.gz

---
+++ packages/vmstate/vmstate-1.2.tar.gz/vmstate-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+name="vmstate",
+version="1.2",
+description="azure virtual info package sub module",
+packages=find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires=">=3.6",
+)
