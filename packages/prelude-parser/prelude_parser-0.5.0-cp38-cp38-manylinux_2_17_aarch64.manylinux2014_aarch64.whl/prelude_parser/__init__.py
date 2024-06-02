896574a6c70e1b6915765a471f4a9bcc52e7af17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:31 +0000

    Add package prelude-parser/0.5.0/prelude_parser-0.5.0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/prelude-parser/prelude_parser-0.5.0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/prelude_parser/__init__.py
@@ -0,0 +1,4 @@
+from prelude_parser._prelude_parser import parse_subject_native_file
+from prelude_parser.parser import parse_to_classes, parse_to_dict
+
+__all__ = ["parse_subject_native_file", "parse_to_classes", "parse_to_dict"]
