a2636b99801f9af1a7c2c75a3cd2ddc48014e3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 15:04:50 +0000

    Add package toru/0.3/toru-0.3.tar.gz

---
+++ packages/toru/toru-0.3.tar.gz/toru-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='toru',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        # 任何依赖项都在这里列出
+    ],
+    author='S.-W. yan',
+    author_email='wq543477641@gamil.com',
+    description='A brief description of your package',
+    license='MIT',
+    keywords='sample setuptools development',
+    url=''
+)
