173e6af440d7cce185b83d5d66c92bdf5d86738e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 14:05:38 +0000

    Add package rl-storage-handlers/0.1.0/rl_storage_handlers-0.1.0-py3-none-any.whl

---
+++ packages/rl-storage-handlers/rl_storage_handlers-0.1.0-py3-none-any.whl/rl-storage-handlers/file/file_handler.py
@@ -0,0 +1,39 @@
+from abc import abstractmethod
+
+from common import Common
+
+from ..params.storage_data import StorageData
+from ..params.storage_destination import StorageDestination
+from ..storage_handler import StorageHandler
+from .params.file_data import FileData
+from .params.file_destination import FileDestination
+
+
+class FileHandler(StorageHandler):
+    __common: Common = Common()
+
+    @abstractmethod
+    def write(self, destination: StorageDestination, data: StorageData) -> None:
+        pass
+
+    @abstractmethod
+    def read(self, destination: StorageDestination) -> StorageData:
+        pass
+
+    def _write(self, destination: FileDestination, mode: str, data: FileData) -> None:
+        file_path: str = self.__get_file_path(destination.get())
+
+        with open(file_path, mode) as file:
+            file.write(data.get())
+
+    def _read(self, destination: FileDestination, mode: str) -> FileData:
+        file_path: str = self.__get_file_path(destination.get())
+        data: FileData | None = None
+
+        with open(file_path, mode) as file:
+            data = FileData(file.read())
+
+        return data
+
+    def __get_file_path(self, file_name: str) -> str:
+        return f'{self.__common.user_files_root}/{file_name}'
