26a170eda4668543a5a1464fc378ccf774704c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:20 +0000

    Add package mintlemon-turkish-nlp/0.3.1/mintlemon-turkish-nlp-0.3.1.tar.gz

---
+++ packages/mintlemon-turkish-nlp/mintlemon-turkish-nlp-0.3.1.tar.gz/mintlemon-turkish-nlp-0.3.1/README.md
@@ -0,0 +1,38 @@
+<h1 align = 'Center'>Mintlemon Türkçe Doğal Dil İşleme Kütüphanesi</h1>
+
+|    | 
+|----|
+|![1](https://user-images.githubusercontent.com/83168207/229226994-d6023420-c88b-48c8-abaf-8429ce050c1f.jpg)|Türkçe Doğal Dil İşleme Kütüphanesi.|
+
+--- 
+
+## Mint-Lemon-Turkish NLP Kütüphanesine'e Katkı Sağlama
+
+Bu projeye doğrudan katkıda bulunmak istiyorsanız, lütfen [CONTRIBUTING](CONTRIBUTING.rst) dosyasına bir göz atın.
+
+
+## Dökümantasyon & Örnekler
+
+* Ek olarak, [ilgili klasöre](examples/) bakarak örneklerden yararlanabilirsiniz.
+
+
+
+|    | 
+|----|
+|<img width="1792" alt="Ekran Resmi 2023-04-06 13 00 48" src="https://user-images.githubusercontent.com/83168207/230344345-4dfe39a3-46f4-4147-bd47-61ee9548c948.png">|
+
+
+
+### Lisans
+
+Bu proje [LİSANS](LICENSE) altında açık kaynaklıdır.
+
+-   Lütfen dikkat edin, bu proje "olduğu gibi" sunulmaktadır ve hiçbir garanti verilmez. (Bu yazılımın kullanımı lisans sözleşmesinin şartlarına tabidir.) Bu yazılım Apache 2.0 lisansı altında lisanslanmıştır. [LİSANS](LICENSE) sayfasına bakın.
+
+### Referanslar
+
+* [Koehn, P. ve Schroeder, J. (n.d.)](https://github.com/mediacloud/sentence-splitter). 
+* ["Türkçe Deasciification Yöntemleri Üzerine Karşılaştırmalı Bir Çalışma"](https://www.sciencedirect.com/science/article/pii/S221509862200101X), Türkçe Deasciification için çeşitli yöntemlerin karşılaştırmasını sunan bir bilimsel makale.
+* [Python ile örnek Türkçe Deasciifier Uygulaması.](https://github.com/aysnrgenc/TurkishDeasciifier)
+* [Bu dizinde bir istatistiksel modele dayalı Python'da bir Türkçe Deasciifier uygulaması bulunmaktadır.](https://github.com/emres/turkish-deasciifier)
+* [Turkish Language Repository ~ TDD.](https://tdd.ai)
---
+++ packages/mintlemon-turkish-nlp/mintlemon-turkish-nlp-0.3.1.tar.gz/mintlemon-turkish-nlp-0.3.1/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+
+def get_long_description():
+    with open("README.md", "r", encoding="utf-8") as f:
+        return f.read()
+
+setup(
+    name="mintlemon-turkish-nlp",
+    version = "0.3.1",
+    description="Mint & Lemon Turkish NLP Library developed by Mint & Lemon Development Team.",
+    author="Mint&Lemon",
+    license="Apache License, Version 2.0",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Teknofest-Nane-Limon/mintlemon-turkish-nlp",
+    project_urls={
+        "Tracker": "https://github.com/Teknofest-Nane-Limon/mintlemon-turkish-nlp/issues",
+        "Documentation": "https://mintlemon-turkish-nlp.readthedocs.io",
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Topic :: Text Processing",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    packages=find_packages(),
+    package_data={"mintlemon": ["data/*"]},
+    include_package_data=True,
+    install_requires=[
+        "numpy>=1.20.0",    
+        "regex>=2021.4.4",    
+        "zeyrek>=0.1.3",    
+        "nltk>=3.8.1",    
+        "pandas>=1.3.4",    
+        "scikit-learn>=1.2.0",
+    ],
+    extras_require={"dev": ["yapf", "bumpver", "flake8", "coverage", "pytest"]},
+)
