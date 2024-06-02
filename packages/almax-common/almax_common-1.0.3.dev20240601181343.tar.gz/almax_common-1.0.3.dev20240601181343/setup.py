1ce3f71cbb5f0799c72db269a4a8266348020bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:41 +0000

    Add package almax-common/1.0.3.dev20240601181343/almax_common-1.0.3.dev20240601181343.tar.gz

---
+++ packages/almax-common/almax_common-1.0.3.dev20240601181343.tar.gz/almax_common-1.0.3.dev20240601181343/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages;
+
+with open("README.md", "r") as fh:
+    readMe = fh.read();
+
+with open("version.txt", "r") as fh:
+    versionFile = fh.read().strip();
+
+setup(
+    name='almax_common',
+    version=versionFile,
+    description='Library with my most used Classes and Methods',
+    long_description=readMe,
+    long_description_content_type='text/markdown',
+    author='AlMax98',
+    author_email='alihaider.maqsood@gmail.com',
+    packages=find_packages(),
+    package_dir={'': '.'}
+);
