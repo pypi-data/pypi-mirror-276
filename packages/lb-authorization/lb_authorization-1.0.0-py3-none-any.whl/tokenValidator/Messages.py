d497b400e6d43a58dd691ad27262f54dc8cec6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:17 +0000

    Add package lb-authorization/1.0.0/lb_authorization-1.0.0-py3-none-any.whl

---
+++ packages/lb-authorization/lb_authorization-1.0.0-py3-none-any.whl/tokenValidator/Messages.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Lucas Barros'
+__version__ = 1.0
+__maintainer__ = 'Lucas Barros'
+__email__ = 'lucasbarros2000@hotmail.com'
+__status__ = 'Production'
+
+
+class Messages:
+
+    TOKEN_IS_REQUIRED: str = "TOKEN_IS_REQUIRED"
+    TOKEN_IS_INVALID: str = "TOKEN_IS_INVALID"
+    TOKEN_IS_EXPIRED: str = "TOKEN_IS_EXPIRED"
