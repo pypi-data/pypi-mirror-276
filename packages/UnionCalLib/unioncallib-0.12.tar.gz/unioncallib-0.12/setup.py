293935eea1b85e3430fd4284c586e9469ee366e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 15:04:40 +0000

    Add package UnionCalLib/0.12/unioncallib-0.12.tar.gz

---
+++ packages/UnionCalLib/unioncallib-0.12.tar.gz/unioncallib-0.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UnionCalLib',
+    version='0.12',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'run=UnionCalLib.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
