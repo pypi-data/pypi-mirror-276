75f547432fe8250c7faea4db5f17f26418c66bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 14:05:33 +0000

    Add package flashback/2.1.0/flashback-2.1.0.tar.gz

---
+++ packages/flashback/flashback-2.1.0.tar.gz/flashback-2.1.0/flashback/iterating/uniq.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import Any, Iterable
+
+
+def uniq(iterable: Iterable[Any]) -> tuple[Any]:
+    """
+    Removes duplicates items from `iterable` while keeping their order.
+
+    Examples:
+        ```python
+        from flashback.iterating import uniq
+
+        for user_id in uniq([1058, 1058, 85, 9264, 19475, 85]):
+            print(user_id)
+        #=> 1058
+        #=> 85
+        #=> 9264
+        #=> 19475
+
+        # Keeps order
+        assert set([1, 1, 3, 4, 5, 5]) != uniq([1, 1, 3, 4, 5, 5])
+        ```
+
+    Params:
+        iterable: the iterable to remove duplicates from
+
+    Returns:
+        the iterable without duplicates
+    """
+    unique = []
+    seen = set()
+
+    for item in iterable:
+        repr_item = repr(item)
+        if repr_item in seen:
+            continue
+
+        unique.append(item)
+        seen.add(repr_item)
+
+    return tuple(unique)
