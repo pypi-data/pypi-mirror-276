ccccdb76bde52645faad8741c7b97f417f181ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 14:05:01 +0000

    Add package schwabtools/0.0.6/schwabtools-0.0.6.tar.gz

---
+++ packages/schwabtools/schwabtools-0.0.6.tar.gz/schwabtools-0.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Schwab API Python Client (unofficial)'
+with open('README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='schwabtools',
+    version=VERSION,
+    author='Tyler Bowers',
+    author_email='tylerebowers@gmail.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'websockets',
+    ],
+    keywords=['python', 'schwab', 'api', 'client'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+    ]
+)
