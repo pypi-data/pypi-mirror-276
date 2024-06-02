0a4bc5fcbeca03df95892b02cfcf295939781db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 15:04:43 +0000

    Add package aws-s3/0.1.0/aws_s3-0.1.0-py3-none-any.whl

---
+++ packages/aws-s3/aws_s3-0.1.0-py3-none-any.whl/aws_s3/__init__.py
@@ -0,0 +1,8 @@
+"""S3 Bucket helper utils. Async list objects by folders
+
+The file is mandatory for build system to find the package.
+"""
+
+from aws_s3.__about__ import __version__
+
+__all__ = ["__version__"]
