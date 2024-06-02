54ab41c9b5ba22baafef2cb1ec54497afd5c1c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 14:04:55 +0000

    Add package pyGetComics/1.4.6/pygetcomics-1.4.6-py3-none-any.whl

---
+++ packages/pyGetComics/pygetcomics-1.4.6-py3-none-any.whl/berhoel/get_comics/__init__.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Download various daily comics.
+"""
+
+# Standard library imports.
+import argparse
+
+# Local library imports.
+from . import peanuts, garfield
+
+__date__ = "2024/06/02 01:09:05 hoel"
+__author__ = "Berthold Höllmann"
+__copyright__ = "Copyright © 2017, 2022 by Berthold Höllmann"
+__credits__ = ["Berthold Höllmann"]
+__maintainer__ = "Berthold Höllmann"
+__email__ = "berhoel@gmail.com"
+__version__ = __import__("importlib.metadata", fromlist=["version"]).version(
+    "get_comics"
+)
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="get_comics", description="Download various periodic comics."
+    )
+    parser.add_argument(
+        "--version",
+        "-V",
+        action="version",
+        version="%(prog)s {version}".format(version=__version__),
+    )
+    return parser
+
+
+def main(args=None):
+    """Get all supported comics."""
+    args = get_parser().parse_args()
+
+    print("Get Peanuts.")
+    peanuts.main(args)
+    print("Get Garfield.")
+    garfield.main(args)
+
+
+if __name__ == "__main__":
+    main()
+
+# Local Variables:
+# mode: python
+# compile-command: "cd ../../ && python setup.py test"
+# time-stamp-pattern: "30/__date__ = \"%:y/%02m/%02d %02H:%02M:%02S %u\""
+# End:
