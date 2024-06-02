ff2c5a93d9f088cc0ff3f98573417a2fd8736c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:04:57 +0000

    Add package helloworld-sdk/0.1/helloworld_sdk-0.1.tar.gz

---
+++ packages/helloworld-sdk/helloworld_sdk-0.1.tar.gz/helloworld_sdk-0.1/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='helloworld_sdk',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+)
