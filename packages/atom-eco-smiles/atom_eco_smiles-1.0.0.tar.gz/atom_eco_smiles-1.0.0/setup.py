e42f39f046bc07c8b2c0a31cd5febfd6994e7901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:04:58 +0000

    Add package atom-eco-smiles/1.0.0/atom_eco_smiles-1.0.0.tar.gz

---
+++ packages/atom-eco-smiles/atom_eco_smiles-1.0.0.tar.gz/atom_eco_smiles-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atom_eco_smiles',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Calculate atom economy for chemical reactions using reaction SMILES',
+    author='Samuele Giani',
+    author_email='samuele.giani@empa.ch',
+    url='https://your-package-url.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)
