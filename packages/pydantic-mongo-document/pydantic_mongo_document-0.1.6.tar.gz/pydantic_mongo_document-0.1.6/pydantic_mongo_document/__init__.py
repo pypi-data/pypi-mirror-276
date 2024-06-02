57f2f127bf4cf7b3e3db6d60de36a715e01aa2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:28 +0000

    Add package pydantic-mongo-document/0.1.6/pydantic_mongo_document-0.1.6.tar.gz

---
+++ packages/pydantic-mongo-document/pydantic_mongo_document-0.1.6.tar.gz/pydantic_mongo_document-0.1.6/pydantic_mongo_document/__init__.py
@@ -0,0 +1,10 @@
+from .document import Document
+from .exceptions import DocumentNotFound
+from .encoder import JsonEncoder
+
+
+__all__ = [
+    "Document",
+    "DocumentNotFound",
+    "JsonEncoder",
+]
