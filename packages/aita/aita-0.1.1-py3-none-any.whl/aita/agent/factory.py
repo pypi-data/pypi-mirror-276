a8ad3673f03dfd373211bba430da6c0241d95957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 14:05:21 +0000

    Add package aita/0.1.1/aita-0.1.1-py3-none-any.whl

---
+++ packages/aita/aita-0.1.1-py3-none-any.whl/aita/agent/factory.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+from aita.agent.base import AitaAgent
+from aita.agent.sql import SqlAgent
+from aita.agent.pandas import PandasAgent
+from aita.agent.pyarrow import PyArrowAgent
+from aita.agent.pyspark import PySparkAgent
+from aita.agent.python import PythonAgent
+
+
+class AgentProvider(Enum):
+    sql = SqlAgent
+    pandas = PandasAgent
+    pyarrow = PyArrowAgent
+    pyspark = PySparkAgent
+    python = PythonAgent
+
+
+def get_supported_agents():
+    return list(AgentProvider)
+
+
+class AgentFactory:
+
+    @staticmethod
+    def create_agent(agent_type: str, *args, **kwargs) -> AitaAgent:
+        if not agent_type or not hasattr(AgentProvider, agent_type):
+            agent_class = AitaAgent
+        else:
+            agent_class = AgentProvider[agent_type].value
+        return agent_class(*args, **kwargs)
---
+++ packages/aita/aita-0.1.1-py3-none-any.whl/aita/datasource/snowflake.py
@@ -0,0 +1,42 @@
+from adbc_driver_manager.dbapi import Connection
+from sqlmodel import Field
+
+from aita.datasource.sql import SqlDataSource
+from typing import Optional
+import adbc_driver_snowflake.dbapi
+
+
+class SnowflakeDataSource(SqlDataSource):
+    connection_url: str = Field(..., description="Connection URL")
+
+    def __init__(
+        self,
+        connection_url: Optional[str] = None,
+        **kwargs,
+    ):
+        if not connection_url:
+            connection_url = self.build_connection_url(
+                **kwargs,
+            )
+        super().__init__(connection_url=connection_url)
+
+    def build_connection_url(
+        self,
+        user: str,
+        password: str,
+        account: str,
+        warehouse: Optional[str] = "COMPUTE_WH",
+        role: Optional[str] = None,
+        database: Optional[str] = None,
+        schema: Optional[str] = None,
+        **kwargs,
+    ) -> str:
+        connection_url = f"{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}"
+        if role:
+            connection_url += f"&role={role}"
+        if kwargs:
+            connection_url += "&" + "&".join([f"{k}={v}" for k, v in kwargs.items()])
+        return connection_url
+
+    def connect(self) -> Connection:
+        return adbc_driver_snowflake.dbapi.connect(self.connection_url)
