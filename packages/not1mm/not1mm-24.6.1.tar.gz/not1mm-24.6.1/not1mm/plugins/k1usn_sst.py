c53cda039565e94260f196f3412d0ce2886e01f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 15:04:28 +0000

    Add package not1mm/24.6.1/not1mm-24.6.1.tar.gz

---
+++ packages/not1mm/not1mm-24.6.1.tar.gz/not1mm-24.6.1/not1mm/plugins/k1usn_sst.py
@@ -0,0 +1,24 @@
+"""K1USN Slow Speed Test"""
+
+# Geographic Focus:	Worldwide
+# Participation:	Worldwide
+# Mode:	CW
+# Bands:	160, 80, 40, 20, 15, 10m
+# Classes:	Single Op (QRP/Low/High)
+# Max power:	HP: >100 watts
+# LP: 100 watts
+# QRP: 5 watts
+# Exchange:	Maximum 20 wpm
+# Name + (state/province/country)
+# Work stations:	Once per band
+# QSO Points:	1 point per QSO (starting with Jan 4 contest)
+# Multipliers:	Each state/province/country once per band
+# W/VE do not count as country mults (starting with Jan 4 contest)
+# Score Calculation:	Total score = total QSO points x total mults
+# Submit logs by:	2359Z May 26, 2024
+# Post log summary at:	http://www.3830scores.com
+# Mail logs to:	(none)
+# Find rules at:	http://www.k1usn.com/sst.html
+
+import datetime
+
