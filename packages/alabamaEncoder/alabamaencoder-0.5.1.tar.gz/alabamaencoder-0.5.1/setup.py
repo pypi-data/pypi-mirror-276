721e9c715b332caf16cf9108c6925aa98199ff14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:26 +0000

    Add package alabamaEncoder/0.5.1/alabamaencoder-0.5.1.tar.gz

---
+++ packages/alabamaEncoder/alabamaencoder-0.5.1.tar.gz/alabamaencoder-0.5.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alabamaEncoder",
+    version="0.5.1",
+    packages=find_packages(),
+    install_requires=[
+        "scenedetect",
+        "tqdm",
+        "celery",
+        "redis",
+        "psutil",
+        "opencv-contrib-python",
+        "requests",
+        "torf",
+        "websockets",
+        "scipy",
+        "numpy",
+        "scikit-image",
+        "argparse_range",
+    ],
+    entry_points="""
+      [console_scripts]
+      alabamaEncoder=alabamaEncode.cli.__main__:main
+      """,
+)
