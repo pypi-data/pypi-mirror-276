80b5ee8922d07f15b563e78838f44badb8e40f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:25 +0000

    Add package almax-common/0.11.20240601152652/almax_common-0.11.20240601152652.tar.gz

---
+++ packages/almax-common/almax_common-0.11.20240601152652.tar.gz/almax_common-0.11.20240601152652/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages;
+from AlmaxUtils import Time;
+
+with open("README.md", "r") as fh:
+    readMe = fh.read();
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines();
+
+setup(
+    name='almax_common',
+    version=f"0.11.{Time.now.strftime('%Y%m%d%H%M%S')}",
+    description='A common library with some of my implementations',
+    long_description=readMe,
+    long_description_content_type='text/markdown',
+    author='AlMax98',
+    author_email='alihaider.maqsood@gmail.com',
+    packages=find_packages(),
+    package_dir={'': '.'},
+    install_requires=required,
+);
