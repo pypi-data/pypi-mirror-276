0272bb87f039cdbb70ad9de3f2a4abeb980d7f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 15:05:23 +0000

    Add package volkanic/0.5.2/volkanic-0.5.2.tar.gz

---
+++ packages/volkanic/volkanic-0.5.2.tar.gz/volkanic-0.5.2/volkanic/compat.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import abc
+import sys
+
+try:
+    from functools import cached_property
+except ImportError:
+    # noinspection PyPep8Naming
+    class cached_property:
+        """
+        A property that is only computed once per instance and then replaces itself
+        with an ordinary attribute. Deleting the attribute resets the property.
+        Source:
+            https://github.com/bottlepy/bottle/commit/fa7733e075da0d790d809aa3d2f53071897e6f76
+        Adapted from github:
+            https://raw.githubusercontent.com/pydanny/cached-property/master/cached_property.py
+        """
+
+        def __init__(self, func):
+            self.__doc__ = getattr(func, "__doc__")
+            self.func = func
+
+        def __get__(self, obj, cls):
+            if obj is None:
+                return self
+            try:
+                return obj.__dict__[self.func.__name__]
+            except KeyError:
+                val = self.func(obj)
+                return obj.__dict__.setdefault(self.func.__name__, val)
+
+
+def abstract_property(func):
+    if sys.version_info > (3, 3):
+        return property(abc.abstractmethod(func))
+    else:
+        return abc.abstractproperty(func)
