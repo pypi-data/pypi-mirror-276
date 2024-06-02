632516255fe672eedf76d031a46a9d335b1826a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:28 +0000

    Add package RealTimeTTS/0.4.1/RealTimeTTS-0.4.1-py3-none-any.whl

---
+++ packages/RealTimeTTS/RealTimeTTS-0.4.1-py3-none-any.whl/RealtimeTTS/__init__.py
@@ -0,0 +1,8 @@
+from .text_to_stream import TextToAudioStream  # noqa: F401
+from .engines import BaseEngine  # noqa: F401
+from .engines import SystemEngine, SystemVoice  # noqa: F401
+from .engines import AzureEngine, AzureVoice  # noqa: F401
+from .engines import ElevenlabsEngine, ElevenlabsVoice  # noqa: F401
+from .engines import CoquiEngine, CoquiVoice  # noqa: F401
+from .engines import OpenAIEngine, OpenAIVoice  # noqa: F401
+from .engines import GTTSEngine, GTTSVoice
---
+++ packages/RealTimeTTS/RealTimeTTS-0.4.1-py3-none-any.whl/RealtimeTTS/engines/__init__.py
@@ -0,0 +1,7 @@
+from .base_engine import BaseEngine  # noqa: F401
+from .azure_engine import AzureEngine, AzureVoice  # noqa: F401
+from .system_engine import SystemEngine, SystemVoice  # noqa: F401
+from .elevenlabs_engine import ElevenlabsEngine, ElevenlabsVoice  # noqa: F401
+from .coqui_engine import CoquiEngine, CoquiVoice  # noqa: F401
+from .openai_engine import OpenAIEngine, OpenAIVoice  # noqa: F401
+from .gtts_engine import GTTSEngine, GTTSVoice  # noqa: F401
