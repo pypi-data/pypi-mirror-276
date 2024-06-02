42d3c03c850fbccbc67f2c71fe2efa45d061a190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:04:51 +0000

    Add package graphmemory/0.1.0/graphmemory-0.1.0.tar.gz

---
+++ packages/graphmemory/graphmemory-0.1.0.tar.gz/graphmemory-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='graphmemory',
+    version='0.1.0',
+    author='BradAGI',
+    author_email='cavemen_summary_0f@icloud.com',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/graphmemory/',
+    license='LICENSE.txt',
+    description='A package for creating a graph database for use with GraphRAG.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'duckdb',
+        'json',
+        'pydantic',
+        'os',
+        'logging'
+    ],
+    keywords='graphrag graph database rag'
+)
