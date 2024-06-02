2965955ddcb8272069a1c786eb7e32a89f8084bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:24 +0000

    Add package ipaacar-python/0.1.4/ipaacar_python-0.1.4.tar.gz

---
+++ packages/ipaacar-python/ipaacar_python-0.1.4.tar.gz/ipaacar_python-0.1.4/ipaacar-python/python/ipaacar/handler.py
@@ -0,0 +1,27 @@
+import abc
+
+from ipaacar.components import IU
+
+
+class IUCallbackHandlerInterface(abc.ABC):
+    """
+    Interface for IU callback handling. Used by the IU for update callback handling and the Input Buffer for new IUs.
+    """
+
+    @abc.abstractmethod
+    async def process_iu_callback(self, iu: IU):
+        """
+        Will be awaited by Callbacks from inside the Rust event loop and send to the same
+        Python eventloop that was used for registering it.
+
+        :param iu: New/Updated IU
+        :return: Nothing
+        """
+        pass
+
+
+class MessageCallbackHandlerInterface(abc.ABC):
+
+    @abc.abstractmethod
+    async def process_message_callback(self, msg: str):
+        pass
