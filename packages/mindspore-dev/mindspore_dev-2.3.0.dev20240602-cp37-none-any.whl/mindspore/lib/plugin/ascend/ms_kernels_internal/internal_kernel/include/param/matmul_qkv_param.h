8caa0f04a171b5dd9281ae130f2ca9a2a320f2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:06:50 +0000

    Add package mindspore-dev/2.3.0.dev20240602/mindspore_dev-2.3.0.dev20240602-cp37-none-any.whl

---
+++ packages/mindspore-dev/mindspore_dev-2.3.0.dev20240602-cp37-none-any.whl/mindspore/lib/plugin/ascend/ms_kernels_internal/internal_kernel/include/param/matmul_qkv_param.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MATMUL_QKV_PARAMS_H_
+#define MATMUL_QKV_PARAMS_H_
+
+#include "types.h"
+#include "op_param.h"
+
+namespace mindspore {
+namespace internal {
+struct MatmulQkvParam : public OpParam {
+  bool transposeA;
+  bool transposeB;
+  MatmulQkvParam() : transposeA(false), transposeB(true) {}
+  MatmulQkvParam(bool tA, bool tB) : transposeA(tA), transposeB(tB) {}
+  bool operator==(const MatmulQkvParam &other) const { 
+    return (this->transposeA == other.transposeA && this->transposeB == other.transposeB); 
+  }
+};
+
+}  // namespace internal
+}  // namespace mindspore
+#endif
