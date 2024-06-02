7c9bb45769aa887b3df408a0a8c3d7dbbcd09d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:18 +0000

    Add package pyaedt/0.9.3/pyaedt-0.9.3.tar.gz

---
+++ packages/pyaedt/pyaedt-0.9.3.tar.gz/pyaedt-0.9.3/pyaedt/generic/desktop_sessions.py
@@ -0,0 +1,6 @@
+# This contains the Desktops database.
+# When AEDT will support multiple desktops, it will be filled with additional properties and methods
+
+_desktop_sessions = {}
+
+_edb_sessions = []
