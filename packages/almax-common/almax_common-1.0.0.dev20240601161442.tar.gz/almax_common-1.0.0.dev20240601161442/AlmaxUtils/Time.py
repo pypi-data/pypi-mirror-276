9d784d6596752e1361f5bbc636637bb49fa9557c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:50 +0000

    Add package almax-common/1.0.0.dev20240601161442/almax_common-1.0.0.dev20240601161442.tar.gz

---
+++ packages/almax-common/almax_common-1.0.0.dev20240601161442.tar.gz/almax_common-1.0.0.dev20240601161442/AlmaxUtils/Time.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta;
+
+now = datetime.now();
+
+def CalculateTime(start: datetime):
+    return datetime.now() - start;
+
+def TimeToString(time: datetime) -> str:
+    seconds = time.total_seconds();
+    minutes = seconds // 60;
+    hours = minutes // 60;
+    days = hours // 24;
+    return f"{round(days)} Days = {round(hours)} Hours = {round(minutes)} Minutes = {round(seconds, 2)} Seconds";
+
+def AddTime(Hours: int) -> str:
+    return (datetime.now() + timedelta(hours=Hours)).strftime('%d-%m-%Y_%H:%M:%S');
