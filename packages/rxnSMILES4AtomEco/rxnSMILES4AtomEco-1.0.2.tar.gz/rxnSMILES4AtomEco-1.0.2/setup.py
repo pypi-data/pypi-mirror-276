61699c60f8e5aeeb41bd136e6e69bd9076181327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:53 +0000

    Add package rxnSMILES4AtomEco/1.0.2/rxnSMILES4AtomEco-1.0.2.tar.gz

---
+++ packages/rxnSMILES4AtomEco/rxnSMILES4AtomEco-1.0.2.tar.gz/rxnSMILES4AtomEco-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rxnSMILES4AtomEco',
+    version='1.0.2',
+    packages=find_packages(),
+    description='Calculate atom economy for chemical reactions using reaction SMILES',
+    author='Samuele Giani',
+    author_email='samuele.giani@empa.ch',
+    url='https://pypi.org/project/rxnSMILES4AtomEco/1.0.0/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)
