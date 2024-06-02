60e52a964b1a67ecef6a02f52068232944421920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:56 +0000

    Add package mpflash/0.8.1/mpflash-0.8.1-py3-none-any.whl

---
+++ packages/mpflash/mpflash-0.8.1-py3-none-any.whl/mpflash/mpboard_id/board_id.py
@@ -0,0 +1,70 @@
+"""
+Translate board description to board designator
+"""
+
+import functools
+from pathlib import Path
+from typing import Optional
+
+from mpflash.errors import MPFlashError
+from mpflash.logger import log
+from mpflash.mpboard_id.store import read_known_boardinfo
+from mpflash.vendor.versions import clean_version, get_stable_mp_version
+
+
+def find_board_id_by_description(
+    descr: str,
+    short_descr: str,
+    *,
+    version: str,
+    board_info: Optional[Path] = None,
+) -> Optional[str]:
+    """Find the MicroPython BOARD_ID based on the description in the firmware"""
+
+    try:
+        boards = _find_board_id_by_description(
+            descr=descr,
+            short_descr=short_descr,
+            board_info=board_info,
+            version=clean_version(version) if version else None,
+        )
+        return boards[-1].board_id
+    except MPFlashError:
+        return "UNKNOWN_BOARD"
+
+
+@functools.lru_cache(maxsize=20)
+def _find_board_id_by_description(
+    *,
+    descr: str,
+    short_descr: str,
+    version: Optional[str] = None,
+    board_info: Optional[Path] = None,
+):
+    """
+    Find the MicroPython BOARD_ID based on the description in the firmware
+    using the pre-built board_info.json file
+    """
+
+    candidate_boards = read_known_boardinfo(board_info)
+
+    if version:
+        # filter for matching version
+        if version in ("preview", "stable"):
+            # match last stable
+            version = get_stable_mp_version()
+        known_versions = sorted({b.version for b in candidate_boards})
+        if version not in known_versions:
+            # FIXME if latest stable is newer than the last version in the boardlist this will fail
+            log.trace(f"Version {version} not found in board info, using latest known version {known_versions[-1]}")
+            version = known_versions[-1]
+        version_matches = [b for b in candidate_boards if b.version.startswith(version)]
+        if not version_matches:
+            raise MPFlashError(f"No board info found for version {version}")
+        candidate_boards = version_matches
+    matches = [b for b in candidate_boards if b.description == descr]
+    if not matches and short_descr:
+        matches = [b for b in candidate_boards if b.description == short_descr]
+    if not matches:
+        raise MPFlashError(f"No board info found for description '{descr}' or '{short_descr}'")
+    return sorted(matches, key=lambda x: x.version)
