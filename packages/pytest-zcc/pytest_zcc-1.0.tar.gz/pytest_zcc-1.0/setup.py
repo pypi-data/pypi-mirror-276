3511fbd57e752cfe3b91ea27785be4e2bbb5138e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 15:04:30 +0000

    Add package pytest-zcc/1.0/pytest_zcc-1.0.tar.gz

---
+++ packages/pytest-zcc/pytest_zcc-1.0.tar.gz/pytest_zcc-1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytest_zcc',
+    url='https://github.com/xx/',
+    version='1.0',
+    author='zcc',
+    author_email="1059995908@qq.com",
+    description='eee',
+    long_description='eee',
+    classifiers=[
+        # pipy搜索索引分类
+        'Framework :: Pytest',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ],
+    license='xxxxxx',
+    packages=find_packages(),
+    keywords=[
+        'pytest', 'py.test', 'pytest_zcc'
+    ],
+    install_requires=[
+        'pytest'
+    ],
+    entry_points={
+        'pytest11':[
+            'pytest_zcc = src.pytest_zccdev'
+        ]
+    },
+    zip_safe=False
+)
