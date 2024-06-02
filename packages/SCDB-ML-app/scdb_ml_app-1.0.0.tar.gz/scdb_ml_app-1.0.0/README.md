02a9c022506b038adf6e43036df014b4904c921a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 15:04:45 +0000

    Add package SCDB-ML-app/1.0.0/scdb_ml_app-1.0.0.tar.gz

---
+++ packages/SCDB-ML-app/scdb_ml_app-1.0.0.tar.gz/scdb_ml_app-1.0.0/README.md
@@ -0,0 +1,15 @@
+# Machine Learning of the Supreme Court Database
+
+## Table of content
+[Installation](#installation)
+[Usage and requirements](#usage-and-requirements)
+[Machine learning models](#machine-learning-models)
+[Interpretation](#interpretation)
+
+# Installation
+
+# Usage and Requirements
+
+# Machine learning models
+
+# Interpretation
---
+++ packages/SCDB-ML-app/scdb_ml_app-1.0.0.tar.gz/scdb_ml_app-1.0.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'SCDB_ML_app',         # How you named your package folder (MyLib)
+  packages=find_packages(),   # Chose the same as "name"
+  version = '1.0.0',      # Start with a small number and increase it with every change you make
+  license='AGPL 3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A MACHINE LEARNING ANALIZER DEPLOYED INTO A WEBPAGE',   # Give a short description about your library
+  author = 'HERMES A V URQUIJO',                   # Type in your name
+  author_email = 'hvurquijo@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/havurquijo/Project-SCDB',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/havurquijo/Project-SCDB/archive/refs/tags/v1.0.0-alpha.tar.gz',    # I explain this later on
+  keywords = [
+      'MACHINE', 
+      'LEARNING', 
+      'SCDB',
+      'SUPREME',
+      'COURT',
+      'WEBPAGE'
+      ],   # Keywords that define your package best
+  install_requires=[            # Required to install
+            'flask',
+            'pathlib',
+            'pandas',
+            'numpy',
+            'scikit-learn',
+            'requests',
+            'pickle',
+            'zipfile',
+            'time',
+            'os'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Choose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU Affero General Public License v3',   # Pick a license
+    'Programming Language :: Python :: 3.12'     # Specify which Python versions you want to support
+]
+)
