7c700f57f03136c153a372b2e8e8c598b7ba86d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:23 +0000

    Add package id-card-recognition/0.3.0/id_card_recognition-0.3.0.tar.gz

---
+++ packages/id-card-recognition/id_card_recognition-0.3.0.tar.gz/id_card_recognition-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='id_card_recognition',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'natsort',
+        'openai',
+        'python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'id_card_recognition = id_card_recognition.recognition:main',
+        ],
+    },
+    author='freedak Wang',
+    author_email='freedak@163.com',
+    description='A package for ID card recognition using OpenAI',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/freedak-wang/id_card_recognition',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
