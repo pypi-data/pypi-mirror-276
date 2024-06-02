656801bbf558886660c696f52377ee66b6a823b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:53 +0000

    Add package polars-lts-cpu/0.20.31/polars_lts_cpu-0.20.31.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.20.31.tar.gz/polars_lts_cpu-0.20.31/crates/polars-core/src/series/ops/mod.rs
@@ -0,0 +1,15 @@
+mod downcast;
+mod extend;
+mod null;
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Copy, Clone, Hash, Eq, PartialEq, Debug, Default)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum NullBehavior {
+    /// drop nulls
+    Drop,
+    /// ignore nulls
+    #[default]
+    Ignore,
+}
