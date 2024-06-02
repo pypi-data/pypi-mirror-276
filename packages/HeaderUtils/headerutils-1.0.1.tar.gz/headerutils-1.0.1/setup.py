57fda689af20acad5b56c8395ee929771fa8c36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:22 +0000

    Add package HeaderUtils/1.0.1/headerutils-1.0.1.tar.gz

---
+++ packages/HeaderUtils/headerutils-1.0.1.tar.gz/headerutils-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.1"
+
+setup(
+    name="HeaderUtils",
+    version=VERSION,
+    author="H4cK3dR4Du",
+    author_email="<stepjestrudy@gmail.com>",
+    url="https://github.com/gyewtools/discord-token-gen",
+    description="The best library for designs, with a wide variety of colors and useful utils for your tools.",
+    packages=find_packages(),
+    install_requires=[
+        "pyautogui",
+        "tls_client",
+        "fake_useragent",
+        "windows-curses; platform_system=='Windows'",
+        "pyfiglet"
+    ],
+    keywords=["python", "utils", "color", "colors", "tools"],
+    python_requires=">=3.8.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
