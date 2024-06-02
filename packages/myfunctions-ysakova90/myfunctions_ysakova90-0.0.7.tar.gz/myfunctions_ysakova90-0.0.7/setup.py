5022d155878db2fa5139832b49bfb14bbf4341ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:05:20 +0000

    Add package myfunctions-ysakova90/0.0.7/myfunctions_ysakova90-0.0.7.tar.gz

---
+++ packages/myfunctions-ysakova90/myfunctions_ysakova90-0.0.7.tar.gz/myfunctions_ysakova90-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_ysakova90',
+    version='0.0.7',
+    packages=find_packages(),
+    install_requires=[""],
+    url='https://github.com/ysakova90',
+    license='MIT',
+    author='Aigul Ysakova',
+    author_email='ysakova90@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
