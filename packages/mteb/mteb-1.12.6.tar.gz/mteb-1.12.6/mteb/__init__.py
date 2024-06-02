b09d9de26faf53e3ec18a48fa1b6bff2ab7f314f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:17 +0000

    Add package mteb/1.12.6/mteb-1.12.6.tar.gz

---
+++ packages/mteb/mteb-1.12.6.tar.gz/mteb-1.12.6/mteb/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from importlib.metadata import version
+
+from mteb.benchmarks import (
+    MTEB_MAIN_EN,
+    MTEB_RETRIEVAL_LAW,
+    MTEB_RETRIEVAL_WITH_INSTRUCTIONS,
+)
+from mteb.evaluation import *
+from mteb.overview import TASKS_REGISTRY, get_task, get_tasks
+
+from .models import get_model, get_model_meta
+
+__version__ = version("mteb")  # fetch version from install metadata
+
+
+__all__ = [
+    "MTEB_MAIN_EN",
+    "MTEB_RETRIEVAL_LAW",
+    "MTEB_RETRIEVAL_WITH_INSTRUCTIONS",
+    "TASKS_REGISTRY",
+    "get_tasks",
+    "get_task",
+    "get_model",
+    "get_model_meta",
+]
