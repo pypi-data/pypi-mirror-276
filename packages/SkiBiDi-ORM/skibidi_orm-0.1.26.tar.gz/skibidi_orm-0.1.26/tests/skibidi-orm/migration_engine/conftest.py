72cb8447fea1628a833856cbfa0ff679f8ab6ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 04:06:20 +0000

    Add package SkiBiDi-ORM/0.1.26/skibidi_orm-0.1.26.tar.gz

---
+++ packages/SkiBiDi-ORM/skibidi_orm-0.1.26.tar.gz/skibidi_orm-0.1.26/tests/skibidi-orm/migration_engine/conftest.py
@@ -0,0 +1,132 @@
+from functools import wraps
+import sqlite3
+from typing import Any, Callable
+from colorama import Style
+import psycopg2
+import pytest
+
+from skibidi_orm.migration_engine.db_config.base_config import (
+    BaseDbConfig,
+)
+import py
+
+from skibidi_orm.migration_engine.db_config.postgres_config import PostgresConfig
+from skibidi_orm.migration_engine.db_inspectors.postgres_inspector import PostgresInspector  # type: ignore
+
+
+@pytest.fixture(autouse=True)
+def reset_config_singleton(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(BaseDbConfig, "_BaseDbConfig__instance", None)
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item: Any) -> Any:
+    """Print tests' docstring when running the test."""
+    outcome: Any = yield
+    report = outcome.get_result()
+    test_fn = item.obj
+    docstring = getattr(test_fn, "__doc__")
+    if docstring:
+        docstring = docstring.strip()
+        report.nodeid = Style.DIM + docstring + Style.RESET_ALL
+
+
+def execute_sqlite3_commands(db_path: str, commands: list[str]):
+    """Executes the given commands in a SQLite DB living under db_path"""
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        for command in commands:
+            cursor.execute(command)
+        conn.commit()
+        cursor.close()
+
+
+@pytest.fixture
+def make_database(request: pytest.FixtureRequest, tmpdir: py.path.local):
+    sql_commands = (
+        request.__getattribute__("param") if hasattr(request, "param") else None
+    )
+    p = tmpdir.join("temp_db.db")  # type: ignore
+    p.write("")  # type: ignore
+    if sql_commands:
+        execute_sqlite3_commands(
+            p.strpath,  # type: ignore
+            sql_commands,
+        )
+    yield p
+    tmpdir.remove()
+
+
+def create_postgres_database(
+    db_name: str,
+    db_user: str,
+    db_password: str,
+    db_host: str,
+    db_port: int,
+    queries: list[str],
+):
+    connection = psycopg2.connect(
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port,
+        database=db_name,
+    )
+    cursor = connection.cursor()
+    for query in queries:
+        cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def clear_postgres_database(
+    db_name: str,
+    db_user: str,
+    db_password: str,
+    db_host: str,
+    db_port: int,
+):
+    connection = psycopg2.connect(
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port,
+        database=db_name,
+    )
+    cursor = connection.cursor()
+    cursor.execute("DROP SCHEMA public CASCADE;")
+    cursor.execute("CREATE SCHEMA public;")
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def postgres_db_fixture(
+    db_name: str,
+    db_user: str,
+    db_password: str,
+    db_host: str,
+    db_port: int,
+    queries: list[str],
+):
+    def decorator(func: Callable[[PostgresConfig, PostgresInspector], Any]):
+        @wraps(func)
+        def wrapper():
+            config = PostgresConfig(
+                db_name=db_name,
+                db_user=db_user,
+                db_password=db_password,
+                db_host=db_host,
+                db_port=db_port,
+            )
+            inspector = PostgresInspector()
+            clear_postgres_database(db_name, db_user, db_password, db_host, db_port)
+            create_postgres_database(
+                db_name, db_user, db_password, db_host, db_port, queries
+            )
+            func(config, inspector)
+
+        return wrapper
+
+    return decorator
---
+++ packages/SkiBiDi-ORM/skibidi_orm-0.1.26.tar.gz/skibidi_orm-0.1.26/tests/skibidi-orm/migration_engine/studio/test_get_db_inspector.py
@@ -0,0 +1,24 @@
+import pathlib
+from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
+from skibidi_orm.migration_engine.db_inspectors.sqlite3_inspector import (
+    SQLite3Inspector,
+)
+from skibidi_orm.migration_engine.studio.utils.get_db_inspector import get_db_inspector
+import py  # type: ignore
+
+
+def write_temp_schema_file(file_path: str, file_content: str):
+    with open(file_path, "w") as f:
+        f.write(file_content)
+
+
+def test_get_db_inspector_sqlite3(tmp_path: pathlib.Path):
+    p = tmp_path.joinpath("schema_test_get_db_inspector_sqlite3.py")
+    p.write_text(
+        """from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
+SQLite3Config(db_path="some/path/to/db.db")"""
+    )
+    db_inspector = get_db_inspector(
+        db_config=SQLite3Config(db_path="some/path/to/db.db")
+    )
+    assert isinstance(db_inspector, SQLite3Inspector)
