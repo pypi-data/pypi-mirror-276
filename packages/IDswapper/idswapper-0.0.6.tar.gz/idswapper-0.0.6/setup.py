635874502016a231097bf2716a14904b88147f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:04:50 +0000

    Add package IDswapper/0.0.6/idswapper-0.0.6.tar.gz

---
+++ packages/IDswapper/idswapper-0.0.6.tar.gz/idswapper-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IDswapper',
+    version='0.0.6',
+    packages=find_packages(),  # Include all packages under the current directory
+    entry_points={
+        'console_scripts': [
+            'IDswapper=idswapper.fetchids:main'
+        ]
+    },
+    install_requires=[
+        'pandas',
+        'mysql-connector-python',
+        'tqdm',
+        'pymysql',
+    ],
+    package_data={'idswapper': ['db_config.json']},
+    include_package_data=True,
+    author='Pr (France) Dr. rer. nat. Vijay K. ULAGANATHAN',
+    author_email=' ',
+    description='A tool for swapping IDs in a file and fetching data from a MySQL database',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vkulaganathan/IDswapper/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
