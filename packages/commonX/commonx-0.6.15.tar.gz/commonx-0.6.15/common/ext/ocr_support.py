5d774d4fe1cea3b48779f21b05ede4411caf1678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:06 +0000

    Add package commonX/0.6.15/commonx-0.6.15.tar.gz

---
+++ packages/commonX/commonx-0.6.15.tar.gz/commonx-0.6.15/common/ext/ocr_support.py
@@ -0,0 +1,10 @@
+# _ocr = None
+#
+#
+# def verify_code(img):
+#     import ddddocr
+#     global _ocr
+#     if _ocr is None:
+#         _ocr = ddddocr.DdddOcr(use_gpu=True, show_ad=False)
+#
+#     return _ocr.classification(img=img)
