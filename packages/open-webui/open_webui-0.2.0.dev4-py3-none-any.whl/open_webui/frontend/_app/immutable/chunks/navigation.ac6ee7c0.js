9afc5aee221d7b94e3081272634f5b537798a7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 14:05:03 +0000

    Add package open-webui/0.2.0.dev4/open_webui-0.2.0.dev4-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.2.0.dev4-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/navigation.ac6ee7c0.js
@@ -0,0 +1,2 @@
+import{h as o}from"./singletons.eb3e3f83.js";const e=o("goto");export{e as g};
+//# sourceMappingURL=navigation.ac6ee7c0.js.map
---
+++ packages/open-webui/open_webui-0.2.0.dev4-py3-none-any.whl/open_webui/frontend/_app/immutable/nodes/8.44f4fb38.js
@@ -0,0 +1,2 @@
+import{s as t,o as e}from"../chunks/scheduler.96ee3362.js";import{S as n,i as r}from"../chunks/index.7d87350f.js";import{g as a}from"../chunks/navigation.ac6ee7c0.js";function i(o){return e(()=>{a("/workspace/models")}),[]}class l extends n{constructor(s){super(),r(this,s,i,null,t,{})}}export{l as component};
+//# sourceMappingURL=8.44f4fb38.js.map
