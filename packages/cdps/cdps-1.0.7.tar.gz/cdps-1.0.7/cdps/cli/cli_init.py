55b8c90bd6820fefce012e70e49e04a4594648b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:28 +0000

    Add package cdps/1.0.7/cdps-1.0.7.tar.gz

---
+++ packages/cdps/cdps-1.0.7.tar.gz/cdps-1.0.7/cdps/cli/cli_init.py
@@ -0,0 +1,11 @@
+import os
+
+from constants import core_constant
+from cdps_server import CDPS
+
+
+def initialize_environment(*, quiet: bool = False, focus: bool = False):
+    CDPS(initialize_environment=True, focus=focus)
+    if not quiet:
+        print('Initialized environment for {} in {}'.format(
+            core_constant.NAME, os.getcwd()))
