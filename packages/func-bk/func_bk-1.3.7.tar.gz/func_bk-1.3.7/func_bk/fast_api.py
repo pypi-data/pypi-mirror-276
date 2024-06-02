71034c834474290f1cd20c3ad1528f75f708dfe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:23 +0000

    Add package func-bk/1.3.7/func_bk-1.3.7.tar.gz

---
+++ packages/func-bk/func_bk-1.3.7.tar.gz/func_bk-1.3.7/func_bk/fast_api.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
+from fastapi.utils import generate_unique_id
+from typing import Any, List, Union
+from enum import Enum
+
+def path(
+        app: FastAPI,
+        func: callable,
+        type: str,
+        path: str,
+        response_model: Any = None,
+        status_code: int = None,
+        tags: List[Union[str, Enum]] = None,
+        dependencies = None,
+        summary: str = None,
+        description: str = None,
+        response_description: str = "Successful Response",
+        responses = None,
+        deprecated: bool = None,
+        operation_id = None,
+        response_model_include = None,
+        response_model_exclude = None,
+        response_model_by_alias: bool = True,
+        response_model_exclude_unset: bool = False,
+        response_model_exclude_defaults: bool = False,
+        response_model_exclude_none: bool = False,
+        include_in_schema: bool = True,
+        response_class: type[Response] = JSONResponse,
+        name: str = None,
+        callbacks = None,
+        openapi_extra = None,
+        generate_unique_id_function = generate_unique_id
+    ):
+    type_lower = type.lower()
+    route_function = None
+    if type_lower == "get":
+        route_function = app.get
+    elif type_lower == "post":
+        route_function = app.post
+    elif type_lower == "put":
+        route_function = app.put
+    elif type_lower == "delete":
+        route_function = app.delete
+    elif type_lower == "patch":
+        route_function = app.patch
+
+    if route_function:
+        route_function(
+            path,
+            response_model=response_model,
+            status_code=status_code,
+            tags=tags,
+            dependencies=dependencies,
+            summary=summary,
+            description=description,
+            response_description=response_description,
+            responses=responses,
+            deprecated=deprecated,
+            operation_id=operation_id,
+            response_model_include=response_model_include,
+            response_model_exclude=response_model_exclude,
+            response_model_by_alias=response_model_by_alias,
+            response_model_exclude_unset=response_model_exclude_unset,
+            response_model_exclude_defaults=response_model_exclude_defaults,
+            response_model_exclude_none=response_model_exclude_none,
+            include_in_schema=include_in_schema,
+            response_class=response_class,
+            name=name,
+            callbacks=callbacks,
+            openapi_extra=openapi_extra,
+            generate_unique_id_function=generate_unique_id_function
+        )(func)
