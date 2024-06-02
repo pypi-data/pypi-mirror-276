8f6370ae22f3d2d346400fa2f002a634b2338ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:52 +0000

    Add package polars/0.20.31/polars-0.20.31.tar.gz

---
+++ packages/polars/polars-0.20.31.tar.gz/polars-0.20.31/crates/polars-core/src/utils/series.rs
@@ -0,0 +1,44 @@
+use crate::prelude::*;
+use crate::series::unstable::UnstableSeries;
+
+/// A utility that allocates an [`UnstableSeries`]. The applied function can then use that
+/// series container to save heap allocations and swap arrow arrays.
+pub fn with_unstable_series<F, T>(dtype: &DataType, f: F) -> T
+where
+    F: Fn(&mut UnstableSeries) -> T,
+{
+    let mut container = Series::full_null("", 0, dtype);
+    let mut us = UnstableSeries::new(&mut container);
+
+    f(&mut us)
+}
+
+pub fn handle_casting_failures(input: &Series, output: &Series) -> PolarsResult<()> {
+    let failure_mask = !input.is_null() & output.is_null();
+    let failures = input.filter_threaded(&failure_mask, false)?;
+
+    let additional_info = match (input.dtype(), output.dtype()) {
+        (DataType::String, DataType::Date | DataType::Datetime(_, _)) => {
+            "\n\nYou might want to try:\n\
+            - setting `strict=False` to set values that cannot be converted to `null`\n\
+            - using `str.strptime`, `str.to_date`, or `str.to_datetime` and providing a format string"
+        },
+        #[cfg(feature = "dtype-categorical")]
+        (DataType::String, DataType::Enum(_,_)) => {
+            "\n\nEnsure that all values in the input column are present in the categories of the enum datatype."
+        }
+        _ => "",
+    };
+
+    polars_bail!(
+        ComputeError:
+        "conversion from `{}` to `{}` failed in column '{}' for {} out of {} values: {}{}",
+        input.dtype(),
+        output.dtype(),
+        output.name(),
+        failures.len(),
+        input.len(),
+        failures.fmt_list(),
+        additional_info,
+    )
+}
---
+++ packages/polars/polars-0.20.31.tar.gz/polars-0.20.31/crates/polars/tests/it/lazy/functions.rs
@@ -0,0 +1,25 @@
+use super::*;
+
+#[test]
+fn test_format_str() {
+    let a = df![
+        "a" => [1, 2],
+        "b" => ["a", "b"]
+    ]
+    .unwrap();
+
+    let out = a
+        .lazy()
+        .select([format_str("({}, {}]", [col("a"), col("b")])
+            .unwrap()
+            .alias("formatted")])
+        .collect()
+        .unwrap();
+
+    let expected = df![
+        "formatted" => ["(1, a]", "(2, b]"]
+    ]
+    .unwrap();
+
+    assert!(out.equals_missing(&expected));
+}
