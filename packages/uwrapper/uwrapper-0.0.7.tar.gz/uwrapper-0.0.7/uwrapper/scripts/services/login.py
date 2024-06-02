e278eaf37e118d823cfb2f83516d1bc236bc7f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 03:05:46 +0000

    Add package uwrapper/0.0.7/uwrapper-0.0.7.tar.gz

---
+++ packages/uwrapper/uwrapper-0.0.7.tar.gz/uwrapper-0.0.7/uwrapper/scripts/services/login.py
@@ -0,0 +1,19 @@
+"""upstox login"""
+import urllib.parse
+import requests
+
+def login(apikey,secretkey,rurl):
+    """ logging into upstox """
+    rurl_parsed=urllib.parse.quote(rurl,safe="")
+    uri=f'https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id={apikey}&redirect_uri={rurl_parsed}'
+    print("Visit the following URL to authorize your application:")
+    print(uri)
+    code = input("Enter the code: ")
+    url=f"https://api.upstox.com/v2/login/authorization/token"
+    headers={'accept': "application/json",'Content-Type': "application/x-www-form-urlencoded"}
+    data={'code':code,'client_id':apikey,'client_secret':secretkey,'redirect_uri':rurl,'grant_type':'authorization_code'}
+    response=requests.post(url,headers=headers,data=data)
+    json_response=response.json()
+    access_token=json_response['access_token']
+    print('access token created')
+    return access_token
