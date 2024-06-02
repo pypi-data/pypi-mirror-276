e28bae716ba6171d4fb2abbe98f69c5c76e691a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-02 15:04:55 +0000

    Add package Sanatio/1.1.0/Sanatio-1.1.0-py3-none-any.whl

---
+++ packages/Sanatio/Sanatio-1.1.0-py3-none-any.whl/sanatio/main.py
@@ -0,0 +1,20 @@
+from sanatio.array_validator import ArrayValidator
+from sanatio.date_validator import DateValidator
+from sanatio.document_validator import DocumentValidator
+from sanatio.email_validator import EmailValidator
+from sanatio.number_validator import NumberValidator
+from sanatio.other_validator import OtherValidator
+from sanatio.password_validator import PasswordValidator
+from sanatio.string_validator import StringValidator
+from sanatio.username_validator import UsernameValidator
+from warnings import warn
+
+
+class Sanatio(ArrayValidator, DateValidator, DocumentValidator, EmailValidator, NumberValidator, OtherValidator, PasswordValidator, StringValidator, UsernameValidator):
+    """ Sanatio class for validating the data """
+    def __init__(self):
+        super().__init__()
+class Validator(Sanatio):
+    """ Validator class for validating the data """
+    def __init__(self):
+        warn("Validator class is deprecated, use Sanatio class instead", DeprecationWarning)
