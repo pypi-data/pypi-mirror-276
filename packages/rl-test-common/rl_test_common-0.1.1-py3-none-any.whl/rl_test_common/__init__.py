61853c770d5e104cb33700ebb7c4923b0c1340f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:44 +0000

    Add package rl-test-common/0.1.1/rl_test_common-0.1.1-py3-none-any.whl

---
+++ packages/rl-test-common/rl_test_common-0.1.1-py3-none-any.whl/rl_test_common/__init__.py
@@ -0,0 +1,4 @@
+from .common import Common
+from .singleton_meta import SingletonMeta
+
+__all__ = ['Common', 'SingletonMeta']
---
+++ packages/rl-test-common/rl_test_common-0.1.1-py3-none-any.whl/rl_test_common/common.py
@@ -0,0 +1,21 @@
+import os
+
+from .singleton_meta import SingletonMeta
+
+
+class Common(metaclass=SingletonMeta):
+    __user_files_root: str = 'user_files'
+
+    @property
+    def user_files_root(self) -> str:
+        return self.__user_files_root
+
+    def get_type(self, obj: object) -> str:
+        return type(obj).__name__
+
+    def clear_screen(self) -> None:
+        if os.name == 'nt':
+            os.system('cls')
+
+        else:
+            os.system('clear')
