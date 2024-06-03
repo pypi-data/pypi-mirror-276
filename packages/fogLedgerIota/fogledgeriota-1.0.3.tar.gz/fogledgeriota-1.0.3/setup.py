0707fbc99df6001165ae92d58ff8f20611ba2acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 16:51:18 +0000

    Add package fogLedgerIota/1.0.3/fogledgeriota-1.0.3.tar.gz

---
+++ packages/fogLedgerIota/fogledgeriota-1.0.3.tar.gz/fogledgeriota-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fogLedgerIota",
+    version="1.0.3",
+    description='Plugin to build DLTs in Fogbed.',
+    long_description='Plugin to build DLT in Fogbed. Suport IOTA. \
+        The FogLedger is a plugin for [Fogbed](https://github.com/larsid/FogLedger-Iota). It allows you to emulate a fog network with distributed ledgers. \
+        Currently, FogLedger has suport for IOTA. IOTA is a groundbreaking cryptocurrency designed for the Internet of Things (IoT), \
+        Offering feeless transactions and scalability for machine-to-machine communication. \
+        Its unique Tangle ledger utilizes a directed acyclic graph (DAG) for efficient data transfer and decentralized consensus, \
+        making it ideal for securing and facilitating microtransactions in IoT ecosystems.',
+    keywords=['networking', 'emulator', 'protocol', 'Internet', 'dlt', 'iota', 'fog'],
+    url='https://github.com/larsid/FogLedger-Iota/tree/v1.0.0',
+    author='Weslei Santos',
+    author_email='weslei.eng.comp@gmail.com',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Testing",
+        "Topic :: System :: Emulators"
+    ],
+    install_requires = [
+        'numpy>=1.24.2',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    package_data={'fogledgerIota': ['iota/data/*.sh', 'iota/data/*.json']}
+)
