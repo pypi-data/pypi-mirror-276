859a6b812db46576444688b7e4feb3eaf64f248c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 05:05:17 +0000

    Add package copilothistoryexporter/1.0.3.7/copilothistoryexporter-1.0.3.7.tar.gz

---
+++ packages/copilothistoryexporter/copilothistoryexporter-1.0.3.7.tar.gz/copilothistoryexporter-1.0.3.7/setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name="copilothistoryexporter",
+    version='1.0.3.7',
+    url='https://github.com/enciyo/gh-copilot-history-export',
+    author='Mustafa Kilic',
+    author_email='enciyomk61@gmail.com',
+    description='This package exports the history of GitHub Copilot chat history.',
+    long_description="This package exports the history of GitHub Copilot chat history.",
+    packages=find_packages(),
+    package_data={'': ['*.md']},
+    install_requires=[
+        "requests",
+        "click",
+        "mitmproxy",
+        "wheel",
+        "setuptools",
+    ],
+    entry_points='''
+     [console_scripts]
+     copilothistoryexporter=copilothistoryexporter.main:sys_main
+     '''
+)
