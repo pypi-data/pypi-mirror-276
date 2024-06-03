45808409489dda12cb14d39f6e88f56e0ab2dd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:39 +0000

    Add package voyage-agents/0.0.4/voyage_agents-0.0.4.tar.gz

---
+++ packages/voyage-agents/voyage_agents-0.0.4.tar.gz/voyage_agents-0.0.4/voyage_agents/core/manager.py
@@ -0,0 +1,24 @@
+import ctypes
+
+from llama_cpp import Llama, llama_log_set, llama_log_callback
+
+@llama_log_callback
+def log_callback(
+    level: int,
+    text: bytes,
+    user_data: ctypes.c_void_p,
+):
+    pass
+
+class LlamaManager:
+    def __init__(self, llama: Llama):
+        self.llm = llama
+        llama_log_set(log_callback, ctypes.c_void_p(0))
+
+    def query(self, messages, grammar = None):
+        res = self.llm.create_chat_completion(
+            messages=messages,
+            response_format={ "type": "text" },
+            grammar=grammar,
+        )
+        return res['choices'][0]['message']['content']
