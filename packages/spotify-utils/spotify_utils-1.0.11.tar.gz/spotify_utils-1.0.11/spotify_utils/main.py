0157186290107d759063003d674c81b2e600a530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:57 +0000

    Add package spotify-utils/1.0.11/spotify_utils-1.0.11.tar.gz

---
+++ packages/spotify-utils/spotify_utils-1.0.11.tar.gz/spotify_utils-1.0.11/spotify_utils/main.py
@@ -0,0 +1,24 @@
+# Built-in modules
+import importlib.metadata
+
+# PyPi modules
+import typer
+
+# Local modules
+from spotify_utils.src import playlists
+
+# Global variables
+__version__ = importlib.metadata.version("spotify-utils")
+
+# Initialize Typer and populate commands
+app = typer.Typer(help=f"spotify-utils v{__version__}")
+app.add_typer(playlists.app, name="playlists")
+
+
+@app.command()
+def version():
+    typer.echo(f"spotify-utils v{__version__}")
+
+
+if __name__ == "__main__":
+    app()
