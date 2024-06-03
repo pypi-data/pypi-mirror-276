8ee8669f4bb08371da7d8c6f6f595113ae89a279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:50 +0000

    Add package test-pkg-zteinck/1.0.0/test_pkg_zteinck-1.0.0.tar.gz

---
+++ packages/test-pkg-zteinck/test_pkg_zteinck-1.0.0.tar.gz/test_pkg_zteinck-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='test_pkg_zteinck',
+    packages=find_packages(),
+    version='1.0.0',
+    description='test package',
+    author='Zachary Einck',
+    # license='MIT',
+    install_requires=[
+        'requests',
+        ],
+    )
