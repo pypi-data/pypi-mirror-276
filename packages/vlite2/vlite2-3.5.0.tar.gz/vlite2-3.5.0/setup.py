cc5862c7904eadf74109fbd2b68b1e266a3f998d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:05:03 +0000

    Add package vlite2/3.5.0/vlite2-3.5.0.tar.gz

---
+++ packages/vlite2/vlite2-3.5.0.tar.gz/vlite2-3.5.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vlite2',
+    version='3.5.0',
+    author='Ray Del Vecchio',
+    author_email='ray@cerebralvalley.ai',
+    description='Improved simple vector database, with many advanced proprietary features. Original VLite by @sdan.',
+    url='https://github.com/raydelvecchio/vlite-v2',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'Requests',
+        'setuptools',
+        'torch',
+        'transformers',
+        'uuid',
+        'usearch',
+        'PyPDF2',
+        'docx2txt',
+    ],
+)
