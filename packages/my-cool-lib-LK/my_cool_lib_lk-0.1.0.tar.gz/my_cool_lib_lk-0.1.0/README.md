19e578360630fdf5e31470275e53ba9aaecd9250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 16:51:57 +0000

    Add package my-cool-lib-LK/0.1.0/my_cool_lib_lk-0.1.0.tar.gz

---
+++ packages/my-cool-lib-LK/my_cool_lib_lk-0.1.0.tar.gz/my_cool_lib_lk-0.1.0/README.md
@@ -0,0 +1,9 @@
+# User friendly documentation
+
+## Introduction
+
+This is my first published library in Python
+
+## Usage
+
+Just run this script
---
+++ packages/my-cool-lib-LK/my_cool_lib_lk-0.1.0.tar.gz/my_cool_lib_lk-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='my_cool_lib_LK',
+    version='0.1.0',
+    author='Lubos Kohut',
+    author_email='luboskohut.shop@gmail.com',
+    packages=find_packages(),
+    license='GNU GPL',
+    include_package_data=True,
+    description='Test library'
+)
