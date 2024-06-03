49b3d64919278cbe34c456cc4e77bce8b44ee654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 05:05:04 +0000

    Add package ovos-plugin-common-play/0.0.7a7/ovos_plugin_common_play-0.0.7a7-py3-none-any.whl

---
+++ packages/ovos-plugin-common-play/ovos_plugin_common_play-0.0.7a7-py3-none-any.whl/ovos_plugin_common_play/ocp/utils.py
@@ -0,0 +1,39 @@
+import shutil
+from os import makedirs
+from os.path import expanduser, isfile, join, dirname, exists
+
+from ovos_plugin_manager.ocp import load_stream_extractors, available_extractors
+
+
+def ocp_plugins():
+    return load_stream_extractors()
+
+
+def is_qtav_available():
+    return exists("/usr/include/qt/QtAV") or \
+        exists("/usr/lib/qt/qml/QtAV") or \
+        exists("/usr/lib/libQtAV.so")
+
+
+def extract_metadata(uri):
+    # backwards compat
+    from ovos_ocp_files_plugin.plugin import OCPFilesMetadataExtractor
+    return OCPFilesMetadataExtractor.extract_metadata(uri)
+
+
+def create_desktop_file():
+    res = join(dirname(__file__), "res", "desktop")
+    desktop_path = expanduser("~/.local/share/applications")
+    icon_path = expanduser("~/.local/share/icons")
+    makedirs(desktop_path, exist_ok=True)
+    makedirs(icon_path, exist_ok=True)
+
+    src_desktop = join(res, "OCP.desktop")
+    dst_desktop = join(desktop_path, "OCP.desktop")
+    if not isfile(dst_desktop):
+        shutil.copy(src_desktop, dst_desktop)
+
+    src_icon = join(res, "OCP.png")
+    dst_icon = join(icon_path, "OCP.png")
+    if not isfile(dst_icon):
+        shutil.copy(src_icon, dst_icon)
