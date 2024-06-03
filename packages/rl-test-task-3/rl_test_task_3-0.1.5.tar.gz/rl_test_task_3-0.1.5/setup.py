1fb77dfc802950389f61aaeebdd1b3854ea8b6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:52 +0000

    Add package rl-test-task-3/0.1.5/rl_test_task_3-0.1.5.tar.gz

---
+++ packages/rl-test-task-3/rl_test_task_3-0.1.5.tar.gz/rl_test_task_3-0.1.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rl-test-task-3',
+    version='0.1.5',
+    description='Task 3',
+    author='Antonio Corluka',
+    author_email='antonio.corluka@outlook.com',
+    packages=find_packages(),
+    python_requires='>=3.11',
+    install_requires=[
+        'rl-test-common',
+        'rl-test-storage-handlers'
+    ]
+)
