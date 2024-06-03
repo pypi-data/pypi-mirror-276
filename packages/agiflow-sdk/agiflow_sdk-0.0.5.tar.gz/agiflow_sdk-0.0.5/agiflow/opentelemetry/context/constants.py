4faf9db1502d3a114e715d06ba48b9a803b50d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 17:04:54 +0000

    Add package agiflow-sdk/0.0.5/agiflow_sdk-0.0.5.tar.gz

---
+++ packages/agiflow-sdk/agiflow_sdk-0.0.5.tar.gz/agiflow_sdk-0.0.5/agiflow/opentelemetry/context/constants.py
@@ -0,0 +1,28 @@
+from typing import TypedDict, NotRequired
+
+
+class PromptSettings(TypedDict):
+    key: NotRequired[str]
+    version: NotRequired[str]
+    version_name: NotRequired[str]
+    version_hash: NotRequired[str]
+    template_variables: NotRequired[dict]
+
+
+class AssociationProperties(TypedDict):
+    action_id: NotRequired[str]
+    task_id: NotRequired[str]
+    user_id: NotRequired[str]
+    session_id: NotRequired[str]
+    auto_trace: NotRequired[bool]
+
+
+class ContextKeys:
+    CORRELATION_ID = "correlation_id"
+    ASSOCIATION_PROPERTIES = "association_properties"
+    WORKFLOW_NAME = "workflow_name"
+    # Prompts
+    PROMPT_SETTINGS = "prompt_settings"
+
+    # Toggle flag
+    OVERRIDE_ENABLE_CONTENT_TRACING = "override_enable_content_tracing"
