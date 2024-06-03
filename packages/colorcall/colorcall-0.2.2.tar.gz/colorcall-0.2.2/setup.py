f5c0417e0306cfa9f3886c8fa83b178a07b62fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 15:27:37 +0000

    Add package colorcall/0.2.2/colorcall-0.2.2.tar.gz

---
+++ packages/colorcall/colorcall-0.2.2.tar.gz/colorcall-0.2.2/setup.py
@@ -0,0 +1,22 @@
+import colorcall
+from setuptools import setup, find_packages
+
+with open("README.md") as file:
+    long_description = file.read()
+
+setup(
+    name=colorcall.__name__,
+    version=colorcall.__version__,
+    author="Mikle Sedrakyan",
+    author_email="scriptdefender@yandex.ru",
+    description="Color call your caracal",
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sw1mmeR/colorcall/tree/main",
+    packages=find_packages(),
+    package_data={
+        colorcall.__name__: ["py.typed"],
+    },
+    python_requires=">=3.11",
+)
