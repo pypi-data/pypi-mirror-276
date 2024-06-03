77bcedfdffb95c812e60e81b12db9e1d78ec3a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 05:05:12 +0000

    Add package hemanth-hello-world/0.3/hemanth_hello_world-0.3.tar.gz

---
+++ packages/hemanth-hello-world/hemanth_hello_world-0.3.tar.gz/hemanth_hello_world-0.3/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='hemanth-hello-world',
+    version='0.3',
+    description='A simple Hello World package',
+    author='hemanth',
+    author_email='hsbollina@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'hemanth-hello-world=hemanth_hello_world.cli:greet',
+        ],
+    },
+)
