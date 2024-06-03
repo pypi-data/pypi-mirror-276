7b83f00e0aff835e69a0fc12117ac94f4f9775d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 15:27:28 +0000

    Add package hemanth-hello-world/0.9/hemanth_hello_world-0.9.tar.gz

---
+++ packages/hemanth-hello-world/hemanth_hello_world-0.9.tar.gz/hemanth_hello_world-0.9/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hemanth_hello_world',
+    version='0.9',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies if required
+    ],
+    entry_points={
+        'console_scripts': [
+            'multiply=hemanth_hello_world.cli:main',
+        ],
+    },
+    description='A simple package to multiply two numbers',
+    author='Hemanthbollina123',
+    author_email='hsbollina@example.com',
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
