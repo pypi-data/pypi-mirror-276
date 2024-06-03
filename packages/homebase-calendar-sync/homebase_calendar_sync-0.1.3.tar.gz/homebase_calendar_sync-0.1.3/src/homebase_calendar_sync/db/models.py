dbb4990b22f93c542947e4d4bd6b311f36895290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:35 +0000

    Add package homebase-calendar-sync/0.1.3/homebase_calendar_sync-0.1.3.tar.gz

---
+++ packages/homebase-calendar-sync/homebase_calendar_sync-0.1.3.tar.gz/homebase_calendar_sync-0.1.3/src/homebase_calendar_sync/db/models.py
@@ -0,0 +1,33 @@
+import sqlite3
+from .. import config
+
+
+def connect_database():
+    config.DB = sqlite3.connect("events.db")
+    config.DB_CURSOR = config.DB.cursor()
+
+
+def setup_database():
+    config.DB = sqlite3.connect("events.db")
+    config.DB_CURSOR = config.DB.cursor()
+    config.DB_CURSOR.execute(
+        """
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            event_id TEXT NOT NULL UNIQUE,
+            hash TEXT NOT NULL,
+            from_homebase INTEGER NOT NULL CHECK (from_homebase IN (0, 1)),
+            homebase_shift_id TEXT UNIQUE
+        )
+    """
+    )
+    config.DB_CURSOR.execute(
+        """
+        CREATE TABLE IF NOT EXISTS shifts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            homebase_shift_id TEXT NOT NULL UNIQUE,
+            hash TEXT NOT NULL
+        )
+    """
+    )
+    config.DB.commit()
