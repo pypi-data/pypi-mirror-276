ade377c225a191f0368cb7ab21c97af171a22383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 15:27:39 +0000

    Add package hemanth-hello-world/0.7/hemanth_hello_world-0.7-py3-none-any.whl

---
+++ packages/hemanth-hello-world/hemanth_hello_world-0.7-py3-none-any.whl/hemanth_hello_world/__init__.py
@@ -0,0 +1,3 @@
+# hello_world/__init__.py
+
+from .cli import subtract
---
+++ packages/hemanth-hello-world/hemanth_hello_world-0.7-py3-none-any.whl/hemanth_hello_world/cli.py
@@ -0,0 +1,19 @@
+
+# hello_world/cli.py
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Subtract two numbers.')
+    parser.add_argument('a', type=float, help='First number')
+    parser.add_argument('b', type=float, help='Second number')
+
+    args = parser.parse_args()
+    result = subtract(args.a, args.b)
+    print(f"The result of {args.a} - {args.b} is {result}")
+
+if __name__ == '__main__':
+    main()
