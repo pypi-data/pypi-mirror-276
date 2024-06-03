580971d069daeb77fa17edd3c8152d190694ac74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:40 +0000

    Add package walledeval/0.1.0/walledeval-0.1.0-py3-none-any.whl

---
+++ packages/walledeval/walledeval-0.1.0-py3-none-any.whl/walledeval/__init__.py
@@ -0,0 +1,14 @@
+# walledeval/__init__.py
+
+
+
+# from walledeval.llm import LLM, HF_LLM, hf_models, Claude
+# from walledeval.judge import Judge, ClaudeJudge
+# from walledeval.benchmark import TestCase, Log, Benchmark, WMDP
+
+
+# __all__ = [
+#     "LLM", "HF_LLM", "hf_models", "Claude",
+#     "Judge", "ClaudeJudge",
+#     "TestCase", "Log", "Benchmark", "WMDP"
+# ]
