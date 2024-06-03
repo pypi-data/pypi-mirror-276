f46836ed4e3da02692d8e5f7929e88c25219f520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 17:04:42 +0000

    Add package draive/0.13.0/draive-0.13.0-py3-none-any.whl

---
+++ packages/draive/draive-0.13.0-py3-none-any.whl/draive/generation/model/generator.py
@@ -0,0 +1,27 @@
+from collections.abc import Iterable, Sequence
+from typing import Any, Protocol, runtime_checkable
+
+from draive.lmm import AnyTool, Toolbox
+from draive.parameters import DataModel
+from draive.types import Instruction, MultimodalContent, MultimodalContentElement
+
+__all__ = [
+    "ModelGenerator",
+]
+
+
+@runtime_checkable
+class ModelGenerator(Protocol):
+    async def __call__[Generated: DataModel](  # noqa: PLR0913
+        self,
+        generated: type[Generated],
+        /,
+        *,
+        instruction: Instruction | str,
+        input: MultimodalContent | MultimodalContentElement,  # noqa: A002
+        schema_variable: str | None = None,
+        tools: Toolbox | Sequence[AnyTool] | None = None,
+        examples: Iterable[tuple[MultimodalContent | MultimodalContentElement, Generated]]
+        | None = None,
+        **extra: Any,
+    ) -> Generated: ...
