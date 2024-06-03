849586d2def04fa8abaf96fa71667f698d83dd1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:44 +0000

    Add package rl-test-storage-handlers/0.1.1/rl_test_storage_handlers-0.1.1.tar.gz

---
+++ packages/rl-test-storage-handlers/rl_test_storage_handlers-0.1.1.tar.gz/rl_test_storage_handlers-0.1.1/rl_test_storage_handlers/file/file_handler.py
@@ -0,0 +1,46 @@
+import logging
+from abc import abstractmethod
+
+from rl_test_common import Common
+
+from ..params.storage_data import StorageData
+from ..params.storage_destination import StorageDestination
+from ..storage_handler import StorageHandler
+from .params.file_data import FileData
+from .params.file_destination import FileDestination
+
+
+class FileHandler(StorageHandler):
+    __logger: logging.Logger = logging.getLogger(__name__)
+
+    __common: Common = Common()
+
+    @abstractmethod
+    def write(self, destination: StorageDestination, data: StorageData) -> None:
+        pass
+
+    @abstractmethod
+    def read(self, destination: StorageDestination) -> StorageData:
+        pass
+
+    def _write(self, destination: FileDestination, mode: str, data: FileData) -> None:
+        file_path: str = self.__get_file_path(destination.get())
+
+        with open(file_path, mode) as file:
+            file.write(data.get())
+
+    def _read(self, destination: FileDestination, mode: str) -> FileData:
+        file_path: str = self.__get_file_path(destination.get())
+        data: FileData = FileData('')
+
+        try:
+            with open(file_path, mode) as file:
+                data = FileData(file.read())
+
+        except FileNotFoundError:
+            self.__logger.error(f'File not found: {file_path}.')
+
+        return data
+
+    def __get_file_path(self, file_name: str) -> str:
+        return f'{self.__common.user_files_root}/{file_name}'
