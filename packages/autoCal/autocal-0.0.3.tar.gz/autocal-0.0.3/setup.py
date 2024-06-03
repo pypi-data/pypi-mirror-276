577d5a33ada7997daba31efc223adbe17fe40a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:55 +0000

    Add package autoCal/0.0.3/autocal-0.0.3.tar.gz

---
+++ packages/autoCal/autocal-0.0.3.tar.gz/autocal-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Collection of automotive calculations'
+LONG_DESCRIPTION = 'This repository provides a collection of mathematical formulas commonly used in the automotive industry, with a particular focus on Brushless DC (BLDC) motors.'
+
+# Setting up
+setup(
+    name="autoCal",
+    version=VERSION,
+    author="GladsonThomas",
+    author_email="<gladson.thomas.official@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'calculations', 'automotive', 'EV', 'BLDC'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
