342800aed4d4f9f002dfb592545fe21b708e2790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:41 +0000

    Add package encryption-ciphers/1.0.0/encryption_ciphers-1.0.0.tar.gz

---
+++ packages/encryption-ciphers/encryption_ciphers-1.0.0.tar.gz/encryption_ciphers-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='encryption_ciphers',
+    version='1.0.0',
+    author='Paul Fruitful',
+    description='A collection of Encryption Algorithms for ciphers',
+    packages=find_packages()
+)
