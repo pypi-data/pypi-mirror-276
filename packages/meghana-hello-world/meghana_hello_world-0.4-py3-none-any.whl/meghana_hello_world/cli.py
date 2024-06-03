5611ee9ed036c108a1c36d1017e72fb4008fffda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 15:27:25 +0000

    Add package meghana-hello-world/0.4/meghana_hello_world-0.4-py3-none-any.whl

---
+++ packages/meghana-hello-world/meghana_hello_world-0.4-py3-none-any.whl/meghana_hello_world/cli.py
@@ -0,0 +1,23 @@
+import click
+from hello_world import hello, sum_numbers
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def greet():
+    """Simple program that greets the user."""
+    click.echo(hello())
+
+@cli.command()
+@click.argument('numbers', nargs=-1, type=int)
+def sum_numbers_cmd(numbers):
+    """Simple program that sums numbers."""
+    if numbers:
+        click.echo(sum_numbers(numbers))
+    else:
+        click.echo("No numbers provided for summing.")
+
+if __name__ == '__main__':
+    cli()
