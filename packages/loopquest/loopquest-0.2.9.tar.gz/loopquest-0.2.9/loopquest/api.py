b537487a6016e2206e5fb013b2a2bce67913f4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:42 +0000

    Add package loopquest/0.2.9/loopquest-0.2.9.tar.gz

---
+++ packages/loopquest/loopquest-0.2.9.tar.gz/loopquest-0.2.9/loopquest/api.py
@@ -0,0 +1,51 @@
+import os
+import getpass
+import webbrowser
+from .crud import get_cloud_user_id
+from .private_api import (
+    is_cloud_instance_initialized,
+    verify_token,
+    save_token_to_env,
+    add_env_to_gitignore,
+    CLOUD_BACKEND_URL,
+    CLOUD_FRONTEND_URL,
+)
+
+
+def init():
+    if is_initialized():
+        print("LoopQuest is already initialized.")
+        return
+    sign_in_url = f"{CLOUD_FRONTEND_URL}/token"
+    print(
+        f"Opening LoopQuest Sign-In page ... \nIf the browser does not open automatically, visit {sign_in_url} manually."
+    )
+    webbrowser.open_new(sign_in_url)
+    while True:
+        token = getpass.getpass("Enter your LoopQuest user token: ").strip()
+        if verify_token(token):
+            print("Token is verified.")
+            break
+        else:
+            print("Token is invalid. Please try again.")
+
+    save_token_to_env(token)
+    print("LoopQuest is initialized.")
+
+
+def is_initialized():
+    return is_cloud_instance_initialized()
+
+
+def get_frontend_url():
+    return CLOUD_FRONTEND_URL
+
+
+def get_backend_url():
+    return CLOUD_BACKEND_URL
+
+
+def get_user_id(backend_url=None):
+    if backend_url is None:
+        backend_url = get_backend_url()
+    return get_cloud_user_id(backend_url)
