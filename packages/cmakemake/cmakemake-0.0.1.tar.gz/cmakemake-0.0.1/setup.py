f7f9063645a83ad258203c86f361c449a9583d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:59 +0000

    Add package cmakemake/0.0.1/cmakemake-0.0.1.tar.gz

---
+++ packages/cmakemake/cmakemake-0.0.1.tar.gz/cmakemake-0.0.1/setup.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+from pathlib import Path
+from configparser import ConfigParser
+
+from setuptools import setup, find_packages
+
+
+def load_readme() -> str:
+    filename = Path.cwd().joinpath('README.md')
+    if filename.is_file():
+        with open(filename, 'r', encoding='utf-8') as fp:
+            return fp.read()
+
+
+def load_requires() -> list[str]:
+    filename = Path.cwd().joinpath('Pipfile')
+    if not filename.is_file():
+        return []
+    parser = ConfigParser()
+    parser.read(filenames=filename, encoding='utf-8')
+    return parser.options('packages')
+
+
+setup(
+    name='cmakemake',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://gitee.com/zhuuuoyue/cmakemake',
+    license='MIT',
+    author='zhuoy',
+    author_email='zhuoyue_cn@yeah.net',
+    description='Make cmake files',
+    long_description=load_readme(),
+    long_description_content_type='text/markdown',
+    install_requires=load_requires()
+)
