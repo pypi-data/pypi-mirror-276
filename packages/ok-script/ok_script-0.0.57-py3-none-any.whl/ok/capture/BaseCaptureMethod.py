044a2f413a4aec522e6bceca06e5047670f7608e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:42 +0000

    Add package ok-script/0.0.57/ok_script-0.0.57-py3-none-any.whl

---
+++ packages/ok-script/ok_script-0.0.57-py3-none-any.whl/ok/capture/BaseCaptureMethod.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+from ok.logging.Logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class CaptureException(Exception):
+    pass
+
+
+class BaseCaptureMethod:
+    name = "None"
+    description = ""
+    _size = (0, 0)
+
+    def __init__(self):
+        # Some capture methods don't need an initialization process
+        pass
+
+    def close(self):
+        # Some capture methods don't need an initialization process
+        pass
+
+    @property
+    def width(self):
+        if self._size[0] == 0:
+            try:
+                self.get_frame()
+            except Exception as e:
+                logger.error('get frame error')
+        return self._size[0]
+
+    @property
+    def height(self):
+        if self._size[1] == 0:
+            try:
+                self.get_frame()
+            except Exception as e:
+                logger.error('get frame error')
+        return self._size[1]
+
+    def get_frame(self) -> np.ndarray | None:
+        try:
+            frame = self.do_get_frame()
+            if frame is not None:
+                self._size = (frame.shape[1], frame.shape[0])
+                if frame.shape[2] == 4:
+                    frame = frame[:, :, :3]
+            return frame
+        except Exception as e:
+            raise CaptureException() from e
+
+    def do_get_frame(self):
+        pass
+
+    def draw_rectangle(self):
+        pass
+
+    def clickable(self):
+        pass
+
+    def connected(self):
+        pass
