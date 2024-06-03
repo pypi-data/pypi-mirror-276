8591f3c1e7651c3c0843d1d1048fdca399308f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 17:04:51 +0000

    Add package wiseAI/1.0.0/wiseai-1.0.0.tar.gz

---
+++ packages/wiseAI/wiseai-1.0.0.tar.gz/wiseai-1.0.0/wiseAI/models/contrib/__init__.py
@@ -0,0 +1,9 @@
+from .automl_binary import AutoBinaryML
+from .automl_multiclass import MultiClassOneVestPredictor
+from .automl_multilabel import MultiClassImbalanceChain
+
+__all__ = [
+    "AutoBinaryML",
+    "MultiClassOneVestPredictor",
+    "MultiClassImbalanceChain",
+]
