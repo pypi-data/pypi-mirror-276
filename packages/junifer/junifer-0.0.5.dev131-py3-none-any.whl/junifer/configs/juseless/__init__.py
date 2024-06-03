e4f5beb64e67041585dfc54d91fa78827002933f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 17:04:43 +0000

    Add package junifer/0.0.5.dev131/junifer-0.0.5.dev131-py3-none-any.whl

---
+++ packages/junifer/junifer-0.0.5.dev131-py3-none-any.whl/junifer/configs/juseless/__init__.py
@@ -0,0 +1,6 @@
+"""Custom components for FZJ INM-7's beloved juseless."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+# License: AGPL
+
+from . import datagrabbers
---
+++ packages/junifer/junifer-0.0.5.dev131-py3-none-any.whl/junifer/pipeline/__init__.py
@@ -0,0 +1,9 @@
+"""Pipeline components."""
+
+# Authors: Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+from . import registry
+from .pipeline_step_mixin import PipelineStepMixin
+from .update_meta_mixin import UpdateMetaMixin
+from .workdir_manager import WorkDirManager
