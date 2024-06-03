6774c942caa6fb81ef71388ffcc17d998d629efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 15:26:47 +0000

    Add package cs-ob-mini-scikit-learn/0.1.10/cs_ob_mini_scikit_learn-0.1.10.tar.gz

---
+++ packages/cs-ob-mini-scikit-learn/cs_ob_mini_scikit_learn-0.1.10.tar.gz/cs_ob_mini_scikit_learn-0.1.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cs_ob_mini_scikit_learn',
+    version='0.1.10',
+    author='basma-arnaoui',
+    author_email='basma.arnaoui@um6p.ma',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn', 
+        'clone',
+        'pandas',
+         
+    ],
+    python_requires='>=3.6',
+    description='A minimal implementation of scikit-learn like functionalities',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Basma-Arnaoui/Mini-Scikit-Learn/tree/main',
+)
