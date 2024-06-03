7b614b59d71aef8864443a7972ea2c07795cc61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 17:04:46 +0000

    Add package biobb-haddock/4.2.1/biobb_haddock-4.2.1-py3-none-any.whl

---
+++ packages/biobb-haddock/biobb_haddock-4.2.1-py3-none-any.whl/biobb_haddock/haddock/__init__.py
@@ -0,0 +1,11 @@
+from . import capri_eval
+from . import clust_fcc
+from . import em_ref
+from . import flex_ref
+from . import rigid_body
+from . import sele_top
+from . import topology
+from . import sele_top_clusts
+
+name = "haddock"
+__all__ = ["capri_eval", "clust_fcc", "em_ref", "flex_ref", "rigid_body", "sele_top_clusts", "sele_top", "topology"]
