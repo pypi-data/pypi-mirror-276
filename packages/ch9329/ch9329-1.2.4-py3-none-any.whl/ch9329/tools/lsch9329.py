92e3aba8fa34874fbc72f2f96048241fb7ce8b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:45 +0000

    Add package ch9329/1.2.4/ch9329-1.2.4-py3-none-any.whl

---
+++ packages/ch9329/ch9329-1.2.4-py3-none-any.whl/ch9329/tools/lsch9329.py
@@ -0,0 +1,32 @@
+import serial.serialutil
+import serial.tools.list_ports
+from serial import Serial
+
+from ch9329.config import get_serial_number
+
+
+def main():
+    comports = serial.tools.list_ports.comports()
+    comports = [i for i in comports if i.vid == 6790 and i.pid == 29987]
+    print(f"{len(comports)} ch9329 found.")
+    for i in serial.tools.list_ports.comports():
+        if i.vid == 6790 and i.pid == 29987:
+            try:
+                ser = Serial(i.name, 9600, timeout=0.05)
+                serial_number = get_serial_number(ser)
+                print(
+                    f"serial_number={serial_number}, "
+                    f"name={i.device}, "
+                    f"location={i.location}"
+                )
+                ser.close()
+            except serial.serialutil.SerialException:
+                print(
+                    f"serial_number=error, "
+                    f"name={i.name}, "
+                    f"location={i.location}"
+                )
+
+
+if __name__ == "__main__":
+    main()
