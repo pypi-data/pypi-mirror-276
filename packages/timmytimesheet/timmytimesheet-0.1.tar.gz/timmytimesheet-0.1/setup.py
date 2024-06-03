9bd450ae3191376b1d5a16fd08f92f777c68115a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 16:51:17 +0000

    Add package timmytimesheet/0.1/timmytimesheet-0.1.tar.gz

---
+++ packages/timmytimesheet/timmytimesheet-0.1.tar.gz/timmytimesheet-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = "timmytimesheet",
+        version = "0.1",
+        description = "a basic timesheeting cli",
+        author="jumper385 (Henry Chen)",
+        install_package_data=True,
+        install_requires = [
+            "typer[all]"],
+        packages = find_packages(),
+        keywords = ["timesheeting"],
+        entry_points = {
+            'console_scripts': [
+                'timmy-time=timmy.main:app']},
+        classifiers = [
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            ])
+
