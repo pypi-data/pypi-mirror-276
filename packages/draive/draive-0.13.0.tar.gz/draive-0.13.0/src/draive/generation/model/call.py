45bd311ad29dcd49e7f4bceab596a1cbed507c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 17:04:42 +0000

    Add package draive/0.13.0/draive-0.13.0.tar.gz

---
+++ packages/draive/draive-0.13.0.tar.gz/draive-0.13.0/src/draive/generation/model/call.py
@@ -0,0 +1,35 @@
+from collections.abc import Iterable, Sequence
+from typing import Any
+
+from draive.generation.model.state import ModelGeneration
+from draive.lmm import AnyTool, Toolbox
+from draive.parameters import DataModel
+from draive.scope import ctx
+from draive.types import Instruction, MultimodalContent, MultimodalContentElement
+
+__all__ = [
+    "generate_model",
+]
+
+
+async def generate_model[Generated: DataModel](  # noqa: PLR0913
+    generated: type[Generated],
+    /,
+    *,
+    instruction: Instruction | str,
+    input: MultimodalContent | MultimodalContentElement,  # noqa: A002
+    schema_variable: str | None = None,
+    tools: Toolbox | Sequence[AnyTool] | None = None,
+    examples: Iterable[tuple[MultimodalContent | MultimodalContentElement, Generated]]
+    | None = None,
+    **extra: Any,
+) -> Generated:
+    return await ctx.state(ModelGeneration).generate(
+        generated,
+        instruction=instruction,
+        input=input,
+        schema_variable=schema_variable,
+        tools=tools,
+        examples=examples,
+        **extra,
+    )
