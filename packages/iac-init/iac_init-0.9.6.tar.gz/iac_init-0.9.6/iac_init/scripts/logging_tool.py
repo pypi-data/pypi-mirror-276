4c57165c0e47241ca0c688b7177c1293586eaad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 16:51:17 +0000

    Add package iac-init/0.9.6/iac_init-0.9.6.tar.gz

---
+++ packages/iac-init/iac_init-0.9.6.tar.gz/iac_init-0.9.6/iac_init/scripts/logging_tool.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2024, Rudy Lei <shlei@cisco.com>
+
+import os
+from loguru import logger
+from iac_init.conf import settings
+
+def setup_logging():
+    logger.add(
+        sink=os.path.join(
+            settings.OUTPUT_BASE_DIR,
+            'iac_init_log',
+            'iac_init_main.log'
+        ),
+        format='{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}',
+        encoding='utf-8'
+    )
+
+    return logger
