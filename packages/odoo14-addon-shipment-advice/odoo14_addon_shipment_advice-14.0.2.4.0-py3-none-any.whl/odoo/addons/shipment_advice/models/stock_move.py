eb6e4f11907c5c80c5a1e922b42d0fe3c33a29eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:53 +0000

    Add package odoo14-addon-shipment-advice/14.0.2.4.0/odoo14_addon_shipment_advice-14.0.2.4.0-py3-none-any.whl

---
+++ packages/odoo14-addon-shipment-advice/odoo14_addon_shipment_advice-14.0.2.4.0-py3-none-any.whl/odoo/addons/shipment_advice/models/stock_move.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    shipment_advice_id = fields.Many2one(
+        comodel_name="shipment.advice",
+        ondelete="set null",
+        string="Planned shipment",
+        index=True,
+    )
+
+    def _plan_in_shipment(self, shipment_advice):
+        """Plan the moves into the given shipment advice."""
+        self.shipment_advice_id = shipment_advice
+
+    def _prepare_merge_moves_distinct_fields(self):
+        res = super()._prepare_merge_moves_distinct_fields()
+        # Avoid having stock move assign to different shipment merged together
+        res.append("shipment_advice_id")
+        return res
