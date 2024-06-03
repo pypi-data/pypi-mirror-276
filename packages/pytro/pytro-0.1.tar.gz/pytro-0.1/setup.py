214b3128ce776cc3863d8ebce9ccf64d99fe4695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:41 +0000

    Add package pytro/0.1/pytro-0.1.tar.gz

---
+++ packages/pytro/pytro-0.1.tar.gz/pytro-0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='pytro',
+    version='0.1',
+    description='Symbios of eel and pywebview',
+    author='NoSleepForTonight',
+    install_requires=['eel', 'pywebview', 'pygobject'],
+    dependency_links=['https://github.com/bottlepy/bottle.git@3fdb8b2a2e0d1641374b53ef2b051fe7f54508b5']
+)
