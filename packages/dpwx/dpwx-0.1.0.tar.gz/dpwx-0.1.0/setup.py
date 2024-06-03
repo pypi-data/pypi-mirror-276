6b0946c2578e6dec614eaebeef4a7a616d407b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 16:51:27 +0000

    Add package dpwx/0.1.0/dpwx-0.1.0.tar.gz

---
+++ packages/dpwx/dpwx-0.1.0.tar.gz/dpwx-0.1.0/setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.1.0'
+
+setup(
+    name='dpwx',
+    version=VERSION,
+    description='my package',
+    packages=find_packages(),
+    install_requires=[
+		'pymem'
+	],
+)
