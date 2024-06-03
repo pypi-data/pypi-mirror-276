b59cb0c9d32c93b206f3dd8b1022bfc4b8e015db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:05:03 +0000

    Add package distlibstat/0.2/distlibstat-0.2.tar.gz

---
+++ packages/distlibstat/distlibstat-0.2.tar.gz/distlibstat-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distlibstat',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distlibstat'],
+      zip_safe=False)
