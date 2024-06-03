d97a8929992b98b91c2e0baf7fa3ec912a89090a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:52 +0000

    Add package pyiwe/0.0.8.post2/pyiwe-0.0.8.post2-py3-none-any.whl

---
+++ packages/pyiwe/pyiwe-0.0.8.post2-py3-none-any.whl/pyiwe/__init__.py
@@ -0,0 +1,24 @@
+"""
+TNT (Tree analysis using New Technology) implied weighting with branch supports Python wrapper.
+TNT source: http://www.lillo.org.ar/phylogeny/tnt/ (Goloboff, Farris, & Nixon, 2003)
+==================================
+See README.md for complete documentation.
+"""
+
+__version__ = '0.0.8post2'
+
+import sys
+
+from .tnt_install import TNTSetup
+
+try:
+    from .iwe import PyIW
+    from .utils import processing
+    from .utils import visualize
+    from .config import pyiw_config
+except ModuleNotFoundError:
+    print('ModuleNotFoundError happened. Check your dependencies. Ignore this error during packaging.')
+
+TNTSetup().setup()
+
+__all__ = ['iwe', 'utils', 'config']
