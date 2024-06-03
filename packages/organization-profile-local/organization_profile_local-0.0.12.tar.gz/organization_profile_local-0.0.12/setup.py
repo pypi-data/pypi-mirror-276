e664a34952a87cf01bce851ef5f55a3dfa14ee40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 15:27:22 +0000

    Add package organization-profile-local/0.0.12/organization_profile_local-0.0.12.tar.gz

---
+++ packages/organization-profile-local/organization_profile_local-0.0.12.tar.gz/organization_profile_local-0.0.12/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+PACKAGE_NAME = "organization-profile-local"
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version='0.0.12', # update only the minor version each time # https://pypi.org/project/organization-profile-local/
+    author="Circles",
+    author_email="info@circlez.ai",
+    description="PyPI Package for Circles organization-profile-local Python",
+    long_description="PyPI Package for Circles organization-profile-local Python",
+    long_description_content_type='text/markdown',
+    url="https://github.com/circles-zone/organization-profile-local-python-package",
+    packages=[package_dir],
+    package_dir={package_dir: f'{package_dir}/src'},
+    package_data={package_dir: ['*.py']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'PyMySQL>=1.0.2',
+        'pytest>=7.4.0',
+        'mysql-connector>=2.2.9',
+        'logzio-python-handler>= 4.1.0',
+        'user-context-remote>=0.0.17',
+        'python-sdk-remote>=0.0.27'
+    ],
+)
