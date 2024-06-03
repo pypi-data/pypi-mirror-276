90570c4c68108d1661f8f2b45f76d0d4614523c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 16:51:56 +0000

    Add package joumaico/0.0.2/joumaico-0.0.2.tar.gz

---
+++ packages/joumaico/joumaico-0.0.2.tar.gz/joumaico-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="joumaico",
+    version="0.0.2",
+    author="Joumaico Maulas",
+    description="Test Package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    packages=['joumaico'],
+    package_dir={'joumaico': 'src/joumaico'},
+    package_data={'joumaico': ['data/*.txt']},
+    python_requires=">=3.11",
+    install_requires=[
+        "aiosqlite",
+    ],
+)
