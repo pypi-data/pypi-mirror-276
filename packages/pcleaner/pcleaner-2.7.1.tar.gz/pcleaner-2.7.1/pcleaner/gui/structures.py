165ef7a351afd8d000d2c9561a5a60ceaf4e6ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:50 +0000

    Add package pcleaner/2.7.1/pcleaner-2.7.1.tar.gz

---
+++ packages/pcleaner/pcleaner-2.7.1.tar.gz/pcleaner-2.7.1/pcleaner/gui/structures.py
@@ -0,0 +1,22 @@
+from typing import Generic, TypeVar, Optional
+
+
+T = TypeVar("T")
+
+
+class Shared(Generic[T]):
+    def __init__(self, initial_value: Optional[T] = None) -> None:
+        self._container = {"data": initial_value}
+
+    def get(self) -> Optional[T]:
+        return self._container["data"]
+
+    def set(self, value: T) -> None:
+        self._container["data"] = value
+
+    def is_none(self) -> bool:
+        return self._container["data"] is None
+
+        # class OCRModel(Protocol):
+        #     def __call__(self, image: Image, **kwargs) -> str:
+        # ...
