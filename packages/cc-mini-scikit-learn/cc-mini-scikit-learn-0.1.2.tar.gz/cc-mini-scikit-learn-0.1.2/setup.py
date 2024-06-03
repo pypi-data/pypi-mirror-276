b6fc45d5f117d0b2e27923fecd21ce0b9463aa72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 15:27:03 +0000

    Add package cc-mini-scikit-learn/0.1.2/cc-mini-scikit-learn-0.1.2.tar.gz

---
+++ packages/cc-mini-scikit-learn/cc-mini-scikit-learn-0.1.2.tar.gz/cc-mini-scikit-learn-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cc-mini-scikit-learn',
+    version='0.1.2',
+    author='basma-arnaoui',
+    author_email='basma.arnaoui@um6p.ma',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn', 
+         
+    ],
+    python_requires='>=3.6',
+    description='A minimal implementation of scikit-learn like functionalities',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Basma-Arnaoui/Mini-Scikit-Learn/tree/main',
+)
