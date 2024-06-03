08bea6c8a25e7e1fc14278f66582d1c69b0099ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 15:27:25 +0000

    Add package pyerualjetwork/2.0.5/pyerualjetwork-2.0.5.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-2.0.5.tar.gz/pyerualjetwork-2.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.0.5",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Advanced python deep learning library. New features: More simple and practical, action_potential changed to activation_potential, all functions and variables are snake_case. (Documentation in desc. Examples in GİTHUB: https://github.com/HCB06/PyerualJetwork)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
