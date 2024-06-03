7b13cb8618984f05fbebabefa1efe650219c4eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:46 +0000

    Add package streamlit-avater/0.1.0/streamlit_avater-0.1.0.tar.gz

---
+++ packages/streamlit-avater/streamlit_avater-0.1.0.tar.gz/streamlit_avater-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-avater",
+    version="0.1.0",
+    author="papasim824",
+    author_email="papasim824@gmail.com",
+    description="Component to display avatar icon in Streamlit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ppspps824/streamlit_avater",
+    download_url="https://github.com/ppspps824/streamlit_avater",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+    extras_require={"devel": ["wheel"]},
+)
