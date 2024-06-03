78f9abc3cb506c3515b9eae630eda769bff2f904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 03:04:42 +0000

    Add package sc3dg/0.0.16/sc3dg-0.0.16-py3-none-any.whl

---
+++ packages/sc3dg/sc3dg-0.0.16-py3-none-any.whl/sc3dg/cli.py
@@ -0,0 +1,17 @@
+
+import click
+from sc3dg.commands.count import count
+from sc3dg.commands.model import model
+from sc3dg.commands.impute import impute
+
+
+
+
+
+
+cli.add_command(count)
+cli.add_command(model)
+cli.add_command(impute)
+
+if __name__ == '__main__':
+    cli()
