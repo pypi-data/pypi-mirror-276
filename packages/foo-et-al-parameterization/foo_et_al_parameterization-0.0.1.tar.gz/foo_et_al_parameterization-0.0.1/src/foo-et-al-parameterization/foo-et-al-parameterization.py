edc1d341dcc180a09d941cf4c87824ef6b0aa24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:04:52 +0000

    Add package foo-et-al-parameterization/0.0.1/foo_et_al_parameterization-0.0.1.tar.gz

---
+++ packages/foo-et-al-parameterization/foo_et_al_parameterization-0.0.1.tar.gz/foo_et_al_parameterization-0.0.1/src/foo-et-al-parameterization/foo-et-al-parameterization.py
@@ -0,0 +1,10 @@
+# Author: Aidan Goldstein
+
+import math
+
+
+def sphere_volume(radius):
+    """ sphere_volume takes a given numerical value radius as a parameter of a sphere
+    and returns the total volume of the sphere. """
+
+    return 4.0 / 3.0 * math.pi * (radius ** 3)
