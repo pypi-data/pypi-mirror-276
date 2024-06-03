2122c182e28502fdf8a2e20db485e17f6a11a5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-03 04:05:06 +0000

    Add package copilothistoryexporter/1.0.3.4/copilothistoryexporter-1.0.3.4.tar.gz

---
+++ packages/copilothistoryexporter/copilothistoryexporter-1.0.3.4.tar.gz/copilothistoryexporter-1.0.3.4/src/copilothistoryexporter/main.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import traceback
+
+import click
+
+from utils import SharedValues
+
+
+@click.command()
+@click.option('--port', default=9696, help='Running port')
+@click.option('--debug', default=False, help='Debug mode')
+@click.argument('project', type=click.Path(exists=True, writable=True), required=False)
+def main(port, debug, project):
+    project = os.path.abspath(project) if project else os.getcwd()
+    SharedValues.change_workspace(project)
+    script = os.path.join(os.path.dirname(__file__), "addons", "binding.py")
+    launch = "mitmdump" if not debug else "mitmweb"
+    shell = f"{launch} -s {script} -p {port}"
+    print(f"running on --port {port} --debug {debug} {project}")
+    print("running mitm with command: ", shell)
+    subprocess.run(shell, shell=True)
+
+
+
+def sys_main():
+    """
+    Runs the main function using the system cli arguments, and
+    returns a system error code.
+    :return: 0 for success, 1 for failure.
+    :rtype: int
+    """
+    try:
+        main()
+        return 0
+    except Exception:
+        print(traceback.format_exc())
+        return 1
+
+
+if __name__ == '__main__':
+    sys_main()
